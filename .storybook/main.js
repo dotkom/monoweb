c562481f8a360b6582ca20a474502f21aac1d37f
kharann <anhkhav@gmail.com>
2021-10-30 22:23:20 +0200

    initial commit with superplate

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-controls",
+        "@storybook/addon-knobs",
+    ],
+    webpackFinal: async (config) => {
+        return config;
+    },
+};
