193417cf45eb56066340fd2a8be6f94c60d10c81
Mats Larsen <me@supergrecko.com>
2021-11-07 16:14:19 +0100

    feat: add remaining text styles

--- src/components/atoms/text/text.stories.tsx
+++ src/components/atoms/text/text.stories.tsx
@@ -8,6 +8,20 @@ export default {
 export const defaultText = (props: TextProps) => <Text>Default text - Lorem
   ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet quam nec
   metus luctus, sit amet vehicula arcu commodo.</Text>;
-export const emphasisText = (props: TextProps) => <Text variant="emphasis">Bold text
+
+export const emphasisText = (props: TextProps) => <Text variant="emphasis">Bold
+  text - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet
+  quam nec metus luctus, sit amet vehicula arcu commodo.</Text>;
+
+export const italicText = (props: TextProps) => <Text variant="italic">Italic
+  text - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet
+  quam nec metus luctus, sit amet vehicula arcu commodo.</Text>;
+
+export const strikethroughText = (props: TextProps) => <Text
+  variant="strikethrough">Strikethrough text
   - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet quam
   nec metus luctus, sit amet vehicula arcu commodo.</Text>;
+
+export const underlineText = (props: TextProps) => <Text variant="underline">Underline
+  text - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet
+  quam nec metus luctus, sit amet vehicula arcu commodo.</Text>;
--- src/styles/components/text.ts
+++ src/styles/components/text.ts
@@ -1,11 +1,26 @@
 import { Theme } from "theme-ui";
 
 export const text: Theme["text"] = {
-  base: {
-    fontFamily: "body",
-    fontSize: "16px",
-  },
-  emphasis: {
-    fontWeight: "bold",
+  text: {
+    base: {
+      fontFamily: "body",
+      fontSize: 16,
+    },
+    emphasis: {
+      variant: "text.base",
+      fontWeight: "bold",
+    },
+    italic: {
+      variant: "text.base",
+      fontStyle: "italic",
+    },
+    strikethrough: {
+      variant: "text.base",
+      textDecoration: "line-through",
+    },
+    underline: {
+      variant: "text.base",
+      textDecoration: "underline",
+    },
   },
 };
