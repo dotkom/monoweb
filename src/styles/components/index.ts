c91f3b0487f28cf141cc50d2afde050b26760dfe
Carl Smestad <carl.smestad@gmail.com>
2021-11-07 16:59:57 +0100

    feat: add checkbox component

--- src/styles/components/index.ts
+++ src/styles/components/index.ts
@@ -1,6 +1,7 @@
 import { buttonTheme } from "./button";
+import checkboxTheme from "./checkbox";
 import inputTheme from "./input";
 import selectTheme from "./select";
 import textareaTheme from "./textarea";
 
-export default { ...buttonTheme, ...inputTheme, ...selectTheme, ...textareaTheme };
+export default { ...buttonTheme, ...inputTheme, ...selectTheme, ...textareaTheme, ...checkboxTheme };
