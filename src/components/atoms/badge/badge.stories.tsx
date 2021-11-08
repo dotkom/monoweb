429e034e799df9c21c56c1dc2cc66d3f0c151067
Sondre Alfnes <60832802+sondrealf@users.noreply.github.com>
2021-11-09 00:41:48 +0100

    Update badge.stories.tsx

--- src/components/atoms/badge/badge.stories.tsx
+++ src/components/atoms/badge/badge.stories.tsx
@@ -1,24 +1,10 @@
 import { Badge, BadgeProps } from "theme-ui";
-import { darken, lighten } from '@theme-ui/color'
 
 export default {
     title: "Badge",
     component: Badge,
 };
 
-export const subtleBadge = (props: BadgeProps) => <Badge variant="test"
-    sx={{
-    }}
+export const subtleBadge = () => <Badge variant="green.subtle">TEST</Badge>
 
->TEST</Badge>
-
-export const solidBadge = () => <Badge variant="green.solid"
-    sx={{
-    }}
-
->TEST</Badge>
-
-export const outlineBadge = () => <Badge variant="green.outline"
-    sx={{
-    }}
-    >TEST</Badge> 
+export const outlineBadge = () => <Badge variant="green.outline">TEST</Badge> 
