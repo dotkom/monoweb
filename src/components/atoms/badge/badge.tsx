66e735f181be53c0bcb39dfce3b11ef743482e66
Sondre Alfnes <60832802+sondrealf@users.noreply.github.com>
2021-11-09 00:38:05 +0100

    Update badge.tsx

--- src/components/atoms/badge/badge.tsx
+++ src/components/atoms/badge/badge.tsx
@@ -1,16 +1,3 @@
-/*
-import { ComponentProps, FC } from "react";
-import { Badge as TBadge } from "theme-ui";
-
-interface BadgeProps extends ComponentProps<typeof TBadge> {
-  color: string;
-}
-
-const Badge: FC<BadgeProps> = ({ children, color, ...props }) => <TBadge {...props}> {children} </TBadge>;
-
-export default Badge;
-*/
-
 import { Badge as TBadge } from "theme-ui";
 
 const Badge = TBadge;
