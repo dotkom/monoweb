c4f661193c1b06cfb7e4882b88165cf9a3b9b56f
Sondre Alfnes <60832802+sondrealf@users.noreply.github.com>
2021-11-09 00:40:01 +0100

    Update badge.ts

--- src/styles/components/badge.ts
+++ src/styles/components/badge.ts
@@ -40,27 +40,5 @@ export const badges: Theme["badges"] = {
   grey: getVariants("grey"),
   purple: getVariants("purple"),
 };
-/*
-const colors = ["green", "blue", "red", "grey", "purple"];
-for (const i of colors) {
-  badges[i] = {
-    subtle: {
-      variant: "badges.base",
-      bg: "background",
-      color: darken(i, 0.02),
-      border: "1px solid",
-      borderColor: darken(i, 0.02),
-    },
-    solid: {
-      variant: "badges.base",
-      color: "background",
-      bg: darken(i, 0.02),
-    },
-    outline: {
-      variant: "badges.base",
-      color: darken(i, 0.1),
-      bg: lighten(i, 0.6),
-    },
-  };
-}
-*/
+
+export default badges;
