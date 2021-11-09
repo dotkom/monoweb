a8f645ce341e44ab7c43d99e1a40f520c704e2a7
kharann <anhkhav@gmail.com>
2021-11-09 23:54:05 +0100

    add all the colored buttons

--- src/components/atoms/stories/Button.stories.tsx
+++ src/components/atoms/stories/Button.stories.tsx
@@ -5,5 +5,8 @@ export default {
   component: Button,
 };
 
-export const primaryButton = () => <Button variant="primary">Default button</Button>;
-export const secondaryButton = () => <Button variant="secondary">Default button</Button>;
+export const Primary = () => <Button variant="primary">Default button</Button>;
+export const Orange = () => <Button variant="Orange">Orange button</Button>;
+export const Green = () => <Button variant="Green">Green button</Button>;
+export const Red = () => <Button variant="Red">Red button</Button>;
+export const Gray = () => <Button variant="gray">Gray Button</Button>;
