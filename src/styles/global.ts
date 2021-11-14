4d77f2bee8c6ca203700417bc662151789802da0
kharann <anhkhav@gmail.com>
2021-11-14 23:03:14 +0100

    feat: global styles and add a format check for prettier

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { ThemeStyles } from "theme-ui";
+
+const globalStyles: ThemeStyles["root"] = {
+  "html, body, #__next": {
+    width: "100%",
+    minHeight: "100vh",
+  },
+  "#__next": {
+    bg: "#FBFCFD",
+  },
+};
+
+export default globalStyles;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,7 @@
 import type { Theme } from "theme-ui";
 import { colors } from "./colors";
 import componentThemes from "./components";
+import globalStyles from "./global";
 import { typography } from "./typography";
 
 export const theme: Theme = {
@@ -8,4 +9,7 @@ export const theme: Theme = {
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   ...typography,
   ...componentThemes,
+  styles: {
+    root: globalStyles,
+  },
 };
