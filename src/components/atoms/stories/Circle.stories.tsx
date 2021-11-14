c28aa72afc450701c8cb94f711b106b5787b99f0
kharann <anhkhav@gmail.com>
2021-11-14 19:16:55 +0100

    format format

--- src/components/atoms/stories/Circle.stories.tsx
+++ src/components/atoms/stories/Circle.stories.tsx
@@ -1,10 +1,22 @@
 import { Circle } from "../Circle";
 
 export default {
-    title: "atoms/Circle",
-    component: Circle,
+  title: "atoms/Circle",
+  component: Circle,
 };
 
-export const Medium = () => <Circle size={50} color={"blue.1"}>1</Circle>;
-export const Large = () => <Circle size={500} color={"red.1"}>2</Circle>;
-export const Tiny = () => <Circle size={25} color={"orange.1"}>3</Circle>;
+export const Medium = () => (
+  <Circle size={50} color={"blue.1"}>
+    1
+  </Circle>
+);
+export const Large = () => (
+  <Circle size={500} color={"red.1"}>
+    2
+  </Circle>
+);
+export const Tiny = () => (
+  <Circle size={25} color={"orange.1"}>
+    3
+  </Circle>
+);
