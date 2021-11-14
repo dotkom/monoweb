8b3daf449ff7c37454aa252b8bc4401d7bbf7f2b
Sondre Alfnes <sondralf@stud.ntnu.no>
2021-11-14 16:10:38 +0100

    export badge styling

--- src/styles/components/index.ts
+++ src/styles/components/index.ts
@@ -2,11 +2,13 @@ import { Theme } from "theme-ui";
 import buttons from "./button";
 import text from "./text";
 import forms from "./forms";
+import badges from "./badge";
 
 const componentThemes: Theme = {
   buttons,
   text,
   forms,
+  badges,
 };
 
 export default componentThemes;
