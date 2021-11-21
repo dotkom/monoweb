aa651a407c4ab3349339c2c4e2ed6f49374617e7
Sondre Alfnes <60832802+sondrealf@users.noreply.github.com>
2021-11-21 14:42:47 +0100

    Update Badge.stories.tsx

--- src/components/atoms/stories/Badge.stories.tsx
+++ src/components/atoms/stories/Badge.stories.tsx
@@ -5,9 +5,9 @@ export default {
   component: Badge,
 };
 
-export const subtleBadge = () => <Badge variant="green.subtle">TEST</Badge>;
-export const solidBadge = () => <Badge variant="gray.solid">TEST</Badge>;
-export const outlineBadge = () => <Badge variant="red.outline">TEST</Badge>;
-export const outline2Badge = () => <Badge variant="green.outline">TEST</Badge>;
-export const outline3Badge = () => <Badge variant="orange.outline">TEST</Badge>;
-export const outline4Badge = () => <Badge variant="gray.outline">TEST</Badge>;
+export const greenSubltle = () => <Badge variant="green.subtle">TEST</Badge>;
+export const graySolid = () => <Badge variant="gray.solid">TEST</Badge>;
+export const redOutline = () => <Badge variant="red.outline">TEST</Badge>;
+export const greenOutline = () => <Badge variant="green.outline">TEST</Badge>;
+export const orangeOutline = () => <Badge variant="orange.outline">TEST</Badge>;
+export const grayOutline = () => <Badge variant="gray.outline">TEST</Badge>;
