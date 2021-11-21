47c22815d637972ad961286fb6fe111b296549c8
Sondre Alfnes <sondralf@stud.ntnu.no>
2021-11-21 14:06:14 +0100

    yarn format Badge

--- src/components/atoms/stories/Badge.stories.tsx
+++ src/components/atoms/stories/Badge.stories.tsx
@@ -1,13 +1,13 @@
 import { Badge } from "theme-ui";
 
 export default {
-    title: "atoms/Badge",
-    component: Badge,
+  title: "atoms/Badge",
+  component: Badge,
 };
 
-export const subtleBadge = () => <Badge variant="green.subtle">TEST</Badge>
-export const solidBadge = () => <Badge variant="gray.solid">TEST</Badge>
-export const outlineBadge = () => <Badge variant="red.outline">TEST</Badge>
-export const outline2Badge = () => <Badge variant="green.outline">TEST</Badge>
-export const outline3Badge = () => <Badge variant="orange.outline">TEST</Badge>
-export const outline4Badge = () => <Badge variant="gray.outline">TEST</Badge>
+export const subtleBadge = () => <Badge variant="green.subtle">TEST</Badge>;
+export const solidBadge = () => <Badge variant="gray.solid">TEST</Badge>;
+export const outlineBadge = () => <Badge variant="red.outline">TEST</Badge>;
+export const outline2Badge = () => <Badge variant="green.outline">TEST</Badge>;
+export const outline3Badge = () => <Badge variant="orange.outline">TEST</Badge>;
+export const outline4Badge = () => <Badge variant="gray.outline">TEST</Badge>;
