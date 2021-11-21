3494b17393b55c74f07b7e5aa3b36cc814031441
Sondre Alfnes <sondralf@stud.ntnu.no>
2021-11-21 18:03:49 +0100

    static colors

--- src/styles/components/badge.ts
+++ src/styles/components/badge.ts
@@ -1,23 +1,22 @@
 import type { Theme } from "theme-ui";
-import { saturate } from "@theme-ui/color";
 
-const getVariants = (color: string) => ({
+const getVariants = (color1: string, color2: string) => ({
   subtle: {
     variant: "badges.base",
     bg: "background",
-    color: saturate(`${color}.2`, 0.2),
+    color: color2,
     border: "1px solid",
-    borderColor: saturate(`${color}.2`, 0.3),
+    borderColor: color2,
   },
   solid: {
     variant: "badges.base",
     color: "background",
-    bg: saturate(`${color}.2`, 0.1),
+    bg: color2,
   },
   outline: {
     variant: "badges.base",
-    color: saturate(`${color}.2`, 0.3),
-    bg: `${color}.9`,
+    color: color2,
+    bg: color1,
   },
 });
 
@@ -27,11 +26,10 @@ const badges: Theme["badges"] = {
     fontWeight: "600",
     paddingX: 2,
   },
-  green: getVariants("green"),
-  gray: getVariants("gray"),
-  blue: getVariants("blue"),
-  red: getVariants("red"),
-  orange: getVariants("orange"),
+  green: getVariants("#D9EFE3", "#43B171"),
+  gray: getVariants("#EDF2F7", "#718096"),
+  blue: getVariants("#bac4f3", "#2544da"),
+  red: getVariants("#FBDDE2", "#EB536E"),
+  orange: getVariants("#FFF0D0", "#FEB515"),
 };
-
 export default badges;
