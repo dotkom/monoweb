b272ecabdf21a11afe12496271387b2f5f8b099a
kharann <anhkhav@gmail.com>
2022-02-27 19:59:37 +0100

    refactor: move styled stuff to the bottom of the file

--- src/components/molecules/EventCard.tsx
+++ src/components/molecules/EventCard.tsx
@@ -17,32 +17,11 @@ interface EventCardProps {
   thumbnailUrl: string;
 }
 
-const Thumbnail = styled(Image, {
-  borderTopRightRadius: "$3",
-  borderTopLeftRadius: "$3",
-});
-
-const EventStart = styled("time", {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  width: "min-content",
-  fontWeight: "bold",
-});
-
-const EventMetadata = styled("div", {
+const EventInfo = styled("div", {
   display: "flex",
   padding: "$2",
   justifyContent: "space-between",
   alignItems: "center",
-  $$background: "$colors$red11",
-  variants: {
-    color: {
-      subtle: {
-        backgroundColor: "$$background",
-      },
-    },
-  },
 });
 
 const EventCard: VFC<EventCardProps> = (props) => {
@@ -50,23 +29,32 @@ const EventCard: VFC<EventCardProps> = (props) => {
   const date = DateTime.fromJSDate(eventStart);
   return (
     <Card shadow css={{ maxWidth: "300px", width: "100%" }}>
-      <Thumbnail src={thumbnailUrl} width="300px" layout="responsive" height="100px" />
-      <Box css={{ display: "flex", padding: "$2" }}>
-        <EventStart dateTime={date.toISO()} color="subtle">
-          <span className={styles.month()}>{date.toFormat("MMM")}</span>
-          <Box css={{ lineHeight: "1.3", fontSize: "$2xl" }}>{date.toFormat("dd")}</Box>
-        </EventStart>
+      <Thumbnail src={thumbnailUrl} width="300px" layout="responsive" height="150px" />
+      <Flex css={{ padding: "$2" }}>
+        <EventDateContainer dateTime={date.toISO()} color="red">
+          <Box as="span" css={{ color: "$blue2", lineHeight: "1.2" }}>
+            {date.toFormat("MMM")}
+          </Box>
+          <Box css={{ lineHeight: "1.2", fontSize: "$2xl" }}>{date.toFormat("dd")}</Box>
+        </EventDateContainer>
         <Text as="h2" css={styles.title}>
           {title}
         </Text>
-      </Box>
-      <EventMetadata>
-        <Box>Badges</Box>
+      </Flex>
+      <EventInfo>
+        <Box>
+          {/* Temp until badges are done*/}
+          {tags.map((tag) => (
+            <Box as="span" css={{ backgroundColor: "$blue10" }}>
+              {tag}
+            </Box>
+          ))}
+        </Box>
         <Flex css={{ alignItems: "center", justifyContent: "center" }}>
-          <FiUsers className={styles.icon()} />
+          <ParticipantIcon />
           {attendees}/{capacity}
         </Flex>
-      </EventMetadata>
+      </EventInfo>
     </Card>
   );
 };
@@ -78,16 +66,27 @@ const styles = {
     fontWeight: 500,
     margin: 0,
     pl: "$3",
-    lineHeight: "1.4",
+    lineHeight: "1.3",
     fontSize: "$md",
   } as CSS,
-  month: css({
-    color: "$blue2",
-  }),
-  icon: css({
-    fontSize: "$xl",
-    paddingRight: "$1",
-  }),
 };
+
+const ParticipantIcon = styled(FiUsers, {
+  fontSize: "$xl",
+  paddingRight: "$1",
+});
+
+const Thumbnail = styled(Image, {
+  borderTopRightRadius: "$3",
+  borderTopLeftRadius: "$3",
+});
+
+const EventDateContainer = styled("time", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "min-content",
+  fontWeight: "bold",
+});
 
 export default EventCard;
