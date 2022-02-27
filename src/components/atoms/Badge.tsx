3b4b1d954852db768c6abef0290c0ce787ae93fb
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-02-27 18:19:36 +0100

    Removing notes for Badge-stiching

--- src/components/atoms/Badge.tsx
+++ src/components/atoms/Badge.tsx
@@ -17,57 +17,11 @@ const getVariants = (color1: string, color2: string) => ({
     },
 });
 
-const getVariant = () => ({
-    color: {
-        green: {
-            bg: "#D9EFE3",
-            color: "#43B171",
-            borderColor: "#43B171"
-        },
-        gray: {
-            bg: "#EDF2F7",
-            color: "##718096",
-            borderColor: "#718096"
-        },
-        blue: {
-            bg: "#bac4f3",
-            color: "#2544da",
-            borderColor: "#2544da"
-        },
-        red: {
-            bg: "#FBDDE2",
-            color: "#EB536E",
-            borderColor: "#EB536E"
-        },
-        orange: {
-            bg: "#FFF0D0",
-            color: "#FEB515",
-            borderColor: "#FEB515"
-        }
-    },
-    variant: {
-        solid: {
-        color: "none",
-        // bg: "inherit color"
-        },
-        subtle: {
-            bg: "none",
-            border: "1px solid",
-        },
-        outline: {
-        },
-    },
-});
-
 const Badge = styled("p", {
     borderRadius: "5px",
     fontWeight: "600",
     padding: "0 0.5rem",
     display: "inline-block",
-    
-    /*
-    variants: getVariant(),
-    */
     
     variants: {
         green: getVariants("#D9EFE3", "#43B171"),
