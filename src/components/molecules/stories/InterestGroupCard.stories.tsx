fdeb534bec4fb5e421ea846f6422d3c864434bc3
Henrik Skog <henrikskog01@gmail.com>
2022-02-27 14:46:31 +0100

    add more storybook examples

--- src/components/molecules/stories/InterestGroupCard.stories.tsx
+++ src/components/molecules/stories/InterestGroupCard.stories.tsx
@@ -9,6 +9,9 @@ export default {
 const DESC =
   " Interessegruppen for folk som er glad i jul er interessegruppen for, you guessed it, de som er glad i jul.  Gruppens største formål er å spre julens glade budskap, samt spre juleglede når det måtte passe seg.  ";
 
+const LONGDESC =
+  "Hovedoppgaven til interessegruppen er å få samlet mennesker for å spille squash, dra å kite, låne hall for å spille håndball osv. Dvs. sporter/idretter der man gjerne trenger å være flere for å kunne drive med. Vi skal etterstrebe å ha et arrangement";
+
 export const RectangleCard = () => (
   <InterestGroupCard
     backgroundImage="/banner.jpeg"
@@ -18,11 +21,29 @@ export const RectangleCard = () => (
   />
 );
 
+export const LongDescRectangleCard = () => (
+  <InterestGroupCard
+    backgroundImage="/banner.jpeg"
+    description={LONGDESC}
+    heading="Folk som er glad i Jul"
+    icon="/icon.png"
+  />
+);
+
 export const SquareCard = () => (
-  <SquareInterestGroupCard 
+  <SquareInterestGroupCard
     backgroundImage="/banner.jpeg"
     description={DESC}
     heading="Folk som er glad i Jul"
     icon="/icon.png"
-/>
+  />
+);
+
+export const LongDescSquareCard = () => (
+  <SquareInterestGroupCard
+    backgroundImage="/banner.jpeg"
+    description={LONGDESC}
+    heading="Folk som er glad i Jul"
+    icon="/icon.png"
+  />
 );
