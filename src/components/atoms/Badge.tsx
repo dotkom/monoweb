d1fa668d34e7b5c3aad27dadf4b3b60a9a382e21
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-02-28 17:52:22 +0100

    Styling Badge to variants

--- src/components/atoms/Badge.tsx
+++ src/components/atoms/Badge.tsx
@@ -17,19 +17,53 @@ const getVariants = (color1: string, color2: string) => ({
   },
 });
 
+const getVariant = () => ({
+  color: {
+    green: {
+      $$main: "#43B171",
+      $$secondary: "#D9EFE3",
+    },
+    gray: {
+      $$main: "#718096",
+      $$secondary: "#EDF2F7",
+    },
+    blue: {
+      $$main: "#2544da",
+      $$secondary: "#bac4f3",
+    },
+    red: {
+      $$main: "#EB536E",
+      $$secondary: "#FBDDE2",
+    },
+    orange: {
+      $$main: "#FEB515",
+      $$secondary: "#FFF0D0",
+    },
+  },
+  variant: {
+    solid: {
+      color: "$white",
+      bg: "$$main",
+    },
+    subtle: {
+      bg: "none",
+      color: "$$main",
+      border: "1px solid",
+      borderColor: "$$main",
+    },
+    outline: {
+      color: "$$main",
+      bg: "$$secondary",
+    },
+  },
+});
+
 const Badge = styled("p", {
   borderRadius: "5px",
   fontWeight: "600",
   padding: "0 0.5rem",
   display: "inline-block",
-
-  variants: {
-    green: getVariants("#D9EFE3", "#43B171"),
-    gray: getVariants("#EDF2F7", "#718096"),
-    blue: getVariants("#bac4f3", "#2544da"),
-    red: getVariants("#FBDDE2", "#EB536E"),
-    orange: getVariants("#FFF0D0", "#FEB515"),
-  },
+  variants: getVariant(),
 });
 
 export default Badge;
--- src/components/atoms/stories/Badge.stories.tsx
+++ src/components/atoms/stories/Badge.stories.tsx
@@ -5,18 +5,18 @@ export default {
   component: Badge,
 };
 
-export const greenSolid = () => <Badge green="solid">GREEN</Badge>;
-export const greenSubtle = () => <Badge green="subtle">GREEN</Badge>;
-export const greenOutline = () => <Badge green="outline">GREEN</Badge>;
-export const redSolid = () => <Badge red="solid">RED</Badge>;
-export const redSubtle = () => <Badge red="subtle">RED</Badge>;
-export const redOutline = () => <Badge red="outline">RED</Badge>;
-export const graySolid = () => <Badge gray="solid">GRAY</Badge>;
-export const graySubtle = () => <Badge gray="subtle">GRAY</Badge>;
-export const grayOutline = () => <Badge gray="outline">GRAY</Badge>;
-export const orangeSolid = () => <Badge orange="solid">ORANGE</Badge>;
-export const orangeSubtle = () => <Badge orange="subtle">ORANGE</Badge>;
-export const orangeOutline = () => <Badge orange="outline">ORANGE</Badge>;
-export const blueSolid = () => <Badge blue="solid">BLUE</Badge>;
-export const blueSubtle = () => <Badge blue="subtle">BLUE</Badge>;
-export const blueOutline = () => <Badge blue="outline">BLUE</Badge>;
+export const greenSolid = () => <Badge color="green" variant="solid">GREEN</Badge>;
+export const greenSubtle = () => <Badge color="green" variant="subtle">GREEN</Badge>;
+export const greenOutline = () => <Badge color="green" variant="outline">GREEN</Badge>;
+export const redSolid = () => <Badge color="red" variant="solid">RED</Badge>;
+export const redSubtle = () => <Badge color="red" variant="subtle">RED</Badge>;
+export const redOutline = () => <Badge color="red" variant="outline">RED</Badge>;
+export const graySolid = () => <Badge color="gray" variant="solid">GRAY</Badge>;
+export const graySubtle = () => <Badge color="gray" variant="subtle">GRAY</Badge>;
+export const grayOutline = () => <Badge color="gray" variant="outline">GRAY</Badge>;
+export const orangeSolid = () => <Badge color="orange" variant="solid">ORANGE</Badge>;
+export const orangeSubtle = () => <Badge color="orange" variant="subtle">ORANGE</Badge>;
+export const orangeOutline = () => <Badge color="orange" variant="outline">ORANGE</Badge>;
+export const blueSolid = () => <Badge color="blue" variant="solid">BLUE</Badge>;
+export const blueSubtle = () => <Badge color="blue" variant="subtle">BLUE</Badge>;
+export const blueOutline = () => <Badge color="blue" variant="outline">BLUE</Badge>;
