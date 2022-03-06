0946e51b46496624936b2244de53b1f487f2f5fd
Njål Sørland <njaalis2000@gmail.com>
2022-03-06 15:57:01 +0100

    Added the products we offer to the company page
    Added a "list" of all our products with dynamic icons

--- src/components/views/CompanyView/OurProducts.tsx
+++ src/components/views/CompanyView/OurProducts.tsx
@@ -2,25 +2,70 @@ import { VFC } from "react";
 import React from "react";
 import Circle from "@components/atoms/Circle";
 import { css } from "@theme";
-import { Box } from "@components/primitives";
+import { Box, Flex } from "@components/primitives";
+import BedpressIcon from "src/components/icons/BedpressIcon";
+import ItexIcon from "src/components/icons/ItexIcon";
+import OfflineIcon from "src/components/icons/OfflineIcon";
+import UtlysningIcon from "src/components/icons/UtlysningIcon";
+import TecktalksIcon from "src/components/icons/TecktalksIcon";
+import Text from "@components/atoms/Text";
+import text from "@styles/components/text";
 
 const OurProducts = () => {
-  return <div className={styles.stepText()}></div>;
+  return (
+    <Flex css={{ justifyContent: "center", flexDirection: "column" }}>
+      <Flex css={{ justifyContent: "space-evenly" }}>
+        <Flex css={{ flexDirection: "column", textAlign: "center" }}>
+          <Flex css={{ justifyContent: "center" }}>
+            <ItexIcon className={styles.icon()} />
+          </Flex>
+          <Text css={{ fontWeight: "bold" }}>ITEX</Text>
+        </Flex>
+        <Flex css={{ flexDirection: "column", textAlign: "center" }}>
+          <Flex css={{ justifyContent: "center" }}>
+            <BedpressIcon className={styles.icon()} />
+          </Flex>
+          <Text css={{ fontWeight: "bold" }}>Bedriftsarrangement</Text>
+        </Flex>
+        <Flex css={{ flexDirection: "column", textAlign: "center" }}>
+          <Flex css={{ justifyContent: "center" }}>
+            <TecktalksIcon className={styles.icon()} />
+          </Flex>
+          <Text css={{ fontWeight: "bold" }}>Tech Talks</Text>
+        </Flex>
+      </Flex>
+      <Flex css={{ justifyContent: "space-evenly" }}>
+        <Flex css={{ flexDirection: "column", textAlign: "center" }}>
+          <Flex css={{ justifyContent: "center" }}>
+            <UtlysningIcon className={styles.icon()} />
+          </Flex>
+          <Text css={{ fontWeight: "bold" }}>Stillingsutlysning</Text>
+        </Flex>
+        <Flex css={{ flexDirection: "column", textAlign: "center" }}>
+          <Flex css={{ justifyContent: "center" }}>
+            <OfflineIcon className={styles.icon()} />
+          </Flex>
+          <Text css={{ fontWeight: "bold" }}>Annonse i Offline</Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
 const styles = {
-  stepText: css({
-    maxWidth: "$lg",
-    padding: "$4 $3",
-    mx: "auto",
-    "@bp3": {
-      display: "flex",
-      justifyContent: "space-between",
+  icon: css({
+    "@bp1": {
+      width: "50px",
+      height: "50px",
     },
-  }),
-  line: css({
-    display: "relative",
-    transform: "rotate(45deg)",
+    "@bp2": {
+      width: "75px",
+      height: "75px",
+    },
+    "@bp3": {
+      width: "100px",
+      height: "100px",
+    },
   }),
 };
 
