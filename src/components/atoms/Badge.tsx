32be6c8d65ea7d029e29cf6963f372dbe707d52a
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-03-06 16:53:03 +0100

    Review resolving

--- src/components/atoms/Badge.tsx
+++ src/components/atoms/Badge.tsx
@@ -1,52 +1,49 @@
 import { styled } from "@theme";
 
-const getVariant = () => ({
-  color: {
-    green: {
-      $$main: "#43B171",
-      $$secondary: "#D9EFE3",
-    },
-    gray: {
-      $$main: "#718096",
-      $$secondary: "#EDF2F7",
-    },
-    blue: {
-      $$main: "#2544da",
-      $$secondary: "#bac4f3",
-    },
-    red: {
-      $$main: "#EB536E",
-      $$secondary: "#FBDDE2",
-    },
-    orange: {
-      $$main: "#FEB515",
-      $$secondary: "#FFF0D0",
-    },
-  },
-  variant: {
-    solid: {
-      color: "$white",
-      bg: "$$main",
-    },
-    subtle: {
-      bg: "none",
-      color: "$$main",
-      border: "1px solid",
-      borderColor: "$$main",
-    },
-    outline: {
-      color: "$$main",
-      bg: "$$secondary",
-    },
-  },
-});
-
-const Badge = styled("p", {
-  borderRadius: "5px",
+const Badge = styled("span", {
+  borderRadius: "$2",
   fontWeight: "600",
   padding: "0 0.5rem",
   display: "inline-block",
-  variants: getVariant(),
+  variants: {
+    color: {
+      green: {
+        $$main: "#43B171",
+        $$secondary: "#D9EFE3",
+      },
+      gray: {
+        $$main: "#718096",
+        $$secondary: "#EDF2F7",
+      },
+      blue: {
+        $$main: "#2544da",
+        $$secondary: "#bac4f3",
+      },
+      red: {
+        $$main: "#EB536E",
+        $$secondary: "#FBDDE2",
+      },
+      orange: {
+        $$main: "#FEB515",
+        $$secondary: "#FFF0D0",
+      },
+    },
+    variant: {
+      solid: {
+        color: "$white",
+        bg: "$$main",
+      },
+      subtle: {
+        bg: "none",
+        color: "$$main",
+        border: "1px solid $$main",
+      },
+      outline: {
+        color: "$$main",
+        bg: "$$secondary",
+      },
+    },
+  },
 });
 
 export default Badge;
