e91d191a17d03d984bda46bcad97f8729afd3b90
kharann <anhkhav@gmail.com>
2022-04-12 20:35:52 +0200

    config: update configuration

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
+const port = process.env.VITE_PORT
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: "./src/assets",
+  server: {
+    port: port ? parseInt(port) : 3002,
+  },
   define: {
     global: {},
   },
