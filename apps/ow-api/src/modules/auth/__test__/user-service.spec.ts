3b33dd28e762c5abfdcf3f1605a5bbc65304292a
kharann <anhkhav@gmail.com>
2022-04-21 00:46:18 +0200

    test(api): add unknown id test

--- apps/ow-api/src/modules/auth/__test__/user-service.spec.ts
+++ apps/ow-api/src/modules/auth/__test__/user-service.spec.ts
@@ -3,6 +3,7 @@ import { InsertUser } from "../user"
 import { initUserService } from "../user-service"
 import { v4 as uuidv4 } from "uuid"
 import { UserRepository } from "../user-repository"
+import { NotFoundError } from "../../../errors/errors"
 
 describe("UserService", () => {
   const userRepository = mockDeep<UserRepository>()
@@ -20,5 +21,12 @@ describe("UserService", () => {
     userRepository.createUser.mockResolvedValueOnce({ id, ...user })
     await expect(userService.register(user)).resolves.toEqual({ id, ...user })
     expect(userRepository.createUser).toHaveBeenCalledWith(user)
+  })
+
+  it("fails on unknown id", async () => {
+    const unknownID = uuidv4()
+    userRepository.getUserByID.mockResolvedValueOnce(undefined)
+    await expect(userService.getUser(unknownID)).rejects.toThrow(NotFoundError)
+    expect(userRepository.getUserByID).toHaveBeenCalledWith(unknownID)
   })
 })
