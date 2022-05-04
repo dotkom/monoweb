d590d03ef3aeb561421c2dbe002cf5d4b5f4347b
Mats Larsen <me@supergrecko.com>
2022-05-04 20:24:16 +0200

    Do JSON parsing

--- apps/mailer/src/lambda.ts
+++ apps/mailer/src/lambda.ts
@@ -7,16 +7,24 @@ const markdownService = initMarkdownService()
 const mailService: MailService = initMailService(markdownService)
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const data = mailSchema.safeParse(event.body)
-  if (data.success) {
-    await mailService.send(data.data)
-    return {
-      statusCode: 201,
-      body: "OK",
+  try {
+    const raw = JSON.parse(event.body ?? "")
+    const data = mailSchema.safeParse(raw)
+    if (data.success) {
+      await mailService.send(data.data)
+      return {
+        statusCode: 201,
+        body: "OK",
+      }
     }
-  }
-  return {
-    statusCode: 400,
-    body: data.error.message,
+    return {
+      statusCode: 400,
+      body: data.error.message,
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: String(err),
+    }
   }
 }
