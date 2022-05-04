19fbed0423ec6d19ffaa8e8cc4d8152e146212f3
Mats Larsen <me@supergrecko.com>
2022-05-04 19:47:45 +0200

    MVP

---
+++ apps/mailer/src/markdown-service.ts
@@ -0,0 +1,13 @@
+import { marked } from "marked"
+
+export interface MarkdownService {
+  transform(markdown: string): string
+}
+
+export const initMarkdownService = (): MarkdownService => {
+    return {
+        transform: (markdown: string): string => {
+            return marked.parse(markdown)
+        }
+    }
+}
