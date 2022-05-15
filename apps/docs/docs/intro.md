753db96ee8eb3a0531d4aa9ffeeafcde7249e6bd
Anhkha Vo <anhkhav@gmail.com>
2022-05-15 19:00:10 +0200

    Docs: Add intro and docsaurus theme (#99)
    * init introduction for monorepo
    
    * feat: docs as homepage
    
    * feat(docs): update docs theme and logo
    
    * feat(docs): finish styling for documentation app

--- apps/docs/docs/intro.md
+++ apps/docs/docs/intro.md
@@ -1,47 +1,22 @@
 ---
+slug: /
 sidebar_position: 1
 ---
 
-# Tutorial Intro: Onlineweb
+# Monoweb | Galactic Thunderdome X
 
-Let's discover **Onlineweb in less than 5 minutes**.
+Galactic Thunderdome X is the monorepo behind Onlineweb, the main application for Online, student organization for Informatics at NTNU. A monorepo is quite simply a collection of different projects at the same place, instead of splitting it to different repositories. The current available applications are:
 
-## Getting Started
+- Mail handler
+- [Core API](https://github.com/dotkom/galactic-thunderdome-x/tree/master/apps/ow-api)
+- [Web](https://github.com/dotkom/galactic-thunderdome-x/tree/master/apps/web)
+- [Dashboard](https://github.com/dotkom/galactic-thunderdome-x/tree/master/apps/dashboard)
+- [Docs](https://github.com/dotkom/galactic-thunderdome-x/tree/master/apps/docs), which you're reading now!
 
-Get started by **creating a new site**.
 
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
+Each application might make use of the internal packages available in [packages](https://github.com/dotkom/galactic-thunderdome-x/tree/master/packages). For instance, both **dashboard** and **web** might make use of [ow-ui](https://github.com/dotkom/galactic-thunderdome-x/tree/master/packages/ow-ui), our design systems. All systems also uses a structured configured [logger](https://github.com/dotkom/galactic-thunderdome-x/tree/master/packages/logger) for logging. The current available packages are:
 
-### What you'll need
-
-- [Node.js](https://nodejs.org/en/download/) version 14 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
-
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+- **eslint-config-ow**: ESLint configuration
+- **ow-db**: Fully typed database client and migrator
+- **ow-logger**: Structured logger
+- **ow-ui**: Design System.
--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,40 +1,36 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          routeBasePath: "/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
+        blog: false,
       }),
     ],
   ],
@@ -43,76 +39,66 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: "Monoweb",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "Monoweb",
+          src: "img/online.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/dotkom/galactic-thunderdome-x",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Slack",
+                href: "https://onlinentnu.slack.com",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/2XB9egU",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/dotkom",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
-};
+}
 
-module.exports = config;
+module.exports = config
