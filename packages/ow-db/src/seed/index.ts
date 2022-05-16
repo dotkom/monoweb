00c281441549412879ac26117bc4d9cc51d4aac8
Thomas Hasvold <thomashasvold@pop-os.localdomain>
2022-05-16 12:48:30 +0200

    feat(punishment): added punishment schema and seed function

--- packages/ow-db/src/seed/index.ts
+++ packages/ow-db/src/seed/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from ".prisma/client"
 import { Chance } from "chance"
 import { seedCompanies } from "./company"
+import { seedPunishments } from "./punishment"
 import { seedUsers } from "./users"
 
 export type Seeder = (chance: Chance.Chance, prisma: PrismaClient) => Promise<void>
@@ -10,3 +11,4 @@ const chance = new Chance("onlineweb")
 
 await seedUsers(chance, prisma)
 await seedCompanies(chance, prisma)
+await seedPunishments(chance, prisma)
