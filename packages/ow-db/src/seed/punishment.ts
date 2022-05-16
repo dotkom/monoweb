7f55cf4baa3471f86ed6d3bcf2cf63170a32b586
Thomas Hasvold <thomashasvold@pop-os.localdomain>
2022-05-16 21:31:34 +0200

    :sparkles: Update punishment seed and schema

--- packages/ow-db/src/seed/punishment.ts
+++ packages/ow-db/src/seed/punishment.ts
@@ -1,18 +1,34 @@
 import { Seeder } from "."
 import { PunishmentType } from "@prisma/client"
+import { initUserRepository } from "../../../../apps/ow-api/src/modules/auth/user-repository"
 
 export const seedPunishments: Seeder = async (chance, prisma) => {
+  const rulesetID = "3ba59ac4-76a5-4b75-a3ed-cdcc68c05e03"
+
+  const punishmentRuleInsert = { id: rulesetID, title: chance.name(), description: chance.paragraph(), duration: 20 }
+  await prisma.punishmentRuleSet.upsert({
+    where: { id: rulesetID },
+    update: punishmentRuleInsert,
+    create: punishmentRuleInsert,
+  })
+
+  const users = await prisma.user.findMany({ take: 10 })
+
   const seedInserts = [...new Array(10)].map(() => ({
     name: chance.name(),
     type: chance.pickone([PunishmentType.MARK, PunishmentType.SUSPENSION]),
-    date: chance.date(),
+    date: new Date(),
   }))
 
   const promises = seedInserts.map((payload) =>
-    prisma.punishment.create({data: {
+    prisma.punishment.create({
+      data: {
         type: payload.type,
-        start_date: payload.date
-    }})
+        ruleset_id: rulesetID,
+        start_date: payload.date,
+        user_id: chance.pickone(users).id,
+      },
+    })
   )
 
   await Promise.all(promises)
