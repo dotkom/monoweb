28597cbdb20459d19a76ea3948bc886491b25df8
Thomas Hasvold <thomas.hasvold@gmail.com>
2022-05-21 17:09:49 +0200

    :fire: Remove logger in index

--- apps/ow-api/src/index.ts
+++ apps/ow-api/src/index.ts
@@ -33,12 +33,4 @@ if (process.env.NODE_ENV === "development") {
   const port = Number(process.env.PORT || 4000)
   createServer(apolloConfig).then((server) => server.listen({ port }))
   logger.info(`Started GraphQL server at http://localhost:${port}/graphql 🚀`)
-  const newPunishment = await punishmentService.registerPunishment({
-    givenDate: new Date(),
-    rulsetID: "3ba59ac4-76a5-4b75-a3ed-cdcc68c05e03",
-    type: "MARK",
-    userID: "0e8c42c9-c68b-4e13-959c-84bf6d644a9f",
-  })
-  logger.info("Start date: " + newPunishment.startDate)
-  logger.info("Given date: " + newPunishment.givenDate)
 }
