bd09f2bee298bc3c58cb1ba912df0e5b798d3ff1
isaksolheim <isak.m.h.s@gmail.com>
2022-07-09 15:04:51 +0200

    Rename ParallaxContainer

--- apps/web/src/components/organisms/Footer/components/Waves.tsx
+++ apps/web/src/components/organisms/Footer/components/Waves.tsx
@@ -1,17 +1,17 @@
-import { styled } from "@theme";
-import { VFC } from "react";
-import "./animation.css";
+import { styled } from "@theme"
+import { VFC } from "react"
+import "./animation.css"
 
 const Wave = styled("div", {
   width: "100%",
   marginBottom: "-10px",
   padding: "0",
-});
+})
 
-const G = styled("g", {
+const ParallaxContainer = styled("g", {
   margin: "0",
   transform: "scaleY(0.2) translateY(180px)",
-});
+})
 
 export const Waves: VFC = () => (
   <Wave>
@@ -19,12 +19,12 @@ export const Waves: VFC = () => (
       <defs>
         <path id="gentle-wave" d="M-0 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
       </defs>
-      <G className="parallax">
+      <ParallaxContainer className="parallax">
         <use href="#gentle-wave" x="0" y="0" fill="rgba(13, 37, 70, 0.4)"></use>
         <use href="#gentle-wave" x="-10" y="5" fill="rgba(13, 37, 70, 0.4)"></use>
         <use href="#gentle-wave" x="-20" y="10" fill="rgba(13, 37, 70, 0.4)"></use>
         <use href="#gentle-wave" x="-30" y="15" fill="rgba(13, 37, 70, 1)"></use>
-      </G>
+      </ParallaxContainer>
     </svg>
   </Wave>
-);
+)
