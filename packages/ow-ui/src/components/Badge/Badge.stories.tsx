762ab8ca7cf3ebd7b85bea08ab949cd950138bbf
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-07-09 18:36:56 +0200

    Badge to storybook

--- packages/ow-ui/src/components/Badge/Badge.stories.tsx
+++ packages/ow-ui/src/components/Badge/Badge.stories.tsx
@@ -1,11 +1,85 @@
-import Badge from "./Badge";
+import Badge from "./Badge"
 export default {
   title: "atoms/Badge",
   component: Badge,
-};
+}
 
-export const Default = () => <Badge>badge</Badge>;
-export const Orange = () => <Badge color="orange">badge</Badge>;
-export const Green = () => <Badge color="green">badge</Badge>;
-export const Red = () => <Badge color="red">badge</Badge>;
-export const Gray = () => <Badge color="gray">badge</Badge>;
+export const GreenSubtle = () => (
+  <Badge color="green" variant="subtle">
+    Badge
+  </Badge>
+)
+export const GreenSolid = () => (
+  <Badge color="green" variant="solid">
+    Badge
+  </Badge>
+)
+export const GreenOutline = () => (
+  <Badge color="green" variant="outline">
+    Badge
+  </Badge>
+)
+
+export const graySubtle = () => (
+  <Badge color="gray" variant="subtle">
+    Badge
+  </Badge>
+)
+export const graySolid = () => (
+  <Badge color="gray" variant="solid">
+    Badge
+  </Badge>
+)
+export const grayOutline = () => (
+  <Badge color="gray" variant="outline">
+    Badge
+  </Badge>
+)
+
+export const blueSubtle = () => (
+  <Badge color="blue" variant="subtle">
+    Badge
+  </Badge>
+)
+export const blueSolid = () => (
+  <Badge color="blue" variant="solid">
+    Badge
+  </Badge>
+)
+export const blueOutline = () => (
+  <Badge color="blue" variant="outline">
+    Badge
+  </Badge>
+)
+
+export const redSubtle = () => (
+  <Badge color="red" variant="subtle">
+    Badge
+  </Badge>
+)
+export const redSolid = () => (
+  <Badge color="red" variant="solid">
+    Badge
+  </Badge>
+)
+export const redOutline = () => (
+  <Badge color="red" variant="outline">
+    Badge
+  </Badge>
+)
+
+export const orangeSubtle = () => (
+  <Badge color="orange" variant="subtle">
+    Badge
+  </Badge>
+)
+export const orangeSolid = () => (
+  <Badge color="orange" variant="solid">
+    Badge
+  </Badge>
+)
+export const orangeOutline = () => (
+  <Badge color="orange" variant="outline">
+    Badge
+  </Badge>
+)
