ac91c240b21921310d043335fbad5d4e7681e133
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-07-10 08:04:55 +0200

    Adding Tertiary,adding to storyboard, Fixing hover

--- packages/ow-ui/src/components/Button/Button.stories.tsx
+++ packages/ow-ui/src/components/Button/Button.stories.tsx
@@ -6,23 +6,96 @@ export default {
 
 export const Default = () => (
   <Button color="blue" variant="solid">
-    Default Button
+    Button
   </Button>
 )
-export const subtleButton = () => (
+export const subtleBlue = () => (
   <Button color="blue" variant="subtle">
-    Subtle Button
+    Button
+  </Button>
+)
+export const tertiaryBlue = () => (
+  <Button color="blue" variant="tertiary">
+    Button
   </Button>
 )
 
-export const orangeButton = () => (
+export const solidOrange = () => (
   <Button color="orange" variant="solid">
-    Orange Button
+    Button
+  </Button>
+)
+export const subtleOrange = () => (
+  <Button color="orange" variant="subtle">
+    Button
+  </Button>
+)
+export const tertiaryOrange = () => (
+  <Button color="orange" variant="tertiary">
+    Button
   </Button>
 )
 
-export const infoButton = () => (
+export const solidInfo = () => (
   <Button color="info" variant="solid">
-    Info Button
+    Button
+  </Button>
+)
+export const subtleInfo = () => (
+  <Button color="info" variant="subtle">
+    Button
+  </Button>
+)
+export const tertiaryInfo = () => (
+  <Button color="info" variant="tertiary">
+    Button
+  </Button>
+)
+
+export const solidGreen = () => (
+  <Button color="green" variant="solid">
+    Button
+  </Button>
+)
+export const subtleGreen = () => (
+  <Button color="green" variant="subtle">
+    Button
+  </Button>
+)
+export const tertiaryGreen = () => (
+  <Button color="green" variant="tertiary">
+    Button
+  </Button>
+)
+
+export const solidRed = () => (
+  <Button color="red" variant="solid">
+    Button
+  </Button>
+)
+export const subtleRed = () => (
+  <Button color="red" variant="subtle">
+    Button
+  </Button>
+)
+export const tertiaryRed = () => (
+  <Button color="red" variant="tertiary">
+    Button
+  </Button>
+)
+
+export const solidGray = () => (
+  <Button color="gray" variant="solid">
+    Button
+  </Button>
+)
+export const subtleGray = () => (
+  <Button color="gray" variant="subtle">
+    Button
+  </Button>
+)
+export const tertiaryGray = () => (
+  <Button color="gray" variant="tertiary">
+    Button
   </Button>
 )
