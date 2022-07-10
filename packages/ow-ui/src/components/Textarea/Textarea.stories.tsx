b4bb04ab28aad77632e9af19637552abbc354a35
kharann <anhkhav@gmail.com>
2022-07-10 19:15:11 +0200

    refactor text area to use css prop

--- packages/ow-ui/src/components/Textarea/Textarea.stories.tsx
+++ packages/ow-ui/src/components/Textarea/Textarea.stories.tsx
@@ -1,4 +1,4 @@
-import Textarea from "./Textarea";
+import Textarea from "./Textarea"
 
 export default {
   title: "atoms/Textarea",
@@ -8,24 +8,30 @@ export default {
 export const DefaultWithPlaceholder = () => <Textarea placeholder="Enter something.." />
 export const DisabledWithPlaceholder = () => <Textarea placeholder="Enter something.." disabled />
 
-export const DefaultWithLabel = () => <Textarea placeholder="Enter something.." label="Comment" />
-export const DisabledWithLabel = () => <Textarea placeholder="Enter something.." label="Comment" disabled />
+export const DefaultWithLabel = () => <Textarea placeholder="Enter something.." label="Comment" id="default" />
+export const DisabledWithLabel = () => (
+  <Textarea placeholder="Enter something.." label="Comment" disabled id="default" />
+)
 
 export const DefaultWithText = () => <Textarea value="Lorem ipsum dolor sit." readOnly />
 export const DisabledWithText = () => <Textarea value="Lorem ipsum dolor sit." readOnly disabled />
 
-export const DefaultWithDanger = () => (<Textarea
-  label="Comment"
-  value="The quick brown fox jumps over the lazy dog"
-  readOnly
-  status="danger"
-  message="Failed to validate text"
-/>)
+export const DefaultWithDanger = () => (
+  <Textarea
+    label="Comment"
+    value="The quick brown fox jumps over the lazy dog"
+    readOnly
+    status="danger"
+    message="Failed to validate text"
+  />
+)
 
-export const DefaultWithSuccess = () => (<Textarea
-  label="Comment"
-  value="The quick brown fox jumps over the lazy dog"
-  readOnly
-  status="success"
-  message="Field validation successful"
-/>)
+export const DefaultWithSuccess = () => (
+  <Textarea
+    label="Comment"
+    value="The quick brown fox jumps over the lazy dog"
+    readOnly
+    status="success"
+    message="Field validation successful"
+  />
+)
--- packages/ow-ui/src/components/Textarea/Textarea.tsx
+++ packages/ow-ui/src/components/Textarea/Textarea.tsx
@@ -1,68 +1,9 @@
 import { ComponentPropsWithoutRef, forwardRef } from "react"
 import * as Label from "@radix-ui/react-label"
-import { styled } from "../../config/stitches.config"
+import { css } from "../../config/stitches.config"
 import { AlertIcon } from "../Alert/AlertIcon"
 
-const BaseTextarea = styled("textarea", {
-  outline: "none",
-  resize: "none",
-  padding: "$2",
-  border: "2px solid $gray10",
-  fontFamily: "$body",
-  borderRadius: "$1",
-  backgroundColor: "$gray12",
-  "&:disabled": {
-    backgroundColor: "$gray11",
-    cursor: "not-allowed",
-  },
-  "&:focus": {
-    borderColor: "$info4",
-  },
-  variants: {
-    status: {
-      danger: {
-        borderColor: "$red5",
-      },
-      success: {
-        borderColor: "$green5",
-      },
-    },
-  },
-})
-
-const InputContainer = styled("div", {
-  display: "flex",
-  flexDirection: "column",
-  width: "100%",
-  gap: "$1",
-})
-
-const MessageContainer = styled("div", {
-  fontSize: "$md",
-  display: "flex",
-  alignItems: "center",
-})
-
-const Message = styled("span", {
-  fontWeight: "600",
-  variants: {
-    status: {
-      danger: {
-        color: "$red0",
-      },
-      success: {
-        color: "$green0",
-      },
-    },
-  },
-})
-
-const InputLabel = styled(Label.Root, {
-  fontWeight: "bold",
-  fontFamily: "$body",
-})
-
-export type TextareaProps = ComponentPropsWithoutRef<typeof BaseTextarea> & {
+export type TextareaProps = ComponentPropsWithoutRef<"textarea"> & {
   label?: string
 } & (
     | {
@@ -76,22 +17,81 @@ export type TextareaProps = ComponentPropsWithoutRef<typeof BaseTextarea> & {
   )
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(function Textarea(props, ref) {
-  const { message, label, status, ...rest } = props
-  // TODO: replace with React 18 useId() when storybook wants to work
-  const id = "foobar"
+  const { id, message, label, status, ...rest } = props
 
   return (
-    <InputContainer>
-      {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
-      <BaseTextarea ref={ref} status={status} id={id} {...rest} />
-      {message && (
-        <MessageContainer>
-          <AlertIcon status={status} />
-          <Message status={status}>{message}</Message>
-        </MessageContainer>
+    <div className={styles.inputContainer()}>
+      {label && (
+        <Label.Root htmlFor={id} className={styles.label()}>
+          {label}
+        </Label.Root>
       )}
-    </InputContainer>
+      <textarea className={styles.base({ status })} ref={ref} id={id} {...rest} />
+      {message && (
+        <div className={styles.messageContainer()}>
+          <AlertIcon status={status} />
+          <span className={styles.message({ status })}>{message}</span>
+        </div>
+      )}
+    </div>
   )
 })
+
+const styles = {
+  base: css({
+    outline: "none",
+    resize: "none",
+    padding: "$2",
+    border: "2px solid $gray10",
+    fontFamily: "$body",
+    borderRadius: "$1",
+    backgroundColor: "$gray12",
+    "&:disabled": {
+      backgroundColor: "$gray11",
+      cursor: "not-allowed",
+    },
+    "&:focus": {
+      borderColor: "$info4",
+    },
+    variants: {
+      status: {
+        danger: {
+          borderColor: "$red5",
+        },
+        success: {
+          borderColor: "$green5",
+        },
+      },
+    },
+  }),
+  label: css({
+    fontWeight: "bold",
+    fontFamily: "$body",
+  }),
+  inputContainer: css({
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    gap: "$1",
+  }),
+  message: css({
+    fontWeight: "600",
+    variants: {
+      status: {
+        danger: {
+          color: "$red0",
+        },
+        success: {
+          color: "$green0",
+        },
+      },
+    },
+  }),
+  messageContainer: css({
+    fontSize: "$md",
+    display: "flex",
+    alignItems: "center",
+  }),
+}
 
 export default Textarea
