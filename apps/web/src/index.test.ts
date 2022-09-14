143ca62d704a550cbcef9d576c066fcd45cc5a1f
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-09-14 17:57:48 +0200

    test: Setup vitest

---
+++ apps/web/src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest"
+
+describe("my test", () => {
+  it("should pass", () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ apps/web/vitest.setup.ts
@@ -0,0 +1,3 @@
+import { loadEnvConfig } from "@next/env"
+
+loadEnvConfig(process.cwd())
