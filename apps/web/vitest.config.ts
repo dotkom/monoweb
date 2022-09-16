264fad1ec2be64187a99621ad198ca3ca43a5a02
Anhkha Vo <anhkhav@gmail.com>
2022-09-16 18:15:08 +0000

    exclude vitest when building

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
-
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
