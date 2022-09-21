452649854e2f343ca6ce8b52334701f499368318
Mats Larsen <me@supergrecko.com>
2022-09-21 17:49:34 +0200

    feat: delete graphql

--- apps/ow-api/src/index.ts
+++ apps/ow-api/src/index.ts
@@ -1,10 +1,6 @@
 import { getLogger } from "@dotkom/logger"
-import { createServer } from "./server.js"
 import { initUserRepository } from "./modules/auth/user-repository.js"
 import { initUserService } from "./modules/auth/user-service.js"
-import { ApolloServerExpressConfig } from "apollo-server-express"
-import graphqlSchema from "./graphql/schema.js"
-import { Request, Response } from "express"
 import { PrismaClient } from "@dotkom/db"
 
 const logger = getLogger(import.meta.url)
@@ -15,18 +11,3 @@ const userRepository = initUserRepository(client)
 
 // Services
 const userService = initUserService(userRepository)
-
-export const apolloConfig: ApolloServerExpressConfig = {
-  schema: graphqlSchema,
-  context: (_req: Request, _res: Response) => {
-    return {
-      userService,
-    }
-  },
-}
-
-if (process.env.NODE_ENV === "development") {
-  const port = Number(process.env.PORT || 4000)
-  createServer(apolloConfig).then((server) => server.listen({ port }))
-  logger.info(`Started GraphQL server at http://localhost:${port}/graphql 🚀`)
-}
