20bf6f46d4809a0fc932be60267e749446d490b8
jogt <jotjernshaugen@gmail.com>
2022-09-21 20:15:22 +0200

    Added draft for coming event cards

---
+++ apps/web/src/components/molecules/ComingEvent/ComingEvent.stories.tsx
@@ -0,0 +1,18 @@
+import { ComingEvent } from "./ComingEvent"
+
+export default {
+    title: "molecules/ComingEvent",
+    component: ComingEvent,
+}
+
+export const SingleEvent = () => (
+    <ComingEvent
+        title="Jo sin bursdag"
+        tag="sosialt"
+        attending={0}
+        max_attending={30}
+        date="31 mai"
+        img="https://handletheheat.com/wp-content/uploads/2015/03/Best-Birthday-Cake-with-milk-chocolate-buttercream-SQUARE.jpg"
+        info_link="https://google.com"
+    />
+)
---
+++ apps/web/src/components/molecules/ComingEvent/ComingEvent.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {Badge, Text} from "@dotkom/ui";
+import {css} from "@stitches/react";
+
+type ComingEventProps = {
+    img: string,
+    title: string,
+    tag: string,
+    attending: number,
+    max_attending: number,
+    date: string,
+    info_link: string,
+}
+
+// import ionicons
+
+export const ComingEvent: React.FC<ComingEventProps> = (props) => {
+    return <div className={styles.comingEvent()}>
+        <div className={styles.comingEventImageContainer()}>
+            <img src={props.img} alt="" className={styles.comingEventImage()}/>
+        </div>
+        <div className={styles.comingEventInfoContainer()}>
+            <span>
+                <span>
+                    { /* PLACEHOLDER */ }
+                </span>
+                <div>
+                    <Text className={styles.noMargin()} size="lg">{props.title}</Text>
+                    <Text className={styles.noMargin()}>{props.date}</Text>
+                    <Text className={styles.noMargin()}>{props.attending}/{props.max_attending}</Text>
+                </div>
+            </span>
+        </div>
+
+        <Badge color="green" variant="solid" className={styles.tag()}>{props.tag}</Badge>
+        <a href={props.info_link}>
+            <Text className={styles.infoLink()}>Info</Text>
+        </a>
+    </div>
+}
+
+const styles = {
+    comingEvent: css({
+        height: "100px",
+        maxWidth: "400px",
+        position: "relative",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+        borderRadius: "10px",
+        overflow: "hidden"
+    }),
+    comingEventImage: css({ // Make image height 50px and fit width
+        height: "100px",
+        width: "100px",
+        objectFit: "cover",
+    }),
+    comingEventImageContainer: css({
+        margin: 0,
+        display: "inline-block",
+        verticalAlign: "top",
+    }),
+    comingEventInfoContainer: css({
+        margin: 0,
+        display: "inline-block",
+        verticalAlign: "top",
+        padding: "5px"
+    }),
+    noMargin: css({
+        margin: 0
+    }),
+    tag: css({
+        position: "absolute",
+        top: "10px",
+        right: "10px"
+    }),
+    infoLink: css({
+        position: "absolute",
+        bottom: "10px",
+        right: "10px",
+        margin: 0
+    })
+}
