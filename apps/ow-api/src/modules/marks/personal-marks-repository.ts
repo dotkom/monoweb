8770b013c22439807238b7c587a31120333dc1d3
Njål Sørland <njaalis2000@gmail.com>
2022-10-06 12:49:38 +0200

    Added functions for start and end date calculation

--- apps/ow-api/src/modules/marks/personal-marks-repository.ts
+++ apps/ow-api/src/modules/marks/personal-marks-repository.ts
@@ -7,10 +7,12 @@ export interface PersonalMarksRepository {
   createPersonalMarks: (personalMarksInsert: InsertPersonalMarks) => Promise<PersonalMarks>
   addActiveMark: (id: string, mark: string) => Promise<PersonalMarks | undefined>
   getActiveMarksByID: (id: string) => Promise<Array<string> | undefined>
-  calculateEndDate: (currentDate: Date, amountOfMarks: number) => Date
+  calculateEndDate: (startDate: Date, amountOfMarks: number) => Date
+  calculateStartDate: () => Date
 }
 
 export const initPersonalMarksRepository = (client: PrismaClient): PersonalMarksRepository => {
+  const markDuration = 20
   const repo: PersonalMarksRepository = {
     getPersonalMarksByID: async (id) => {
       const personalMarks = await client.personalMarks.findUnique({
@@ -45,8 +47,28 @@ export const initPersonalMarksRepository = (client: PrismaClient): PersonalMarks
       })
       return personalMarks
     },
-    calculateEndDate: (currentDate, amountOfMarks) => {
-      return currentDate
+    calculateStartDate: () => {
+      let startDate = new Date()
+      if (startDate.getMonth() == 11) {
+        startDate = new Date(startDate.getFullYear() + 1, 0, 15, 1)
+      } else if (startDate.getMonth() == 0 && startDate.getDay() < 15) {
+        startDate = new Date(startDate.getFullYear(), 0, 15, 1)
+      } else if (startDate.getMonth() > 4 && startDate.getMonth() + startDate.getDate() / 100 < 7.15) {
+        startDate = new Date(startDate.getFullYear(), 7, 15, 2)
+      }
+      return startDate
+    },
+    calculateEndDate: (startDate, amountOfMarks) => {
+      const endDate = startDate
+      for (let i = 0; i < amountOfMarks; i++) {
+        endDate.setDate(endDate.getDate() + markDuration)
+        if (endDate.getMonth() == 11 || (endDate.getMonth() == 0 && endDate.getDay() < 15)) {
+          endDate.setDate(endDate.getDate() + 45)
+        } else if (endDate.getMonth() > 4 && endDate.getMonth() + endDate.getDate() / 100 < 7.15) {
+          endDate.setDate(endDate.getDate() + 75)
+        }
+      }
+      return endDate
     },
   }
   return repo
