8ed95115c1d3d16eb77797cfb305c5054db9f1c1
kharann <anhkhav@gmail.com>
2022-10-25 10:58:17 +0200

    add cors and trpc

--- apps/ow-api/src/server.ts
+++ apps/ow-api/src/server.ts
@@ -2,9 +2,12 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express"
 import express from "express"
 import { appRouter } from "./trpc.js"
 import { createContext } from "./index.js"
+import cors from "cors"
 
 export const createServer = () => {
   const app = express()
+  app.use(cors())
+
   // TODO: add express cors extension here
   const handler = createExpressMiddleware({
     router: appRouter,
