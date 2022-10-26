87bf90d780c03ce69510015841927dac8208ac78
kharann <anhkhav@gmail.com>
2022-10-27 01:52:09 +0200

    add sample tailwind config

---
+++ packages/config/tailwind-preset.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontFamily: {
+      poppins: ['"Poppins"'],
+    },
+    extend: {
+      colors: {
+        blue: {
+          0: "#091C34",
+          1: "#0D2546",
+          2: "#11325E",
+          3: "#153E75",
+          4: "#2C5183",
+          5: "#446591",
+          6: "#5B789E",
+          7: "#738BAC",
+          8: "#8A9FBA",
+          9: "#A1B2C8",
+          10: "#B9C5D6",
+          11: "#D0D8E3",
+          12: "#E8ECF1",
+          bg: "#EBF3FE",
+        },
+      },
+    },
+  },
+}
