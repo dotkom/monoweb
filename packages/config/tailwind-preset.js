830576ed5b391312c5f9ad9a20dbac2eb67bd63b
kharann <anhkhav@gmail.com>
2022-10-31 11:20:43 +0100

    add content in tw config

--- packages/config/tailwind-preset.js
+++ packages/config/tailwind-preset.js
@@ -77,6 +77,7 @@ const red = {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       poppins: ['"Poppins"'],
