92b7223ddf2406389218a27f3dfe92b446daffa2
isaksolheim <mail@isaks.io>
2022-11-02 19:10:03 +0100

    Fix broken github icon

--- apps/web/src/components/icons/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "@theme";
-
-export const Icon = styled("figure", {
-  width: "55px",
-  height: "55px",
-  display: "inline-block",
-  cursor: "pointer",
-  margin: "0px 13px 0px 13px",
-});
