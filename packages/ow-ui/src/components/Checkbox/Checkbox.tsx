ab6278d585a3a74f8a53a15cfd9d051a6678205c
kharann <anhkhav@gmail.com>
2022-11-02 13:26:43 +0100

    increase brightness of checkbox

--- packages/ow-ui/src/components/Checkbox/Checkbox.tsx
+++ packages/ow-ui/src/components/Checkbox/Checkbox.tsx
@@ -15,7 +15,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(({ label, ..
       className={clsx(
         "bg-slate-3 h-7 w-7 appearance-none rounded-sm outline-none transition-colors",
         "hover:bg-slate-4 focus:ring-2",
-        "rdx-state-checked:bg-blue-3 rdx-state-checked:hover:bg-blue-4",
+        "rdx-state-checked:bg-blue-4 rdx-state-checked:hover:bg-blue-5",
         "rdx-disabled:bg-slate-5 rdx-state-checked:rdx-disabled:bg-slate-5 rdx-disabled:cursor-not-allowed"
       )}
       {...props}
