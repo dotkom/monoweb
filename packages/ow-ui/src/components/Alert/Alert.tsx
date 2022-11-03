611d8043eb279b12c8e072acf3a2eb4c889c6805
Anhkha Vo <anhkhav@gmail.com>
2022-11-03 21:32:42 +0100

    Redesign Alert component in design system (#214)
    * bundle from index
    
    * convert alert component
    
    * fix linting issues
    
    * wrap icon in div and add margin
    
    * more rounding

--- packages/ow-ui/src/components/Alert/Alert.tsx
+++ packages/ow-ui/src/components/Alert/Alert.tsx
@@ -1,51 +1,44 @@
-import { FC } from "react"
+import { cva } from "cva"
+import { FC, PropsWithChildren } from "react"
 
-import { css } from "../../config/stitches.config"
 import { AlertIcon } from "./AlertIcon"
 
 export interface AlertProps {
   status: "info" | "warning" | "success" | "danger"
-  text: string
+  title: string
   showIcon?: boolean
 }
 
-export const Alert: FC<AlertProps> = ({ status, text, showIcon = true }) => {
+export const Alert: FC<PropsWithChildren<AlertProps>> = ({ status, title, children, showIcon = true }) => {
   return (
-    <div className={styles.root({ status })}>
-      {showIcon && <AlertIcon status={status} />}
-      {text}
+    <div className={alert({ status })}>
+      <div className="mr-2">{showIcon && <AlertIcon status={status} />}</div>
+      <div className="ml-4 flex flex-col">
+        <span className={alertTitle({ status })}>{title}</span>
+        <div className="text-slate-12">{children}</div>
+      </div>
     </div>
   )
 }
 
-const styles = {
-  root: css({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "$2",
-    fontSize: "$md",
-    fontWeight: "semibold",
-    borderRadius: "$2", // XD
-    variants: {
-      status: {
-        info: {
-          backgroundColor: "$info12",
-          color: "$info0",
-        },
-        success: {
-          backgroundColor: "$green12",
-          color: "$green0",
-        },
-        warning: {
-          backgroundColor: "$orange12",
-          color: "$orange0",
-        },
-        danger: {
-          backgroundColor: "$red12",
-          color: "$red0",
-        },
-      },
+const alert = cva("flex justify-center flex-row px-5 py-4 rounded-md text-base", {
+  variants: {
+    status: {
+      info: "bg-blue-4",
+      success: "bg-green-4",
+      warning: "bg-amber-4",
+      danger: "bg-red-4",
     },
-  }),
-}
+  },
+})
+
+const alertTitle = cva("font-semibold mb-3", {
+  variants: {
+    status: {
+      info: "text-blue-11",
+      success: "text-green-11",
+      warning: "text-amber-11",
+      danger: "text-red-11",
+    },
+  },
+})
