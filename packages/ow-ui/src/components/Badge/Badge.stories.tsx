1a80cb28574ace8cdf3af18b23a182c8537eeb1a
Anhkha Vo <anhkhav@gmail.com>
2022-11-04 00:13:23 +0000

    add all badge variants

--- packages/ow-ui/src/components/Badge/Badge.stories.tsx
+++ packages/ow-ui/src/components/Badge/Badge.stories.tsx
@@ -21,3 +21,36 @@ export const Subtle = () => (
     </Badge>
   </div>
 )
+
+export const Filled = () => (
+  <div className="grid gap-2">
+    <Badge color="green" variant="filled">
+      Red
+    </Badge>
+    <Badge color="red" variant="filled">
+      Red
+    </Badge>
+    <Badge color="blue" variant="filled">
+      Blue
+    </Badge>
+    <Badge color="amber" variant="filled">
+      Amber
+    </Badge>
+  </div>
+)
+export const Outline= () => (
+  <div className="grid gap-2">
+    <Badge color="green" variant="outline">
+      Green
+    </Badge>
+    <Badge color="red" variant="outline">
+      Red
+    </Badge>
+    <Badge color="blue" variant="outline">
+      Blue
+    </Badge>
+    <Badge color="amber" variant="outline">
+      Amber
+    </Badge>
+  </div>
+)
--- packages/ow-ui/src/components/Badge/Badge.tsx
+++ packages/ow-ui/src/components/Badge/Badge.tsx
@@ -1,28 +1,49 @@
 import { cva } from "cva"
 import { FC, PropsWithChildren } from "react"
+import { twMerge } from "tailwind-merge"
 
 import { styled } from "../../config/stitches.config"
 
 export interface BadgeProps {
   color: "red" | "green" | "amber" | "blue"
-  variant: "subtle" | "filled"
+  variant: "subtle" | "filled" | "outline"
 }
 
 export const Badge: FC<PropsWithChildren<BadgeProps>> = ({ children, color, variant }) => {
-  return (
-    <span className={badge({ color })}>
-      {children}
-    </span>
-  )
+  const style = twMerge(styles[variant]({ color }), "flex w-fit items-center rounded-md px-4 font-medium leading-")
+  return <span className={style}>{children}</span>
 }
-
-const badge = cva("px-4 rounded-md font-medium leading-7 flex items-center w-fit", {
-  variants: {
-    color: {
-      red: "bg-red-4 text-red-11",
-      blue: "bg-blue-4 text-blue-11",
-      green: "bg-green-4 text-green-11",
-      amber: "bg-amber-4 text-amber-11",
+const styles = {
+  filled: cva("text-slate-12", {
+    variants: {
+      color: {
+        red: "bg-red-9",
+        blue: "bg-blue-9",
+        green: "bg-green-9",
+        amber: "bg-amber-9 text-slate-1",
+      },
     },
-  },
-})
+  }),
+
+  subtle: cva("", {
+    variants: {
+      color: {
+        red: "bg-red-4 text-red-11",
+        blue: "bg-blue-4 text-blue-11",
+        green: "bg-green-4 text-green-11",
+        amber: "bg-amber-4 text-amber-11",
+      },
+    },
+  }),
+
+  outline: cva("border border-solid", {
+    variants: {
+      color: {
+        red: "border-red-7 text-red-11 bg-red-1",
+        blue: "border-blue-7 text-blue-11 bg-blue-1",
+        green: "border-green-7 text-green-11 bg-green-1",
+        amber: "border-amber-7 text-amber-11 bg-amber-1",
+      },
+    },
+  }),
+}
