9fcdbc79a7031d88ad6e4bc77d0c5ad46f276479
Anhkha Vo <hey@akvo.no>
2022-11-05 20:57:27 +0100

    Redesign Button (#217)
    * start on button styling
    
    * Forwardref the button
    
    * forward html props
    
    * remove errors from missing attributes
    
    * cjs support for build
    
    * export from .prisma too
    
    * add subtle variant
    
    * set default variant to solid
    
    * fix linting issues

--- apps/storybook/.storybook/preview.jsx
+++ apps/storybook/.storybook/preview.jsx
@@ -1,10 +1,19 @@
 import "../../web/src/styles/globals.css"
+import { themes } from "@storybook/theming"
 
 export const parameters = {
   darkMode: {
     current: "dark",
   },
+  backgrounds: {
+    default: "black",
+    values: [{name: "black", value: "#000212"}]
+  },
+  docs: {
+    theme: themes.dark,
+  }
 }
+
 
 export const decorators = [
   (Story) => {
