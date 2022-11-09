69054f7c1b19ce5b64cfbe62b321a627f3f259da
Mats Larsen <me@supergrecko.com>
2022-11-09 20:45:03 +0100

    feat: set up trpc with react-query on dashboard

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,8 +1,28 @@
-import { FC } from "react"
+import { FC, useState } from "react";
 import { RouterProvider } from "react-router-dom"
 
 import { router } from "./routes/router"
+import { trpc } from "./trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client"
 
 export const App: FC = () => {
-  return <RouterProvider router={router} />
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:4000/trpc",
+        }),
+      ],
+    })
+  )
+
+  return (
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </trpc.Provider>
+  )
 }
