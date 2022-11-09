cefd27e51d37b570c385311955ce565f9860e0eb
Mats Larsen <me@supergrecko.com>
2022-11-09 19:59:15 +0000

    fix: avoid circular dependency in trpc (#230)

--- apps/ow-api/src/server.ts
+++ apps/ow-api/src/server.ts
@@ -1,10 +1,10 @@
+import { appRouter } from "@/router"
 import cors from "cors"
 import express from "express"
 
 import { createExpressMiddleware } from "@trpc/server/adapters/express"
 
 import { createContext } from "./context"
-import { appRouter } from "./trpc.js"
 
 export const createServer = () => {
   const app = express()
--- apps/ow-api/src/trpc.ts
+++ apps/ow-api/src/trpc.ts
@@ -1,19 +1,14 @@
+import { appRouter } from "@/router"
+
 import { initTRPC } from "@trpc/server"
 
 import { Context } from "./context"
-import { authRouter } from "./modules/auth/auth-router"
-import { eventRouter } from "./modules/event/event-router"
 
 // TODO: Superjson
 export const t = initTRPC.context<Context>().create({
   errorFormatter({ shape }) {
     return shape
   },
-})
-
-export const appRouter = t.router({
-  event: eventRouter,
-  auth: authRouter,
 })
 
 export type AppRouter = typeof appRouter
