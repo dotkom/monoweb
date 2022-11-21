8cb6b4260eadb06cf4b6b9a073dd49f727795539
kharann <anhkhav@gmail.com>
2022-11-21 22:03:02 +0100

    remove prettier preset

--- packages/config/prettier-preset.js
+++ packages/config/prettier-preset.js
@@ -5,7 +5,5 @@ module.exports = {
   printWidth: 120,
   endOfLine: "auto",
   arrowParens: "always",
-  importOrder: ["^@(dotkom)/(.*)$", "^@lib/(.*)$", "^@components/(.*)$", "^@(server|trpc)/(.*)$", "^~/(.*)$", "^[./]"],
-  importOrderSeparation: true,
   plugins: [require("prettier-plugin-tailwindcss")],
 }
