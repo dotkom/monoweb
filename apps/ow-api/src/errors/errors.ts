ef59f9ee1a4e92fedff8868bb4878c293c0e2e22
kharann <anhkhav@gmail.com>
2022-11-27 12:51:46 +0100

    delete old api

--- apps/ow-api/src/errors/errors.ts
+++
@@ -1,7 +0,0 @@
-export abstract class OwError extends Error {
-  abstract readonly statusCode: number
-}
-
-export class NotFoundError extends OwError {
-  statusCode = 404
-}
--- apps/ow-api/src/router.ts
+++
@@ -1,8 +0,0 @@
-import { authRouter } from "./modules/auth/auth-router"
-import { eventRouter } from "./modules/event/event-router"
-import { t } from "./trpc"
-
-export const appRouter = t.router({
-  event: eventRouter,
-  auth: authRouter,
-})
