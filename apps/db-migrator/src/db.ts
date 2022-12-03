3f30b8603b38275b306351ec4cbaca4b616a3916
kharann <anhkhav@gmail.com>
2022-12-03 16:36:03 +0100

    Createa cockroach adapter for kysely

--- apps/db-migrator/src/db.ts
+++ apps/db-migrator/src/db.ts
@@ -1,11 +1,11 @@
-import { Database } from "@dotkomonline/db"
-import { Kysely, PostgresDialect, CamelCasePlugin } from "kysely"
+import { CockroachDialect, Database } from "@dotkomonline/db"
+import { Kysely, CamelCasePlugin } from "kysely"
 import pg from "pg"
 
 import { env } from "./env"
 
 export const db = new Kysely<Database>({
-  dialect: new PostgresDialect({
+  dialect: new CockroachDialect({
     pool: new pg.Pool({
       connectionString: env.DATABASE_URL,
     }),
