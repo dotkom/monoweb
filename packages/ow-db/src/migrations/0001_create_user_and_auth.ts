2ef480e672f5610a1281829c7c3749d4895ff3fc
kharann <anhkhav@gmail.com>
2022-12-07 00:42:06 +0100

    refactor(db): Revert back to snake case tables

--- packages/ow-db/src/migrations/0001_create_user_and_auth.ts
+++ packages/ow-db/src/migrations/0001_create_user_and_auth.ts
@@ -5,7 +5,7 @@ import { createTableWithDefaults } from "../utils"
 // Kysely reccomends using "any" in migrations
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await createTableWithDefaults("User", { id: true, createdAt: true }, db.schema)
+  await createTableWithDefaults("ow_user", { id: true, createdAt: true }, db.schema)
     .addColumn("name", "varchar(255)")
     .addColumn("email", "varchar(255)", (col) => col.notNull().unique())
     .addColumn("email_verified", "timestamptz", (col) => col.notNull().defaultTo(sql`now()`))
@@ -13,26 +13,26 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn("image", "varchar(255)")
     .execute()
 
-  await createTableWithDefaults("Session", { id: true, createdAt: true }, db.schema)
+  await createTableWithDefaults("session", { id: true, createdAt: true }, db.schema)
     .addColumn("session_token", "text", (col) => col.notNull().unique())
     .addColumn("expires", "timestamptz", (col) => col.notNull())
-    .addColumn("user_id", "uuid", (col) => col.references("User.id").onDelete("cascade").notNull())
+    .addColumn("user_id", "uuid", (col) => col.references("ow_user.id").onDelete("cascade").notNull())
     .execute()
 
-  await createTableWithDefaults("VerificationToken", { id: true }, db.schema)
+  await createTableWithDefaults("verification_token", { id: true }, db.schema)
     .addColumn("identifier", "varchar(250)", (col) => col.notNull().unique())
     .addColumn("token", "varchar(250)", (col) => col.notNull())
     .addColumn("expires", "timestamptz", (col) => col.notNull())
     .execute()
 
   await db.schema
-    .createIndex("VerificationToken_identifier_token_unique_index")
-    .on("VerificationToken")
+    .createIndex("verification_token_identifier_token_unique_index")
+    .on("verification_token")
     .columns(["identifier", "token"])
     .unique()
     .execute()
 
-  await createTableWithDefaults("Account", { id: true, createdAt: true }, db.schema)
+  await createTableWithDefaults("account", { id: true, createdAt: true }, db.schema)
     .addColumn("type", "varchar(100)", (col) => col.notNull())
     .addColumn("provider", "varchar(100)", (col) => col.notNull())
     .addColumn("provider_account_id", "varchar(100)", (col) => col.notNull())
@@ -45,20 +45,20 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn("session_state", "varchar(100)")
     .addColumn("oauth_token_secret", "text")
     .addColumn("oauth_token", "text")
-    .addColumn("user_id", "uuid", (col) => col.notNull().references("User.id").onDelete("cascade"))
+    .addColumn("user_id", "uuid", (col) => col.notNull().references("ow_user.id").onDelete("cascade"))
     .execute()
 
   await db.schema
-    .createIndex("Account_provider_account_unique_index")
-    .on("Account")
+    .createIndex("account_provider_account_unique_index")
+    .on("account")
     .columns(["provider", "provider_account_id"])
     .unique()
     .execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable("Account").execute()
-  await db.schema.dropTable("VerificationToken").execute()
-  await db.schema.dropTable("Session").execute()
-  await db.schema.dropTable("User").execute()
+  await db.schema.dropTable("account").execute()
+  await db.schema.dropTable("verification_token").execute()
+  await db.schema.dropTable("session").execute()
+  await db.schema.dropTable("ow_user").execute()
 }
