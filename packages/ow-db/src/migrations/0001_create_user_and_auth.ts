da3003808ab91f5791d659494f7a2ab75e304f42
kharann <anhkhav@gmail.com>
2023-01-02 22:06:15 +0100

    do not set email_verified to now as default

--- packages/ow-db/src/migrations/0001_create_user_and_auth.ts
+++ packages/ow-db/src/migrations/0001_create_user_and_auth.ts
@@ -8,7 +8,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await createTableWithDefaults("ow_user", { id: true, createdAt: true }, db.schema)
     .addColumn("name", "varchar(255)")
     .addColumn("email", "varchar(255)", (col) => col.notNull().unique())
-    .addColumn("email_verified", "timestamptz", (col) => col.notNull().defaultTo(sql`now()`))
+    .addColumn("email_verified", "timestamptz")
     .addColumn("password", "varchar(255)", (col) => col.notNull())
     .addColumn("image", "varchar(255)")
     .execute()
