c17f6c58b2c31559f3d258fc1e7563aa3353ff8d
madsab <madsab@stud.ntnu.no>
2023-01-18 18:53:11 +0100

    Finished password component

---
+++ packages/ow-ui/src/components/PasswordInput/Password.tsx
@@ -0,0 +1,61 @@
+import { cva } from "cva"
+import { forwardRef, HTMLInputTypeAttribute, ReactComponentElement, ReactElement, useState } from "react"
+import { Label } from "@radix-ui/react-label"
+import { IconEyeCheck, IconEyeOff, IconEye} from "@tabler/icons"
+import { slate } from "@radix-ui/colors"
+
+
+
+export interface InputProps extends React.HTMLProps<HTMLInputElement> {
+    placeholder?: string
+    label?: string
+    withAsterisk?: boolean
+    error?: boolean | string
+    inputInfo?: string
+    eyeColor: "default" | "slate" | "white"
+  }
+
+export const PasswordInput = forwardRef<HTMLInputElement, InputProps>(({label, withAsterisk, error, inputInfo, eyeColor, ...props}, ref) => {
+  const [visible,setVisibility] = useState(false)
+  const Icon = visible ? <IconEyeOff onClick={() => setVisibility(visibility => !visibility)}/> 
+    : <IconEyeCheck onClick={() => setVisibility(visibility => !visibility)}/>;
+  const InputType = visible ? "text" : "password";
+  
+  return (
+        <div className="flex flex-col">
+            {label && (
+            <Label htmlFor={props.id} className="mb-2">
+              {label} {withAsterisk && <span className="text-red-11">*</span>}
+            </Label>
+            )}
+            <span className="text-xs">{inputInfo}</span>
+            <div className="relative">
+              <input type={InputType} {...props} ref={ref} className={input({error: !!error})} />
+                {typeof error === "string" && <span className="text-red-11 mt-1 text-xs"></span>}
+                <div>
+                    <span className={eye({color: eyeColor})}>{Icon}</span>
+                </div>
+            </div>
+        </div>
+    )
+  })
+ 
+
+  const input = cva("border-solid border outline-none focus:border-blue-7 bg-slate-3 rounded-md p-2 w-full ", {
+    variants: {
+      error: {
+        true: "text-red-11 border-red-7",
+        false: "text-slate-12 border-slate-6",
+      },
+    },
+  })
+
+  const eye = cva("absolute top-2 right-2 hover: cursor-pointer", {
+    variants: {
+        color: {
+            default: "text-solid",
+            slate: "text-slate-7",
+            white: "text-slate-11" 
+        }
+    }
+  })
