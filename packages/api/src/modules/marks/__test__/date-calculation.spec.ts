96742a02cf154ad3e15f8a2db2e53ac203007f33
Njål Sørland <njaalis2000@gmail.com>
2023-01-18 18:30:11 +0100

    snake_case -> camelCase

--- packages/api/src/modules/marks/__test__/date-calculation.spec.ts
+++ packages/api/src/modules/marks/__test__/date-calculation.spec.ts
@@ -17,104 +17,104 @@ describe("PersonalMarkDateCalculation", () => {
   // These tests are written to work until the year 3022. If you are reading this in 3022, please update the tests. Let those 4022 guys deal with it.
 
   it("Adds the correct duration to the current start date", () => {
-    const start_date = new Date("3022-10-01")
+    const startDate = new Date("3022-10-01")
     const marks = [
       {
         id: randomUUID(),
-        givenAt: start_date,
+        givenAt: startDate,
         duration: 24,
       },
     ]
 
     expect(personalMarkService.calculateExpiryDate(marks)).toEqual(
-      new Date(start_date.setDate(start_date.getDate() + marks[0].duration))
+      new Date(startDate.setDate(startDate.getDate() + marks[0].duration))
     )
   }),
     it("Adds durations iteratively for several active marks", () => {
-      const start_date = new Date("3022-10-01")
+      const startDate = new Date("3022-10-01")
       const marks = [
         {
           id: randomUUID(),
-          givenAt: start_date,
+          givenAt: startDate,
           duration: 22,
         },
         {
           id: randomUUID(),
-          givenAt: start_date,
+          givenAt: startDate,
           duration: 23,
         },
       ]
       expect(personalMarkService.calculateExpiryDate(marks)).toEqual(
-        new Date(start_date.setDate(start_date.getDate() + marks[0].duration + marks[1].duration))
+        new Date(startDate.setDate(startDate.getDate() + marks[0].duration + marks[1].duration))
       )
     }),
     it("Skips holidays and continues after they're done", () => {
-      const start_date_winter = new Date("3022-11-01")
-      const winter_marks = [
+      const startDateWinter = new Date("3022-11-01")
+      const winterMarks = [
         {
           id: randomUUID(),
-          givenAt: start_date_winter,
+          givenAt: startDateWinter,
           duration: 30,
         },
       ]
 
-      const start_date_summer = new Date("3022-05-01")
-      const summer_marks = [
+      const startDateSummer = new Date("3022-05-01")
+      const summerMarks = [
         {
           id: randomUUID(),
-          givenAt: start_date_summer,
+          givenAt: startDateSummer,
           duration: 31,
         },
       ]
-      expect(personalMarkService.calculateExpiryDate(winter_marks)).toEqual(
-        new Date(start_date_winter.setDate(start_date_winter.getDate() + winter_marks[0].duration + 45))
+      expect(personalMarkService.calculateExpiryDate(winterMarks)).toEqual(
+        new Date(startDateWinter.setDate(startDateWinter.getDate() + winterMarks[0].duration + 45))
       )
 
-      expect(personalMarkService.calculateExpiryDate(summer_marks)).toEqual(
-        new Date(start_date_summer.setDate(start_date_summer.getDate() + summer_marks[0].duration + 75))
+      expect(personalMarkService.calculateExpiryDate(summerMarks)).toEqual(
+        new Date(startDateSummer.setDate(startDateSummer.getDate() + summerMarks[0].duration + 75))
       )
     }),
     it("Doesn't add expired marks to the duration", () => {
-      const start_date = new Date("3022-10-01")
-      const old_date = new Date("1970-01-01")
+      const startDate = new Date("3022-10-01")
+      const oldDate = new Date("1970-01-01")
       const marks = [
         {
           id: randomUUID(),
-          givenAt: old_date,
+          givenAt: oldDate,
           duration: 20,
         },
         {
           id: randomUUID(),
-          givenAt: old_date,
+          givenAt: oldDate,
           duration: 24,
         },
         {
           id: randomUUID(),
-          givenAt: start_date,
+          givenAt: startDate,
           duration: 21,
         },
       ]
       expect(personalMarkService.calculateExpiryDate(marks)).toEqual(
-        new Date(start_date.setDate(start_date.getDate() + marks[2].duration))
+        new Date(startDate.setDate(startDate.getDate() + marks[2].duration))
       )
     }),
     it("Doesn't add expired marks to the duration, even if they're the only marks", () => {
-      const old_date = new Date("1970-01-01")
+      const oldDate = new Date("1970-01-01")
       const marks = [
         {
           id: randomUUID(),
-          givenAt: old_date,
+          givenAt: oldDate,
           duration: 1000,
         },
       ]
       expect(personalMarkService.calculateExpiryDate(marks)).toEqual(null)
     }),
     it("Correctly adjusts for marks that would have expired, but don't because they add onto a previous mark", () => {
-      const start_date = new Date("3022-10-01")
+      const startDate = new Date("3022-10-01")
       const marks = [
         {
           id: randomUUID(),
-          givenAt: start_date,
+          givenAt: startDate,
           duration: 10,
         },
         {
@@ -129,7 +129,7 @@ describe("PersonalMarkDateCalculation", () => {
         },
       ]
       expect(personalMarkService.calculateExpiryDate(marks)).toEqual(
-        new Date(start_date.setDate(start_date.getDate() + 30))
+        new Date(startDate.setDate(startDate.getDate() + 30))
       )
     }),
     it("Returns null for an empty array (no marks)", () => {
