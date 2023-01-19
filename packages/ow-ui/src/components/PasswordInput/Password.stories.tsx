5e3663efc0f8fc0f59cbf048560d808c8a353a8e
madsab <madsab@stud.ntnu.no>
2023-01-19 10:58:38 +0100

    new stories password with error message

--- packages/ow-ui/src/components/PasswordInput/Password.stories.tsx
+++ packages/ow-ui/src/components/PasswordInput/Password.stories.tsx
@@ -21,3 +21,6 @@ const TemplatePassword: ComponentStory<typeof PasswordInput> = (args) => {
 
 export const Password = TemplatePassword.bind({})
 Password.args = { placeholder: "Pass your word", inputInfo: "At least one password in password", eyeColor: "gray"}
+
+export const PasswordWithErrormessage = TemplatePassword.bind({})
+PasswordWithErrormessage.args = {placeholder: "Type a password", inputInfo: "Must have at least one animecharacter in password", eyeColor: "gray", error: "Password too short"}
