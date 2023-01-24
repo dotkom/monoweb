5cd740377e55810a97219a7d75dd4cd74b9afbf6
Mats Larsen <me@supergrecko.com>
2023-01-24 22:08:21 +0100

    wip

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client"
 
-import { signIn, signOut, useSession } from "next-auth/react"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react"
 
 export default function Home() {
   const session = useSession()
 
   return (
-    <div>
-      <main>Hello world</main>
-      <button onClick={() => signIn("onlineweb")}>sign in</button>
-      <button onClick={() => signOut()}>sign out</button>
-      <p>{session?.data?.user ? JSON.stringify(session.data.user) : "No logged in user"}</p>
-    </div>
+    <SessionProvider>
+      <div>
+        <main>Hello world</main>
+        <button onClick={() => signIn("onlineweb")}>sign in</button>
+        <button onClick={() => signOut()}>sign out</button>
+        <p>{session?.data?.user && JSON.stringify(session.data.user)}</p>
+      </div>
+    </SessionProvider>
   )
 }
