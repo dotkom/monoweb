cd512b273865af776dbbae33bc5ca21a707412c3
kharann <hey@akvo.no>
2023-01-24 23:56:43 +0100

    next auth jwt token store session

--- apps/dashboard/src/pages/api/auth/[...nextauth].ts
+++ apps/dashboard/src/pages/api/auth/[...nextauth].ts
@@ -9,13 +9,6 @@ export const authOptions: NextAuthOptions = {
   // TODO: use next-auth default jwt adapter
   //  context: it failed because for some reason, user would be undefined in
   //  session callback. investigate later
-  adapter: UpstashRedisAdapter(
-    new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL as string,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
-    }),
-    { baseKeyPrefix: "owdashboard:" }
-  ),
   // Configure one or more authentication providers
   providers: [
     {
@@ -25,7 +18,6 @@ export const authOptions: NextAuthOptions = {
       wellKnown: `${process.env.HYDRA_PUBLIC_URL as string}/.well-known/openid-configuration`,
       authorization: { params: { grant_type: "authorization_code", scope: "openid email profile" } },
       profile(profile): DefaultUser {
-        console.log({ profile })
         return {
           email: profile.sub,
           id: profile.user.id,
@@ -37,25 +29,15 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.NEXTAUTH_DASHBOARD_CLIENT_SECRET as string,
     },
   ],
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    async signIn({ profile }) {
-      return !!profile
-    },
-    async redirect({ url, baseUrl }) {
-      return url.startsWith(baseUrl) ? `${baseUrl}/` : baseUrl
-    },
-    async session({ session, user }) {
-      console.log(session, user)
-      if (user.id) {
-        session.user.id = user.id
+    async session({ session, user, token }) {
+      if (token.sub) {
+        session.user.id = token.sub
       }
       return session
-    },
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
     },
   },
 }
