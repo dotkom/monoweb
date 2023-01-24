82bf84b379cb7cf8d9dc76f9bd1c94837d10196f
Mats Jun Larsen <me@supergrecko.com>
2023-01-24 21:07:05 +0000

    refactor: turn dashboard into nextjs app (#286)

--- apps/dashboard/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react"
-import { RouterProvider } from "react-router-dom"
-
-import { router } from "./routes/router"
-
-export const App: FC = () => {
-  return <RouterProvider router={router} />
-}
