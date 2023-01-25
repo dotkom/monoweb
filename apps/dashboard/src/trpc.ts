c2b85b2a1b8ae4a06287da8ef786fa7d4ce5cde8
Mats Larsen <me@supergrecko.com>
2023-01-25 18:25:02 +0100

    fix: remove unused deps

--- apps/dashboard/src/trpc.ts
+++ apps/dashboard/src/trpc.ts
@@ -5,9 +5,8 @@ import type { AppRouter } from "@dotkomonline/api"
 import superjson from "superjson"
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return "" // browser should use relative url
+  if (process.env.NODE_ENV === "production") return "https://new.online.ntnu.no/"
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-
   return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
 
