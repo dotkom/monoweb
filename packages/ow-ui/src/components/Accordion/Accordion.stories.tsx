f4f442800dc2437a5d5de6a33779b60445047dd6
Thomas Hasvold <thomas.hasvold@gmail.com>
2023-02-01 18:45:08 +0100

    :art: Improved structure of Accordion component

--- packages/ow-ui/src/components/Accordion/Accordion.stories.tsx
+++ packages/ow-ui/src/components/Accordion/Accordion.stories.tsx
@@ -1,24 +1,30 @@
 import { ComponentStory } from "@storybook/react"
 import React from "react"
-import { Accordion, AccordionItem } from "./Accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./Accordion"
 
 export default {
   title: "atoms/Accordion",
   component: Accordion,
 }
 
-const items: AccordionItem[] = [
-  { title: "Is it accessible?", content: "Yes. It adheres to the WAI-ARIA design pattern." },
-  {
-    title: "Is it styled?",
-    content: "Yes. It comes with default styles that matches the other components aesthetic.",
-  },
-  { title: "Is it animated?", content: "Yes. It's animated by default, but you can disable it if you prefer." },
-]
-
 const Template: ComponentStory<typeof Accordion> = (args) => (
   <div className="max-w-[400px]">
-    <Accordion items={items} />
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other components' aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
+      </AccordionItem>
+    </Accordion>
   </div>
 )
 
--- packages/ow-ui/src/components/Accordion/index.tsx
+++ packages/ow-ui/src/components/Accordion/index.tsx
@@ -1,1 +1,1 @@
-export { Accordion } from "./Accordion"
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./Accordion"
