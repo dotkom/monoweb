8201ad792dcd6e31840792404534fced31b9eca4
Mats Larsen <me@supergrecko.com>
2023-02-02 00:43:57 +0100

    refactor: every day we stray further from god

--- apps/dashboard/src/index.tsx
+++ apps/dashboard/src/index.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from "react-dom/client"
 import { StrictMode } from "react"
-import { EuiProvider } from "@elastic/eui"
 import { QueryProvider } from "./components/QueryProvider"
 import { App } from "./app"
-import "@elastic/eui/dist/eui_theme_light.css"
+import './index.css'
 
 const root = document.querySelector<HTMLDivElement>("#root")
 if (root === null) {
@@ -13,9 +12,7 @@ if (root === null) {
 createRoot(root).render(
   <StrictMode>
     <QueryProvider>
-      <EuiProvider colorMode="light">
-        <App />
-      </EuiProvider>
+      <App />
     </QueryProvider>
   </StrictMode>
 )
---
+++ apps/dashboard/tailwind.config.cjs
@@ -0,0 +1,6 @@
+const base = require("@dotkomonline/config/tailwind-preset")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...base,
+}
