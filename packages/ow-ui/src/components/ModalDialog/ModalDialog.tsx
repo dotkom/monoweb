4370797b560d2d2a439eca4a35d6110ee099ae38
kharann <anhkhav@gmail.com>
2023-02-01 21:15:05 +0100

    finish up alert dialog

--- packages/ow-ui/src/components/ModalDialog/ModalDialog.tsx
+++ packages/ow-ui/src/components/ModalDialog/ModalDialog.tsx
@@ -1,9 +1,6 @@
-"use client"
-
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 import { cn } from "../../utils"
-import { Button } from "../Button"
 import { buttonStyles, getColorStyles } from "../Button/Button"
 
 const AlertDialog = AlertDialogPrimitive.Root
