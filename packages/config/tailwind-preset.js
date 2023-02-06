b24757b55e3cf1a567e3802555f4e0d3ae998ba1
Mats Jun Larsen <me@supergrecko.com>
2023-02-06 23:02:46 +0000

    feat: create toggle for color themes in web (#309)
    * feat: create toggle for color themes in web
    
    * fix: add back brand theme
    
    * fix: hsl color values
    
    * fix: close brackets

--- packages/config/tailwind-preset.js
+++ packages/config/tailwind-preset.js
@@ -1,84 +1,14 @@
-const brand = {
-  lighter: "#1277A5",
-  light: "#106A93",
-  DEFAULT: "#0d5474",
-  dark: "#0A425C",
-  darker: "#083549",
-}
+const colors = require("@radix-ui/colors")
 
-const slate = {
-  1: "#151718",
-  2: "#1a1d1e",
-  3: "#202425",
-  4: "#26292b",
-  5: "#2b2f31",
-  6: "#313538",
-  7: "#3a3f42",
-  8: "#4c5155",
-  9: "#697177",
-  10: "#787f85",
-  11: "#9ba1a6",
-  12: "#ecedee",
-}
+const createColorScale = (name) => {
+  const entries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((step) => [
+    step,
+    `hsl(var(--color-${name}-${step}, ${colors[`${name}Dark`][`${name}${step}`]}) / <alpha-value>)`,
+  ])
 
-const blue = {
-  1: "#0f1720",
-  2: "#0f1b2d",
-  3: "#10243e",
-  4: "#102a4c",
-  5: "#0f3058",
-  6: "#0d3868",
-  7: "#0a4481",
-  8: "#0954a5",
-  9: "#0091ff",
-  10: "#369eff",
-  11: "#52a9ff",
-  12: "#eaf6ff",
-}
-
-const amber = {
-  1: "#1f1300",
-  2: "#271700",
-  3: "#341c00",
-  4: "#3f2200",
-  5: "#4a2900",
-  6: "#573300",
-  7: "#693f05",
-  8: "#824e00",
-  9: "#ffb224",
-  10: "#ffcb47",
-  11: "#f1a10d",
-  12: "#fef3dd",
-}
-
-const green = {
-  1: "#0d1912",
-  2: "#0c1f17",
-  3: "#0f291e",
-  4: "#113123",
-  5: "#133929",
-  6: "#164430",
-  7: "#1b543a",
-  8: "#236e4a",
-  9: "#30a46c",
-  10: "#3cb179",
-  11: "#4cc38a",
-  12: "#e5fbeb",
-}
-
-const red = {
-  1: "#1d1412",
-  2: "#2a1410",
-  3: "#3b1813",
-  4: "#481a14",
-  5: "#541c15",
-  6: "#652016",
-  7: "#7f2315",
-  8: "#a42a12",
-  9: "#e54d2e",
-  10: "#ec5e41",
-  11: "#f16a50",
-  12: "#feefec",
+  // Give the default the third step on the scale
+  entries.push(["DEFAULT", `hsl(var(--color-${name}-3, ${colors[`${name}Dark`][`${name}3`]}) / <alpha-value>)`])
+  return Object.fromEntries(entries)
 }
 
 /** @type {import('tailwindcss').Config} */
@@ -88,12 +18,11 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "../../packages/ow-ui/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      slate: { ...slate, DEFAULT: slate[3] },
-      blue: { ...blue, DEFAULT: blue[3] },
-      amber: { ...amber, DEFAULT: amber[3] },
-      red: { ...red, DEFAULT: red[3] },
-      green: { ...green, DEFAULT: green[3] },
-      brand: brand,
+      slate: createColorScale("slate"),
+      blue: createColorScale("blue"),
+      amber: createColorScale("amber"),
+      red: createColorScale("red"),
+      green: createColorScale("green"),
     },
     extend: {
       fontFamily: {
@@ -105,9 +34,16 @@ module.exports = {
         inherit: "inherit",
         current: "current",
         transparent: "transparent",
-        background: "#000212",
-        foreground: slate[12],
+        background: "var(--color-background)",
+        foreground: "var(--color-slate-12)",
         accent: "#FFEDB3",
+        brand: {
+          lighter: "#1277A5",
+          light: "#106A93",
+          DEFAULT: "#0d5474",
+          dark: "#0A425C",
+          darker: "#083549",
+        },
       },
       keyframes: {
         "accordion-down": {
@@ -124,7 +60,7 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       ringColor: {
-        DEFAULT: blue[7],
+        DEFAULT: "var(--color-blue-7)",
       },
       borderRadius: {
         md: "4px",
