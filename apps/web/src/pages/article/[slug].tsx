e8aa2ca2003403aaa4451382c7774951cac36970
kharann <hey@akvo.no>
2023-02-06 23:06:46 +0100

    Upgrade sanity packages

--- apps/web/src/pages/article/[slug].tsx
+++ apps/web/src/pages/article/[slug].tsx
@@ -1,18 +1,58 @@
-import { GetServerSideProps } from "next"
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next"
 import { FC } from "react"
-import { fetchArticleData } from "src/api/get-article"
 import { Article } from "src/api/get-article"
 
+import sanityClient from "@/api/sanity"
 import { ArticleView } from "@components/views/ArticleView"
 
 interface ArticleProps {
   article: Article
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const slug = ctx.query.slug as string
-  const data = await fetchArticleData(slug)
-  return { props: { article: data } }
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = await sanityClient.fetch<string[]>(`
+    *[_type == "article" && !(_id in path("drafts.**"))]{
+      slug {
+      current
+      }
+    }.slug.current
+  `)
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: "blocking",
+  }
+}
+
+const ARTICLE_QUERY = `
+*[_type == "article" && slug.current==$slug && !(_id in path("drafts.**"))][0]{
+    title,
+    author,
+    photographer,
+    _createdAt,
+    _updatedAt,
+    tags,
+    excerpt,
+    cover_image {
+    asset->{url}
+      },
+    content,
+    "numberOfCharacters": length(pt::text(content)),
+    // assumes 5 characters as mean word length
+    "estimatedWordCount": round(length(pt::text(content)) / 5),
+    // Words per minute: 180
+    "estimatedReadingTime": round(length(pt::text(content)) / 5 / 180 )
+  }
+`
+
+export const getStaticProps: GetStaticProps<{}, { slug: string }> = async (ctx) => {
+  const slug = ctx.params?.slug
+  if (!slug) {
+    return {
+      notFound: true,
+    }
+  }
+  const article = await sanityClient.fetch<Article>(ARTICLE_QUERY, { slug })
+  return { props: { article } }
 }
 
 const ArticlePage: FC<ArticleProps> = (props: ArticleProps) => {
