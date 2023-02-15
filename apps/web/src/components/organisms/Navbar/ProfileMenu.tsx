2bf8cf4660a684f14caf17984f87290e8b596371
kharann <anhkhav@gmail.com>
2023-02-15 21:14:14 +0100

    Add droppdownmenu to avatar

--- apps/web/src/components/organisms/Navbar/ProfileMenu.tsx
+++ apps/web/src/components/organisms/Navbar/ProfileMenu.tsx
@@ -1,6 +1,22 @@
-import { Avatar, AvatarFallback, AvatarImage, Button, cn, Icon } from "@dotkomonline/ui"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  cn,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+  Icon,
+} from "@dotkomonline/ui"
 import { signIn, useSession } from "next-auth/react"
 import { useRouter } from "next/router"
+import { FC, PropsWithChildren } from "react"
 import { navigationMenuTriggerStyle } from "./NavigationMenu"
 
 export const ProfileMenu = () => {
@@ -34,13 +50,77 @@ export const ProfileMenu = () => {
 
   return (
     <div>
-      <Avatar>
-        <AvatarImage
-          src="https://www.nicepng.com/png/detail/9-92047_pickle-rick-transparent-rick-and-morty-pickle-rick.png"
-          alt="@rick"
-        />
-        <AvatarFallback>PR</AvatarFallback>
-      </Avatar>
+      <AvatarDropdown>
+        <Avatar>
+          <AvatarImage
+            src="https://www.nicepng.com/png/detail/9-92047_pickle-rick-transparent-rick-and-morty-pickle-rick.png"
+            alt="@rick"
+          />
+          <AvatarFallback>PR</AvatarFallback>
+        </Avatar>
+      </AvatarDropdown>
     </div>
+  )
+}
+
+const AvatarDropdown: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Min bruker</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Icon icon="tabler:user" className="mr-2 h-4 w-4" />
+            <span>Profil</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Icon icon="tabler:credit-card" className="mr-2 h-4 w-4" />
+            <span>Saldo</span>
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Icon icon="tabler:settings" className="mr-2 h-4 w-4" />
+            <span>Instillinger</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Icon icon="tabler:device-desktop-analytics" className="mr-2 h-4 w-4" />
+            <span>Dashboard</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Icon icon="tabler:adjustments" className="mr-2 h-4 w-4" />
+            <span>Adminpanel</span>
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Icon icon="tabler:mail-forward" className="mr-2 h-4 w-4" />
+            <span>Kontakt oss</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Icon icon="noto:eggplant" className="mr-2 h-4 w-4" />
+            <span>Opplevd noe ugreit?</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Icon icon="tabler:bug" className="mr-2 h-4 w-4" />
+            <span>Rapporter en feil</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Icon icon="tabler:logout" className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
