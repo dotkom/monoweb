c433032e64b35879e284abb20ab48440c03a8e72
madsab <madsab@stud.ntnu.no>
2023-02-22 19:33:11 +0100

    Layout for ProfileMarks page

--- apps/web/src/components/views/ProfileView/components/ProfileMarks.tsx
+++ apps/web/src/components/views/ProfileView/components/ProfileMarks.tsx
@@ -1,6 +1,39 @@
 import React from "react"
 
 const ProfileMarks = () => {
-  return <div>Marks</div>
+  return (
+    <div className="flex flex-col space-y-10">
+      <Headline text="Prikker" />
+      <div >
+        <Headline text="Aktive Prikker"/>
+        <Accordian />
+        <Accordian />
+        <Accordian />
+      </div>
+      <div>
+        <Headline text="Gamle Prikker"/>
+        <Accordian />
+        <Accordian />
+        <Accordian />
+      </div>
+      <div>
+        <Headline text="Suspensjoner"/>
+      </div>
+    </div>
+  )
+}
+
+const Headline = ({text}: {text: string}) => {
+  return (
+    <p className="text-2xl">{text}</p>
+  )
+}
+
+const Accordian = () => {
+  return (
+  <div>
+    <p></p>
+  </div>
+  )
 }
 export default ProfileMarks
