0be8f4bd3b34d9930f1f7eb438300b9d5bda93b4
Thomas Hasvold <thomas.hasvold@gmail.com>
2023-02-22 21:05:28 +0100

    :bug: Fix linting again

--- apps/web/src/pages/profile/privacy.tsx
+++ apps/web/src/pages/profile/privacy.tsx
@@ -1,17 +1,9 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
-import { Toggle } from "@dotkomonline/ui"
-import { useState } from "react"
 import { NextPageWithLayout } from "../_app"
 
 const PrivacyPage: NextPageWithLayout = () => {
-  const [checked, setIsChecked] = useState(false)
-
-  return (
-    <div>
-      <Toggle label="Default toggle" isChecked={checked} setIsChecked={() => setIsChecked(!checked)} />
-    </div>
-  )
+  return <div>Privacy</div>
 }
 
 PrivacyPage.getLayout = (page) => {
