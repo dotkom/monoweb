2883fdc19a98bed85adcf08892a6dc8e6c8a433c
Anhkha Vo <hey@akvo.no>
2023-02-22 20:18:32 +0100

    Dropdown (#325)
    * Add droppdownmenu to avatar
    
    * Fix theme switching
    
    * finish navigaiton menu
    
    * increase dropdown menu size
    
    * finish temp dropdown
    
    * Remove unused import

--- apps/web/src/components/organisms/Navbar/MainNavigation.tsx
+++ apps/web/src/components/organisms/Navbar/MainNavigation.tsx
@@ -37,10 +37,10 @@ const DesktopNavigationLink: FC<{ link: MenuLink }> = ({ link }) => {
               <NavigationMenuLink asChild key={`${link.title}-${item.title}`}>
                 <Link
                   href={item.href}
-                  className="hover:bg-slate-3 focus:bg-slate-3 text-slate-12 block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors "
+                  className="hover:bg-slate-3 focus:bg-slate-3 block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors "
                 >
-                  <div className="text-sm font-medium leading-none">{item.title}</div>
-                  <p className="line-clamp-2 text-slate-11 text-sm leading-snug">{item.description}</p>
+                  <div className="text-slate-12 text-sm font-bold leading-none">{item.title}</div>
+                  <p className="line-clamp-2 text-slate-11 text-sm font-medium leading-snug">{item.description}</p>
                 </Link>
               </NavigationMenuLink>
             ))}
---
+++ packages/ow-ui/src/components/DropdownMenu/index.tsx
@@ -0,0 +1,17 @@
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./DropdownMenu"
