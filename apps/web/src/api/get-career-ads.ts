c7d6a1ba6492b61761a2674957433834c16a6cfb
Sondre Alfnes <sondre.alfnes@gmail.com>
2023-03-22 20:22:08 +0100

    Working version

--- apps/web/src/api/get-career-ads.ts
+++ apps/web/src/api/get-career-ads.ts
@@ -36,7 +36,22 @@ const adQuery = `*[_type == "career" && slug.current==$slug && !(_id in path("dr
   slug
 }`
 
-const adsQuery = `*[_type == "career" && !(_id in path("drafts.**"))]`
+const adsQuery = `*[_type == "career" && !(_id in path("drafts.**"))]{
+  title,
+  company_name,
+  image { asset->{url} },
+  career_type,
+  location,
+  deadline,
+  company_info,
+  content,
+  link,
+  career_role,
+  facebook,
+  twitter,
+  linkdin,
+  slug
+}`
 
 export const fetchCareerAd = async (slug: string): Promise<CareerAd> => {
   const res = await client.fetch(adQuery, { slug })
--- apps/web/src/components/molecules/CompanyAdListItem/CompanyAdListItem.tsx
+++ apps/web/src/components/molecules/CompanyAdListItem/CompanyAdListItem.tsx
@@ -4,38 +4,28 @@ import { format } from "date-fns"
 import Image from "next/image"
 import { FC } from "react"
 
-interface CompanyAdListItemProps {  
-  /*
-  name: string
-  logo: string
-  position: string
-  location: string[]
-  deadline: Date
-  showApplyLink?: boolean
-  applyLink?: string*/
+interface CompanyAdListItemProps {
   career: CareerAd
 }
 
-const CompanyAdListItem: FC<CompanyAdListItemProps> = (props) => {
-  //const { name, logo, position, location, deadline, applyLink, showApplyLink = false } = props
-  const { company_name, image, career_type, location, deadline, slug } = props.career
-
-  return <h1>{slug.current}</h1>
+const CompanyAdListItem: FC<CompanyAdListItemProps> = (props: CompanyAdListItemProps) => {
+  const { company_name, image, career_type, location, deadline, slug, link } = props.career
 
   return (
-    <div className="grid grid-cols-5">
-      <div>
-        <Image src={logo} width={70} height={40} alt={`${name}'s job posting`} />
+    <div className="grid grid-cols-5 mt-5">
+      <div className="flex">
+        <Image src={image.asset.url} width={70} height={40} alt={`${company_name}'s job posting`} />
+        <p>{company_name}</p>
       </div>
-      <p>{name}</p>
+
       <div>
         <Badge color="red" variant="light">
-          {position}
+          {career_type}
         </Badge>
       </div>
       <span>{location.concat("")}</span>
-      <span>{format(deadline, "DDD")}</span>
-      {showApplyLink && <p>{applyLink}</p>}
+      <span>{format(new Date(deadline), "dd.MM.yyyy")}</span>
+      <a href={`/career/${slug.current}`}>Les mer</a>
     </div>
   )
 }
