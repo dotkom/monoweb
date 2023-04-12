57c8f555103daab63dd577aaeb074fc78ad9b6b5
madsab <mads.barnes@hotmail.com>
2023-04-12 20:47:46 +0200

    one accordion, and styling

--- apps/web/src/pages/profile/penalties.tsx
+++ apps/web/src/pages/profile/penalties.tsx
@@ -2,26 +2,18 @@ import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@dotkomonline/ui"
+import { trpc } from "@/utils/trpc"
 
 const PenaltiesPage: NextPageWithLayout = () => {
   return (
         <div className="flex flex-col space-y-5 ml-3">
           <p className="text-3xl">Prikker</p>
-          <div>
-            <p className="text-2xl">Aktive Prikker</p>
-            <Accordion type="single" collapsible>
+          <div className="flex flex-col">
+            <p className="text-2xl bg-blue-6">Aktive Prikker</p>
+            <Accordion className="pt-0" type="single" collapsible>
               <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </div>
-          <div>
-            <p className="text-2xl">Gamle Prikker</p>
-            <Accordion type="single" collapsible>
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+                <AccordionTrigger className="text-lg">Is it accessible?</AccordionTrigger>
+                <AccordionContent>It is here</AccordionContent>
               </AccordionItem>
             </Accordion>
           </div>
