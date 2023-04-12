4833b9f930238b30abe5f050e0387a365d963ae1
Sondre Alfnes <sondre.alfnes@gmail.com>
2023-04-12 20:07:41 +0200

    Styling carrer

--- apps/web/src/components/molecules/CompanyAdListItem/CompanyAdListItem.tsx
+++ apps/web/src/components/molecules/CompanyAdListItem/CompanyAdListItem.tsx
@@ -11,21 +11,23 @@ interface CompanyAdListItemProps {
 const CompanyAdListItem: FC<CompanyAdListItemProps> = (props: CompanyAdListItemProps) => {
   const { company_name, image, career_type, location, deadline, slug, link } = props.career
 
+  const color = career_type == "Sommerjobb" ? "amber" : career_type == "Fulltid" ? "red" : "blue"
+
   return (
-    <div className="grid grid-cols-5 mt-5">
-      <div className="flex">
+    <div className="flex items-center h-16 border-slate-11 border-b justify-between">
+      <div className="flex w-1/4 items-center h-10 overflow-hidden gap-2">
         <Image src={image.asset.url} width={70} height={40} alt={`${company_name}'s job posting`} />
         <p>{company_name}</p>
       </div>
 
-      <div>
-        <Badge color="red" variant="light">
+      <div className="w-1/4">
+        <Badge color={color} variant="light">
           {career_type}
         </Badge>
       </div>
-      <span>{location.concat("")}</span>
-      <span>{format(new Date(deadline), "dd.MM.yyyy")}</span>
-      <a href={`/career/${slug.current}`}>Les mer</a>
+      <span className="w-[17.5%]">{location.concat("")}</span>
+      <span className="w-[17.5%]">{format(new Date(deadline), "dd.MM.yyyy")}</span>
+      <a className="w-[15%]" href={`/career/${slug.current}`}>Les mer</a>
     </div>
   )
 }
--- apps/web/src/components/views/CareerView.tsx
+++ apps/web/src/components/views/CareerView.tsx
@@ -7,31 +7,34 @@ const CareerView: FC<CareerProps> = (props: CareerProps) => {
 
   return (
     <div>
-      <div className="bg-amber-10 w-full">
+      <div className="absolute left-0 h-[250px] top-[56px] z-0 w-full opacity-30 bg-amber-9" />
+      <div className="absolute left-0 h-[250px] top-[56px] z-10 flex w-full flex-col justify-center">
         <div className="m-auto h-[200px] max-w-[800px] p-5 text-center">
-          <p className="text-slate-3 leading-1.4 mt-5 text-4xl font-bold">
+          <p className="leading-1.4 mt-5 text-4xl font-bold">
             Er du på jakt etter{" "}
             <span className="bg-50% bg-[url('/for-company-text-decor.svg')] bg-center bg-no-repeat">jobb</span>?
           </p>
-          <p className="text-slate-3 leading-1.4 text-2xl font-bold">
+          <p className="leading-1.4 text-2xl font-bold mt-7">
             Bedrifter betaler 15 000 for å være med på denne lista så pls søk eller så får ikke Online penger uwu
           </p>
         </div>
       </div>
-      <div className="m-4 my-auto max-w-[800px]">
-        <div className="border-slate-11 grid grid-cols-4 gap-4 border-b-2 pl-2">
-          <p className="mb-2 text-xl font-medium">Bedrift</p>
-          <p className="mb-2 text-xl font-medium">Rolle</p>
-          <p className="mb-2 text-xl font-medium">Sted</p>
-          <p className="mb-2 text-xl font-medium">Frist</p>
+      <div className="m-4 my-auto mt-[250px] w-[900px]">
+        <div className="border-slate-11 flex justify-between border-b-2">
+          <p className="mb-2 w-[25%] text-xl font-medium">Bedrift</p>
+          <p className="mb-2 w-[25%] text-xl font-medium">Rolle</p>
+          <p className="mb-2 w-[17.5%] text-xl font-medium">Sted</p>
+          <p className="mb-2 w-[17.5%] text-xl font-medium">Frist</p>
+          <p className="mb-2 w-[15%] text-xl font-medium">Søknadslink</p>
         </div>
         <div className="flex flex-col">
           {props.careers.map((c) => (
             <>
-            <CompanyAdListItem career={c} />
+              <CompanyAdListItem career={c} />
             </>
           ))}
         </div>
+        <div className="h-36"></div>
       </div>
     </div>
   )
