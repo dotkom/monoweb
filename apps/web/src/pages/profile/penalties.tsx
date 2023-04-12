087e502879de5ff90127f7e320ce13e16be89e9d
madsab <mads.barnes@hotmail.com>
2023-04-12 19:36:58 +0200

    started profile penalties layout

--- apps/web/src/pages/profile/penalties.tsx
+++ apps/web/src/pages/profile/penalties.tsx
@@ -1,10 +1,35 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@dotkomonline/ui"
 
 const PenaltiesPage: NextPageWithLayout = () => {
-  return <div>Penalties</div>
-}
+  return (
+        <div className="flex flex-col space-y-5 ml-3">
+          <p className="text-3xl">Prikker</p>
+          <div>
+            <p className="text-2xl">Aktive Prikker</p>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div>
+            <p className="text-2xl">Gamle Prikker</p>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div>
+            <p className="text-2xl">PrikkeRegler</p>
+          </div>
+        </div>
+)}
 
 PenaltiesPage.getLayout = (page) => {
   return (
