a58607f0d2036c5f56faa0d4af33bfebe4d40f93
madsab <mads.barnes@hotmail.com>
2023-04-12 20:15:59 +0200

    added markRouter in appRouter

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -3,12 +3,14 @@ import { authRouter } from "./modules/auth/auth-router"
 import { eventRouter } from "./modules/event/event-router"
 import { committeeRouter } from "./modules/committee/committee-router"
 import { companyRouter } from "./modules/company/company-router"
+import { markRouter } from "./modules/marks/mark-router"
 
 export const appRouter = t.router({
   committee: committeeRouter,
   event: eventRouter,
   auth: authRouter,
   company: companyRouter,
+  mark: markRouter,
 })
 
 export type AppRouter = typeof appRouter
