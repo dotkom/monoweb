08aed8a76921952cb97c709837e369d8b725ec96
Mats Larsen <me@supergrecko.com>
2023-04-26 01:25:52 +0200

    fix: bump next to support new auth config

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,22 +1,18 @@
-"use client"
 import type { PropsWithChildren } from "react"
 import { AuthProvider } from "./AuthProvider"
 import { QueryProvider } from "./QueryProvider"
 import { MantineColorSchemeProvider, MantineProvider } from "./MantineProvider"
 import { ApplicationShell } from "./ApplicationShell"
 import "./root.css"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@dotkomonline/auth"
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const session = await getServerSession(authOptions)
   return (
     <html lang="en">
       <head />
       <body>
         <MantineColorSchemeProvider>
           <MantineProvider>
-            <AuthProvider session={session}>
+            <AuthProvider>
               <QueryProvider>
                 <ApplicationShell>{children}</ApplicationShell>
               </QueryProvider>
