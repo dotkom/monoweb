e47779dc7fa3faf765e856e41093908f8b0f0f4e
madsab <mads.barnes@hotmail.com>
2023-04-25 10:47:30 +0200

    Penalty Rules and layout styling

--- apps/web/src/pages/profile/penalties.tsx
+++ apps/web/src/pages/profile/penalties.tsx
@@ -4,26 +4,72 @@ import { NextPageWithLayout } from "../_app"
 import { Icon } from "@dotkomonline/ui"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@dotkomonline/ui"
 import { DotFilledIcon} from "@radix-ui/react-icons"
-import { trpc } from "@/utils/trpc"
+import { useEffect } from "react"
+import PenaltyRules from "./penaltyRules"
+
+const dummyMark = {
+  id: "",
+  title: "Første Mark",
+  givenAt: new Date,
+  updatedAt: new Date,
+  category: "",
+  details: "manglende møte på Dåp",
+  duration: 2000000
+}
+const dummyMark1 = {
+  id: "",
+  title: "Andre Mark",
+  givenAt: new Date,
+  updatedAt: new Date,
+  category: "",
+  details: "flexing av hook med 05",
+  duration: 9000000000009
+}
+const dummyMark2 = {
+  id: "",
+  title: "Tredje Mark",
+  givenAt: new Date,
+  updatedAt: new Date,
+  category: "Sosialt",
+  details: "manglende prikker",
+  duration: 100000
+}
+
+const listOfDummyMarks = [dummyMark,dummyMark1,dummyMark2,];
+
 
 const PenaltiesPage: NextPageWithLayout = () => {
+  useEffect(() => {
+    const AccordionList = Array.from(document.getElementsByClassName("taper"))
+    const closeAllAccordions = (event: MouseEvent) => {
+        console.log("hei")
+    }
+
+    document.addEventListener("click", closeAllAccordions)
+  })
   return (
-        <div className="flex flex-col space-y-7 ml-3">
-          <p className="text-2xl">Prikker</p>
+    <div className="flex flex-col space-y-12 ml-3">
+          <p className="text-[28px] font-medium">Prikker</p>
           <div className="flex flex-col">
-            <p className="text-xl bg-blue-6">Aktive Prikker</p>
-            <PenaltyAccordion header={"Hallo"} content={"ashsh"} />
+            <p className="text-[24px] font-medium">Aktive Prikker</p>
+            {activePenalties}
           </div>
           <div>
-            <p className="text-xl">Gamle Prikker</p>
-            <PenaltyAccordion header={"Flexet hook med 05"} content={"Utløpt"} />
-            <PenaltyAccordion header={"Ble ikke med på shots i Åre"} content={"Utløpt"} />
+            <p className="text-[24px] font-medium">Gamle Prikker</p>
+            {oldPenalties}
+          </div>
+          <div className="flex flex-col space-y-2">
+            <p className="text-[24px] font-medium">Suspensjoner</p>
+            {suspensions}
           </div>
           <div>
-            <p className="text-xl">Suspensjoner</p>
-          </div>
-          <div>
-            <p className="text-xl">PrikkeRegler</p>
+            <p className="text-[24px] font-medium">PrikkeRegler</p>
+            <Accordion className="max-w-[590px]" type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-lg ml-1">Prikkeregler</AccordionTrigger>
+                <AccordionContent className="ml-4"> <PenaltyRules/> </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
 )}
@@ -41,71 +87,33 @@ PenaltiesPage.getLayout = (page) => {
 
 
 interface AccordionSetup {
-    header : String;
-    content: String;
+  title: String;
+  category: String;
+  givenAt: Date;
+  duration: number;
+  details: String;
 }
 
+// TODO: Set up router for Users personal marks
+
 const PenaltyAccordion = (props : AccordionSetup) => {
+  const expirationDate = new Date(props.givenAt.getTime() + props.duration).toLocaleString()
   return (
   <Accordion type="single" collapsible>
     <AccordionItem value="item-1">
-      <AccordionTrigger className="text-lg ml-1"><span className="flex"><DotFilledIcon className="mt-1"/>{props.header}</span></AccordionTrigger>
-      <AccordionContent className="ml-4">{props.content}</AccordionContent>
+      <AccordionTrigger className="text-lg ml-1"><span className="flex"><DotFilledIcon className="mt-1"/>{props.title}</span></AccordionTrigger>
+      <AccordionContent className="ml-4">
+        <div className="flex flex-col space-y-4">
+        <p> Du har fått en prikk på grunn av {props.title}</p>
+        <p className="text-lg"><span className="font-bold">Katergori: </span>{props.category}</p>
+        <p className="text-lg"><span className="font-bold">Utløpsdato: </span>
+          {new Date(props.givenAt.getTime() + props.duration).toLocaleString()}
+          </p>
+        </div>
+      </AccordionContent>
     </AccordionItem>
   </Accordion>
 )}
-
-interface AccordionSetup {
-  title: string
-  category: string
-  givenAt: Date
-  duration: number
-  details: string
-}
-
-const PenaltyAccordion = (props: AccordionSetup) => {
-  const givenAtDate =
-    props.givenAt.getDate().toString() +
-    "." +
-    props.givenAt.getMonth().toString() +
-    "." +
-    props.givenAt.getFullYear().toString()
-  const expirationDate = new Date(props.givenAt.getTime() + props.duration)
-  const expirationDateFormat =
-    expirationDate.getDate().toString() +
-    "." +
-    expirationDate.getMonth().toString() +
-    "." +
-    expirationDate.getFullYear().toString()
-  return (
-    <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
-        <AccordionTrigger className="accordions ml-1 text-lg">
-          <span className="flex">
-            <DotFilledIcon className="mt-1" />
-            {props.title}
-          </span>
-        </AccordionTrigger>
-        <AccordionContent className="ml-4">
-          <div className="flex flex-col space-y-4">
-            <p>
-
-              Du har fått en prikk på grunn av {props.details} den {givenAtDate}
-            </p>
-            <p className="text-lg">
-              <span className="font-bold">Katergori: </span>
-              {props.category}
-            </p>
-            <p className="text-lg">
-              <span className="font-bold">Utløpsdato: </span>
-              {expirationDateFormat}
-            </p>
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  )
-}
 
 const activePenalties = listOfDummyMarks.length
   ? listOfDummyMarks.map((mark) => (
