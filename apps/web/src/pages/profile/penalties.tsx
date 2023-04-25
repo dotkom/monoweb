ce4b4b7bd90d85dc004c3b49384e3faa40d70eb7
madsab <mads.barnes@hotmail.com>
2023-04-25 10:47:30 +0200

    Penalty Rules and layout styling

--- apps/web/src/pages/profile/penalties.tsx
+++ apps/web/src/pages/profile/penalties.tsx
@@ -3,9 +3,8 @@ import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@dotkomonline/ui"
 import { DotFilledIcon} from "@radix-ui/react-icons"
-import { trpc } from "@/utils/trpc"
-import { format } from "path"
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
+import PenaltyRules from "./penaltyRules"
 
 const dummyMark = {
   id: "",
@@ -13,7 +12,7 @@ const dummyMark = {
   givenAt: new Date,
   updatedAt: new Date,
   category: "",
-  details: "",
+  details: "manglende møte på Dåp",
   duration: 2000000
 }
 const dummyMark1 = {
@@ -22,7 +21,7 @@ const dummyMark1 = {
   givenAt: new Date,
   updatedAt: new Date,
   category: "",
-  details: "",
+  details: "flexing av hook med 05",
   duration: 9000000000009
 }
 const dummyMark2 = {
@@ -31,31 +30,45 @@ const dummyMark2 = {
   givenAt: new Date,
   updatedAt: new Date,
   category: "Sosialt",
-  details: "",
+  details: "manglende prikker",
   duration: 100000
 }
 
-const listdum = [dummyMark,dummyMark1,dummyMark2,];
+const listOfDummyMarks = [dummyMark,dummyMark1,dummyMark2,];
 
 
 const PenaltiesPage: NextPageWithLayout = () => {
+  useEffect(() => {
+    const AccordionList = Array.from(document.getElementsByClassName("taper"))
+    const closeAllAccordions = (event: MouseEvent) => {
+        console.log("hei")
+    }
+  
+    document.addEventListener("click", closeAllAccordions)
+  })
   return (
     <div className="flex flex-col space-y-12 ml-3">
-          <p className="text-2xl">Prikker</p>
+          <p className="text-[28px] font-medium">Prikker</p>
           <div className="flex flex-col">
-            <p className="text-xl">Aktive Prikker</p>
+            <p className="text-[24px] font-medium">Aktive Prikker</p>
             {activePenalties}
           </div>
           <div>
-            <p className="text-xl">Gamle Prikker</p>
+            <p className="text-[24px] font-medium">Gamle Prikker</p>
             {oldPenalties}
           </div>
           <div className="flex flex-col space-y-2">
-            <p className="text-xl">Suspensjoner</p>
+            <p className="text-[24px] font-medium">Suspensjoner</p>
             {suspensions}
           </div>
           <div>
-            <p className="text-xl">PrikkeRegler</p>
+            <p className="text-[24px] font-medium">PrikkeRegler</p>
+            <Accordion className="max-w-[590px]" type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-lg ml-1">Prikkeregler</AccordionTrigger>
+                <AccordionContent className="ml-4"> <PenaltyRules/> </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
 )}
@@ -74,30 +87,31 @@ interface AccordionSetup {
   category: String;
   givenAt: Date;
   duration: number;
+  details: String;
 }
 
+// TODO: Set up router for Users personal marks
 
 const PenaltyAccordion = (props : AccordionSetup) => {
+  const expirationDate = new Date(props.givenAt.getTime() + props.duration).toLocaleString()
   return (
     <Accordion type="single" collapsible>
     <AccordionItem value="item-1">
       <AccordionTrigger className="text-lg ml-1"><span className="flex"><DotFilledIcon className="mt-1"/>{props.title}</span></AccordionTrigger>
       <AccordionContent className="ml-4">
         <div className="flex flex-col space-y-4">
-        <p> Du har fått en prikk på grunn av {props.title}</p>
-        <p className="text-lg"><span className="font-bold">Katergori: </span>{props.category}</p>
-        <p className="text-lg"><span className="font-bold">Utløpsdato: </span>
-          {new Date(props.givenAt.getTime() + props.duration).toLocaleString()}    
-          </p>
+          <p> Du har fått en prikk på grunn av {props.details} den {props.givenAt.toLocaleString()}</p>
+          <p className="text-lg"><span className="font-bold">Katergori: </span>{props.category}</p>
+          <p className="text-lg"><span className="font-bold">Utløpsdato: </span>{expirationDate}</p>
         </div>
       </AccordionContent>
     </AccordionItem>
   </Accordion>
 )}
 
-const activePenalties = listdum.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration}/>);
-const oldPenalties = listdum.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration}/>);
-const suspensions = listdum.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration}/>);
+const activePenalties = listOfDummyMarks.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration} details={mark.details}/>);
+const oldPenalties = listOfDummyMarks.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration} details={mark.details}/>);
+const suspensions = listOfDummyMarks.map(mark => <PenaltyAccordion title={mark.title} category={mark.category} givenAt={mark.givenAt} duration={mark.duration} details={mark.details}/>);
 
 
 export default PenaltiesPage
