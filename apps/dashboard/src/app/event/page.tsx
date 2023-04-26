febfc862e57e82f9336205ca34828c9be5297ffe
Mats Larsen <me@supergrecko.com>
2023-04-26 13:29:34 +0200

    feat: show committe name in event table

--- apps/dashboard/src/app/event/page.tsx
+++ apps/dashboard/src/app/event/page.tsx
@@ -4,7 +4,7 @@ import { Title, Text, Button, Flex, Loader } from "@mantine/core"
 import { trpc } from "../../trpc"
 import { Event } from "@dotkomonline/types"
 import { createColumnHelper, getCoreRowModel, useReactTable } from "@tanstack/react-table"
-import { FC, useState } from "react"
+import { FC, useMemo, useState } from "react";
 import { EventCreationModal } from "./EventCreationModal"
 import { Icon } from "@iconify/react"
 import Link from "next/link"
@@ -38,8 +38,9 @@ export default function EventPage() {
 type EventTableProps = { events: Event[] }
 
 const EventTable: FC<EventTableProps> = ({ events }) => {
+  const { data: committees = [], isLoading: isCommitteesLoading } = trpc.committee.all.useQuery({ take: 999 })
   const columnHelper = createColumnHelper<Event>()
-  const columns = [
+  const columns = useMemo(() => [
     columnHelper.accessor("title", {
       header: () => "Arrangementnavn",
     }),
@@ -53,7 +54,13 @@ const EventTable: FC<EventTableProps> = ({ events }) => {
     }),
     columnHelper.accessor("committeeId", {
       header: () => "Arrangør",
-      cell: (info) => info.getValue() ?? "Ingen arrangør",
+      cell: (info) => {
+        if (isCommitteesLoading) {
+          return "Laster..."
+        }
+        const match = committees.find((committee) => committee.id === info.getValue())
+        return match?.name ?? 'Ingen arrangør'
+      },
     }),
     columnHelper.accessor("type", {
       header: () => "Type",
@@ -63,7 +70,7 @@ const EventTable: FC<EventTableProps> = ({ events }) => {
       header: () => "Detaljer",
       cell: (info) => <EventTableDetailsCell event={info.getValue()} />,
     }),
-  ]
+  ], [committees, isCommitteesLoading])
   const table = useReactTable({
     data: events,
     getCoreRowModel: getCoreRowModel(),
