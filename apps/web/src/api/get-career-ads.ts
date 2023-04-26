92535f4629a1e2a2f2c8782a4926cc0b41e5c1c2
Sondre Alfnes <60832802+sondrealf@users.noreply.github.com>
2023-04-26 19:05:29 +0200

    feat: create job ad view (#367)
    Creating Carrer page to view job listings
    
    ---------
    
    Co-authored-by: Martine Malmo Knutsen <martimkn@stud.ntnu.no>

--- apps/web/src/api/get-career-ads.ts
+++ apps/web/src/api/get-career-ads.ts
@@ -16,6 +16,7 @@ export interface CareerAd {
   twitter: string
   linkdin: string
   career_role: string
+  slug: { current: string }
 }
 
 const adQuery = `*[_type == "career" && slug.current==$slug && !(_id in path("drafts.**"))][0]{
@@ -31,10 +32,26 @@ const adQuery = `*[_type == "career" && slug.current==$slug && !(_id in path("dr
   career_role,
   facebook,
   twitter,
-  linkdin
+  linkdin,
+  slug
 }`
 
-const adsQuery = `*[_type == "career" && !(_id in path("drafts.**"))]`
+const adsQuery = `*[_type == "career" && !(_id in path("drafts.**"))]{
+  title,
+  company_name,
+  image { asset->{url} },
+  career_type,
+  location,
+  deadline,
+  company_info,
+  content,
+  link,
+  career_role,
+  facebook,
+  twitter,
+  linkdin,
+  slug
+}`
 
 export const fetchCareerAd = async (slug: string): Promise<CareerAd> => {
   const res = await client.fetch(adQuery, { slug })
---
+++ apps/web/src/pages/career/index.tsx
@@ -0,0 +1,20 @@
+import { CareerAd, fetchCareerAds } from "@/api/get-career-ads"
+import CareerView from "@/components/views/CareerView"
+import { GetServerSideProps } from "next"
+import { FC } from "react"
+
+export interface CareerProps {
+  careers: CareerAd[]
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await fetchCareerAds()
+  return { props: { careers: data } }
+}
+
+const Career: FC<CareerProps> = (props: CareerProps) => {
+  return <CareerView careers={props.careers} />
+  // return <div>404 - Sanity not found</div>
+}
+
+export default Career
