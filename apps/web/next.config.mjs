1941d6fc8c19052a055e8d39d313ed44c5c52896
Anhkha Vo <hey@akvo.no>
2023-04-28 11:44:28 +0200

    Upgrade dependencies (#374)
    * upgrade trpc, next and more
    
    * remove @next/font
    
    * upgrade typescript
    
    * zod :)
    
    * Fix typings
    
    ---------
    
    Co-authored-by: Sondre Alfnes <sondre.alfnes@gmail.com>

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,11 +8,10 @@ const config = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io", "onlineweb4-prod.s3.eu-north-1.amazonaws.com"],
   },
   experimental: {
     appDir: true,
-    swcPlugins: [["next-superjson-plugin", {}]],
   },
   transpilePackages: ["@dotkomonline/ui", "@dotkomonline/types", "@dotkomonline/api"],
   eslint: {
