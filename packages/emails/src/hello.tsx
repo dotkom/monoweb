be3cc09c0d72cdba984e6265fc350643ccc35b19
Mats Larsen <mats@jun.codes>
2023-07-30 18:28:25 +0200

    fix: fix build issues

--- packages/emails/src/hello.tsx
+++ packages/emails/src/hello.tsx
@@ -4,7 +4,7 @@ import { Html } from "@react-email/html"
 import { Container } from "@react-email/container"
 import { Heading } from "@react-email/heading"
 import { Text } from "@react-email/text"
-import { Tailwind } from "@react-email/tailwind"
+import { Tailwind, TailwindProps } from "@react-email/tailwind"
 import { z } from "zod"
 import { createTemplate } from "./template"
 
@@ -16,7 +16,7 @@ export const HelloEmailTemplate = createTemplate(input)({
   key: "hello-world",
   render: ({ name }) => (
     <Html>
-      <Tailwind config={TailwindConfig}>
+      <Tailwind config={TailwindConfig as TailwindProps['config']}>
         <Container>
           <Heading>Hello world</Heading>
           <Text className="text-amber-12 text-4xl">This is a special message especially for you {name}!</Text>
