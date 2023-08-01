9a0b55ea88f3442920dc7034e5ad86c2a1f175c8
Mats Larsen <mats@jun.codes>
2023-08-01 17:27:17 +0200

    build: remove tsconfig paths from api

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,13 +1,6 @@
-/// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      root: "./",
-    }),
-  ],
   test: {
     globals: true,
     environment: "node",
