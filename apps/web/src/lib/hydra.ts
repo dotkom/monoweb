26fb0f802ac55c7a3ab4396c41a0d91325b9deda
Mats Larsen <mats@jun.codes>
2023-08-22 22:00:10 +0200

    fix: remove references to hydra

--- apps/web/src/lib/hydra.ts
+++
@@ -1,8 +0,0 @@
-import { Configuration, OAuth2Api as HydraApiClient } from "@ory/client"
-import { env } from "../env/server.mjs"
-
-export const hydraAdmin = new HydraApiClient(
-  new Configuration({
-    basePath: env.HYDRA_ADMIN_URL,
-  })
-)
