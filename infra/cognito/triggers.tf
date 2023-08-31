44c77aeaddeaef6ab6b4961f1d9aae6a5f7afe5c
Mats Larsen <mats@jun.codes>
2023-08-31 18:13:52 -0400

    fix: give lambda access to ses

--- infra/cognito/triggers.tf
+++ infra/cognito/triggers.tf
@@ -10,9 +10,28 @@ data "aws_iam_policy_document" "lambda_execute_assume" {
   }
 }
 
+data "aws_iam_policy_document" "lambda_execute_ses" {
+  statement {
+    sid = "LambdaExecuteSES"
+    effect = "Allow"
+    actions = ["ses:SendEmail"]
+    resources = ["arn:aws:ses:eu-north-1:891459268445:identity/online.ntnu.no"]
+  }
+}
+
 resource "aws_iam_role" "trigger_execution_role" {
   name               = "LambdaExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.lambda_execute_assume.json
+
+  inline_policy {
+    name = "lambda-execute-ses"
+    policy = data.aws_iam_policy_document.lambda_execute_ses.json
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lambda_execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.trigger_execution_role.name
 }
 
 resource "aws_lambda_permission" "cognito" {
