48f5fc3cfb4b22c6253f322d4596c2edafe5de07
Terbau <bragebau@hotmail.no>
2023-09-25 20:04:57 +0200

    Fix fixtures

--- apps/db-migrator/src/fixtures/user.ts
+++ apps/db-migrator/src/fixtures/user.ts
@@ -3,11 +3,13 @@ import { Insertable } from "kysely"
 
 export const users: Insertable<Database["owUser"]>[] = [
   {
-    id: "user_2PAEtJHM2O9RogYmT1yhx0JifPn", // bragebau
+    id: "user_2PAEtJHM2O9RogYmT1yhx0JifPn",
+    cognitoSub: "4dd4698d-c376-4e5e-b5fb-4db9bf6cd417",  // brage.baugerod@online.ntnu.no
     createdAt: new Date("2023-04-30 19:22:17.627253+00"),
   },
   {
-    id: "user_2PAKWhPTgypXLhisUS5RVHGec1o", // bragebaugerod
+    id: "user_2PAKWhPTgypXLhisUS5RVHGec1o",
+    cognitoSub: "2d92881d-f0c7-4e1f-853c-e0ee9a0993c8",  // mats@jun.codes
     createdAt: new Date("2023-04-30 21:22:17.627253+00"),
   },
 ]
