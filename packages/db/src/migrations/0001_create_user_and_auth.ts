fb74d2f448a026263ce36fe77dce48e50f3d52c0
Mats Larsen <mats@jun.codes>
2023-09-25 13:36:55 +0200

    refactor: switch to ulid

--- packages/db/src/migrations/0001_create_user_and_auth.ts
+++ packages/db/src/migrations/0001_create_user_and_auth.ts
@@ -1,10 +1,12 @@
-import { Kysely } from "kysely"
+import { Kysely, sql } from "kysely"
 
 import { createTableWithDefaults } from "../utils"
 
 // Kysely reccomends using "any" in migrations
 
 export async function up(db: Kysely<any>): Promise<void> {
+  const query = sql`CREATE EXTENSION IF NOT EXISTS ulid;`.compile(db)
+  await db.executeQuery(query)
   await createTableWithDefaults("ow_user", { createdAt: true }, db.schema)
     .addColumn("id", "text", (col) => col.primaryKey())
     .execute()
