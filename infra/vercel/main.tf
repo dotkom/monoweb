cfd4afc26959b65cd6fa3d936e42f62ca4677fee
Mats Larsen <mats@jun.codes>
2023-09-29 10:07:19 +0200

    feat: add deployment for dashboard

--- infra/vercel/main.tf
+++ infra/vercel/main.tf
@@ -25,3 +25,26 @@ resource "vercel_project_domain" "domain" {
   domain     = aws_route53_record.domain.name
   project_id = vercel_project.monoweb.id
 }
+
+resource "vercel_project" "dashboard" {
+  name      = "dashboard"
+  framework = "nextjs"
+
+  git_repository = {
+    production_branch = "main"
+    type              = "github"
+    repo              = "dotkom/monoweb"
+
+    build_command  = "cd ../.. &&  pnpm build:dashboard"
+    root_directory = "apps/dashboard"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "vercel_project_domain" "dashboard_domain" {
+  domain     = aws_route53_record.dashboard_domain.name
+  project_id = vercel_project.dashboard.id
+}
