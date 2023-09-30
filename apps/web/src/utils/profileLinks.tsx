e0fb6a0bf3e2c62aa86dacec303ede3a527e1586
madsab <mads.barnes@hotmail.com>
2023-09-30 22:04:37 +0200

    accountIcon for testing

--- apps/web/src/utils/profileLinks.tsx
+++ apps/web/src/utils/profileLinks.tsx
@@ -1,38 +1,49 @@
+import AccountIcon from "@/components/icons/ProfileIcons/AccountIcon";
+
 export const profileItems = [
   {
     title: "Min profil",
     slug: "/profile",
+    icon: AccountIcon
   },
   {
     title: "Personvern",
     slug: "/profile/privacy",
+    icon: AccountIcon,
   },
   {
     title: "Prikker & Suspensjoner",
     slug: "/profile/penalties",
+    icon: AccountIcon,
   },
   {
     title: "Passord",
     slug: "/profile/password",
+    icon: AccountIcon,
   },
   {
     title: "Epost",
     slug: "/profile/email",
+    icon: AccountIcon,
   },
   {
     title: "Medlemskap",
     slug: "/profile/membership",
+    icon: AccountIcon,
   },
   {
     title: "Betalinger",
     slug: "/profile/payments",
+    icon: AccountIcon,
   },
   {
     title: "Varsler",
     slug: "/profile/notifications",
+    icon: AccountIcon,
   },
   {
     title: "Adgangskort (NTNU)",
     slug: "/profile/access-card",
+    icon: AccountIcon,
   },
 ]
