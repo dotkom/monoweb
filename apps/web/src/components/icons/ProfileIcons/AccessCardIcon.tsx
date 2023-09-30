682fc05433ea721fb46b1a1bfa97eab1f97eb721
madsab <mads.barnes@hotmail.com>
2023-09-30 22:42:58 +0200

    new icon

---
+++ apps/web/src/components/icons/ProfileIcons/AccessCardIcon.tsx
@@ -0,0 +1,15 @@
+
+const AccessCardIcon = () => (
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="School" clip-path="url(#clip0_1105_2065)">
+        <path id="Vector" d="M4.79166 12.6308V16.4642L11.5 20.125L18.2083 16.4642V12.6308L11.5 16.2917L4.79166 12.6308ZM11.5 2.875L0.958328 8.625L11.5 14.375L20.125 9.66958V16.2917H22.0417V8.625L11.5 2.875Z" className="fill-slate-12"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_1105_2065">
+        <rect width="23" height="23" fill="white"/>
+        </clipPath>
+        </defs>
+    </svg>
+
+)
+export default AccessCardIcon
---
+++ apps/web/src/components/icons/ProfileIcons/PrivacyIcon.tsx
@@ -0,0 +1,16 @@
+
+
+const PrivacyIcon = () => (
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="Security" clip-path="url(#clip0_1105_1675)">
+        <path id="Vector" d="M11.5 0.958252L2.875 4.79159V10.5416C2.875 15.8603 6.555 20.8341 11.5 22.0416C16.445 20.8341 20.125 15.8603 20.125 10.5416V4.79159L11.5 0.958252ZM11.5 11.4903H18.2083C17.7004 15.4387 15.065 18.9558 11.5 20.0578V11.4999H4.79167V6.03742L11.5 3.057V11.4903Z" className="fill-slate-12" />
+        </g>
+        <defs>
+        <clipPath id="clip0_1105_1675">
+        <rect width="23" height="23" fill="white"/>
+        </clipPath>
+        </defs>
+    </svg>
+  )
+
+export default PrivacyIcon
