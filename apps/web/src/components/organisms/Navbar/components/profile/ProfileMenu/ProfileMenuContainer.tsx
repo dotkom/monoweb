6fe93dcd7d71ef805bd4294de06a759736dfaf95
madsab <mads.barnes@hotmail.com>
2023-09-30 18:22:29 +0200

    added vertical line between menu and context

--- apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer.tsx
+++ apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer.tsx
@@ -3,7 +3,7 @@ import ProfileMenuItem from "./ProfileMenuItem"
 
 const ProfileMenuContainer = () => {
   return (
-    <div>
+    <div className=" pt-[42.5px] border-r-[1px] border-slate-5 pr-5">
       {profileItems.map((item) => (
         <ProfileMenuItem key={item.title} menuItem={item} />
       ))}
