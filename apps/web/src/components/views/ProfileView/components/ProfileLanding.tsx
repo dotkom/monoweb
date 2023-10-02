4e407ad4bec95470a05ac3d995c4e9977e6742c2
madsab <mads.barnes@hotmail.com>
2023-10-02 13:00:09 +0200

    Basic layout of ProfileLanding

--- apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
+++ apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
@@ -1,4 +1,3 @@
-import AccountIcon from "@/components/icons/ProfileIcons/AccountIcon"
 import { AvatarImage, Icon } from "@dotkomonline/ui"
 import { Avatar } from "@radix-ui/react-avatar"
 import { NextPage } from "next"
@@ -8,21 +7,26 @@ interface IFormInput {
   name: string
   children?: JSX.Element
   addMore?: string
+  clickable?: boolean
 }
 
-const FormInput: React.FC<IFormInput> = ({ name, children, addMore }) => {
+const FormInput: React.FC<IFormInput> = ({ name, children, addMore, clickable = true }) => {
   return (
     <div className="my-10 ">
       <div className="ml-4">
         <label>{name}</label>
       </div>
       <hr className="border-slate-12 w-full opacity-50" />
-      <div className="w-2/3 ml-10 space-y-3 ">
-        <div className="mt-3 pl-2 flex items-center rounded-lg justify-between hover:cursor-pointer hover:bg-slate-3">
+      <div className="ml-10 w-2/3 space-y-3 ">
+        <div
+          className={`mt-3 flex items-center justify-between rounded-lg pl-2 ${
+            clickable ? "hover:bg-slate-3 hover:cursor-pointer" : ""
+          }`}
+        >
           {children}
-          <Icon icon="simple-line-icons:arrow-right" width={10} />
+          {clickable ? <Icon icon="simple-line-icons:arrow-right" width={10} /> : ""}
         </div>
-        <p className="text-blue-10 text-sm ">{addMore? "+ " + addMore : ""}</p>
+        <p className="text-blue-10 text-sm hover:cursor-pointer ">{addMore ? "+ " + addMore : ""}</p>
       </div>
     </div>
   )
@@ -30,24 +34,36 @@ const FormInput: React.FC<IFormInput> = ({ name, children, addMore }) => {
 
 const Landing: NextPage<{ user: User }> = ({ user }) => {
   return (
-    <div className="w-full my-8">
-      <div className="flex flex-col w-full">
+    <div className="my-8 w-full">
+      <div className="flex w-full flex-col">
         <div className="ml-7 mt-4">
           <p className="text-4xl">Profil</p>
           <p className="opacity-70">Administrer dine kontoinnstillinger</p>
           <FormInput name="Profil">
             <div>
               <Avatar></Avatar>
-              {user.name}
+              {user.name ?? "No registred name"}
             </div>
           </FormInput>
           <FormInput name="Epost" addMore="Add Email Address">
-            <div>{user.email}</div>
+            <div>{user.email ?? "No registred email"}</div>
           </FormInput>
           <FormInput name="Telefon" addMore="Add Phone Number">
             <div>482 49 100</div>
           </FormInput>
-          <FormInput name="Studie"></FormInput>
+          <FormInput name="Studie" clickable={false}>
+            <div>
+              <div>
+                <p>Klassetrinn:</p>
+              </div>
+              <div>
+                <p>Startår:</p>
+              </div>
+              <div>
+                <p>StudieLøp:</p>
+              </div>
+            </div>
+          </FormInput>
         </div>
         {/* <Avatar></Avatar>
         <div className="ml-8 flex w-full justify-between">
