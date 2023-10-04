447f0e1bb20db7f2bb1463415f3f08eb00ab37bc
Mats Jun <mats@jun.codes>
2023-10-04 10:38:52 +0200

    refactor: restructre emails to work with email dev (#543)
    Does some minor changes in order to make `email dev` compatible with our
    email setup.
    
    Also temporarily removes the email handler (to be moved to lambda)

---
+++ packages/emails/src/template.tsx
@@ -0,0 +1,28 @@
+import { z } from "zod"
+import { render } from "@react-email/render"
+import { FC } from "react"
+
+export class InvalidTemplateArguments extends Error {}
+
+export type Template<T extends Record<string, unknown>> = {
+  (args: z.infer<z.ZodSchema<T>>): string
+  displayName: string
+}
+
+export function createTemplate<T extends Record<string, unknown>>(
+  name: string,
+  schema: z.ZodSchema<T>,
+  Component: FC<z.infer<typeof schema>>
+): Template<T> {
+  const handler = (args: z.infer<typeof schema>) => {
+    const result = schema.safeParse(args)
+    if (!result.success) {
+      throw new InvalidTemplateArguments("Invalid arguments passed to email template: " + result.error.message)
+    }
+    return render(<Component {...args} />)
+  }
+  handler.displayName = name
+  return handler
+}
+
+export type TemplateProps<S extends z.ZodSchema> = z.infer<S>
