7da8e15373384e6a3463a8eb2ead88b3f74a7027
Jonas <jonas@jole.xyz>
2023-10-04 22:54:58 +0200

    work on real data

--- packages/gateway-edge-nextjs/src/cal/cal.ts
+++ packages/gateway-edge-nextjs/src/cal/cal.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import ical from "ical-generator";
+import { createServerSideHelpers } from "@trpc/react-query/server"
+import { appRouter, createContextInner, transformer } from "@dotkomonline/gateway-trpc"
 
 export async function CalHandler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
@@ -13,19 +15,29 @@ export async function CalHandler(req: NextApiRequest, res: NextApiResponse) {
     return
   }
 
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: await createContextInner({
+      auth: null,
+    }),
+    transformer, // optional - adds superjson serialization
+  })
+
+  const events = await helpers.event.all.fetch()
   const instance = ical({name: `${user} online kalender`});
 
-  instance.createEvent({
-    start: new Date(),
-    end: new Date(),
-    summary: 'Example Event',
-    description: 'It works ;)',
-    location: 'my room',
-    url: 'http://online.ntnu.no'
+  events.forEach(event => {
+    instance.createEvent({
+      start: event.start,
+      end: event.end,
+      summary: event.title,
+      description: event.description,
+      location: event.location,
+      url: 'http://online.ntnu.no'
+    });
   });
+
 
   res.status(200).send(instance.toString())
   return;
-
-  res.status(200).json({ message: `Hello ${user}` })
 }
