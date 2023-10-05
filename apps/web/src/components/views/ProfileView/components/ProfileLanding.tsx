cf77feb513fea9645b8509c5ae1983f37849ea8e
madsab <mads.barnes@hotmail.com>
2023-10-05 20:31:30 +0200

    add cn import, silly me

--- apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
+++ apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
@@ -1,4 +1,4 @@
-import { Icon } from "@dotkomonline/ui"
+import { Icon, cn } from "@dotkomonline/ui"
 import StudentProgress from "@/components/molecules/StudentProgress/StudentProgress"
 import { Avatar } from "@radix-ui/react-avatar"
 import { NextPage } from "next"
