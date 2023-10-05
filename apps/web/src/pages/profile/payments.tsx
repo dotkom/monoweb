7a46732cd164d289b0d5d65411c55ef52b5ceae8
Mads <113549555+madsab@users.noreply.github.com>
2023-10-05 20:37:45 +0200

    Update profile design (#567)
    ## Checklist
    
    - [x] I updated the design of the ProfileLanding (Min Profil) page
    
    ## Changelog
    
    - I have updated the design of the ProfileLanding (Min profil) page
    based of the design in figma
    - I have also updated the overall layout of the profile page
    - I have updated the links in the ProfileMenu to match the design in
    Figma
    
    ## How to test
    1. Run project locally
    2. Go to <code>/profile</code> in the url
    
    ---------
    
    Co-authored-by: Mats Jun <mats@jun.codes>

--- apps/web/src/pages/profile/payments.tsx
+++ apps/web/src/pages/profile/payments.tsx
@@ -1,15 +1,28 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
+import { Icon } from "@dotkomonline/ui"
 
 const PaymentPage: NextPageWithLayout = () => {
   return <div>Payment</div>
 }
 
+const PaymentHeader = () => {
+  return (
+    <div className="flex items-center">
+      <Icon icon={"tabler:credit-card"} width={24} />
+      <p className="ml-2">Betalinger</p>
+    </div>
+  )
+}
+
 PaymentPage.getLayout = (page) => {
   return (
     <MainLayout>
-      <ProfileLayout>{page}</ProfileLayout>
+      <ProfileLayout>
+        <PaymentHeader />
+        {page}
+      </ProfileLayout>
     </MainLayout>
   )
 }
