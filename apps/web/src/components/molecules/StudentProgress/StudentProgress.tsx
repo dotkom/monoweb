754b99893a25184cf08490cc0bde9478ca3e6077
madsab <mads.barnes@hotmail.com>
2023-10-05 18:01:22 +0200

    removed hr and title

--- apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
+++ apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
@@ -10,13 +10,11 @@ interface IStudentProgress {
 const ProgressCircle: React.FC<IStudentProgressComponent> = ({ year, id }) => {
   return (
     <div
-      className={`${year <= id ? "border-[#153e75]" : "border-[#36b37e]"} 
+      className={`${year <= id ? "border-[#153e75]" : "border-[#36b37e]"}
       flex h-[45px] w-[45px] items-center  justify-around rounded-[50%] border-[4px] border-solid`}
     >
       <div
-        className={`${
-          year < id ? "text-[#153e75]" : year == id ? "bg-[#153e75] text-white" : "bg-[#36b37e] text-white"
-        } 
+        className={`${year < id ? "text-[#153e75]" : year == id ? "bg-[#153e75] text-white" : "bg-[#36b37e] text-white"}
           h-[20px] w-[20px] rounded-[50%] text-center`}
       >
         {id}
@@ -37,7 +35,6 @@ const StudentProgress: React.FC<IStudentProgress> = ({ year }) => {
   return (
     <>
       <div className="flex w-full items-center justify-between">
-        <p>Studieløp</p>
         <div className="flex flex-row">
           <ProgressCircle year={year} id={1} />
           <HorizontalLine year={year} id={1} />
@@ -52,7 +49,6 @@ const StudentProgress: React.FC<IStudentProgress> = ({ year }) => {
           <ProgressCircle year={year} id={5} />
         </div>
       </div>
-      <hr className="border-slate-12 my-5 w-full" />
     </>
   )
 }
