d7596440e1d8bf9c4d85b3ef6b3309ba35b1f680
Mats Larsen <mats@jun.codes>
2023-10-06 14:43:51 +0200

    fix: move columns out of component

--- apps/dashboard/src/app/(dashboard)/company/page.tsx
+++ apps/dashboard/src/app/(dashboard)/company/page.tsx
@@ -16,61 +16,56 @@ import {
 } from "@mantine/core"
 import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
 import { Company } from "@dotkomonline/types"
-import { useMemo } from "react"
 import { useCompanyAllQuery } from "../../../modules/company/queries/use-company-all-query"
 import { CompanyName } from "../../../components/molecules/company-name/company-name"
 import Link from "next/link"
 import { useCreateCompanyModal } from "../../../modules/company/modals/create-company-modal"
 
+const columnHelper = createColumnHelper<Company>()
+const columns = [
+  columnHelper.accessor((evt) => evt, {
+    id: "name",
+    header: () => "Bedrift",
+    cell: (info) => <CompanyName company={info.getValue()} />,
+  }),
+  columnHelper.accessor("email", {
+    id: "email",
+    header: () => "Kontakt e-post",
+    cell: (info) => (
+      <Anchor size="sm" href={`mailto:${info.getValue()}`}>
+        {info.getValue()}
+      </Anchor>
+    ),
+  }),
+  columnHelper.accessor("phone", {
+    id: "phone",
+    header: () => "Kontakt telefon",
+    cell: (info) => {
+      const phoneNumber = info.getValue()
+      if (phoneNumber) {
+        return (
+          <Anchor size="sm" href={`tel:${phoneNumber}`}>
+            {phoneNumber}
+          </Anchor>
+        )
+      }
+      return null
+    },
+  }),
+  columnHelper.accessor((evt) => evt, {
+    id: "actions",
+    header: () => "Detaljer",
+    cell: (info) => (
+      <Anchor size="sm" component={Link} href={`/company/${info.getValue().id}`}>
+        Se mer
+      </Anchor>
+    ),
+  }),
+]
+
 export default function CompanyPage() {
   const { companies, isLoading } = useCompanyAllQuery()
   const open = useCreateCompanyModal()
-
-  const columnHelper = createColumnHelper<Company>()
-  const columns = useMemo(
-    () => [
-      columnHelper.accessor((evt) => evt, {
-        id: "name",
-        header: () => "Bedrift",
-        cell: (info) => <CompanyName company={info.getValue()} />,
-      }),
-      columnHelper.accessor("email", {
-        id: "email",
-        header: () => "Kontakt e-post",
-        cell: (info) => (
-          <Anchor size="sm" href={`mailto:${info.getValue()}`}>
-            {info.getValue()}
-          </Anchor>
-        ),
-      }),
-      columnHelper.accessor("phone", {
-        id: "phone",
-        header: () => "Kontakt telefon",
-        cell: (info) => {
-          const phoneNumber = info.getValue()
-          if (phoneNumber) {
-            return (
-              <Anchor size="sm" href={`tel:${phoneNumber}`}>
-                {phoneNumber}
-              </Anchor>
-            )
-          }
-          return null
-        },
-      }),
-      columnHelper.accessor((evt) => evt, {
-        id: "actions",
-        header: () => "Detaljer",
-        cell: (info) => (
-          <Anchor size="sm" component={Link} href={`/company/${info.getValue().id}`}>
-            Se mer
-          </Anchor>
-        ),
-      }),
-    ],
-    [columnHelper]
-  )
-
   const table = useReactTable({
     data: companies,
     getCoreRowModel: getCoreRowModel(),
