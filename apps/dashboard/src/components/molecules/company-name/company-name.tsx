679da16dee00e48d5cb08efcd4a5bfe3e569b530
henrikskog <henrikskog01@gmail.com>
2023-10-07 15:58:17 +0200

    notify on image error by using broken image icon

--- apps/dashboard/src/components/molecules/company-name/company-name.tsx
+++ apps/dashboard/src/components/molecules/company-name/company-name.tsx
@@ -1,23 +1,38 @@
-import { FC } from "react"
 import { Company } from "@dotkomonline/types"
-import { Group, Image } from "@mantine/core"
 import { Icon } from "@iconify/react"
+import { Group, Image } from "@mantine/core"
+import { FC, useState } from "react"
 
 export type CompanyNameProps = {
   company: Company
 }
 
 export const CompanyName: FC<CompanyNameProps> = ({ company }) => {
+  const [imageError, setImageError] = useState(false)
+
   return (
     <Group>
-      {company.image !== null ? (
+      {company.image === null ? (
         <>
-          <Image w={40} h={40} fit="contain" src={company.image} alt={`${company.name} logo icon`} />
+          <Icon width={40} height={40} icon="tabler:user-circle" />
           {company.name}
         </>
       ) : (
         <>
-          <Icon width={40} height={40} icon="tabler:user-circle" />
+          {imageError ? (
+            <Icon width={40} height={40} icon="tabler:photo-x" />
+          ) : (
+            <Image
+              w={40}
+              h={40}
+              fit="contain"
+              src={company.image}
+              alt={`${company.name} logo icon`}
+              onError={(_) => {
+                setImageError(true)
+              }}
+            />
+          )}
           {company.name}
         </>
       )}
