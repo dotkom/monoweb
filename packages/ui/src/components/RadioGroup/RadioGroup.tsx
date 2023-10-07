8483f014d90eab8c2f094f19d2f65bbd30c9e223
Kharann <hey@akvo.no>
2023-10-07 16:22:00 +0200

    remove children unused prop

--- packages/ui/src/components/RadioGroup/RadioGroup.tsx
+++ packages/ui/src/components/RadioGroup/RadioGroup.tsx
@@ -3,35 +3,28 @@ import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { cn } from "../../utils"
 import { Icon } from "../Icon"
 
-
 const RadioGroup = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
 >(({ className, ...props }, ref) => {
-  return (
-    <RadioGroupPrimitive.Root
-      className={cn("grid gap-2", className)}
-      {...props}
-      ref={ref}
-    />
-  )
+  return <RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />
 })
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, children, ...props }, ref) => {
+>(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "text:fill-slate-3 h-4 w-4 rounded-full border border-slate-5 text-slate-12 hover:border-slate-4 focus:outline-none focus:ring-1 focus:ring-slate-9 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "text:fill-slate-3 border-slate-5 text-slate-12 hover:border-slate-4 focus:ring-slate-9 h-4 w-4 rounded-full border focus:outline-none focus:ring-1 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="grid place-items-center relative">
+      <RadioGroupPrimitive.Indicator className="relative grid place-items-center">
         {/* Center icon inside */}
         <Icon icon="tabler:circle-filled" width={12} className="fill-slate-4" />
       </RadioGroupPrimitive.Indicator>
