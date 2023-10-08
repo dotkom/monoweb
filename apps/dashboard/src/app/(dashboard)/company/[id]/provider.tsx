265dc985b8199dcccfb983d3ac02fb97fe548094
Henrik Skog <henrikskog01@gmail.com>
2023-10-08 23:40:34 +0200

    Create dashboard pages for listing events made by company (#580)
    Add company section to dashboard
    - Create new company modal
    - List of registered companies
    - Detail page: edit basic fields
    - Detail page: see related events to company
    
    Create necessary repo/servce/routers.

---
+++ apps/dashboard/src/app/(dashboard)/company/[id]/provider.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { createContext, useContext } from "react"
+import { Company } from "@dotkomonline/types"
+
+/** Context consisting of everything required to use and render the form */
+export const CompanyDetailsContext = createContext<{
+  company: Company
+} | null>(null)
+
+export const useCompanyDetailsContext = () => {
+  const ctx = useContext(CompanyDetailsContext)
+  if (ctx === null) {
+    throw new Error("useCompanyDetailsContext called without Provider in tree")
+  }
+  return ctx
+}
