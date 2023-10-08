d357c2b86f666d987f3ea6c754692531505912fb
henrikskog <henrikskog01@gmail.com>
2023-10-08 12:52:18 +0200

    don't use winston logger for now

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -2,10 +2,6 @@ import { appRouter, createContext } from "@dotkomonline/gateway-trpc"
 import { createNextApiHandler } from "@trpc/server/adapters/next"
 import { applyCorsMiddleware } from "@/middlewares/cors"
 
-import { getLogger } from "@dotkomonline/logger"
-
-export const logger = getLogger("migrator")
-
 // export API handler
 export default applyCorsMiddleware(
   createNextApiHandler({
