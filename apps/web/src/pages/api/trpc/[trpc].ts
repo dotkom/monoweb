f89a8d693ba7b7b247da9b72c3ec1a161a4382e4
Henrik Skog <henrikskog01@gmail.com>
2023-10-08 23:40:34 +0200

    Create dashboard pages for listing events made by company (#580)
    Add company section to dashboard
    - Create new company modal
    - List of registered companies
    - Detail page: edit basic fields
    - Detail page: see related events to company
    
    Create necessary repo/servce/routers.

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -7,5 +7,8 @@ export default applyCorsMiddleware(
   createNextApiHandler({
     router: appRouter,
     createContext,
+    onError({ error }) {
+      console.error(error)
+    },
   })
 )
--- packages/types/src/company.ts
+++ packages/types/src/company.ts
@@ -3,14 +3,15 @@ import { z } from "zod"
 export const CompanySchema = z.object({
   id: z.string().uuid(),
   createdAt: z.date(),
-  name: z.string().max(50),
-  description: z.string(),
-  phone: z.string().nullable(),
-  email: z.string(),
-  website: z.string(),
+  name: z.string().max(50).min(1),
+  description: z.string().min(1),
+  email: z.string().email().min(1),
+  website: z.string().min(1),
+  type: z.enum(["Consulting", "Research", "Development", "Other"]),
+
   location: z.string().nullable(),
-  type: z.enum(["Consulting", "Research", "Development", "Other"]).nullable(),
   image: z.string().nullable(),
+  phone: z.string().nullable(),
 })
 
 export type Company = z.infer<typeof CompanySchema>
