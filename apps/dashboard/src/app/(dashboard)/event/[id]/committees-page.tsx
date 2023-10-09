4af5536aa8804ef50bbfbf06df7941a8ff335cb8
brage-andreas <draaangooo@gmail.com>
2023-10-09 19:43:10 +0200

    feat: rename `company` to `committee`

--- apps/dashboard/src/app/(dashboard)/event/[id]/committees-page.tsx
+++ apps/dashboard/src/app/(dashboard)/event/[id]/committees-page.tsx
@@ -1,36 +1,36 @@
-import { Company, CompanySchema, EventSchema } from "@dotkomonline/types"
+import { Committee, CommitteeSchema, EventSchema } from "@dotkomonline/types"
 import { Icon } from "@iconify/react"
 import { Box, Button, Group, Image, Text, Title } from "@mantine/core"
 import { createColumnHelper, getCoreRowModel, useReactTable } from "@tanstack/react-table"
 import { FC, useMemo } from "react"
 import { z } from "zod"
 import { GenericTable } from "../../../../components/GenericTable"
-import { useCompanyAllQuery } from "../../../../modules/company/queries/use-company-all-query"
-import { useAddCompanyToEventMutation } from "../../../../modules/event/mutations/use-add-company-to-event-mutation"
-import { useRemoveCompanyFromEventMutation } from "../../../../modules/event/mutations/use-remove-company-from-event-mutation"
-import { useEventCompanyGetQuery } from "../../../../modules/event/queries/use-event-company-get-query"
+import { useCommitteeAllQuery } from "../../../../modules/committee/queries/use-committee-all-query"
+import { useAddCommitteeToEventMutation } from "../../../../modules/event/mutations/use-add-committee-to-event-mutation"
+import { useRemoveCommitteeFromEventMutation } from "../../../../modules/event/mutations/use-remove-committee-from-event-mutation"
+import { useEventCommitteeGetQuery } from "../../../../modules/event/queries/use-event-committee-get-query"
 import { createSelectInput, useFormBuilder } from "../../../form"
 import { useEventDetailsContext } from "./provider"
 
 export const EventCommiteesPage: FC = () => {
   const { event } = useEventDetailsContext()
-  const { eventCompanies } = useEventCompanyGetQuery(event.id)
-  const { companies } = useCompanyAllQuery()
-  const add = useAddCompanyToEventMutation()
-  const remove = useRemoveCompanyFromEventMutation()
+  const { eventCommittiees } = useEventCommitteeGetQuery(event.id)
+  const { committees } = useCommitteeAllQuery()
+  const add = useAddCommitteeToEventMutation()
+  const remove = useRemoveCommitteeFromEventMutation()
 
-  const columnHelper = createColumnHelper<Company>()
+  const columnHelper = createColumnHelper<Committee>()
   const columns = useMemo(
     () => [
       columnHelper.accessor((eventCompany) => eventCompany, {
-        id: "eventCompany",
+        id: "eventCommittee",
         header: () => "Navn",
         cell: (info) => {
-          const name = companies.find((x) => x.id === info.getValue().id)?.name ?? "Ingen navn"
+          const name = committees.find((x) => x.id === info.getValue().id)?.name ?? "Ingen navn"
           const image = info.getValue().image
           return image !== null ? (
             <Group>
-              <Image width={40} height={40} fit="contain" src={image} alt="company logo" />
+              <Image width={40} height={40} fit="contain" src={image} alt="committee logo" />
               {name}
             </Group>
           ) : (
@@ -48,37 +48,37 @@ export const EventCommiteesPage: FC = () => {
           <Button
             variant="outline"
             leftSection={<Icon icon="tabler:trash" />}
-            onClick={() => remove.mutate({ id: event.id, company: info.getValue().id })}
+            onClick={() => remove.mutate({ id: event.id, committee: info.getValue().id })}
           >
             Fjern
           </Button>
         ),
       }),
     ],
-    [companies, columnHelper, remove, event.id]
+    [committees, columnHelper, remove, event.id]
   )
-  const table = useReactTable<Company>({
-    data: eventCompanies,
+  const table = useReactTable<Committee>({
+    data: eventCommittiees,
     columns,
     getCoreRowModel: getCoreRowModel(),
   })
   const FormComponent = useFormBuilder({
     schema: z.object({
       id: EventSchema.shape.id,
-      company: CompanySchema.shape.id,
+      committee: CommitteeSchema.shape.id,
     }),
     defaultValues: {
       id: event.id,
     },
     fields: {
-      company: createSelectInput({
-        label: "Bedriftsnavn",
-        data: companies
-          .filter((company) => !eventCompanies.map((x) => x.id).includes(company.id))
-          .map((company) => ({ label: company.name, value: company.id })),
+      committee: createSelectInput({
+        label: "Komiténavn",
+        data: committees
+          .filter((committee) => !eventCommittiees.map((x: { id: unknown }) => x.id).includes(committee.id))
+          .map((committee) => ({ label: committee.name, value: committee.id })),
       }),
     },
-    label: "Legg til ny bedrift",
+    label: "Legg til ny komité",
     onSubmit: (data) => {
       add.mutate(data)
     },
@@ -86,8 +86,8 @@ export const EventCommiteesPage: FC = () => {
 
   return (
     <Box>
-      <Title order={3}>Bedrifter</Title>
-      <Text>Dette er en oversikt over hvilke bedrifter som arrangerer dette arrangementet.</Text>
+      <Title order={3}>Komiteer</Title>
+      <Text>Dette er en oversikt over hvilke komiteer som arrangerer dette arrangementet.</Text>
       <FormComponent />
       <GenericTable table={table} />
     </Box>
