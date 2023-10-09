761528fd7c6cb135f34ade3483de3af6546cb3e8
henrikskog <henrikskog01@gmail.com>
2023-10-09 21:06:13 +0200

    parse user input in event create modal

--- apps/dashboard/src/modules/event/modals/create-event-modal.tsx
+++ apps/dashboard/src/modules/event/modals/create-event-modal.tsx
@@ -1,14 +1,16 @@
-import { FC } from "react"
-import { useCreateEventMutation } from "../mutations/use-create-event-mutation"
-import { useEventWriteForm } from "../../../app/(dashboard)/event/write-form"
+import { EventWriteSchema } from "@dotkomonline/types"
 import { ContextModalProps, modals } from "@mantine/modals"
+import { FC } from "react"
+import { useEventWriteForm } from "../../../app/(dashboard)/event/write-form"
+import { useCreateEventMutation } from "../mutations/use-create-event-mutation"
 
 export const CreateEventModal: FC<ContextModalProps> = ({ context, id }) => {
   const close = () => context.closeModal(id)
   const create = useCreateEventMutation()
   const FormComponent = useEventWriteForm({
     onSubmit: (data) => {
-      create.mutate(data)
+      const result = EventWriteSchema.parse(data)
+      create.mutate(result)
       close()
     },
   })
