24ebf0658bba49e72aa52edffa8f6b990b42fe3c
Mats Larsen <mats@jun.codes>
2023-10-11 20:28:55 +0200

    fix: regenerate types

--- packages/db/src/migrations/0001_create_user_and_auth.ts
+++ packages/db/src/migrations/0001_create_user_and_auth.ts
@@ -7,8 +7,7 @@ import { createTableWithDefaults } from "../utils"
 export async function up(db: Kysely<any>): Promise<void> {
   const query = sql`CREATE EXTENSION IF NOT EXISTS ulid;`.compile(db)
   await db.executeQuery(query)
-  await createTableWithDefaults("ow_user", { id: true, createdAt: true }, db.schema)
-    .execute()
+  await createTableWithDefaults("ow_user", { id: true, createdAt: true }, db.schema).execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
