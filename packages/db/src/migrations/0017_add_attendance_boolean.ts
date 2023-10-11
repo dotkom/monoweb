4725f55b60b830e73d5274cf447c224467f2f141
Njål Sørland <njaalis2000@gmail.com>
2023-10-11 20:40:29 +0200

    add default false value to attendance

--- packages/db/src/migrations/0017_add_attendance_boolean.ts
+++ packages/db/src/migrations/0017_add_attendance_boolean.ts
@@ -3,6 +3,6 @@ import { Kysely } from "kysely"
 export async function up(db: Kysely<any>) {
   await db.schema
     .alterTable("attendee")
-    .addColumn("attended", "boolean", (col) => col.notNull())
+    .addColumn("attended", "boolean", (col) => col.notNull().defaultTo(false))
     .execute()
 }
