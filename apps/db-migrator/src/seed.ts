ff452140156ce8eea41e42277e6afcd5fc9c38f9
henrikskog <henrikskog01@gmail.com>
2023-10-13 18:01:17 +0200

    sync with main

--- apps/db-migrator/src/seed.ts
+++ apps/db-migrator/src/seed.ts
@@ -1,7 +1,5 @@
-import { type Database } from "@dotkomonline/db"
 import { faker } from "@faker-js/faker"
-import { addHours } from "date-fns"
-import { type Insertable } from "kysely"
+
 import { db } from "./db"
 import { users } from "./fixtures/user"
 import { events } from "./fixtures/event"
@@ -9,49 +7,7 @@ import { attendances } from "./fixtures/attendance"
 
 faker.seed(69)
 
-const createRandomUser = (): Insertable<Database["owUser"]> => ({
-  id: faker.datatype.uuid(),
-  name: faker.name.firstName(),
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-  image: faker.image.avatar(),
-})
-
-const createRandomEvent = (): Insertable<Database["event"]> => {
-  const start = faker.date.future()
-  return {
-    id: faker.datatype.uuid(),
-    title: faker.lorem.sentence(),
-    subtitle: faker.helpers.arrayElement([faker.lorem.sentence(), undefined]),
-    description: faker.lorem.paragraph(),
-    start,
-    end: addHours(start, 8),
-    location: faker.address.streetAddress(),
-    public: faker.datatype.boolean(),
-    status: faker.helpers.arrayElement(["TBA", "PUBLIC", "NO_LIMIT", "ATTENDANCE"]),
-    type: faker.helpers.arrayElement(["BEDPRES", "ACADEMIC"]),
-    waitlist: null,
-  }
-}
-
-const createRandomAttendance = (eventIds: string[]): Insertable<Database["attendance"]> => ({
-  id: faker.datatype.uuid(),
-  eventId: faker.helpers.arrayElement(eventIds),
-  start: faker.date.future(),
-  end: faker.date.future(),
-  deregisterDeadline: faker.date.future(),
-  limit: faker.datatype.number({ min: 5, max: 100 }),
-  min: 0,
-  max: 0,
-})
-
 export const seed = async () => {
-  const users = Array.from({ length: 15 }).map(() => createRandomUser())
-  const event = Array.from({ length: 15 }).map(() => createRandomEvent())
-  const attendance = Array.from({ length: 15 }).map(() =>
-    createRandomAttendance(event.map((e) => e && e.id).filter((e): e is string => Boolean(e)))
-  )
-
   await db
     .insertInto("owUser")
     .values(users)
