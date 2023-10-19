c060d3ccc60f7f51c63671c1695bf68ac0311135
BillySBarrett <billysb@stud.ntnu.no>
2023-10-19 14:52:18 +0200

    add qr reader that updates attendance to dashboard
    known issues:
    * when the video is unmounted the qr scanner still scans
    * Might not work on slower devices due to timeouts used for syncing
    * No error handeling

--- apps/dashboard/src/app/(dashboard)/event/[id]/details-attendance.tsx
+++ apps/dashboard/src/app/(dashboard)/event/[id]/details-attendance.tsx
@@ -1,11 +1,12 @@
-import React, { FC, useMemo } from "react"
+import React, { FC, useMemo, useState } from "react"
 import { useEventDetailsContext } from "./provider"
-import { Box, Title, Checkbox } from "@mantine/core"
+import { Box, Title, Checkbox, Button } from "@mantine/core"
 import { useEventAttendanceGetQuery } from "src/modules/event/queries/use-event-attendance-get-query"
 import { useUpdateEventAttendanceMutation } from "src/modules/event/mutations/use-update-event-attendance-mutation"
 import { createColumnHelper, useReactTable, getCoreRowModel } from "@tanstack/react-table"
 import { GenericTable } from "src/components/GenericTable"
 import { Attendee } from "@dotkomonline/types"
+import { QrReader } from "react-qr-reader"
 
 interface CustomCheckboxProps {
   userId: string
@@ -29,6 +30,43 @@ const CustomCheckbox = React.memo(({ attendanceId, userId, defaultChecked }: Cus
 })
 
 CustomCheckbox.displayName = "attendanceToggle"
+
+const ViewFinder = ({ data, updateAttendance, setData }: { data: string; updateAttendance: any; setData: any }) => {
+  if (data === "") {
+    return null
+  }
+  return (
+    <div
+      style={{
+        top: 0,
+        left: 0,
+        zIndex: 1,
+        boxSizing: "border-box",
+        background: "rgba(0, 0, 0, 0.5)",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Button
+        onClick={() => {
+          const userId = data.split("/")[0]
+          const attendanceId = data.split("/")[1]
+          updateAttendance.mutate({ userId, attendanceId, attended: true })
+          //TODO: Fix this hack
+          setTimeout(() => {
+            setData("")
+          }, 100)
+        }}
+      >
+        Møtt
+      </Button>
+    </div>
+  )
+}
 
 export const EventAttendancePage: FC = () => {
   const { event } = useEventDetailsContext()
@@ -61,9 +99,37 @@ export const EventAttendancePage: FC = () => {
     columns,
   })
 
+  const [data, setData] = useState("")
+  const updateAttendance = useUpdateEventAttendanceMutation()
+  const [showScanner, setShowScanner] = useState(false)
+
   return (
     <Box>
       <Title order={3}>Påmeldte</Title>
+      <Button
+        onClick={() => {
+          setTimeout(() => {
+            setShowScanner(!showScanner)
+          }, 100)
+        }}
+      >
+        {showScanner ? "Skru av" : "Skru på"} QR-leser
+      </Button>
+      {showScanner && (
+        <div style={{ position: "relative", width: "50%" }}>
+          <QrReader
+            onResult={(result, _, _codeReader) => {
+              if (result) {
+                setData(result.getText())
+                // updateAttendance.mutate({ userId: result, attendanceId: "1", attended: true })
+              }
+            }}
+            containerStyle={{ position: "relative", width: "100%", height: "100%" }}
+            constraints={{ facingMode: "none" }}
+          />
+          <ViewFinder data={data} updateAttendance={updateAttendance} setData={setData} />
+        </div>
+      )}
       {eventAttendance?.map((attendance) => (
         <Box key={attendance.id} mb="sm">
           <Title order={4}>
