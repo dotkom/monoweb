43fc483df07cd22b23e0185434b57012abef3c5a
madsab <mads.barnes@hotmail.com>
2023-10-19 13:50:50 +0200

    removed unused import

--- apps/web/src/pages/profile/privacy.tsx
+++ apps/web/src/pages/profile/privacy.tsx
@@ -1,7 +1,6 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
-import { Icon } from "@dotkomonline/ui"
 
 const PrivacyPage: NextPageWithLayout = () => {
   return <div>Privacy</div>
