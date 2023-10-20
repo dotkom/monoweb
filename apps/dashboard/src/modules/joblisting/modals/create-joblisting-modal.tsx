1ef16334ecb83cce46b1f3e4282dddf39802e840
henrikskog <henrikskog01@gmail.com>
2023-10-21 01:37:23 +0200

    create new modal

---
+++ apps/dashboard/src/modules/joblisting/modals/create-joblisting-modal.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react"
+import { useJobListingWriteForm } from "src/app/(dashboard)/joblisting/write-form"
+import { useCreateJobListingMutation } from "../mutations/use-create-joblisting-mutation"
+import { ContextModalProps, modals } from "@mantine/modals"
+
+export const CreateJobListingModal: FC<ContextModalProps> = ({ context, id }) => {
+  const close = () => context.closeModal(id)
+  const create = useCreateJobListingMutation()
+  const FormComponent = useJobListingWriteForm({
+    onSubmit: (data) => {
+      create.mutate(data)
+      close()
+    },
+  })
+  return <FormComponent />
+}
+
+export const useCreateJobListingModal = () => {
+  return () =>
+    modals.openContextModal({
+      modal: "jobListing/create",
+      title: "Opprett ny stillingsannonse",
+      innerProps: {},
+    })
+}
