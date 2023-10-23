1d2ef8ba661001bad24de6728e9c10853619ad90
Mats Larsen <mats@jun.codes>
2023-10-23 13:44:41 +0200

    Add pgx-ulid custom docker build

---
+++ packages/pgx-ulid/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+[ ! -f v0.1.3.tar.gz ] && curl -Lo https://github.com/pksunkara/pgx_ulid/archive/refs/tags/v0.1.3.tar.gz
+tar -xvf v0.1.3.tar.gz
+
+sed -i '/ENV POSTGRES_HOST_AUTH_METHOD=trust/d' ./pgx_ulid-0.1.3/Dockerfile
+docker build --build-arg PG_MAJOR=15 -f pgx_ulid-0.1.3/Dockerfile -t pgx_ulid:0.1.3 pgx_ulid-0.1.3
