497024f0f6458b8e4515f85b50e36737792c6dde
Mads <113549555+madsab@users.noreply.github.com>
2023-10-23 11:23:19 +0200

    Create penalty page (#630)
    Closes: #..
    
    ## Checklist
    
    - [x] I have created penalty page
    - [x] I have updated to automatic titles
    - [x] I have removed titles from existing titles 
    
    ## Changelog
    - Created penalty page without functionality
    - Updated to automatically show titles in profile pages
    - Removed hardcoded title/header from pages in profile (Not
    notifications, accesscard and payments because they will be removed in
    another branch by @hanlun0804 )
    - Exported Accordion from storybook
    
    ## How to test
    - Run monoweb
    - Navigate to <code>/profile</code> in URL

--- apps/web/src/pages/profile/penalties.tsx
+++ apps/web/src/pages/profile/penalties.tsx
@@ -1,17 +1,39 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ProfileLayout from "@/components/layout/ProfileLayout"
 import { NextPageWithLayout } from "../_app"
-import { Icon } from "@dotkomonline/ui"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@dotkomonline/ui"
+import { DotFilledIcon } from "@radix-ui/react-icons"
+
+import { FC } from "react"
+import PenaltyRules from "@/utils/penalty-rules"
+import { addMinutes, format } from "date-fns"
+
+/* TODO - Set up connection to Users marks router */
 
 const PenaltiesPage: NextPageWithLayout = () => {
-  return <div>Penalties</div>
-}
-
-const PenaltyHeader = () => {
   return (
-    <div className="flex items-center">
-      <Icon icon="pajamas:cancel" className="w-4" />
-      <p className="ml-2">Prikker og Suspensjoner</p>
+    <div className="ml-3 flex flex-col space-y-12">
+      <p className="text-slate-10">Oversikt over dine prikker</p>
+      <div className="flex flex-col">
+        <p className="text-2xl font-medium">Aktive Prikker</p>
+        {/* TODO - Get active marks */}
+        <PenaltyAccordion title="TestPrikk" category="Sosial" givenAt={new Date()} duration={2000} details="Testing" />
+        <p>Ingen aktive prikker</p>
+      </div>
+      <div>
+        <p className="text-2xl font-medium">Gamle Prikker</p>
+        {/* TODO - Get old marks */}
+        <p>Ingen gamle prikker</p>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <p className="text-2xl font-medium">Suspensjoner</p>
+        {/* TODO - Get suspensions */}
+        <p>Ingen suspensjoner</p>
+      </div>
+      <div>
+        <p className="text-2xl font-medium">PrikkeRegler</p>
+        <PenaltyRules />
+      </div>
     </div>
   )
 }
@@ -19,11 +41,46 @@ const PenaltyHeader = () => {
 PenaltiesPage.getLayout = (page) => {
   return (
     <MainLayout>
-      <ProfileLayout>
-        <PenaltyHeader />
-        {page}
-      </ProfileLayout>
+      <ProfileLayout>{page}</ProfileLayout>
     </MainLayout>
+  )
+}
+
+interface PenaltyAccordionProps {
+  title: string
+  category: string
+  givenAt: Date
+  duration: number
+  details: string
+}
+
+const PenaltyAccordion: FC<PenaltyAccordionProps> = (props) => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="accordions ml-1 text-lg">
+          <span className="flex">
+            <DotFilledIcon className="mt-1" />
+            {props.title}
+          </span>
+        </AccordionTrigger>
+        <AccordionContent className="ml-4">
+          <div className="flex flex-col space-y-4">
+            <p>
+              Du har fått en prikk på grunn av {props.details} den {format(props.givenAt, "dd/MM/yyyy")}
+            </p>
+            <p className="text-lg">
+              <span className="font-bold">Katergori: </span>
+              {props.category}
+            </p>
+            <p className="text-lg">
+              <span className="font-bold">Utløpsdato: </span>
+              {format(addMinutes(props.givenAt, props.duration), "dd/MM/yyyy")}
+            </p>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
 
