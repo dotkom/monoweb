9a7cb59ef0604562f87ec238e9c3695b7149b59b
madsab <mads.barnes@hotmail.com>
2023-10-25 20:04:16 +0200

    responisivness in profilelanding

--- apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
+++ apps/web/src/components/views/ProfileView/components/ProfileLanding.tsx
@@ -4,21 +4,21 @@ import { Avatar } from "@radix-ui/react-avatar"
 import { NextPage } from "next"
 import { User } from "next-auth"
 
-interface IFormInput {
+interface FormInputProps {
   name: string
   children?: JSX.Element
   addMore?: string
   clickable?: boolean
 }
 
-const FormInput: React.FC<IFormInput> = ({ name, children, addMore, clickable = true }) => {
+const FormInput: React.FC<FormInputProps> = ({ name, children, addMore, clickable = true }) => {
   return (
     <div className="my-10 ">
       <div className="ml-4">
         <label>{name}</label>
       </div>
       <hr className="border-slate-12 w-full opacity-50" />
-      <div className="ml-10 w-2/3 space-y-3 ">
+      <div className="ml-10 space-y-3 max-md:ml-1.5 sm:w-2/3 ">
         <div
           className={cn(
             "mt-3 flex items-center justify-between rounded-lg pl-2",
@@ -36,36 +36,34 @@ const FormInput: React.FC<IFormInput> = ({ name, children, addMore, clickable = 
 
 const Landing: NextPage<{ user: User }> = ({ user }) => {
   return (
-    <div className="w-full">
-      <div className="flex w-full flex-col">
-        <p className="text-slate-10">Administrer dine kontoinnstillinger</p>
-        <FormInput name="Profil">
+    <div className="flex w-full flex-col">
+      <p className="text-slate-10">Administrer dine kontoinnstillinger</p>
+      <FormInput name="Profil">
+        <div>
+          <Avatar></Avatar>
+          {user.name ?? "No registred name"}
+        </div>
+      </FormInput>
+      <FormInput name="Epost" addMore="Add Email Address">
+        <div>{user.email ?? "No registred email"}</div>
+      </FormInput>
+      <FormInput name="Telefon" addMore="Add Phone Number">
+        <div> (+47) 482 49 100</div>
+      </FormInput>
+      <FormInput name="Studie" clickable={false}>
+        <div className="space-y-8">
           <div>
-            <Avatar></Avatar>
-            {user.name ?? "No registred name"}
+            <p>Klassetrinn: </p>
           </div>
-        </FormInput>
-        <FormInput name="Epost" addMore="Add Email Address">
-          <div>{user.email ?? "No registred email"}</div>
-        </FormInput>
-        <FormInput name="Telefon" addMore="Add Phone Number">
-          <div> (+47) 482 49 100</div>
-        </FormInput>
-        <FormInput name="Studie" clickable={false}>
-          <div className="space-y-8">
-            <div>
-              <p>Klassetrinn: </p>
-            </div>
-            <div>
-              <p>Startår:</p>
-            </div>
-            <div className="flex items-center space-x-10 ">
-              <p>Studieløp:</p>
-              <StudentProgress year={0} />
-            </div>
+          <div>
+            <p>Startår:</p>
           </div>
-        </FormInput>
-      </div>
+          <div className="flex items-center space-x-10 ">
+            <p>Studieløp:</p>
+            <StudentProgress year={0} />
+          </div>
+        </div>
+      </FormInput>
     </div>
   )
 }
