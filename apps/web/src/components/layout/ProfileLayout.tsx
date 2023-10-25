f152bd0eeecba6954fa2916a3362d16b64012915
madsab <mads.barnes@hotmail.com>
2023-10-25 20:02:33 +0200

    responsive layout

--- apps/web/src/components/layout/ProfileLayout.tsx
+++ apps/web/src/components/layout/ProfileLayout.tsx
@@ -4,6 +4,7 @@ import { ProfileContext } from "../views/ProfileView/context/ProfileContext"
 import { Icon } from "@dotkomonline/ui"
 import { usePathname } from "next/navigation"
 import { profileItems } from "@/utils/profileLinks"
+import MobileMenuContainer from "../organisms/Navbar/components/profile/ProfileMenu/MobileMenuContainer"
 
 interface PageTitleProps {
   title: string
@@ -12,7 +13,7 @@ interface PageTitleProps {
 
 const PageTitle: FC<PageTitleProps> = ({ title, icon }) => {
   return (
-    <div className="flex h-10 space-x-2">
+    <div className="flex h-10 space-x-2 max-md:hidden">
       <Icon icon={icon} width={"w-10"} />
       <p className="text-3xl">{title}</p>
     </div>
@@ -25,17 +26,16 @@ const ProfileLayout: FC<PropsWithChildren> = ({ children }) => {
   const [editMode, setEditMode] = useState(false)
 
   return (
-    <div className="m-x-auto mb-5 max-w-[1000px]">
-      <div className="shadow-slate-6 rounded-3xl px-6 shadow-lg">
-        <div className="flex w-full flex-row">
-          <ProfileMenuContainer />
-          <ProfileContext.Provider value={{ editMode, setEditMode }}>
-            <div className="mx-5 mt-16 md:min-w-[600px]">
-              {currentLink && <PageTitle title={currentLink?.title} icon={currentLink.icon} />}
-              <div className="my-2 ml-5">{children}</div>
-            </div>
-          </ProfileContext.Provider>
-        </div>
+    <div className="m-x-auto shadow-slate-6 relative mx-3 mb-5 max-w-[1000px] rounded-3xl px-6 shadow-lg max-md:w-[90vw] ">
+      <div className="md:flex">
+        <MobileMenuContainer />
+        <ProfileMenuContainer />
+        <ProfileContext.Provider value={{ editMode, setEditMode }}>
+          <div className="md:mx-5 md:mt-16 md:w-[550px]">
+            {currentLink && <PageTitle title={currentLink?.title} icon={currentLink.icon} />}
+            <div className="my-2 ml-5 ">{children}</div>
+          </div>
+        </ProfileContext.Provider>
       </div>
     </div>
   )
