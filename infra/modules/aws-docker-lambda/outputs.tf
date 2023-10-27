d4c3f4f3a0b0229324e4f11779ea188fa00a5f2a
Mats Jun <mats@jun.codes>
2023-10-27 08:20:08 +0200

    Create Cognito automated trigger for inserting new user (#646)
    Depends on common lambda infrastructure in #544 
    
    Lambda/Cognito automation for creating a user link in the ow_users table
    when a user has signed up.

--- infra/modules/aws-docker-lambda/outputs.tf
+++ infra/modules/aws-docker-lambda/outputs.tf
@@ -6,6 +6,10 @@ output "lambda_invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
 }
 
+output "lambda_arn" {
+  value = aws_lambda_function.this.arn
+}
+
 output "lambda_name" {
   value = aws_lambda_function.this.function_name
 }
