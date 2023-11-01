3309963607df53f860148800edf51b86aeca024d
madsab <mads.barnes@hotmail.com>
2023-11-01 20:29:52 +0100

    add: resize on mobile

--- apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
+++ apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
@@ -11,31 +11,41 @@ const ProgressCircle: React.FC<IStudentProgressComponent> = ({ year, id }) => {
   return (
     <div
       className={`${year <= id ? "border-[#153e75]" : "border-[#36b37e]"}
-      flex h-[45px] w-[45px] items-center  justify-around rounded-[50%] border-[4px] border-solid`}
+      flex h-[45px] w-[45px] items-center justify-around rounded-[50%]  border-[4px] border-solid max-sm:h-[30px] max-sm:w-[30px]`}
     >
       <div
         className={`${year < id ? "text-[#153e75]" : year == id ? "bg-[#153e75] text-white" : "bg-[#36b37e] text-white"}
-          h-[20px] w-[20px] rounded-[50%] text-center`}
+          flex h-[20px] w-[20px] justify-center rounded-[50%] max-sm:h-[10px] max-sm:w-[10px] max-sm:items-center max-sm:p-2 max-sm:text-center`}
       >
-        {id}
+        <p className="max-sm:text-[1rem]">{id}</p>
       </div>
     </div>
   )
 }
 
 const HorizontalLine: React.FC<IStudentProgressComponent> = ({ year, id }) => {
-  return <div className={`${year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"} m-[-1px] h-[4px] w-[30px] self-center`} />
+  return (
+    <div
+      className={`${
+        year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"
+      } m-[-1px] h-[4px] w-[30px] self-center max-sm:w-[20px]`}
+    />
+  )
 }
 
 const VerticalLine: React.FC<IStudentProgressComponent> = ({ year, id }) => {
-  return <div className={`${year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"} z-10 h-[50px] w-[4px] self-center`} />
+  return (
+    <div
+      className={`${year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"} z-10 h-10 w-1 self-center sm:h-[50px] sm:w-[4px]`}
+    />
+  )
 }
 
 const StudentProgress: React.FC<IStudentProgress> = ({ year }) => {
   return (
     <>
-      <div className="flex w-full items-center justify-between">
-        <div className="flex flex-row">
+      <div className="flex w-full items-center justify-evenly ">
+        <div className="relative flex flex-row items-center">
           <ProgressCircle year={year} id={1} />
           <HorizontalLine year={year} id={1} />
           <ProgressCircle year={year} id={2} />
