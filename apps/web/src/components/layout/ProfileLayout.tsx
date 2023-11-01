7143ce85723ccbfba3f70a7fd3e5b6bcb83765ca
madsab <mads.barnes@hotmail.com>
2023-11-01 20:33:14 +0100

    change padding sizes on mobile

--- apps/web/src/components/layout/ProfileLayout.tsx
+++ apps/web/src/components/layout/ProfileLayout.tsx
@@ -26,14 +26,14 @@ const ProfileLayout: FC<PropsWithChildren> = ({ children }) => {
   const [editMode, setEditMode] = useState(false)
 
   return (
-    <div className="m-x-auto shadow-slate-6 relative mx-3 mb-5 max-w-[1000px] rounded-3xl px-6 shadow-lg max-md:w-[90vw] ">
+    <div className="m-x-auto shadow-slate-6 relative mb-5 max-w-[1000px] rounded-3xl px-4 shadow-lg max-md:w-[90vw] sm:mx-3  sm:px-6 ">
       <div className="md:flex">
         <MobileMenuContainer />
         <ProfileMenuContainer />
         <ProfileContext.Provider value={{ editMode, setEditMode }}>
           <div className="md:mx-5 md:mt-16 md:w-[550px]">
             {currentLink && <PageTitle title={currentLink?.title} icon={currentLink.icon} />}
-            <div className="my-2 ml-5 ">{children}</div>
+            <div className="my-2 sm:ml-5 ">{children}</div>
           </div>
         </ProfileContext.Provider>
       </div>
