2d5de0be2235e9cff64b0c81e68138111ef96a08
Njål Ingersønn Sørland <55444303+NjaalSoerland@users.noreply.github.com>
2023-11-02 18:09:49 +0100

    Add study years to users (#699)
    Adds study years as an integer from -1 to 6 associated with all types of
    membership a user can have.
    Adds ability to edit study years for users through the dashboard

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -4,6 +4,7 @@ export const UserSchema = z.object({
   id: z.string(),
   createdAt: z.date(),
   cognitoSub: z.string().uuid(),
+  studyYear: z.number().int().min(-1).max(6),
 })
 
 export type UserId = User["id"]
@@ -15,3 +16,30 @@ export const UserWriteSchema = UserSchema.omit({
 })
 
 export type UserWrite = z.infer<typeof UserWriteSchema>
+
+export interface StudyYears {
+  [-1]: string
+  0: string
+  1: string
+  2: string
+  3: string
+  4: string
+  5: string
+  6: string
+}
+
+export const StudyYearAliases = {
+  [-1]: "Ingen medlemskap",
+  [0]: "Sosialt medlem",
+  [1]: "1. klasse",
+  [2]: "2. klasse",
+  [3]: "3. klasse",
+  [4]: "4. klasse",
+  [5]: "5. klasse",
+  [6]: "PhD",
+} as StudyYears
+
+export const studyYearOptions = Object.entries(StudyYearAliases).map(([value, label]) => ({
+  value: parseInt(value),
+  label,
+}))
