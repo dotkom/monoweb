02606d65cd05b7792d3bc3135000b46a7622ebf5
madsab <mads.barnes@hotmail.com>
2023-11-04 19:50:32 +0100

    responsive layout

--- apps/web/src/components/layout/ProfileLayout.tsx
+++ apps/web/src/components/layout/ProfileLayout.tsx
@@ -1,21 +1,24 @@
-import { type FC, type PropsWithChildren, useState } from "react"
+import { FC, PropsWithChildren, useState } from "react"
+import ProfileMenuContainer from "../organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer"
+import { ProfileContext } from "../views/ProfileView/context/ProfileContext"
 import { Icon } from "@dotkomonline/ui"
 import { usePathname } from "next/navigation"
 import { profileItems } from "@/utils/profileLinks"
-import { ProfileContext } from "../views/ProfileView/context/ProfileContext"
-import ProfileMenuContainer from "../organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer"
+import MobileMenuContainer from "../organisms/Navbar/components/profile/ProfileMenu/MobileMenuContainer"
 
 interface PageTitleProps {
   title: string
   icon: string
 }
 
-const PageTitle: FC<PageTitleProps> = ({ title, icon }) => (
-  <div className="flex h-10 space-x-2">
-    <Icon icon={icon} width={"w-10"} />
-    <p className="text-3xl">{title}</p>
-  </div>
-)
+const PageTitle: FC<PageTitleProps> = ({ title, icon }) => {
+  return (
+    <div className="flex h-10 space-x-2 max-md:hidden">
+      <Icon icon={icon} width={"w-10"} />
+      <p className="text-3xl">{title}</p>
+    </div>
+  )
+}
 
 const ProfileLayout: FC<PropsWithChildren> = ({ children }) => {
   const currentSlug = usePathname()
@@ -23,17 +26,16 @@ const ProfileLayout: FC<PropsWithChildren> = ({ children }) => {
   const [editMode, setEditMode] = useState(false)
 
   return (
-    <div className="m-x-auto mb-5 max-w-[1000px]">
-      <div className="shadow-slate-6 rounded-3xl px-6 shadow-lg">
-        <div className="flex w-full flex-row">
-          <ProfileMenuContainer />
-          <ProfileContext.Provider value={{ editMode, setEditMode }}>
-            <div className="mx-5 mt-16 md:min-w-[600px]">
-              {currentLink && <PageTitle title={currentLink?.title} icon={currentLink.icon} />}
-              <div className="my-2 ml-5">{children}</div>
-            </div>
-          </ProfileContext.Provider>
-        </div>
+    <div className="m-x-auto shadow-slate-6 relative mb-5 max-w-[1000px] rounded-3xl p-2 shadow-lg max-md:w-[90vw] md:mx-3 ">
+      <div className="md:flex">
+        <MobileMenuContainer />
+        <ProfileMenuContainer />
+        <ProfileContext.Provider value={{ editMode, setEditMode }}>
+          <div className="md:mx-5 md:mt-16 md:w-[550px]">
+            {currentLink && <PageTitle title={currentLink?.title} icon={currentLink.icon} />}
+            <div className="my-2 md:ml-5 ">{children}</div>
+          </div>
+        </ProfileContext.Provider>
       </div>
     </div>
   )
