62fbdd8a076f816dc538c94827e1d6b3653911b2
madsab <mads.barnes@hotmail.com>
2023-11-04 22:34:11 +0100

    fix: close Popover after clicked Menu Link

--- apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/MobileMenuContainer.tsx
+++ apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/MobileMenuContainer.tsx
@@ -20,7 +20,7 @@ const MobileMenuContainer = () => {
 
   return (
     <div className="mx-auto flex items-center md:hidden">
-      <Popover.Root onOpenChange={(val) => setOpen(val)}>
+      <Popover.Root open={open} onOpenChange={(val: boolean) => setOpen(val)}>
         <Popover.Trigger>
           <div className="mt-3 flex">
             <span className="float-left">
@@ -31,16 +31,18 @@ const MobileMenuContainer = () => {
               )}
             </span>
             <p className="flex grow justify-center">
-              <Icon icon={currentLink?.icon ? currentLink.icon : ""} width={28} />
+              <Icon icon={currentLink?.icon && currentLink.icon} width={28} />
               <span className="ml-2 mt-1 text-lg">{currentLink?.title}</span>
             </p>
           </div>
         </Popover.Trigger>
         <Popover.Portal>
           <Popover.Content className="animate-in fade-in-10">
-            <div className="bg-indigo-1 shadow-slate-8 w-screen rounded-lg p-3 shadow-sm md:hidden">
+            <div className="bg-indigo-1 shadow-slate-8 flex w-screen flex-col rounded-lg p-3 shadow-sm md:hidden">
               {profileItems.map((item) => (
-                <ProfileMenuItem key={item.title} menuItem={item} />
+                <Popover.Close>
+                  <ProfileMenuItem key={item.title} menuItem={item} />
+                </Popover.Close>
               ))}
             </div>
           </Popover.Content>
