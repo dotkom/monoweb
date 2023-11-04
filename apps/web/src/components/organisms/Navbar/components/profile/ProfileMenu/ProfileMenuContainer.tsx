9b3cd83879f9b0861a622d5d04800caf64060c33
madsab <mads.barnes@hotmail.com>
2023-11-04 19:50:43 +0100

    hide on smaller screens

--- apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer.tsx
+++ apps/web/src/components/organisms/Navbar/components/profile/ProfileMenu/ProfileMenuContainer.tsx
@@ -1,12 +1,14 @@
 import { profileItems } from "@/utils/profileLinks"
 import ProfileMenuItem from "./ProfileMenuItem"
 
-const ProfileMenuContainer = () => (
-  <div className=" border-slate-5 border-r-[1px] pr-5 pt-10 ">
-    {profileItems.map((item) => (
-      <ProfileMenuItem key={item.title} menuItem={item} />
-    ))}
-  </div>
-)
+const ProfileMenuContainer = () => {
+  return (
+    <div className=" border-slate-5 pr-5 pt-10 max-md:hidden md:border-r-[1px]">
+      {profileItems.map((item) => (
+        <ProfileMenuItem key={item.title} menuItem={item} />
+      ))}
+    </div>
+  )
+}
 
 export default ProfileMenuContainer
