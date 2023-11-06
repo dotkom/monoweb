e20477c7f0d40930b5142034ea99ad7bfda96115
Mats Larsen <mats@jun.codes>
2023-11-06 11:05:29 +0100

    remove

--- packages/pgx-ulid/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-[ ! -f v0.1.3.tar.gz ] && curl -LO https://github.com/pksunkara/pgx_ulid/archive/refs/tags/v0.1.3.tar.gz
-tar -xvf v0.1.3.tar.gz
-
-patch pgx_ulid-0.1.3/Dockerfile 0001-dockerfile.patch
-
-BUILDKIT_PROGRESS=plain docker build --build-arg PG_MAJOR=15 -f pgx_ulid-0.1.3/Dockerfile -t pgx_ulid:0.1.3 pgx_ulid-0.1.3
