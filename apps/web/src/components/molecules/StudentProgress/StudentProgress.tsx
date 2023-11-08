cb3ac6b44246a52b6a52a6f336fde7ca2036f8a9
Mads <113549555+madsab@users.noreply.github.com>
2023-11-08 10:13:02 +0100

    Make profile view responsive (#704)
    Closes: #...
    
    ## Checklist
    
    - [x] I have made ProfileLayout responsive
    - [x] I have made ProfileLanding responsive
    
    ## Changelog
    
    - made ProfileLanding responsive
    - made ProfileLayout responsive
    - added resized StudentProgress for smaller screens
    - added MobileMenuContainer for displaying profile menu links for
    smaller screens
    
    ## How to test
    - Run monoweb
    - Navigate to <code>/profile</code>
    - Resize window

--- apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
+++ apps/web/src/components/molecules/StudentProgress/StudentProgress.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@dotkomonline/ui"
+
 interface IStudentProgressComponent {
   year: number
   id: number
@@ -9,30 +11,41 @@ interface IStudentProgress {
 
 const ProgressCircle: React.FC<IStudentProgressComponent> = ({ year, id }) => (
   <div
-    className={`${year <= id ? "border-[#153e75]" : "border-[#36b37e]"}
-      flex h-[45px] w-[45px] items-center  justify-around rounded-[50%] border-[4px] border-solid`}
+    className={cn(
+      year <= id ? "border-[#153e75]" : "border-[#36b37e]",
+      "flex h-[45px] w-[45px] items-center justify-around rounded-[50%]  border-[4px] border-solid max-sm:h-[30px] max-sm:w-[30px]"
+    )}
   >
     <div
-      className={`${year < id ? "text-[#153e75]" : year === id ? "bg-[#153e75] text-white" : "bg-[#36b37e] text-white"}
-          h-[20px] w-[20px] rounded-[50%] text-center`}
+      className={cn(
+        year < id ? "text-[#153e75]" : year === id ? "bg-[#153e75] text-white" : "bg-[#36b37e] text-white",
+        "flex h-[20px] w-[20px] justify-center rounded-[50%] max-sm:h-[10px] max-sm:w-[10px] max-sm:items-center max-sm:p-2 max-sm:text-center"
+      )}
     >
-      {id}
+      <p className="max-sm:text-[1rem]">{id}</p>
     </div>
   </div>
 )
 
 const HorizontalLine: React.FC<IStudentProgressComponent> = ({ year, id }) => (
-  <div className={`${year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"} m-[-1px] h-[4px] w-[30px] self-center`} />
+  <div
+    className={cn(
+      year <= id ? "bg-[#153e75]" : "bg-[#36b37e]",
+      "m-[-1px] h-[4px] w-[30px] self-center max-sm:w-[20px]"
+    )}
+  />
 )
 
 const VerticalLine: React.FC<IStudentProgressComponent> = ({ year, id }) => (
-  <div className={`${year <= id ? "bg-[#153e75]" : "bg-[#36b37e]"} z-10 h-[50px] w-[4px] self-center`} />
+  <div
+    className={cn(year <= id ? "bg-[#153e75]" : "bg-[#36b37e]", "z-10 h-10 w-1 self-center sm:h-[50px] sm:w-[4px]")}
+  />
 )
 
 const StudentProgress: React.FC<IStudentProgress> = ({ year }) => (
   <>
-    <div className="flex w-full items-center justify-between">
-      <div className="flex flex-row">
+    <div className="justify-evenl flex items-center">
+      <div className="relative flex flex-row items-center">
         <ProgressCircle year={year} id={1} />
         <HorizontalLine year={year} id={1} />
         <ProgressCircle year={year} id={2} />
