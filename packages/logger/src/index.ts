086bc3644d09ed87f309fcf8a07551b847db14fd
Mats Larsen <mats@jun.codes>
2023-11-11 11:57:11 +0100

    Create MVP version of deploy tool

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,11 +1,10 @@
 import winston, { format } from "winston"
-import { env } from "@dotkomonline/env"
 export type { Logger } from "winston"
 
 export const getLogger = (path: string) =>
   winston.createLogger({
     level: "info",
-    silent: env.NODE_ENV === "test",
+    silent: process.env.NODE_ENV === "test",
     format: format.json(),
     transports: [
       new winston.transports.Console({
---
+++ tools/ship/src/aws.ts
@@ -0,0 +1,31 @@
+import { ECRClient, GetAuthorizationTokenCommand } from "@aws-sdk/client-ecr"
+import invariant from "tiny-invariant"
+import { LambdaClient } from "@aws-sdk/client-lambda"
+
+export interface ClientContext {
+  ecr: ECRClient
+  lambda: LambdaClient
+}
+
+export const createClientContext = async (): Promise<ClientContext> => ({
+  ecr: new ECRClient({ region: "eu-north-1" }),
+  lambda: new LambdaClient({ region: "eu-north-1" }),
+})
+
+export type ECRCredential = Awaited<ReturnType<typeof getEcrAuthorizationToken>>
+
+export const getEcrAuthorizationToken = async (client: ClientContext) => {
+  const cmd = new GetAuthorizationTokenCommand({})
+  const response = await client.ecr.send(cmd)
+
+  invariant(response.authorizationData !== undefined, "Expected authorization data not to be undefined")
+  invariant(response.authorizationData.length !== 0, "Expected authorization data to include at least one credential")
+
+  const token = response.authorizationData[0].authorizationToken
+  const endpoint = response.authorizationData[0].proxyEndpoint
+
+  invariant(token !== undefined, "Token must not be undefined")
+  invariant(endpoint !== undefined, "Proxy endpoint must not be undefined")
+
+  return { token, endpoint }
+}
