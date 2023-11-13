50d986988a488007ea3eaf2a4f2c70b4c7d09755
Mats Larsen <mats@jun.codes>
2023-11-13 11:40:34 +0100

    Add type check scripts to all packages

--- apps/dashboard/src/app/(dashboard)/user/[id]/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/user/[id]/layout.tsx
@@ -2,8 +2,8 @@
 
 import { type PropsWithChildren } from "react"
 import { Loader } from "@mantine/core"
-import { useUserGetQuery } from "src/modules/user/queries/use-user-get-query"
 import { UserDetailsContext } from "./provider"
+import { useUserGetQuery } from "../../../../modules/user/queries/use-user-get-query"
 
 export default function UserDetailsLayout({ children, params }: PropsWithChildren<{ params: { id: string } }>) {
   const { user, isLoading } = useUserGetQuery(params.id)
--- apps/dashboard/src/app/ModalProvider.tsx
+++ apps/dashboard/src/app/ModalProvider.tsx
@@ -2,12 +2,12 @@
 
 import { ModalsProvider } from "@mantine/modals"
 import { type FC, type PropsWithChildren } from "react"
-import { CreateCompanyModal } from "src/modules/company/modals/create-company-modal"
-import { CreateEventModal } from "src/modules/event/modals/create-event-modal"
-import { CreateJobListingModal } from "src/modules/job-listing/modals/create-job-listing-modal"
 import { CreateOfflineModal } from "../modules/offline/modals/create-offline-modal"
 import { CreateEventExtrasModal } from "../modules/event/modals/create-event-extras-modal"
 import { UpdateEventExtrasModal } from "../modules/event/modals/edit-event-extras-modal"
+import { CreateEventModal } from "../modules/event/modals/create-event-modal"
+import { CreateJobListingModal } from "../modules/job-listing/modals/create-job-listing-modal"
+import { CreateCompanyModal } from "../modules/company/modals/create-company-modal"
 
 const modals = {
   "event/create": CreateEventModal,
