abae33d4dd35d2150b3b0c3b0ce9fea3c141217c
Hanna Lunne <hanlun0804@outlook.com>
2023-11-15 19:06:10 +0100

    Delete unused files

--- apps/web/src/components/views/ProfileView/components/ProfilePermissionsOption.tsx
+++
@@ -1,9 +0,0 @@
-import { Toggle } from "@dotkomonline/ui"
-
-const PermissionOption = () => (
-  <div className="col-span-1 ml-auto mr-auto mt-1">
-    <Toggle />
-  </div>
-)
-
-export default PermissionOption
