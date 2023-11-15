46307bc2607e9c570be88325cb67e45a5a3e26da
Mats Larsen <mats@jun.codes>
2023-11-15 19:26:30 +0100

    Deploy VPC with internet gateway

---
+++ infra/modules/aws-alb/variables.tf
@@ -0,0 +1,4 @@
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = object({})
+}
---
+++ infra/modules/aws-vpc/README.md
@@ -0,0 +1,3 @@
+# Terraform Module AWS VPC
+
+Provisions an AWS Virtual Private Cloud network
