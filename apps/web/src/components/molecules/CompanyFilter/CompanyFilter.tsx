7e11236e75a1eb1f424ddaaee547f9ffbed1d16e
Erlend L Saeveraas <erlenlsa@stud.ntnu.no>
2024-01-21 21:10:36 +0100

    refactor: Removed unnecessary components from filter
    shorted down to make it clear that only the jobselection was used in the companyfilter file

--- apps/web/src/components/molecules/CompanyFilter/CompanyFilter.tsx
+++ apps/web/src/components/molecules/CompanyFilter/CompanyFilter.tsx
@@ -3,35 +3,29 @@ import { FC } from "react"
 import { FilterItems } from "./filters"
 export interface CompanyFilter {}
 
-const Filter: FC<{ name: string }> = (name) => {
-  return (
-    <div className="flex-col rounded-md py-2 ">
-      <input className="mr-2 align-middle" type="checkbox"></input>
-      <div className=" inline-block text-base">{name.name}</div>
-    </div>
-  )
-}
 const FilterGrid: FC<{ filters: string[] }> = (filters) => {
   return (
     <div>
       {filters.filters.map((content) => (
-        <Filter name={content} />
+        <div className="flex-col rounded-md py-2 ">
+        <input className="mr-2 align-middle" type="checkbox"></input>
+        <div className=" inline-block text-base">{content}</div>
+      </div>
       ))}
     </div>
   )
 }
-export const CompanyFilter: FC<{ filterContent: FilterItems[] }> = (filterContent) => {
+//Shows the filter
+export const CompanyFilter: FC<{ jobTypes: string[] }> = (jobtypes) => {
   return (
     <div>
-      {filterContent.filterContent.map((content) => (
         <div className="mx-4">
-          <p className="mt-4 font-semibold">{content.title}</p>
+          <p className="mt-4 font-semibold">Jobbtyper</p>
           <div>
-            <FilterGrid filters={content.items} />
+            <FilterGrid filters={jobtypes.jobTypes} />
           </div>
         </div>
-      ))}
-      <div className="border-slate-10 border-b-2" />
+      <div className="border-slate-10 border-b-[1px]" />
     </div>
   )
 }
--- apps/web/src/components/molecules/CompanyFiltersContainer/CompanyFiltersContainer.tsx
+++ apps/web/src/components/molecules/CompanyFiltersContainer/CompanyFiltersContainer.tsx
@@ -13,25 +13,20 @@ import {
 
 export interface CompanyFiltersContainer {}
 
-const filterJobs: FilterItems[] = [
-  {
-    title: "Jobbtyper",
-    items: ["Sommerjobb", "Fulltidsjobb", "Deltidsjobb", "Graduate"],
-  },
-]
+const jobTypes = ["Sommerjobb", "Fulltidsjobb", "Deltidsjobb", "Graduate"]
 const places = ["Oslo", "Bergen", "Trondheim", "Annet"]
 
 const CompanyFiltersContainer: FC<CompanyFiltersContainer> = (props: CompanyFiltersContainer) => {
   return (
-    <div className="border-slate-9 w-72 rounded-lg border-2 shadow-md">
-      <div className="border-slate-10 flex flex-row justify-between border-b-2 py-4">
+    <div className="border-slate-9 border-[1px] w-72 rounded-lg shadow-md">
+      <div className="border-slate-10 flex flex-row justify-between border-b-[1px] py-4">
         <h4 className="mx-4 my-auto align-middle">Filter</h4>
         <button className="text-red-9 mx-4 my-auto font-semibold">Reset</button>
       </div>
-      <CompanyFilter filterContent={filterJobs} />
-      <div className="border-slate-10 mx-4 mb-4 border-b-2">
+      <CompanyFilter jobTypes={jobTypes} />
+      <div className="border-slate-10 mx-4 mb-4 border-b-[1px]">
         <p className=" mt-4 font-semibold ">Sted</p>
-        <select className="border-slate-10 radius h-10 w-full rounded-md border-2" name="places">
+        <select className="border-slate-10 radius h-10 w-full rounded-md border-[1px]" name="places">
           {places.map((place, key) => {
             return <option key={key}>{place}</option>
           })}
