59b42b4366b9559fee76a8ba574bcfc219cfd47e
Erlend Løken Sæveraas <erlenlsa@stud.ntnu.no>
2024-01-21 20:46:07 +0100

    feat: Added select
    Added select

--- apps/web/src/components/molecules/CompanyFilter/CompanyFilter.tsx
+++ apps/web/src/components/molecules/CompanyFilter/CompanyFilter.tsx
@@ -1,43 +1,37 @@
 import { FC } from "react"
 
-import { FilterItems } from "./filters";
-export interface CompanyFilter {
+import { FilterItems } from "./filters"
+export interface CompanyFilter {}
 
-}
-
-const Filter: FC<{name: string}> = ( name ) =>{
-    return (
-        <div className="p-4 rounded-md flex-col ">
-            <input className="mr-2 align-middle" type="checkbox"></input>
-            <div className=" inline-block text-base">
-                {name.name}
-            </div>
-        </div>
-    );
-
-}
-const FilterGrid: FC<{filters: string[]}> = ( filters ) =>{
-    return (<div>
-        {filters.filters.map((content)=>(
-            <Filter name={content} />
-        ))}
-    </div>);
-
-}
-export const CompanyFilter: FC< {filterContent: FilterItems[]} > = ( filterContent ) => {
+const Filter: FC<{ name: string }> = (name) => {
   return (
-  <div className="px-4">
+    <div className="flex-col rounded-md py-2 ">
+      <input className="mr-2 align-middle" type="checkbox"></input>
+      <div className=" inline-block text-base">{name.name}</div>
+    </div>
+  )
+}
+const FilterGrid: FC<{ filters: string[] }> = (filters) => {
+  return (
+    <div>
+      {filters.filters.map((content) => (
+        <Filter name={content} />
+      ))}
+    </div>
+  )
+}
+export const CompanyFilter: FC<{ filterContent: FilterItems[] }> = (filterContent) => {
+  return (
+    <div>
       {filterContent.filterContent.map((content) => (
+        <div className="mx-4">
+          <p className="mt-4 font-semibold">{content.title}</p>
           <div>
-            <p className="my-4 font-semibold">
-            {content.title}
-            </p>
-            <div>
-                <FilterGrid filters={content.items} />
-            </div>
+            <FilterGrid filters={content.items} />
           </div>
-        ))}
-  </div>
-  );
-
+        </div>
+      ))}
+      <div className="border-slate-10 border-b-2" />
+    </div>
+  )
 }
