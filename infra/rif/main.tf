41d5e14a6fc2b3e0b0a4d8d6b45f5411a8e5d40b
Mats Larsen <mats@jun.codes>
2024-01-28 20:35:19 +0100

    Deploy to production

--- infra/rif/main.tf
+++ infra/rif/main.tf
@@ -1,12 +1,12 @@
 locals {
-  rif_domain_name = "${terraform.workspace}.interesse.online.ntnu.no"
+  rif_domain_name = terraform.workspace == "dev" ? "${terraform.workspace}.interesse.online.ntnu.no" : "interesse.online.ntnu.no"
   zone_id         = data.aws_route53_zone.online.zone_id
 }
 
 module "rif_vercel_project" {
   source = "../modules/vercel-application"
 
-  project_name   = "rif"
+  project_name   = "rif-${terraform.workspace}"
   domain_name    = local.rif_domain_name
   zone_id        = local.zone_id
   build_command  = "cd ../.. && pnpm build:rif"
