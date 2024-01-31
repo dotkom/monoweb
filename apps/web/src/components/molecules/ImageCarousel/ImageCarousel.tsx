c5bcc2aac643691837f9966dd57c95d03c04695b
Hanna Lunne <hanlun0804@outlook.com>
2024-01-31 20:07:38 +0100

    Add simple logic for scrolling between elements

--- apps/web/src/components/molecules/ImageCarousel/ImageCarousel.tsx
+++ apps/web/src/components/molecules/ImageCarousel/ImageCarousel.tsx
@@ -1,21 +1,31 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 
 interface ImageCarouselProps{
-    images: string
+    images: string[];
 }
-const ImageCarousel: FC<ImageCarouselProps> = (props) => {
+const ImageCarousel: FC<ImageCarouselProps> = ({images}) => {
+    const [startIndex, setStartIndex] = useState(0);
+
+    const handleLeftButtonClick = () => {
+        const newIndex = Math.max(0, startIndex - 5);
+        setStartIndex(newIndex);
+    };
+    const handleRightButtonClick = () => {
+        const newIndex = Math.min(startIndex + 5, images.length - 5);
+        setStartIndex(newIndex);
+    };
+
+    const visibleImages = images.slice(startIndex, startIndex + 5);
     return <div>
     <p>OFFLINE</p>
     <div className="flex flex-row space-x-8 ">
-        <button>&lt;</button>
+        <button onClick={handleLeftButtonClick}>&lt;</button>
         <div className="flex flex-row space-x-8 ">
-            <div className="bg-blue-11 w-12 h-20">1</div>
-            <div className="bg-blue-11 w-12 h-20">2</div>
-            <div className="bg-blue-11 w-12 h-20">3</div>
-            <div className="bg-blue-11 w-12 h-20">4</div>
-            <div className="bg-blue-11 w-12 h-20">5</div>
+            {visibleImages.map((image, index) => (
+                <div key={index} className="bg-blue-11 w-12 h-20">{image}</div>
+            ))}
         </div>
-        <button>></button>
+        <button onClick={handleRightButtonClick}>&gt;</button>
     </div>
   </div>
 } 
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <div>
       <p>Homepage</p>
       <pre>{JSON.stringify(auth, null, 2)}</pre>
-      <ImageCarousel images="String"/>
+      <ImageCarousel images={["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]} />
     </div>
   )
 }
