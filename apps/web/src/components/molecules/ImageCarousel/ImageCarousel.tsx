018d1a401d217df12023ae8ed28763ec6fc9f094
Hanna Lunne <hanlun0804@outlook.com>
2024-01-31 19:37:20 +0100

    Add barebones setup for partner to work on

--- apps/web/src/components/molecules/ImageCarousel/ImageCarousel.tsx
+++ apps/web/src/components/molecules/ImageCarousel/ImageCarousel.tsx
@@ -4,7 +4,20 @@ interface ImageCarouselProps{
     images: string
 }
 const ImageCarousel: FC<ImageCarouselProps> = (props) => {
-    return <div className=" bg-red-8 rounded-[31px] w-full">ghgf{props.images}</div>
+    return <div>
+    <p>OFFLINE</p>
+    <div className="flex flex-row space-x-8 ">
+        <button>&lt;</button>
+        <div className="flex flex-row space-x-8 ">
+            <div className="bg-blue-11 w-12 h-20">1</div>
+            <div className="bg-blue-11 w-12 h-20">2</div>
+            <div className="bg-blue-11 w-12 h-20">3</div>
+            <div className="bg-blue-11 w-12 h-20">4</div>
+            <div className="bg-blue-11 w-12 h-20">5</div>
+        </div>
+        <button>></button>
+    </div>
+  </div>
 } 
 
 export default ImageCarousel
