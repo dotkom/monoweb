b0cac32c83fae75e591c94b207a562227376f715
Mats Larsen <mats@jun.codes>
2024-02-07 14:48:51 +0100

    Add google spreadsheet integration to interest form

---
+++ apps/rif/src/email.ts
@@ -0,0 +1,54 @@
+import { type FormSchema } from "./app/form-schema"
+
+const endpoint = process.env.EMAIL_ENDPOINT ?? "https://dev.gateway.online.ntnu.no/integrations/email"
+const token = process.env.EMAIL_TOKEN ?? "__NO_TOKEN_PROVIDED__"
+
+export const deliverConfirmationEmail = async (form: FormSchema) => {
+  const response = await fetch(endpoint, {
+    headers: {
+      "X-Email-Token": token,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      template: "interest-form-for-bedkom",
+      source: "bedkom@online.ntnu.no",
+      to: ["mats.jun.larsen@online.ntnu.no"],
+      cc: [],
+      bcc: [],
+      subject: `[Interesse] ${form.companyName}`,
+      replyTo: [form.contactEmail],
+      arguments: {
+        ...form,
+      },
+    }),
+  })
+  console.info(`Sent request to email endpoint, received ${response.status} (${await response.text()})`)
+  return response
+}
+
+export const deliverNotificationEmail = async (form: FormSchema) => {
+  const response = await fetch(endpoint, {
+    headers: {
+      "X-Email-Token": token,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      template: "interest-form-for-company",
+      source: "bedkom@online.ntnu.no",
+      to: [form.contactEmail],
+      cc: [],
+      bcc: [],
+      subject: `Kvittering for meldt interesse til Online`,
+      replyTo: ["bedkom@online.ntnu.no"],
+      arguments: {
+        ...form,
+      },
+    }),
+  })
+  console.info(`Sent confirmation to email endpoint, received ${response.status} (${await response.text()})`)
+  return response
+}
