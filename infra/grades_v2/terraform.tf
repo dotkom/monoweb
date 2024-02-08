76edc3f1061f2a386e60aa4185fa2c88c34bf97e
Mats Jun <mats@jun.codes>
2024-02-08 16:55:49 +0100

    Create postgres database for Grades v2 (#793)
    Contains only a database for now

---
+++ infra/grades_v2/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket = "monoweb-terraform"
+    key    = "grades_v2.tfstate"
+    region = "eu-north-1"
+  }
+
+  required_version = "~> 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+    neon = {
+      source  = "dotkom/neon"
+      version = "~> 0.1.1"
+    }
+  }
+}
+
+locals {
+  valid_workspaces = {
+    dev = 1
+    prd = 1
+  }
+  valid_workspaces_current = local.valid_workspaces[terraform.workspace]
+}
