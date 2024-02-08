ce96fb545f91f081b68a015e332fa794733e7378
Mats Larsen <mats@jun.codes>
2024-02-08 13:34:36 +0100

    Deploy web application

--- infra/grades-lightsail/main.tf
+++ infra/grades-lightsail/main.tf
@@ -32,3 +32,33 @@ module "gradestats_server" {
     Environment = terraform.workspace
   }
 }
+
+module "gradestats_web_certificate" {
+  source = "../modules/aws-lightsail-certificate"
+
+  certificate_name   = "grades-web-${terraform.workspace}"
+  public_domain_name = local.app_public_domain_name
+  tags = {
+    Project     = "grades-lightsail"
+    Environment = terraform.workspace
+  }
+}
+
+module "gradestats_web" {
+  source = "../modules/aws-lightsail-container-service"
+
+  dns_zone_id           = data.aws_route53_zone.grades.zone_id
+  public_domain_name    = local.app_public_domain_name
+  service_name          = "gradestats-web-${terraform.workspace}"
+  environment_variables = data.doppler_secrets.grades.map
+  image_tag             = "0.1.1"
+
+  certificate_domain_validation_options = module.gradestats_web_certificate.certificate_domain_validation_options
+  certificate_name                      = module.gradestats_web_certificate.certificate_name
+
+  container_port = 3000
+  tags = {
+    Project     = "grades-lightsail"
+    Environment = terraform.workspace
+  }
+}
