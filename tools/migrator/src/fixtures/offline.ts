37de27796e5f1afc2e511a25d87297396261c8a7
henrikskog <henrikskog01@gmail.com>
2024-02-14 15:49:48 +0100

    offline fixtures

--- tools/migrator/src/fixtures/offline.ts
+++ tools/migrator/src/fixtures/offline.ts
@@ -3,17 +3,39 @@ import { type Insertable } from "kysely"
 
 export const offlines: Insertable<Database["offline"]>[] = [
   {
-    id: "01HD77R4Y4S3WJ44NZ8029VP4P",
     title: "Offline #1",
     published: new Date("2023-10-09T10:00:00+02:00"),
-    fileUrl: null,
-    imageUrl: null,
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/1-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%231-Image.png",
   },
   {
-    id: "01HD77R4Y4S3WJ44NZ8029VP4C",
     title: "Offline #2",
     published: new Date("2023-10-09T10:00:00+02:00"),
-    fileUrl: null,
-    imageUrl: null,
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/2-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%232-Image.png",
+  },
+  {
+    title: "Offline #3",
+    published: new Date("2023-10-09T10:00:00+02:00"),
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/3-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%233-Image.png",
+  },
+  {
+    title: "Offline #4",
+    published: new Date("2023-10-09T10:00:00+02:00"),
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/4-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%234-Image.png",
+  },
+  {
+    title: "Offline #5",
+    published: new Date("2023-10-09T10:00:00+02:00"),
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/5-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%235-Image.png",
+  },
+  {
+    title: "Offline #6",
+    published: new Date("2023-10-09T10:00:00+02:00"),
+    fileUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/6-offline",
+    imageUrl: "https://s3.eu-north-1.amazonaws.com/dev.cdn.online.ntnu.no/offlines/Offline-%236-Image.png",
   },
 ]
