17a6b9fc3b6c88637c8541f0644617f2130668ef
Mats Jun <mats@jun.codes>
2024-02-21 20:04:09 +0100

    Remove AWS Cognito remnants (#813)

--- infra/modules/aws-cognito-client/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "client_id" {
-  value = aws_cognito_user_pool_client.this.id
-}
-
-output "client_secret" {
-  value = aws_cognito_user_pool_client.this.client_secret
-}
--- infra/web/web.tf
+++ infra/web/web.tf
@@ -1,13 +1,4 @@
 locals {
-  web_callback_urls = concat(
-    ["https://${terraform.workspace}.web.online.ntnu.no/api/auth/callback/cognito"],
-    terraform.workspace == "dev"
-    ? ["http://localhost:3000/api/auth/callback/cognito"]
-    : [],
-    terraform.workspace == "prd"
-    ? ["https://online.ntnu.no/api/auth/callback/cognito"]
-    : []
-  )
   web_project_name = "web-${terraform.workspace}"
 }
 
@@ -16,12 +7,4 @@ module "web_database" {
 
   project_name = local.web_project_name
   role_name    = "web"
-}
-
-module "web_cognito_client" {
-  source = "../modules/aws-cognito-client"
-
-  user_pool_id  = data.aws_cognito_user_pools.user_pools.ids[0]
-  client_name   = "web"
-  callback_urls = local.web_callback_urls
 }
