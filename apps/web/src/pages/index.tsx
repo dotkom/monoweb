fecf54d451ee2bdf973c5057c9b528fdcb4beed1
Mats Larsen <mats@jun.codes>
2024-02-21 20:52:08 +0100

    Revert "Merge branch 'main' of https://github.com/dotkom/monoweb"
    This reverts commit f1d9a64d4b8158a59a8749ddde56b92779bf2775, reversing
    changes made to 74a517f28950bfb7853caecd80f189144bffcc1d.

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { useSession } from "next-auth/react"
-import ImageCarousel from "@/components/molecules/ImageCarousel/ImageCarousel"
 
 const Home: React.FC = () => {
   const auth = useSession()
@@ -8,8 +7,6 @@ const Home: React.FC = () => {
     <div>
       <p>Homepage</p>
       <pre>{JSON.stringify(auth, null, 2)}</pre>
-      <ImageCarousel images="hei" />
-      hei
     </div>
   )
 }
