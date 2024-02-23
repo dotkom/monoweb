135e243a772b4c2a4cea00ae30501189823701cd
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-23 18:51:43 +0100

    Add links to icons in footer (#721)
    Co-authored-by: Mats Jun <mats@jun.codes>
    Co-authored-by: Anhkha Vo <hey@akvo.no>

--- apps/web/src/components/organisms/Footer/sections/SoMeSection.tsx
+++ apps/web/src/components/organisms/Footer/sections/SoMeSection.tsx
@@ -1,19 +1,22 @@
+import Link from "next/link"
 import { FacebookIcon, GitHubIcon, InstagramIcon, SlackIcon } from "../../../icons"
 
 export const SoMeSection = () => {
-  const icons = [
-    <SlackIcon key="slack" />,
-    <GitHubIcon key="github" />,
-    <InstagramIcon key="instagram" />,
-    <FacebookIcon key="facebook" />,
+  const links = [
+    { icon: <SlackIcon key="slack" />, url: "https://onlinentnu.slack.com/" },
+    { icon: <GitHubIcon key="github" />, url: "https://github.com/dotkom/monoweb" },
+    { icon: <InstagramIcon key="instagram" />, url: "https://www.instagram.com/online_ntnu/" },
+    { icon: <FacebookIcon key="facebook" />, url: "https://www.facebook.com/groups/1547182375336132" },
   ]
 
   return (
     <ul className="mx-8 mb-4 flex sm:justify-center">
-      {icons.map((icon) => (
-        <li key={icon.key} className="mx-4 w-16 cursor-pointer">
-          {icon}
-        </li>
+      {links.map((link) => (
+        <Link href={link.url} key={link.icon.key}>
+          <li key={link.icon.key} className="mx-4 w-16 cursor-pointer">
+            {link.icon}
+          </li>
+        </Link>
       ))}
     </ul>
   )
