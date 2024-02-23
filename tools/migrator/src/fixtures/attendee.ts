1be208d5c974d5dc540aef288d7954205b6cb72f
henrikskog <henrikskog01@gmail.com>
2024-02-23 14:49:21 +0100

    remove console.log and gen db

--- tools/migrator/src/fixtures/attendee.ts
+++ tools/migrator/src/fixtures/attendee.ts
@@ -5,47 +5,44 @@ import { type ResultIds } from "../fixture"
 export const getAttendeeFixtures: (
   attendanceIds: ResultIds["attendance"],
   userIds: ResultIds["owUser"]
-) => Insertable<Database["attendee"]>[] = (attendanceIds, userIds) => {
-  console.log("attendanceIds", attendanceIds, "userIds", userIds)
-  return [
-    {
-      createdAt: new Date("2023-02-22 13:30:04.713+00"),
-      updatedAt: new Date("2023-02-22 13:30:04.713+00"),
-      userId: userIds[0],
-      attendanceId: attendanceIds[0],
-      extrasChoices: JSON.stringify([
-        {
-          id: "1",
-          choice: "1",
-        },
-        {
-          id: "2",
-          choice: "3",
-        },
-      ]),
-    },
-    {
-      createdAt: new Date("2023-02-22 13:30:04.713+00"),
-      updatedAt: new Date("2023-02-22 13:30:04.713+00"),
-      userId: userIds[1],
-      attendanceId: attendanceIds[0],
-      extrasChoices: JSON.stringify([
-        {
-          id: "1",
-          choice: "2",
-        },
-        {
-          id: "2",
-          choice: "3",
-        },
-      ]),
-    },
-    {
-      id: "01HB64JAPW4Q0XR46MK831NTB2",
-      createdAt: new Date("2023-02-22 13:30:04.713+00"),
-      updatedAt: new Date("2023-02-22 13:30:04.713+00"),
-      userId: userIds[2],
-      attendanceId: attendanceIds[0],
-    },
-  ]
-}
+) => Insertable<Database["attendee"]>[] = (attendanceIds, userIds) => [
+  {
+    createdAt: new Date("2023-02-22 13:30:04.713+00"),
+    updatedAt: new Date("2023-02-22 13:30:04.713+00"),
+    userId: userIds[0],
+    attendanceId: attendanceIds[0],
+    extrasChoices: JSON.stringify([
+      {
+        id: "1",
+        choice: "1",
+      },
+      {
+        id: "2",
+        choice: "3",
+      },
+    ]),
+  },
+  {
+    createdAt: new Date("2023-02-22 13:30:04.713+00"),
+    updatedAt: new Date("2023-02-22 13:30:04.713+00"),
+    userId: userIds[1],
+    attendanceId: attendanceIds[0],
+    extrasChoices: JSON.stringify([
+      {
+        id: "1",
+        choice: "2",
+      },
+      {
+        id: "2",
+        choice: "3",
+      },
+    ]),
+  },
+  {
+    id: "01HB64JAPW4Q0XR46MK831NTB2",
+    createdAt: new Date("2023-02-22 13:30:04.713+00"),
+    updatedAt: new Date("2023-02-22 13:30:04.713+00"),
+    userId: userIds[2],
+    attendanceId: attendanceIds[0],
+  },
+]
