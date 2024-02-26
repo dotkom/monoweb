96b9c300b2bf32156b497464477937f3155b24dc
Mats Larsen <mats@jun.codes>
2024-02-26 15:39:01 +0100

    Migrate tools/migrator to biome

--- tools/migrator/src/fixtures/personal-mark.ts
+++ tools/migrator/src/fixtures/personal-mark.ts
@@ -1,12 +1,10 @@
 import { type Database } from "@dotkomonline/db"
 import { type Insertable } from "kysely"
 
-export const getPersonalMarkFixtures: (
-  markIds: string[],
-  userIds: string[]
-) => Insertable<Database["personalMark"]>[] = (markIds, userIds) => [
-  {
-    markId: markIds[0],
-    userId: userIds[0],
-  },
-]
+export const getPersonalMarkFixtures: (markIds: string[], userIds: string[]) => Insertable<Database["personalMark"]>[] =
+  (markIds, userIds) => [
+    {
+      markId: markIds[0],
+      userId: userIds[0],
+    },
+  ]
