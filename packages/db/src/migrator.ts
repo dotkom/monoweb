76bfd3a4f9aca239bc5c1ad890b5cde107a4bfe1
Billy Barrett <91070629+BillySBarrett@users.noreply.github.com>
2024-02-29 16:51:57 +0100

    add uri decoder to migration folder path (#823)
    Migrations won't run when the migrations folder path has any URL encoded
    characters. This is a problem if the repo is inside a folder with a
    space character (definitely not me 👀)
    
    NOTE: The migration url path could still be an issue in the build when it can't find migration folder.

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -3,12 +3,21 @@ import path from "path"
 import { FileMigrationProvider, type Kysely, Migrator } from "kysely"
 import { type Database } from "./"
 
-export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) =>
-  new Migrator({
+export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) => {
+  const url = urlOverride ?? new URL("migrations", import.meta.url)
+  const folderPath = decodeURIComponent(url.pathname)
+
+  // Uncomment this if it isn't working for windows. Windows paths might start with a slash that needs to be removed.
+  //   if (process.platform === "win32") {
+  //     folderPath = folderPath.replace(/^\/(\w:)/, '$1');
+  //   }
+
+  return new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
       path,
-      migrationFolder: (urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname,
+      migrationFolder: folderPath,
     }),
   })
+}
