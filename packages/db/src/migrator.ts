6b62a8eef7ab355e64fd7a257fe3c37a3ecedc35
BillySBarrett <billysb@stud.ntnu.no>
2024-02-29 17:02:47 +0100

    Revert "add uri decoder to migration folder path (#823)"
    This reverts commit 76bfd3a4f9aca239bc5c1ad890b5cde107a4bfe1.

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -3,21 +3,12 @@ import path from "path"
 import { FileMigrationProvider, type Kysely, Migrator } from "kysely"
 import { type Database } from "./"
 
-export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) => {
-  const url = urlOverride ?? new URL("migrations", import.meta.url)
-  const folderPath = decodeURIComponent(url.pathname)
-
-  // Uncomment this if it isn't working for windows. Windows paths might start with a slash that needs to be removed.
-  //   if (process.platform === "win32") {
-  //     folderPath = folderPath.replace(/^\/(\w:)/, '$1');
-  //   }
-
-  return new Migrator({
+export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) =>
+  new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
       path,
-      migrationFolder: folderPath,
+      migrationFolder: (urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname,
     }),
   })
-}
