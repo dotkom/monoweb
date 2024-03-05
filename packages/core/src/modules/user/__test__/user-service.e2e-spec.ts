8bec5197c92f6bddb606c193d128f3e18c91be34
henrikskog <henrikskog01@gmail.com>
2024-03-05 21:29:53 +0100

    fix test after user type change

--- packages/core/src/modules/user/__test__/user-service.e2e-spec.ts
+++ packages/core/src/modules/user/__test__/user-service.e2e-spec.ts
@@ -4,6 +4,16 @@ import { ulid } from "ulid"
 import { createEnvironment } from "@dotkomonline/env"
 import { createKysely } from "@dotkomonline/db"
 import { createServiceLayer, type ServiceLayer } from "../../core"
+import { User, UserWrite } from "@dotkomonline/types"
+
+const fakeUser = (subject?: string): UserWrite => ({
+  auth0Sub: subject ?? crypto.randomUUID(),
+  studyYear: 0,
+  email: "",
+  firstName: "Test",
+  lastName: "User",
+  name: "Test User",
+})
 
 describe("users", () => {
   let core: ServiceLayer
@@ -18,10 +28,7 @@ describe("users", () => {
     const none = await core.userService.getAllUsers(100)
     expect(none).toHaveLength(0)
 
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
 
     const users = await core.userService.getAllUsers(100)
     expect(users).toContainEqual(user)
@@ -29,24 +36,15 @@ describe("users", () => {
 
   it("will not allow two users the same subject", async () => {
     const subject = crypto.randomUUID()
-    const first = await core.userService.createUser({
-      auth0Sub: subject,
-      studyYear: 0,
-    })
+    const first = await core.userService.createUser(fakeUser(subject))
     expect(first).toBeDefined()
     await expect(
-      core.userService.createUser({
-        auth0Sub: subject,
-        studyYear: 0,
-      })
+      core.userService.createUser(fakeUser(subject))
     ).rejects.toThrow()
   })
 
   it("will find users by their user id", async () => {
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
 
     const match = await core.userService.getUserById(user.id)
     expect(match).toEqual(user)
@@ -60,10 +58,7 @@ describe("users", () => {
         auth0Sub: crypto.randomUUID(),
       })
     ).rejects.toThrow()
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
     const updated = await core.userService.updateUser(user.id, {
       auth0Sub: crypto.randomUUID(),
     })
