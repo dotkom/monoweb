8f707e05853a16e6936cf795d2d3da9304bef295
henrikskog <henrikskog01@gmail.com>
2024-03-05 21:30:22 +0100

    sync user from auth0 every 24h

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,6 +1,15 @@
 import { type ServiceLayer } from "@dotkomonline/core"
 import { type DefaultSession, type DefaultUser, type User, type NextAuthOptions } from "next-auth"
 import Auth0Provider from "next-auth/providers/auth0"
+import { createNewUser, syncUserWithAuth0 as handleUserSyncLocal } from "./utils"
+
+function splitName(name: string) {
+  const [firstName, ...lastName] = name.split(" ")
+  return {
+    firstName,
+    lastName: lastName.join(" "),
+  }
+}
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -51,15 +60,24 @@ export const getAuthOptions = ({
   },
   callbacks: {
     async session({ session, token }) {
+      console.log("CALLBACK")
       if (token.sub) {
-        let user = await core.userService.getUserBySubject(token.sub)
+        const user = await core.userService.getUserBySubject(token.sub)
+
         if (user === undefined) {
-          user = await core.userService.createUser({ auth0Sub: token.sub, studyYear: -1 })
+          const newUser = await createNewUser(core, token)
+
+          session.user.id = newUser.id
+          session.sub = token.sub
+          return session
         }
+
+        handleUserSyncLocal(core, user)
+
         session.user.id = user.id
         session.sub = token.sub
       }
       return session
     },
-  },
+   },
 })
