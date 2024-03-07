e02d020fbd42f837dcdfa6106480312131993b15
henrikskog <henrikskog01@gmail.com>
2024-03-07 22:24:59 +0100

    validate user get from auth0 with zod schema

--- packages/core/src/lib/auth0-repository.ts
+++ packages/core/src/lib/auth0-repository.ts
@@ -1,22 +1,36 @@
-import { type OidcUser } from "@dotkomonline/types"
-import { ManagementClient } from "auth0"
+import { OidcUser } from "@dotkomonline/types"
+import { ManagementApiError, ManagementClient } from "auth0"
 
 export interface Auth0Repository {
-  getBySubject(sub: string): Promise<OidcUser | undefined>
+  getBySubject(sub: string): Promise<OidcUser | null>
 }
 
 export class Auth0RepositoryImpl implements Auth0Repository {
   constructor(private readonly client: ManagementClient) {}
-  async getBySubject(sub: string): Promise<OidcUser | undefined> {
-    const res = await this.client.users.get({
-      id: sub,
-    })
-    return {
-      email: res.data.email,
-      // familyName: res.data.family_name,
-      // givenName: res.data.given_name,
-      name: res.data.name,
-      subject: res.data.user_id,
+  async getBySubject(sub: string) {
+    try {
+      const res = await this.client.users.get({
+        id: sub,
+      })
+
+      const parsed = OidcUser.parse({
+        email: res.data.email,
+        subject: res.data.user_id,
+        // familyName: res.data.family_name,
+        // givenName: res.data.given_name,
+      })
+
+      return parsed
+    } catch (e) {
+      if (e instanceof ManagementApiError) {
+        if (e.errorCode === "inexistent_user") {
+          return null
+        }
+
+      }
+
+      // Error was caused by other reasons than user not existing
+      throw e
     }
   }
 }
