dotkom/monoweb

commit 98deb99e546ab61e22be0b3215a8036787a37125

Sync all user data to local db table (#825)
Change approach for how we store user data.

packages/core/mock.ts
@@ -4,6 +4,11 @@ import { addWeeks, addYears } from "date-fns"
 export const getUserMock = (defaults: Partial<UserWrite> = {}): UserWrite => ({
   auth0Sub: "8697a463-46fe-49c2-b74c-f6cc98358298",
   studyYear: 0,
+  email: "test-mail-that-does-not-exist6123123@gmail.com",
+  // givenName: "Test",
+  // familyName: "User",
+  name: "Test User",
+  lastSyncedAt: null,
   ...defaults,
 })
 

packages/db/src/db.generated.d.ts
@@ -1,4 +1,4 @@
-import { type ColumnType } from "kysely"
+import type { ColumnType } from "kysely"
 
 export type EventStatus = "ATTENDANCE" | "NO_LIMIT" | "PUBLIC" | "TBA"
 
@@ -192,8 +192,12 @@ export interface Offline {
 export interface OwUser {
   auth0Sub: string | null
   createdAt: Generated<Timestamp>
+  email: string
   id: Generated<string>
+  lastSyncedAt: Timestamp | null
+  name: string
   studyYear: Generated<number>
+  updatedAt: Generated<Timestamp>
 }
 
 export interface Payment {

packages/db/src/migrations/0024_user.js
@@ -1,0 +1,42 @@
+import { sql } from "kysely"
+
+/** @param db {import('kysely').Kysely} */
+export async function up(db) {
+  await db.schema
+    .alterTable("ow_user")
+    .addColumn("email", "varchar(255)")
+    .addColumn("name", "varchar(255)")
+    .addColumn("updated_at", "timestamptz", (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn("last_synced_at", "timestamp with time zone", (col) => col.defaultTo(sql`now()`).notNull())
+    .execute()
+
+  // populate data for new columns
+  const users = await db.selectFrom("ow_user").selectAll().execute()
+  for (const user of users) {
+    const email = `${user.id}@invalid.local`
+    const name = `${user.id}`
+    await db.updateTable("ow_user").set({ email, name }).where("id", "=", user.id).execute()
+  }
+
+  // enforce constraints
+  await db.schema.alterTable("ow_user").addUniqueConstraint("", ["email"]).execute()
+  await db.schema
+    .alterTable("ow_user")
+    .alterColumn("email", (col) => col.setNotNull())
+    .execute()
+  await db.schema
+    .alterTable("ow_user")
+    .alterColumn("name", (col) => col.setNotNull())
+    .execute()
+}
+
+/** @param db {import('kysely').Kysely} */
+export async function down(db) {
+  await db.schema
+    .alterTable("ow_user")
+    .dropColumn("email")
+    .dropColumn("name")
+    .dropColumn("last_synced_at")
+    .dropColumn("updated_at")
+    .execute()
+}

packages/types/src/user.ts
@@ -5,6 +5,12 @@ export const UserSchema = z.object({
   createdAt: z.date(),
   auth0Sub: z.string(),
   studyYear: z.number().int().min(-1).max(6),
+  updatedAt: z.date(),
+  email: z.string().email(),
+  // givenName: z.string(),
+  // familyName: z.string(),
+  name: z.string(),
+  lastSyncedAt: z.date().nullable(),
 })
 
 export type UserId = User["id"]
@@ -13,6 +19,7 @@ export type User = z.infer<typeof UserSchema>
 export const UserWriteSchema = UserSchema.omit({
   id: true,
   createdAt: true,
+  updatedAt: true,
 })
 
 export type UserWrite = z.infer<typeof UserWriteSchema>

tools/migrator/src/fixtures/user.ts
@@ -5,21 +5,31 @@ export const getUserFixtures: () => Insertable<Database["owUser"]>[] = () => [
   {
     auth0Sub: "auth0|4dd4698d-c376-4e5e-b5fb-4db9bf6cd417", // brage.baugerod@online.ntnu.no
     createdAt: new Date("2023-04-30 19:22:17.627253+00"),
+    email: "bragebaugerod@gmail.com",
+    // givenName: "brage",
+    // familyName: "baugertod",
+    name: "brage baugerod",
     studyYear: 3,
+    lastSyncedAt: null,
   },
   {
     auth0Sub: "auth0|7f6e0389-b49f-4bbc-b401-505f4b53fb3c", // njal.sorland@online.ntnu.no
     createdAt: new Date("2023-08-11 21:22:17.627253+00"),
+    email: "njalsorland@gmail.com",
+    // givenName: "njal",
+    // familyName: "sorland",
+    name: "njal sorland",
     studyYear: 5,
+    lastSyncedAt: null,
   },
   {
-    createdAt: new Date("2023-04-30 21:22:17.627253+00"),
-    auth0Sub: "auth0|dddddddd-c376-4e5e-b5fb-4db9bf6cd417",
-    studyYear: 0,
-  },
-  {
-    createdAt: new Date("2023-04-30 21:22:17.627253+00"),
-    auth0Sub: "auth0|7ba93bf4-2e11-40a9-bcd7-9078ee9090bb",
+    createdAt: new Date("2023-04-30 21:22:17.627253+00"), // henrikskog01@gmail.com
+    auth0Sub: "auth0|61ea20bf-6964-4484-b7b8-e81952606973",
+    email: "henrikskog01@gmail.com",
+    // givenName: "henrik",
+    // familyName: "skog",
+    name: "henrik skog",
     studyYear: 3,
+    lastSyncedAt: null,
   },
 ]

packages/core/src/modules/user/__test__/user-service.e2e-spec.ts
@@ -4,6 +4,17 @@ import { ulid } from "ulid"
 import { createEnvironment } from "@dotkomonline/env"
 import { createKysely } from "@dotkomonline/db"
 import { createServiceLayer, type ServiceLayer } from "../../core"
+import { UserWrite } from "@dotkomonline/types"
+
+const fakeUser = (subject?: string): UserWrite => ({
+  auth0Sub: subject ?? crypto.randomUUID(),
+  studyYear: 0,
+  email: "testuser512312412@gmail.com",
+  // givenName: "Test",
+  // familyName: "User",
+  name: "Test User",
+  lastSyncedAt: null,
+})
 
 describe("users", () => {
   let core: ServiceLayer
@@ -18,10 +29,7 @@ describe("users", () => {
     const none = await core.userService.getAllUsers(100)
     expect(none).toHaveLength(0)
 
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
 
     const users = await core.userService.getAllUsers(100)
     expect(users).toContainEqual(user)
@@ -29,24 +37,13 @@ describe("users", () => {
 
   it("will not allow two users the same subject", async () => {
     const subject = crypto.randomUUID()
-    const first = await core.userService.createUser({
-      auth0Sub: subject,
-      studyYear: 0,
-    })
+    const first = await core.userService.createUser(fakeUser(subject))
     expect(first).toBeDefined()
-    await expect(
-      core.userService.createUser({
-        auth0Sub: subject,
-        studyYear: 0,
-      })
-    ).rejects.toThrow()
+    await expect(core.userService.createUser(fakeUser(subject))).rejects.toThrow()
   })
 
   it("will find users by their user id", async () => {
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
 
     const match = await core.userService.getUserById(user.id)
     expect(match).toEqual(user)
@@ -60,10 +57,7 @@ describe("users", () => {
         auth0Sub: crypto.randomUUID(),
       })
     ).rejects.toThrow()
-    const user = await core.userService.createUser({
-      auth0Sub: crypto.randomUUID(),
-      studyYear: 0,
-    })
+    const user = await core.userService.createUser(fakeUser())
     const updated = await core.userService.updateUser(user.id, {
       auth0Sub: crypto.randomUUID(),
     })

.github/workflows/ci.yml
@@ -23,6 +23,9 @@ env:
   FAGKOM_STRIPE_SECRET_KEY: ${{ secrets.FAGKOM_STRIPE_SECRET_KEY }}
   FAGKOM_STRIPE_WEBHOOK_SECRET: ${{ secrets.FAGKOM_STRIPE_WEBHOOK_SECRET }}
   S3_BUCKET_MONOWEB: ${{ secrets.S3_BUCKET_MONOWEB }}
+  GTX_AUTH0_CLIENT_ID: ${{ secrets.GTX_AUTH0_CLIENT_ID }}
+  GTX_AUTH0_CLIENT_SECRET: ${{ secrets.GTX_AUTH0_CLIENT_SECRET }}
+  GTX_AUTH0_ISSUER: ${{ secrets.GTX_AUTH0_ISSUER }}
 
 jobs:
   build:

packages/auth/src/auth-options.ts
@@ -2,6 +2,24 @@ import { type ServiceLayer } from "@dotkomonline/core"
 import { type DefaultSession, type DefaultUser, type User, type NextAuthOptions } from "next-auth"
 import Auth0Provider from "next-auth/providers/auth0"
 
+interface Auth0IdTokenClaims {
+  given_name: string
+  family_name: string
+  nickname: string
+  name: string
+  picture: string
+  gender: string
+  updated_at: string
+  email: string
+  email_verified: boolean
+  iss: string
+  aud: string
+  iat: number
+  exp: number
+  sub: string
+  sid: string
+}
+
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: User
@@ -14,6 +32,8 @@ declare module "next-auth" {
     name: string
     email: string
     image?: string
+    givenName?: string
+    familyName?: string
   }
 }
 
@@ -38,11 +58,13 @@ export const getAuthOptions = ({
       clientId: oidcClientId,
       clientSecret: oidcClientSecret,
       issuer: oidcIssuer,
-      profile: (profile): User => ({
+      profile: (profile: Auth0IdTokenClaims): User => ({
         id: profile.sub,
-        name: `${profile.given_name} ${profile.family_name}`,
+        name: profile.name,
         email: profile.email,
         image: profile.picture ?? undefined,
+        // givenName: profile.given_name,
+        // familyName: profile.family_name,
       }),
     }),
   ],
@@ -52,10 +74,18 @@ export const getAuthOptions = ({
   callbacks: {
     async session({ session, token }) {
       if (token.sub) {
-        let user = await core.userService.getUserBySubject(token.sub)
+        const user = await core.userService.getUserBySubject(token.sub)
+
         if (user === undefined) {
-          user = await core.userService.createUser({ auth0Sub: token.sub, studyYear: -1 })
+          const newUser = await core.auth0SynchronizationService.createUser(token)
+
+          session.user.id = newUser.id
+          session.sub = token.sub
+          return session
         }
+
+        await core.auth0SynchronizationService.synchronizeUser(user)
+
         session.user.id = user.id
         session.sub = token.sub
       }

commit 0d78c3d99530fc22bcc1d36c7eeb853b77bac4e8

Fix migration bug introduced in #825 (#828)
last_synced_at was set to not null but the fixtures were not fixed

packages/core/mock.ts
@@ -8,7 +8,7 @@ export const getUserMock = (defaults: Partial<UserWrite> = {}): UserWrite => ({
   // givenName: "Test",
   // familyName: "User",
   name: "Test User",
-  lastSyncedAt: null,
+  lastSyncedAt: new Date(),
   ...defaults,
 })
 

packages/db/src/db.generated.d.ts
@@ -194,7 +194,7 @@ export interface OwUser {
   createdAt: Generated<Timestamp>
   email: string
   id: Generated<string>
-  lastSyncedAt: Timestamp | null
+  lastSyncedAt: Generated<Timestamp>
   name: string
   studyYear: Generated<number>
   updatedAt: Generated<Timestamp>

packages/db/src/migrations/0024_user.js
@@ -6,16 +6,20 @@ export async function up(db) {
     .alterTable("ow_user")
     .addColumn("email", "varchar(255)")
     .addColumn("name", "varchar(255)")
-    .addColumn("updated_at", "timestamptz", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("last_synced_at", "timestamp with time zone", (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn("last_synced_at", "timestamptz", (col) => col.defaultTo(sql`now()`))
+    .addColumn("updated_at", "timestamptz", (col) => col.defaultTo(sql`now()`))
     .execute()
 
   // populate data for new columns
   const users = await db.selectFrom("ow_user").selectAll().execute()
   for (const user of users) {
     const email = `${user.id}@invalid.local`
-    const name = `${user.id}`
-    await db.updateTable("ow_user").set({ email, name }).where("id", "=", user.id).execute()
+    const now = new Date()
+    await db
+      .updateTable("ow_user")
+      .set({ email, name: user.id, updated_at: now, last_synced_at: now })
+      .where("id", "=", user.id)
+      .executeTakeFirst()
   }
 
   // enforce constraints
@@ -23,10 +27,9 @@ export async function up(db) {
   await db.schema
     .alterTable("ow_user")
     .alterColumn("email", (col) => col.setNotNull())
-    .execute()
-  await db.schema
-    .alterTable("ow_user")
     .alterColumn("name", (col) => col.setNotNull())
+    .alterColumn("updated_at", (col) => col.setNotNull())
+    .alterColumn("last_synced_at", (col) => col.setNotNull())
     .execute()
 }
 

packages/types/src/user.ts
@@ -10,7 +10,7 @@ export const UserSchema = z.object({
   // givenName: z.string(),
   // familyName: z.string(),
   name: z.string(),
-  lastSyncedAt: z.date().nullable(),
+  lastSyncedAt: z.date(),
 })
 
 export type UserId = User["id"]

tools/migrator/src/fixtures/user.ts
@@ -10,7 +10,7 @@ export const getUserFixtures: () => Insertable<Database["owUser"]>[] = () => [
     // familyName: "baugertod",
     name: "brage baugerod",
     studyYear: 3,
-    lastSyncedAt: null,
+    lastSyncedAt: new Date(),
   },
   {
     auth0Sub: "auth0|7f6e0389-b49f-4bbc-b401-505f4b53fb3c", // njal.sorland@online.ntnu.no
@@ -20,7 +20,7 @@ export const getUserFixtures: () => Insertable<Database["owUser"]>[] = () => [
     // familyName: "sorland",
     name: "njal sorland",
     studyYear: 5,
-    lastSyncedAt: null,
+    lastSyncedAt: new Date(),
   },
   {
     createdAt: new Date("2023-04-30 21:22:17.627253+00"), // henrikskog01@gmail.com
@@ -30,6 +30,6 @@ export const getUserFixtures: () => Insertable<Database["owUser"]>[] = () => [
     // familyName: "skog",
     name: "henrik skog",
     studyYear: 3,
-    lastSyncedAt: null,
+    lastSyncedAt: new Date(),
   },
 ]

packages/core/src/modules/user/__test__/user-service.e2e-spec.ts
@@ -13,7 +13,7 @@ const fakeUser = (subject?: string): UserWrite => ({
   // givenName: "Test",
   // familyName: "User",
   name: "Test User",
-  lastSyncedAt: null,
+  lastSyncedAt: new Date(),
 })
 
 describe("users", () => {