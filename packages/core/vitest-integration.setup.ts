15aabe113bc444c056d5f7cb889ff40f0d48f3bc
henrikskog <henrikskog01@gmail.com>
2024-03-09 13:40:07 +0100

    make it right

--- packages/core/vitest-integration.setup.ts
+++ packages/core/vitest-integration.setup.ts
@@ -2,66 +2,76 @@ import { beforeEach, beforeAll, afterEach } from "vitest"
 import { PostgreSqlContainer, type StartedPostgreSqlContainer } from "@testcontainers/postgresql"
 import { createMigrator, createKysely, Database } from "@dotkomonline/db"
 import { Environment, createEnvironment } from "@dotkomonline/env"
-import { exec } from "child_process"
-import { Kysely } from "kysely"
-import util from "util"
+import { sql } from "kysely"
 
-const promiseExec = util.promisify(exec)
+// Configuration settings for the database container
+const testDBConfig = {
+  password: "local",
+  username: "local",
+  database: "main",
+  imageName: "public.ecr.aws/z5h0l8j6/dotkom/pgx-ulid:0.1.3",
+}
 
-const password = "local"
-const username = "local"
-const database = "main"
-
-const containers: StartedPostgreSqlContainer[] = []
-
+// Used globally for all tests
 let container: StartedPostgreSqlContainer
-let kysely: Kysely<Database>
-let env: Environment
 
-beforeAll(async () => {
-  container = await new PostgreSqlContainer("public.ecr.aws/z5h0l8j6/dotkom/pgx-ulid:0.1.3")
+async function setupDatabaseContainer() {
+  const container = await new PostgreSqlContainer(testDBConfig.imageName)
     .withExposedPorts(5432)
-    .withUsername(username)
-    .withPassword(password)
-    .withDatabase(database)
+    .withUsername(testDBConfig.username)
+    .withPassword(testDBConfig.password)
+    .withDatabase(testDBConfig.database)
+    .withReuse()
     .start()
 
-  containers.push(container)
   process.env.DATABASE_URL = container.getConnectionUri()
+  return container
+}
+
+async function runMigrations(env: Environment) {
+  const kysely = createKysely(env)
+  const migrator = createMigrator(kysely, new URL("node_modules/@dotkomonline/db/src/migrations", import.meta.url))
+  await migrator.migrateToLatest().catch(console.warn)
+  await kysely.destroy()
+}
+
+async function resetTestDatabase(container: StartedPostgreSqlContainer, env: Environment) {
+  try {
+
+  // Create client for the default database and use it to drop and recreate the test database
+  const dbString = container.getConnectionUri().replace("/main", "/postgres")
+  const kysely = createKysely({
+    ...env,
+    DATABASE_URL: dbString,
+  })
+
+  const deleteQuery = sql`drop database if exists ${sql.ref(testDBConfig.database)}`.compile(kysely)
+  const createQuery = sql`create database ${sql.ref(testDBConfig.database)}`.compile(kysely)
+
+  await kysely.executeQuery(deleteQuery)
+  await kysely.executeQuery(createQuery)
+
+  await kysely.destroy()
+
+  } catch (e) {
+    console.error(e)
+}
+}
+
+beforeAll(async () => {
+  container = await setupDatabaseContainer()
 }, 30000)
 
 beforeEach(async () => {
-  env = createEnvironment()
-  kysely = createKysely(env)
-  const migrator = createMigrator(kysely, new URL("node_modules/@dotkomonline/db/src/migrations", import.meta.url))
-  const result = await migrator.migrateToLatest()
-  if (result.error) {
-    console.warn(`Error running migrations in test: ${result.error}`)
-  }
-  await kysely.destroy()
+  const env = createEnvironment()
+  await runMigrations(env)
 })
 
 afterEach(async () => {
-  try {
-    const command = `echo "drop database ${database}; create database ${database};" | PGPASSWORD=${password} psql -h ${container.getHost()} -p ${container.getMappedPort(
-      5432
-    )} -U ${username} -d postgres`
-
-    const { stderr, stdout } = await promiseExec(command)
-
-    if (stderr) {
-      console.error(`Error running command: ${stderr}`)
-      return
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(`Error running command: ${error.message}`)
-      return
-    }
-  }
+  const env = createEnvironment()
+  await resetTestDatabase(container, env)
 })
 
-
 process.on("beforeExit", async () => {
-  await Promise.all(containers.map(async (container) => container.stop()))
+  // await container.stop()
 })
