b07f80716a6f7066bf1a43385f5806b3d29d7f71
henrikskog <henrikskog01@gmail.com>
2024-03-09 15:25:30 +0100

    start on attendance service test

--- packages/core/src/modules/attendance/__test__/attendance-service.spec.ts
+++ packages/core/src/modules/attendance/__test__/attendance-service.spec.ts
@@ -1,28 +1,86 @@
-import { randomUUID } from "crypto"
 import { Kysely } from "kysely"
-import { describe, vi } from "vitest"
-import { type Company } from "@dotkomonline/types"
+import { describe } from "vitest"
+import { EventCompanyRepository, EventCompanyRepositoryImpl } from "../../event/event-company-repository"
+import { EventCompanyService } from "../../event/event-company-service"
+import { AttendanceRepository, AttendanceRepositoryImpl } from "../attendance-repository"
+import { AttendeeRepository, AttendeeRepositoryImpl } from "../attendee-repository"
+import { AttendancePoolRepository, AttendancePoolRepositoryImpl } from "../attendance-pool-repository"
+import { Attendance, AttendancePool, Event, User } from "@dotkomonline/types"
+import { ulid } from "ulid"
+import { AttendeeService, AttendeeServiceImpl } from "../attendee-service"
+import { UserRepository, UserRepositoryImpl } from "../../user/user-repository"
+import { UserService, UserServiceImpl } from "../../user/user-service"
 
-describe("EventCompanyService", () => {
+describe("Attendance before merge time", () => {
+  // const db = vi.mocked(Kysely.prototype)
+
+  // 1 create event
+  // 2 create attendance for event
+  // 3 create attendance pools for event
+  // 4 sign up attendee for attendance
+  // save snapshot of how attendance has changed
+  // verify that attendance has changed as expected
+
+  it("Can not attend if there is no matching group", async () => {})
+})
+
+describe("Attendance after merge time", () => {
   const db = vi.mocked(Kysely.prototype)
-  const eventCompanyRepository: EventCompanyRepository = new EventCompanyRepositoryImpl(db)
-  const eventCompanyService: EventCompanyService = new EventCompanyServiceImpl(eventCompanyRepository)
+  const attendanceRepository: AttendanceRepository = new AttendanceRepositoryImpl(db)
+  const attendeeRepository: AttendeeRepository = new AttendeeRepositoryImpl(db)
+  const attendancePoolRepository: AttendancePoolRepository = new AttendancePoolRepositoryImpl(db)
+  const userRepository: UserRepository = new UserRepositoryImpl(db)
 
-  const bekk: Company = {
-    createdAt: new Date(),
-    description: "Bekk er et konsulentselskap",
-    email: "bekk@bekk.no",
-    id: randomUUID(),
-    name: "Bekk",
-    phone: "+47 123 45 678",
-    type: "Consulting",
-    website: "https://bekk.no",
-    location: "Oslo & Trondheim",
-    image: null,
+  const userService: UserService = new UserServiceImpl(userRepository)
+
+  const attendeeService: AttendeeService = new AttendeeServiceImpl(attendeeRepository, attendancePoolRepository)
+
+  const attendance: Attendance = {
+    id: ulid(),
+    deregisterDeadline: new Date(),
+    mergeTime: new Date(),
+    registerEnd: new Date(),
+    registerStart: new Date(),
   }
 
-  it("creates links a company to an event", async () => {
-    const id = randomUUID()
+  const pool012: AttendancePool = {
+    attendanceId: attendance.id,
+    limit: 10,
+    id: ulid(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    numAttendees: 0,
+    yearCriteria: [0, 1, 2],
+  }
+
+  const user1: User = {
+    id: ulid(),
+    name: "name",
+    email: "email",
+    auth0Sub: "auth0Sub",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    lastSyncedAt: new Date(),
+    studyYear: 1,
+  }
+
+  const event: Event = {
+    attendanceId: attendance.id,
+    createdAt: new Date(),
+    description: "description",
+    end: new Date(),
+    id: ulid(),
+    extras: [],
+    imageUrl: "imageUrl",
+    location: "location",
+    public: true,
+    start: new Date(),
+    status: "ATTENDANCE",
+    title: "",
+    updatedAt: new Date(),
+  }
+
+  it("user can attend event when they have reserved spot and there is capacity", async () => {
     vi.spyOn(eventCompanyRepository, "createCompany").mockResolvedValueOnce(undefined)
     const event = await eventCompanyService.createCompany(id, bekk.id)
     expect(event).toEqual(undefined)
