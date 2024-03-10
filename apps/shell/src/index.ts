dec1b2298e3e7ad52c72a878b7ffe4edf035c2db
henrikskog <henrikskog01@gmail.com>
2024-03-10 23:14:47 +0100

    rm shell app

--- apps/shell/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createServiceLayer } from "@dotkomonline/core"
-import { kysely } from "@dotkomonline/db"
-// import { CognitoIDPRepositoryImpl } from "@dotkomonline/core/src/lib/IDP-repository"
-
-createServiceLayer({ db: kysely }).then(async (core) => {
-  const test = await core.auth0Repository.getBySubject("auth0|61ea20bf-6964-4484-b7b8-e81952601111")
-  console.log(test)
-})
