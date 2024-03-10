af5ba17931bfe830608443861eca59b1bab5ca2c
Henrik Skog <henrikskog01@gmail.com>
2024-03-10 18:13:18 +0100

    Improve performance in integration tests (#829)
    Set up _one_ container at the start of running tests. Instead of
    starting new containers for each test. 
    
    Co-authored-by: Mats Jun <mats@jun.codes>

--- packages/core/vitest-integration.setup.ts
+++ packages/core/vitest-integration.setup.ts
@@ -1,28 +1,68 @@
-import { beforeEach } from "vitest"
+import { Database, createKysely, createMigrator } from "@dotkomonline/db"
+import { createEnvironment, Environment } from "@dotkomonline/env"
 import { PostgreSqlContainer, type StartedPostgreSqlContainer } from "@testcontainers/postgresql"
-import { createMigrator, createKysely } from "@dotkomonline/db"
-import { createEnvironment } from "@dotkomonline/env"
+import { Kysely, sql } from "kysely"
+import { beforeAll, afterAll } from "vitest"
 
-const containers: StartedPostgreSqlContainer[] = []
+let container: StartedPostgreSqlContainer
+let host: Kysely<Database>
 
-beforeEach(async () => {
-  const container = await new PostgreSqlContainer("public.ecr.aws/z5h0l8j6/dotkom/pgx-ulid:0.1.3")
+async function runMigrations(env: Environment, dbName: string) {
+  const db = createKyselyForDatabase(env, dbName)
+  const migrator = createMigrator(db, new URL("node_modules/@dotkomonline/db/src/migrations", import.meta.url))
+  await migrator.migrateToLatest().catch(console.warn)
+  await db.destroy()
+}
+
+function createKyselyForDatabase(env: Environment, dbName: string): Kysely<Database> {
+  return createKysely({
+    ...env,
+    DATABASE_URL: `postres://local:local@${container.getHost()}:${container.getFirstMappedPort()}/${dbName}`,
+  })
+}
+
+async function createTestDatabase(dbName: string) {
+  try {
+    await host.executeQuery(sql`drop database if exists ${sql.ref(dbName)}`.compile(host))
+    await host.executeQuery(sql`create database ${sql.ref(dbName)}`.compile(host))
+  } catch (e) {
+    console.error("Error resetting database")
+    console.error(e)
+  }
+}
+
+export type CleanupFunction = () => Promise<void>
+
+export async function createServiceLayerForTesting(env: Environment, database: string) {
+  await createTestDatabase(database)
+  await runMigrations(env, database)
+  const kysely = createKyselyForDatabase(env, database)
+
+  return {
+    kysely,
+    cleanup: async () => {
+      await kysely.destroy()
+    },
+  }
+}
+
+beforeAll(async () => {
+  container = await new PostgreSqlContainer("public.ecr.aws/z5h0l8j6/dotkom/pgx-ulid:0.1.3")
     .withExposedPorts(5432)
     .withUsername("local")
     .withPassword("local")
     .withDatabase("main")
+    .withReuse()
     .start()
+
   process.env.DATABASE_URL = container.getConnectionUri()
-  const env = createEnvironment()
-  const kysely = createKysely(env)
-  const migrator = createMigrator(kysely, new URL("node_modules/@dotkomonline/db/src/migrations", import.meta.url))
-  const result = await migrator.migrateToLatest()
-  if (result.error) {
-    console.warn(`Error running migrations in test: ${result.error}`)
-  }
-  containers.push(container)
+  host = createKysely(createEnvironment())
+}, 30000)
+
+afterAll(async () => {
+  await host.destroy()
 })
 
 process.on("beforeExit", async () => {
-  await Promise.all(containers.map(async (container) => container.stop()))
+  // await container.stop()
 })
