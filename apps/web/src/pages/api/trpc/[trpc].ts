5acb66be076b557511f969a9aa0928a8ffb71723
Mats Larsen <mats@jun.codes>
2024-03-11 18:22:46 +0100

    Implement it for web as well

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -2,12 +2,13 @@ import { appRouter, createContext } from "@dotkomonline/gateway-trpc"
 import { createNextApiHandler } from "@trpc/server/adapters/next"
 import { applyCorsMiddleware } from "@/middlewares/cors"
 import { env } from "@dotkomonline/env"
+import { JwtService } from "@dotkomonline/jwt-crypto"
 
 // export API handler
 export default applyCorsMiddleware(
   createNextApiHandler({
     router: appRouter,
-    createContext: (args) => createContext(args, env),
+    createContext: (args) => createContext(args, new JwtService(env.GTX_AUTH0_ISSUER)),
     onError({ error }) {
       console.error(error)
     },
