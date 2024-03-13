9bf86ed88d1712c2326aa590fccf08fc83d9ce78
madsab <mads.barnes@hotmail.com>
2024-03-13 19:11:57 +0100

    mod: remove unused + add new links + new link icons

--- apps/web/src/utils/profileLinks.tsx
+++ apps/web/src/utils/profileLinks.tsx
@@ -2,26 +2,37 @@ export const profileItems = [
   {
     title: "Min Profil",
     slug: "/profile",
-    icon: "tabler:user-circle",
+    icon: "tabler:user",
+    description: "Endre din profilinformasjon",
   },
   {
     title: "Personvern",
     slug: "/profile/privacy",
-    icon: "tabler:shield-half-filled",
-  },
-  {
-    title: "Prikker & Suspensjoner",
-    slug: "/profile/penalties",
-    icon: "tabler:alert-triangle",
+    icon: "uil:shield-check",
+    description: "Endre dine personverninnstillinger",
   },
   {
     title: "Passord",
     slug: "/profile/password",
-    icon: "tabler:lock",
+    icon: "lucide:lock",
+    description: "Endre ditt passord",
+  },
+  {
+    title: "Varsler",
+    slug: "/profile/notifications",
+    icon: "tabler:bell-ringing",
+    description: "Endre dine varslingsinnstillinger",
   },
   {
     title: "Medlemskap",
     slug: "/profile/membership",
     icon: "tabler:award",
+    description: "Se ditt medlemskap",
   },
-]
+  {
+    title: "Prikker & Suspensjoner",
+    slug: "/profile/penalties",
+    icon: "tabler:circle-x",
+    description: "Skal fjernes",
+  },
+];
