4d187e6d6cf3461e09d7b689e0f01f14637a9bdd
henrikskog <henrikskog01@gmail.com>
2024-03-18 18:34:12 +0100

    revert my changes for attendance service

--- packages/core/src/modules/event/attendance-service.ts
+++ packages/core/src/modules/event/attendance-service.ts
@@ -1,6 +1,6 @@
 import { type AttendanceId, type Attendee, type EventId, type UserId } from "@dotkomonline/types"
 import { type AttendanceRepository } from "./attendance-repository"
-import { EventNotFoundError } from "./event-error"
+import { AttendeeNotFoundError } from "./attendee-error"
 
 export interface AttendanceService {
   canAttend(eventId: EventId): Promise<Date | undefined>
@@ -31,12 +31,12 @@ export class AttendanceServiceImpl implements AttendanceService {
   /**
    * Register for an attendance
    *
-   * @throws {EventNotFoundError} if attendee is not found
+   * @throws {AttendeeNotFoundError} if attendee is not found
    */
   async registerForAttendance(_userId: UserId, _attendanceId: AttendanceId, _attended: boolean) {
     const attendee = await this.attendanceRepository.getAttendeeByIds(_userId, _attendanceId)
     if (!attendee) {
-      throw new EventNotFoundError(_userId)
+      throw new AttendeeNotFoundError(_userId)
     }
     const attendedAttendee = await this.attendanceRepository.updateAttendee(
       { ...attendee, attended: _attended },
@@ -49,12 +49,12 @@ export class AttendanceServiceImpl implements AttendanceService {
   /**
    * Add a choice to an attendee
    *
-   * @throws {EventNotFoundError} if attendee is not found
+   * @throws {AttendeeNotFoundError} if attendee is not found
    */
   async addChoice(eventId: string, attendanceId: string, questionId: string, choiceId: string) {
     const attendee = await this.attendanceRepository.getAttendeeByIds(eventId, attendanceId)
     if (!attendee) {
-      throw new EventNotFoundError(attendanceId)
+      throw new AttendeeNotFoundError(eventId)
     }
     const choice = await this.attendanceRepository.addChoice(eventId, attendanceId, questionId, choiceId)
     return choice
