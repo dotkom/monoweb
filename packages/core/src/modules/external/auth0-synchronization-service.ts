afc366f299335fad322b32b1a4b840f52e3db5bc
henrikskog <henrikskog01@gmail.com>
2024-03-18 23:09:01 +0100

    clean up synchronization service

--- packages/core/src/modules/external/auth0-synchronization-service.ts
+++ packages/core/src/modules/external/auth0-synchronization-service.ts
@@ -3,35 +3,18 @@ import { UserService } from "../user/user-service"
 import { Auth0Repository } from "./auth0-repository"
 import { getLogger, Logger } from "@dotkomonline/logger"
 
-// Id token returned from Auth0. We don't want core to depend on next-auth, so we duplicate the type here.
-type Auth0IdToken = {
-  email?: string | null
-  sub?: string | null
-  name?: string | null
-  givenName?: string | null
-  familyName?: string | null
-}
-
 export interface Auth0SynchronizationService {
   /**
-   * If no record of the user exists in the local database, save it to the database.
-   */
-  createUser(token: Auth0IdToken): Promise<User>
-
-  /**
    * Auth0 is the source of truth for user data. We keep a local read only copy of the user data in the application database to make it easier to query and to avoid having to cache results from Auth0.
-   * 
+   *
    * The only place user data is updated from is monoweb. Monoweb makes sure to keep the local user data in sync with Auth0 when updating user data.
    * However, the monoweb db can still get out of sync with the auth0 db. It is possible that users are updated from the Auth0 dashboard or that a monoweb update succeeds to auth0 but the update to the local database fails.
    *
    * To make sure the db user table is synchronized with Auth0 on such occasions, this method synchronizes the user data from Auth0 to the local database based on some given criteria left to the implementation.
+   * 
+   * @returns The synchronized user object.
    */
-  handleUserSync(user: User): Promise<void>
-
-  /**
-   * Handle synchronization of a user from Auth0 to the local database.
-   */
-  synchronizeUser(user: User): Promise<void>
+  handleUserSync(sub: string): Promise<User>
 }
 
 export class Auth0SynchronizationServiceImpl implements Auth0SynchronizationService {
@@ -42,39 +25,31 @@ export class Auth0SynchronizationServiceImpl implements Auth0SynchronizationServ
     private readonly auth0Repository: Auth0Repository
   ) {}
 
-  // TODO: Include givenName and familyName when we gather this from our users.
-  async createUser(token: Auth0IdToken) {
-    if (
-      !token.email ||
-      !token.sub ||
-      !token.name
-      //  || !token.givenName || !token.familyName
-    ) {
-      throw new Error("Missing user data in claims")
-    }
-
-    const userData: UserWrite = {
-      auth0Sub: token.sub,
-      studyYear: -1,
-      email: token.email,
-      name: token.name,
-      // givenName: token.givenName,
-      // familyName: token.familyName,
-      lastSyncedAt: new Date(),
-    }
-
-    return this.userService.createUser(userData)
-  }
-
-  async synchronizeUser(user: User) {
-    this.logger.log("info", "Synchronizing user with Auth0", { userId: user.id })
-    const auth0User = await this.auth0Repository.getBySubject(user.auth0Sub)
+  private async synchronizeUser(auth0Subject: string) {
+    this.logger.log("info", "Synchronizing user with Auth0", { userId: auth0Subject })
+    const auth0User = await this.auth0Repository.getBySubject(auth0Subject)
 
     if (auth0User === null) {
       throw new Error("User does not exist in Auth0")
     }
 
-    await this.userService.updateUser(user.id, {
+    const user = await this.userService.getUserBySubject(auth0Subject)
+
+    if (user === undefined) {
+      this.logger.log("info", "User does not exist in local db, creating user", { userId: auth0Subject })
+
+      const userData: UserWrite = {
+        auth0Sub: auth0User.subject,
+        studyYear: -1,
+        email: auth0User.email,
+        name: auth0User.name,
+        lastSyncedAt: new Date(),
+      }
+  
+      return this.userService.createUser(userData)
+    }
+
+    return await this.userService.updateUser(user.id, {
       email: auth0User.email,
       name: auth0User.name,
       lastSyncedAt: new Date(),
@@ -82,18 +57,26 @@ export class Auth0SynchronizationServiceImpl implements Auth0SynchronizationServ
   }
 
   /**
-   * Sync the user if the user has not been synced in the last 24 hours.
-   * 
-   * @param user - The user to be synchronized.
-   * @returns A promise that resolves when the user synchronization is complete.
-   */
-  async handleUserSync(user: User) {
+    * Syncs down user if not synced within the last 24 hours.
+    * @param auth0Sub The Auth0 subject of the user to synchronize.
+    * @returns User
+    */
+  async handleUserSync(auth0Sub: string) {
+    const user = await this.userService.getUserBySubject(auth0Sub)
+
+    if (user === undefined) {
+      return this.synchronizeUser(auth0Sub)
+    }
+
     const oneDay = 1000 * 60 * 60 * 24
     const oneDayAgo = new Date(Date.now() - oneDay)
 
-    const userShouldBeSynced =  user.lastSyncedAt < oneDayAgo
+    const userShouldBeSynced = user.lastSyncedAt < oneDayAgo
     if (userShouldBeSynced) {
-      await this.synchronizeUser(user)
+      return this.synchronizeUser(user.auth0Sub)
     }
+
+    // User was not updated from Auth0
+    return user
   }
 }
