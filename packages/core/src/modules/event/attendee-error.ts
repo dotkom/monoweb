c7020d5a92683b0d0a4dea06a2fdbdee5a1ba115
henrikskog <henrikskog01@gmail.com>
2024-03-18 19:07:16 +0100

    revert accidental change

--- packages/core/src/modules/event/attendee-error.ts
+++ packages/core/src/modules/event/attendee-error.ts
@@ -2,7 +2,7 @@ import { ApplicationError } from "../../error"
 import { PROBLEM_DETAILS } from "../../problem-details-registry"
 
 export class AttendeeNotFoundError extends ApplicationError {
-  constructor(detail: string) {
-    super(PROBLEM_DETAILS.NotFound, detail)
+  constructor(id: string) {
+    super(PROBLEM_DETAILS.NotFound, `Attendee with ID:${id} not found`)
   }
 }
