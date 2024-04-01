d9cc140d657a2f0f0ac9144ce2857a673cea46e3
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-04-01 16:17:33 +0200

    Swap mobile navbar icons

--- apps/web/src/components/organisms/Navbar/MobileNavigation.tsx
+++ apps/web/src/components/organisms/Navbar/MobileNavigation.tsx
@@ -1,32 +1,41 @@
-"use client"
+"use client";
 
-import { cn, Icon } from "@dotkomonline/ui"
-import { type FC, useEffect, useState } from "react"
-import * as Popover from "@radix-ui/react-popover"
-import Link from "next/link"
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "./NavigationMenu"
-import { type MenuLink } from "./types"
+import { cn, Icon } from "@dotkomonline/ui";
+import { type FC, useEffect, useState } from "react";
+import * as Popover from "@radix-ui/react-popover";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./NavigationMenu";
+import { type MenuLink } from "./types";
 
 export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (open) {
-      document.body.classList.add("overflow-hidden")
+      document.body.classList.add("overflow-hidden");
     } else {
-      document.body.classList.remove("overflow-hidden")
+      document.body.classList.remove("overflow-hidden");
     }
-  }, [open])
+  }, [open]);
 
   return (
     <Popover.Root onOpenChange={(val) => setOpen(val)}>
       <Popover.Trigger>
         <div className={cn("mr-7 flex items-center md:hidden")}>
-          {open ? <Icon height={32} icon="tabler:menu-2" /> : <Icon height={32} icon="tabler:x" />}
+          {open ? (
+            <Icon height={32} icon="tabler:x" />
+          ) : (
+            <Icon height={32} icon="tabler:menu-2" />
+          )}
         </div>
       </Popover.Trigger>
       <Popover.Portal>
-        <Popover.Content className="animate-in fade-in-20 bg-indigo-1 h-screen w-screen">
+        <Popover.Content className="h-screen w-screen bg-indigo-1 animate-in fade-in-20">
           <NavigationMenu className="[&>div]:w-full">
             <NavigationMenuList className="flex-col">
               {links.map((link) => (
@@ -35,7 +44,10 @@ export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
                   {"items" in link ? (
                     <>
                       {link.items.map((item) => (
-                        <MobileMenuItem link={item} key={`menu-item-${item.title}`} />
+                        <MobileMenuItem
+                          link={item}
+                          key={`menu-item-${item.title}`}
+                        />
                       ))}
                     </>
                   ) : (
@@ -49,16 +61,16 @@ export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
-  )
-}
+  );
+};
 
 const MobileMenuItem = ({ link }: { link: MenuLink }) => (
   <NavigationMenuLink asChild>
     <Link
       href={"href" in link ? link.href : "#"}
-      className="border-blue-12/10 mx-8 flex h-14 w-[calc(100%-theme(space.16))] items-center border-b text-lg font-semibold"
+      className="mx-8 flex h-14 w-[calc(100%-theme(space.16))] items-center border-b border-blue-12/10 text-lg font-semibold"
     >
       {link.title}
     </Link>
   </NavigationMenuLink>
-)
+);
