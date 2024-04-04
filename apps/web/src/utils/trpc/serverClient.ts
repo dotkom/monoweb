430f0343ac753145eebb53e4e65db3a0057c00c5
Mats Larsen <mats@jun.codes>
2024-04-04 19:43:10 +0200

    Typefix

--- apps/web/src/utils/trpc/serverClient.ts
+++ apps/web/src/utils/trpc/serverClient.ts
@@ -4,5 +4,5 @@ import { appRouter, createCallerFactory, createContextInner } from "@dotkomonlin
 
 const createCaller = createCallerFactory(appRouter)
 // TODO: Add a way to get the userId from the request
-export const getServerClient = async () => createCaller(createContextInner({ auth: { userId: null } }))
-export const getUnauthorizedServerClient = async () => createCaller(createContextInner({ auth: { userId: null } }))
+export const getServerClient = async () => createCaller(await createContextInner({ auth: null }))
+export const getUnauthorizedServerClient = async () => createCaller(await createContextInner({ auth: null }))
