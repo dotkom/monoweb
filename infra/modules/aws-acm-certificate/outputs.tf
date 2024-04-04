6cb7642313b0d13cbe761e910d857418d69872ca
Mats Larsen <mats@jun.codes>
2024-04-04 17:26:30 +0200

    Remove Grades from Monoweb

--- infra/modules/aws-acm-certificate/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "certificate_arn" {
-  value = aws_acm_certificate.certificate.arn
-}
--- infra/modules/aws-api-gateway/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_gateway_id" {
-  value = aws_apigatewayv2_api.this.id
-}
-
-output "api_gateway_execution_arn" {
-  value = aws_apigatewayv2_api.this.execution_arn
-}
