f5543831c4d9b491389782ad5561c398c47a1b0b
henrikskog <henrikskog01@gmail.com>
2024-04-06 16:59:52 +0200

    dont know what this is

---
+++ apps/web/src/app/profile/page.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import ProfilePoster from "@/components/views/ProfileView"
+
+const ProfilePage = async () => {
+  const session = await getServerSession()
+
+  if (session === null) {
+    redirect("/")
+  }
+
+  return <ProfilePoster user={session.user} />
+}
+
+export default ProfilePage
