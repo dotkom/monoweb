af7d84d4cb2349d4a262c9c5b6bbe22679aa928d
madsab <mads.barnes@hotmail.com>
2024-04-10 19:43:11 +0200

    create new password view

--- apps/web/src/components/views/SettingsView/components/SettingsPassword.tsx
+++ apps/web/src/components/views/SettingsView/components/SettingsPassword.tsx
@@ -1,6 +1,27 @@
-import type { User } from "@dotkomonline/types"
-import type { NextPage } from "next"
+import { Button, TextInput } from "@dotkomonline/ui";
 
-const SettingsPassword: NextPage<{ user: User }> = ({ user }) => <div>password</div>
+const SettingsPassword = () => (
+  <div className="pb-8">
+    <p className="text-2xl font-semibold mb-5">Endre passord</p>
+    <div className="flex flex-col space-y-6 mb-6">
+      <TextInput
+        label="Nåværende passord"
+        placeholder="Passord"
+        type="password"
+      />
+      <TextInput
+        label="Nytt passord"
+        placeholder="Nytt Passord"
+        type="password"
+      />
+      <TextInput
+        label="Gjenta nytt passord"
+        placeholder="Gjenta passord"
+        type="password"
+      />
+    </div>
+    <Button className="float-right mb-6">Endre passord</Button>
+  </div>
+);
 
-export default SettingsPassword
+export default SettingsPassword;
