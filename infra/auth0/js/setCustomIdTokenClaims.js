10d32310b5a30ce4bfde26a8180a89aeb1f62021
henrikskog <henrikskog01@gmail.com>
2024-04-12 10:29:59 +0200

    Manage Auth0 login flow from terraform and add new action

---
+++ infra/auth0/js/setCustomIdTokenClaims.js
@@ -0,0 +1,23 @@
+/**
+ * Handler that will be called during the execution of a PostLogin flow.
+ *
+ * @param {Event} event - Details about the user and the context in which they are logging in.
+ * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+ */
+exports.onExecutePostLogin = async (event, api) => {
+  api.idToken.setCustomClaim("userId", event.user.user_id)
+  api.idToken.setCustomClaim("gender", event.user.user_metadata.gender)
+  api.idToken.setCustomClaim("studyYear", event.user.app_metadata.studyYear)
+  api.idToken.setCustomClaim("phoneNumber", event.user.app_metadata.phoneNumber)
+  api.idToken.setCustomClaim("allergies", event.user.app_metadata.allergies)
+  api.idToken.setCustomClaim("profilePicture", event.user.app_metadata.profilePicture)
+  api.idToken.setCustomClaim("givenName", event.user.app_metadata.givenName)
+  api.idToken.setCustomClaim("familyName", event.user.app_metadata.familyName)
+  api.idToken.setCustomClaim("name", event.user.app_metadata.name)
+  api.idToken.setCustomClaim("emailVerified", event.user.app_metadata.emailVerified)
+  api.idToken.setCustomClaim("onBoarded", event.user.app_metadata.onBoarded)
+
+  api.idToken.setCustomClaim("id", event.user.user_id)
+  api.idToken.setCustomClaim("user_id", event.user.user_id)
+  api.idToken.setCustomClaim("sub", event.user.user_id)
+}
