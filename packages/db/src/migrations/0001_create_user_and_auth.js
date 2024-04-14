b1c2b8a28df44ac71e6d3ade6ed015aa4cfe33e5
henrikskog <henrikskog01@gmail.com>
2024-04-14 23:50:59 +0200

    create new migration instead of modifying

--- packages/db/src/migrations/0001_create_user_and_auth.js
+++ packages/db/src/migrations/0001_create_user_and_auth.js
@@ -1,23 +1,11 @@
 import { sql } from "kysely"
-import { createTableWithDefaults } from "../utils"
+import { createTableWithDefaults } from "../utils.js"
 
 /** @param db {import('kysely').Kysely} */
 export async function up(db) {
   const query = sql`CREATE EXTENSION IF NOT EXISTS ulid;`.compile(db)
   await db.executeQuery(query)
-  await createTableWithDefaults("ow_user", { id: true }, db.schema)
-    .addColumn("auth0_id", "varchar(255)", (col) => col.notNull().unique())
-    .addColumn("email", "varchar(255)", (col) => col.unique().notNull())
-    .addColumn("name", "varchar(255)", (col) => col.notNull())
-    .addColumn("family_name", "varchar(255)", (col) => col.notNull())
-    .addColumn("given_name", "varchar(255)", (col) => col.notNull())
-    .addColumn("picture", "varchar(255)")
-    .addColumn("allergies", "json", (col) => col.notNull())
-    .addColumn("study_year", "integer", (col) => col.notNull())
-    .addColumn("last_synced_at", "timestamptz", (col) => col.defaultTo(sql`now()`))
-    .addColumn("phone", "varchar(255)")
-    .addColumn("gender", "varchar(255)")
-    .execute()
+  await createTableWithDefaults("ow_user", { id: true, createdAt: true }, db.schema).execute()
 }
 
 /** @param db {import('kysely').Kysely} */
