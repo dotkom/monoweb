3fe0b0b074c3491f5d60233dea28b7b2b8e7a8ef
henrikskog <henrikskog01@gmail.com>
2024-04-14 23:57:29 +0200

    Turn off retry and refetch on window focus

--- apps/dashboard/src/app/QueryProvider.tsx
+++ apps/dashboard/src/app/QueryProvider.tsx
@@ -5,7 +5,17 @@ import { type PropsWithChildren, useState } from "react"
 import { trpc, trpcConfig } from "../utils/trpc"
 
 export const QueryProvider = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: process.env.NODE_ENV === "production" ? 3 : false,
+            refetchOnWindowFocus: process.env.NODE_ENV === "production",
+          },
+        },
+      })
+  )
   const [trpcClient] = useState(() => trpc.createClient(trpcConfig))
 
   return (
