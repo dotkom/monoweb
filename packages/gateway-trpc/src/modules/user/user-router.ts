e19ded286547422e2f40aea162fe0734b9a05d72
henrikskog <henrikskog01@gmail.com>
2024-04-28 12:49:01 +0200

    fix type error after renaming of function

--- packages/gateway-trpc/src/modules/user/user-router.ts
+++ packages/gateway-trpc/src/modules/user/user-router.ts
@@ -14,7 +14,7 @@ export const userRouter = t.router({
         data: UserWriteSchema,
       })
     )
-    .mutation(async ({ input: changes, ctx }) => ctx.syncedUserService.update(changes.data)),
+    .mutation(async ({ input: changes, ctx }) => ctx.auth0SynchronizationService.updateUserInAuth0AndLocalDb(changes.data)),
   getPrivacyPermissionssByUserId: protectedProcedure
     .input(z.string())
     .query(async ({ input, ctx }) => ctx.userService.getPrivacyPermissionsByUserId(input)),
