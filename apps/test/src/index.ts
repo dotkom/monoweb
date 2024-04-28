9e71d06bb3b35e9be8edc0377c0939beda942024
henrikskog <henrikskog01@gmail.com>
2024-04-28 18:19:54 +0200

    example of types package bug

---
+++ apps/test/src/index.ts
@@ -0,0 +1,6 @@
+
+// const typeExports = await import("@dotkomonline/types");
+// console.log(typeExports.ArticleSchema.safeParse({}))
+
+import { ArticleSchema } from "@dotkomonline/types";
+console.log(typeof ArticleSchema);
