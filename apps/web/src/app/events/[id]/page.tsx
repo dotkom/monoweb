dotkom/monoweb

commit c61af771d94228b8b227c31ff05578fa64f6caf5

Fix event page crashing when not authorized (#892)

apps/web/src/app/events/[id]/page.tsx
@@ -27,14 +27,14 @@ const EventDetailPageInner = ({ id }: { id: string }) => {
     return <div>Laster</div>
   }
 
-  if (!event || !session?.data) {
+  if (!event) {
     return <div>Kunne ikke hente data</div>
   }
 
   if (event.hasAttendance) {
     return (
       <EventDetailWithAttendancePage
-        user={session.data.user}
+        user={session.data?.user}
         attendance={event.attendance}
         pools={event.pools}
         event={event.event}
@@ -64,7 +64,7 @@ const EventDetailWithoutAttendancePage: FC<WithoutAttendanceProps> = ({ user, ev
 }
 
 interface WithAttendanceProps {
-  user: NonNullable<Session["user"]>
+  user?: Session["user"]
   attendance: Attendance
   pools: AttendancePool[]
   event: Event
@@ -74,7 +74,7 @@ interface WithAttendanceProps {
 const EventDetailWithAttendancePage: FC<WithAttendanceProps> = ({ user, attendance, pools, event, committees }) => {
   const { data: attendee } = useGetAttendee({
     attendanceId: attendance.id,
-    userId: user.id,
+    userId: user?.id,
   })
 
   return (

apps/web/src/app/events/components/queries.ts
@@ -1,12 +1,14 @@
 import { trpc } from "@/utils/trpc/client"
 
 interface Props {
-  userId: string
+  userId?: string
   attendanceId: string
 }
 export const useGetAttendee = ({ userId, attendanceId }: Props) => {
   return trpc.event.attendance.getAttendee.useQuery({
     attendanceId,
-    userId,
+    userId: userId ?? ""
+  }, {
+    enabled: Boolean(userId)
   })
 }

apps/web/src/app/events/components/AttendanceBox.tsx
@@ -80,7 +80,7 @@ const dateToString = (attendanceOpeningDate: Date): DateString => {
 }
 
 interface Props {
-  sessionUser: Session["user"]
+  sessionUser?: Session["user"]
   attendance: Attendance
   pools: AttendancePool[]
   event: Event
@@ -99,7 +99,7 @@ export const AttendanceBox: FC<Props> = ({ sessionUser, attendance, pools, event
 
   const registerMutation = useRegisterMutation()
   const unregisterMutation = useUnregisterMutation()
-  const { data: attendee } = useGetAttendee({ userId: sessionUser.id, attendanceId })
+  const { data: attendee } = useGetAttendee({ userId: sessionUser?.id, attendanceId })
 
   const attendanceStatus = calculateStatus({
     registerStart: attendance.registerStart,
@@ -116,9 +116,13 @@ export const AttendanceBox: FC<Props> = ({ sessionUser, attendance, pools, event
       throw new Error("Tried to register user for attendance without a group")
     }
 
+    if (!sessionUser) {
+      throw new Error("Tried to register user without session")
+    }
+
     registerMutation.mutate({
       attendancePoolId: myGroups?.id,
-      userId: sessionUser.id,
+      userId: sessionUser?.id,
     })
   }
 

commit f0cadf1e7d9203c2e6614afa2a833064df480a9f

Fix broken types from c61af771d94228b8b227c31ff05578fa64f6caf5 (#898)
Fixes broken types regressed from
c61af771d94228b8b227c31ff05578fa64f6caf5

apps/web/src/app/events/[id]/page.tsx
@@ -43,17 +43,14 @@ const EventDetailPageInner = ({ id }: { id: string }) => {
     )
   }
 
-  return (
-    <EventDetailWithoutAttendancePage user={session.data.user} event={event.event} committees={event.eventCommittees} />
-  )
+  return <EventDetailWithoutAttendancePage event={event.event} committees={event.eventCommittees} />
 }
 
 interface WithoutAttendanceProps {
-  user: NonNullable<Session["user"]>
   event: Event
   committees: Committee[]
 }
-const EventDetailWithoutAttendancePage: FC<WithoutAttendanceProps> = ({ user, event, committees }) => {
+const EventDetailWithoutAttendancePage: FC<WithoutAttendanceProps> = ({ event, committees }) => {
   return (
     <div>
       <div className="flex w-full">
@@ -83,7 +80,7 @@ const EventDetailWithAttendancePage: FC<WithAttendanceProps> = ({ user, attendan
         <EventInfoBox event={event} />
         <div className="flex flex-1 flex-col">
           <AttendanceBox sessionUser={user} attendance={attendance} pools={pools} event={event} />
-          {attendee && <TicketButton userid={user.id} />}
+          {attendee && user && <TicketButton userId={user.id} />}
           {committees.length && <OrganizerBox committees={committees} />}
           <TimeLocationBox
             datetimeStart={event.start}

apps/web/src/app/events/components/TicketButton.tsx
@@ -5,10 +5,10 @@ import { QRCodeSVG } from "qrcode.react"
 import { useState } from "react"
 
 interface TicketButtonProps {
-  userid: UserId | null
+  userId: UserId | null
 }
 
-const TicketButton = ({ userid }: TicketButtonProps) => {
+const TicketButton = ({ userId }: TicketButtonProps) => {
   const [open, setOpen] = useState(false)
   return (
     <div className="">
@@ -29,9 +29,9 @@ const TicketButton = ({ userid }: TicketButtonProps) => {
             <p className="text-lg">Dette er din billett til arrangementet</p>
           </div>
           <div className="flex flex-col items-center pt-10">
-            {userid && (
+            {userId && (
               <QRCodeSVG
-                value={userid}
+                value={userId}
                 size={256}
                 imageSettings={{
                   src: "https://old.online.ntnu.no/wiki/70/plugin/attachments/download/680/",

apps/web/src/app/events/components/queries.ts
@@ -5,10 +5,13 @@ interface Props {
   attendanceId: string
 }
 export const useGetAttendee = ({ userId, attendanceId }: Props) => {
-  return trpc.event.attendance.getAttendee.useQuery({
-    attendanceId,
-    userId: userId ?? ""
-  }, {
-    enabled: Boolean(userId)
-  })
+  return trpc.event.attendance.getAttendee.useQuery(
+    {
+      attendanceId,
+      userId: userId ?? "",
+    },
+    {
+      enabled: Boolean(userId),
+    }
+  )
 }