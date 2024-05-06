5eef988350b14311a6b4f4f17373020c7c557850
henrikskog <henrikskog01@gmail.com>
2024-05-06 12:49:38 +0200

    revert page

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,49 +1,27 @@
-"use client"
-import { Loader } from "@mantine/core"
-import { useMemo } from "react"
-import { useCreateOfflineMutation } from "../modules/offline/mutations/use-create-offline-mutation"
-import { trpc } from "../utils/trpc"
-import { OfflineEditCard } from "./(dashboard)/offline/[id]/edit-card"
-import { OfflineDetailsContext } from "./(dashboard)/offline/[id]/provider"
-import { useOfflineWriteForm } from "./(dashboard)/offline/write-form"
+import { authOptions } from "@dotkomonline/auth/src/dashboard.app"
+import { Card, Container, Flex, Text, Title } from "@mantine/core"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { SignInButton } from "./SignInButton"
 
-export default function DashboardPage() {
-  const { data, isLoading } = trpc.offline.all.useQuery()
-  console.log(data)
-
-  const value = useMemo(
-    () =>
-      data === undefined || isLoading
-        ? null
-        : {
-            offline: data[0],
-          },
-    [data, isLoading]
-  )
-
-  const createOffline = useCreateOfflineMutation()
-
-  const FormComponent = useOfflineWriteForm({
-    onSubmit: async (data) => {
-      console.log("onSubmit", data)
-      createOffline.mutate({
-        fileId: data.fileId,
-        imageId: data.image.id,
-        title: data.title,
-        published: data.published,
-      })
-      console.log("Created offline")
-    },
-  })
-
-  if (value === null) {
-    return <Loader />
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions)
+  if (session !== null) {
+    redirect("/event")
   }
 
   return (
-    <OfflineDetailsContext.Provider value={value}>
-      <OfflineEditCard />
-      <FormComponent />
-    </OfflineDetailsContext.Provider>
+    <Flex justify="center" align="center">
+      <Container mt="xl">
+        <Card>
+          <Flex direction="column" gap="2">
+            <Title>Logg inn</Title>
+            <Text>Vennligst logg inn for å bruke Monoweb Admin</Text>
+
+            <SignInButton />
+          </Flex>
+        </Card>
+      </Container>
+    </Flex>
   )
 }
