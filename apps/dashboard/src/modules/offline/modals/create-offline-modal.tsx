d45deedd2da3f618c84ca2dcf5556da9fa348c0f
henrikskog <henrikskog01@gmail.com>
2024-05-06 19:52:53 +0200

    remove debugging code

--- apps/dashboard/src/modules/offline/modals/create-offline-modal.tsx
+++ apps/dashboard/src/modules/offline/modals/create-offline-modal.tsx
@@ -18,19 +18,6 @@ export const CreateOfflineModal: FC<ContextModalProps> = ({ context, id }) => {
       })
       close()
     },
-    defaultValues: {
-      image: {
-        id: "01HX71J61P58FHHQ8TGZ3ZMZAF",
-        altText: "test",
-        assetKey: "2b8f47bd-ff45-4b0f-b5d7-83b886899100img2.jpeg",
-        crop: {
-          top: 69.03125,
-          left: 33.9921875,
-          width: 266.7578125,
-          height: 254.35546875,
-        },
-      },
-    },
   })
   return <FormComponent />
 }
