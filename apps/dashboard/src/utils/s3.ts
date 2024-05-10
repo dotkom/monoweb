4f86fc2c694af4cdfd4f5bced27ac10f5baa624d
henrikskog <henrikskog01@gmail.com>
2024-05-10 14:22:48 +0200

    add cloudflare url

--- apps/dashboard/src/utils/s3.ts
+++ apps/dashboard/src/utils/s3.ts
@@ -1,5 +1,6 @@
 import { env } from "@dotkomonline/env"
 import type { File } from "../../stubs/file/File"
+import type { ImageVariant } from "@dotkomonline/types"
 
 // Expected response: 204 No Content. Returns resource URL if successful.
 export async function s3UploadFile(file: File, fields: Record<string, string>, url: string) {
@@ -28,4 +29,45 @@ export async function s3UploadFile(file: File, fields: Record<string, string>, u
 
 export function buildAssetUrl(key: string) {
   return `https://${env.NEXT_PUBLIC_STATIC_ASSETS_BUCKET}/testing/${key}`
+}
+
+function buildFinalCloudflareUrl(options: string, assetUrl: string) {
+
+  const cloudflareImagesBaseUrl = "https://onli.no/cdn-cgi/image"
+
+  return `${cloudflareImagesBaseUrl}/${options}/${assetUrl}`
+}
+
+type Size = { w?: number; h?: number }
+export function buildImgUrl(image: ImageVariant, size?: Size) {
+  const assetUrl = buildAssetUrl(image.asset.key)
+
+  const options: string[] = []
+
+  const addOpt = (key: string, value: string) => options.push(`${key}=${value}`)
+
+  addOpt("scale", "fit-down") // https://developers.cloudflare.com/images/transform-images/transform-via-url/#recommended-image-sizes
+
+  if (image.crop) {
+    addOpt("trim.width", image.crop.width.toString())
+    addOpt("trim.height", image.crop.height.toString())
+    addOpt("trim.left", image.crop.left.toString())
+    addOpt("trim.top", image.crop.top.toString())
+  }
+
+  if (size?.w) {
+    addOpt("width", size.w.toString())
+  }
+
+  if (size?.h) {
+    addOpt("height", size.h.toString())
+  }
+
+  if (!size?.h && !size?.w) {
+    addOpt("width", "1920")
+  }
+
+  const optionsString = options.join(",")
+
+  return buildFinalCloudflareUrl(optionsString, assetUrl)
 }
