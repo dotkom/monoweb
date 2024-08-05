6bcff5adaa528d29ac41b7db797a554c4f06ddad
madsab <mads.barnes@hotmail.com>
2024-08-05 14:41:08 +0200

    add PhoneInput component + validatePhoneNumber method

---
+++ packages/ui/src/components/PhoneInput/PhoneInput.stories.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { PhoneInput } from "./PhoneInput";
+
+export default {
+  title: "PhoneInput",
+};
+export const Default = () => {
+  const [phoneValue, setPhoneValue] = useState();
+  return <PhoneInput onChange={() => setPhoneValue} value={phoneValue} />;
+};
---
+++ packages/ui/src/components/PhoneInput/PhoneInput.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { Component, FC, forwardRef, LegacyRef } from "react"
+import PhoneInputWithCountry, {
+  DefaultInputComponentProps,
+  Props,
+  isValidPhoneNumber as isValidPhoneNumberOrigin,
+} from "react-phone-number-input"
+import { cn } from "../../utils"
+import "react-phone-number-input/style.css"
+import { Label } from "@radix-ui/react-label"
+import { cva } from "cva"
+
+export interface CustomPhoneInputProps extends Props<DefaultInputComponentProps> {
+  defaultValue?: string
+  label?: string
+  width?: string
+  error?: string
+}
+
+const PhoneInput = forwardRef<CustomPhoneInputProps, DefaultInputComponentProps>(
+  ({ defaultValue, label, width, error, ...props }, ref) => (
+    <div className={cn("flex flex-col", width)}>
+      {label && (
+        <Label htmlFor={props.id} className="mb-2">
+          {label} {props.required && <span className="text-red-11">*</span>}
+        </Label>
+      )}
+      <PhoneInputWithCountry
+        onChange={props.onChange}
+        value={defaultValue}
+        className={cn("flex w-full space-x-2")}
+        numberInputProps={{ className: input({ error: Boolean(error), disabled: props.disabled }) }}
+        limitMaxLength
+        defaultCountry="NO"
+        {...props}
+      />
+      {typeof error === "string" && <span className="text-red-11 mt-1 text-xs ml-12">{error}</span>}
+    </div>
+  )
+)
+
+const input = cva(
+  "border-solid border outline-none focus:border-blue-7 bg-white-3 hover:bg-white-4 active:bg-white-5 rounded-md p-2 focus:ring-2 focus:ring-brand",
+  {
+    variants: {
+      error: {
+        true: "text-red-11 border-red-7",
+        false: "text-slate-12 border-slate-6",
+      },
+      disabled: {
+        true: "cursor-not-allowed text-slate-10",
+      },
+    },
+  }
+)
+
+const isValidPhoneNumber = (phoneNumber: string) => {
+  return isValidPhoneNumberOrigin(phoneNumber)
+}
+
+export { PhoneInput, isValidPhoneNumber }
