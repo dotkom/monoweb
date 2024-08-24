c2394b7d8ab65491865b7dce82ee13f35465890a
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-08-24 12:13:13 +0200

    Remove test coide

--- apps/web/src/components/views/ProfileView/index.tsx
+++ apps/web/src/components/views/ProfileView/index.tsx
@@ -1,26 +1,8 @@
 import type { NextPage } from "next"
 import type { User } from "next-auth"
-import {Button} from "../../../../../../packages/ui";
-import { Issuer } from "openid-client";
-import {oidcConfig} from "@/app/dataporten/study/callback/route";
-import {getAuthorizationUrl} from "@/utils/dataporten";
-import client from "@/api/sanity";
-import {cookies} from "next/headers";
 
-const ProfilePoster: NextPage<{ user: User }> = async ({ user }) => {
-  const authorizationUrl = await getAuthorizationUrl();
-
-  const groups = cookies().getAll().map(cookie => cookie.name);
-  return <div className="w-full p-3 mt-3 flex justify-center border-2 rounded-lg">
-    <p>
-      <a href={authorizationUrl}>Logg inn med FEIDE</a>
-    </p>
-    <br/>
-
-    <pre>
-      {JSON.stringify(groups, null, 2)}
-    </pre>
-  </div>
+const ProfilePoster: NextPage<{ user: User }> = ({ user }) => {
+  return <div className="w-full p-3 mt-3 flex justify-center border-2 rounded-lg">ProfilePoster</div>
 }
 
 export default ProfilePoster
