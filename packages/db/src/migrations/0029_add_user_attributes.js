03e4bd454dff9ecb2d51b93d6eee83ed1053c53d
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-09-17 21:03:18 +0200

    Fix down migration

--- packages/db/src/migrations/0029_add_user_attributes.js
+++ packages/db/src/migrations/0029_add_user_attributes.js
@@ -2,8 +2,6 @@
 export async function up(db) {
   await db.schema
     .alterTable("ow_user")
-    .dropColumn("auth0_sub") // rename to auth0_id
-    .addColumn("auth0_id", "varchar(128)", (col) => col.unique().notNull()) //https://community.auth0.com/t/is-256-a-safe-max-length-for-a-user-id/34040/9
     .addColumn("family_name", "varchar(255)", (col) => col.notNull())
     .addColumn("middle_name", "varchar(255)", (col) => col.notNull())
     .addColumn("given_name", "varchar(255)", (col) => col.notNull())
@@ -12,9 +10,26 @@ export async function up(db) {
     .addColumn("phone", "varchar(255)")
     .addColumn("gender", "varchar(255)")
     .execute()
+
+  await db.schema
+    .alterTable("ow_user")
+    .renameColumn("auth0_sub", "auth0_id").execute()
 }
 
 /** @param db {import('kysely').Kysely} */
 export async function down(db) {
-  await db.schema.dropTable("ow_user").execute()
+  await db.schema
+    .alterTable("ow_user")
+    .dropColumn("family_name")
+    .dropColumn("middle_name")
+    .dropColumn("given_name")
+    .dropColumn("picture")
+    .dropColumn("allergies")
+    .dropColumn("phone")
+    .dropColumn("gender")
+    .execute()
+
+  await db.schema
+    .alterTable("ow_user")
+    .renameColumn("auth0_id", "auth0_sub").execute()
 }
