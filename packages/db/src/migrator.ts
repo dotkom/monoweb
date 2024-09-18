a8b925f639b8628efd39d691b5a0da0382b33f31
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-09-18 14:34:55 +0200

    test

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -1,17 +1,18 @@
 import fs from "node:fs/promises"
 import path from "node:path"
+import os from "node:os"
 import { FileMigrationProvider, type Kysely, Migrator } from "kysely"
 import type { Database } from "./"
 
 export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) => {
-  console.log("VALUE:");
-  console.log((urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname)
+  const migrationFolder = (urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname
+  console.log(`migrationFolder: ${migrationFolder}`)
   return new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
       path,
-      migrationFolder: (urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname,
+      migrationFolder: os.platform() === "win32" ? migrationFolder.slice(1) : migrationFolder,
     }),
   })
 }
