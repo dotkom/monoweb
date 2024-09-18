f6517c59a2a08b68f6e5be35d97dc4a1327ef136
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-09-18 14:29:20 +0200

    add test code

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -3,8 +3,10 @@ import path from "node:path"
 import { FileMigrationProvider, type Kysely, Migrator } from "kysely"
 import type { Database } from "./"
 
-export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) =>
-  new Migrator({
+export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) => {
+  console.log("VALUE:");
+  console.log((urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname)
+  return new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
@@ -12,3 +14,4 @@ export const createMigrator = (db: Kysely<Database>, urlOverride?: URL) =>
       migrationFolder: (urlOverride ?? new URL("migrations", new URL(import.meta.url))).pathname,
     }),
   })
+}
