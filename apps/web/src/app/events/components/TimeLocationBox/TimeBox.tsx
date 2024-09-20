13e07018f38b1a050f4bd1ba6976dbca685daf36
brage-andreas <bragerino+github@gmail.com>
2024-09-20 12:47:57 +0200

    feat: remove weekdays

--- apps/web/src/app/events/components/TimeLocationBox/TimeBox.tsx
+++ apps/web/src/app/events/components/TimeLocationBox/TimeBox.tsx
@@ -18,12 +18,11 @@ export const TimeBox: FC<TimeBoxProps> = ({ event }) => {
   const { start, end, locationAddress, description, title: eventSummary } = event
 
   const weekdays = [formatWithIntl(start, IntlFormats.Weekday)]
-  const dates = [formatWithIntl(start, IntlFormats.Date)]
-  const time = formatWithIntl(start, IntlFormats.Time)
+  const dates = [formatWithIntl(start, IntlFormats.ShortDate)]
 
   if (start.getDate() !== end.getDate()) {
     weekdays.push(formatWithIntl(end, IntlFormats.Weekday))
-    dates.push(formatWithIntl(end, IntlFormats.Date))
+    dates.push(formatWithIntl(end, IntlFormats.ShortDate))
   }
 
   const gcalLink = createGoogleCalendarLink({
@@ -34,15 +33,28 @@ export const TimeBox: FC<TimeBoxProps> = ({ event }) => {
     end,
   })
 
+  // TODO refactor
   return (
     <div className="flex mb-8 mt-4">
       <div className="w-12 flex items-center">
         <Icon icon="tabler:clock" width={24} height={24} />
       </div>
-      <div className="flex flex-1 flex-col">
-        <span className="text-lg">{dates.join(" til ")}</span>
-        <span className="text-sm">{capitalize(weekdays.join(" til "))}</span>
-        <span className="text-sm">{time}</span>
+      <div className="flex flex-col">
+        <span className="flex flex-row gap-[1ch]">
+            <span className="flex flex-col">
+                <span className="text-lg">{dates[0]}</span>
+                <span className="text-lg">{formatWithIntl(start, IntlFormats.Time)} {dates.length === 1 ? ` til ${formatWithIntl(end, IntlFormats.Time)}` : null }</span>
+            </span>
+            {dates.length > 1 && (
+                <>
+                    <span className="text-lg">til</span>
+                    <span className="flex flex-col">
+                        <span className="text-lg">{dates[1]}</span>
+                        <span className="text-lg">kl. {formatWithIntl(end, IntlFormats.Time)}</span>
+                    </span>
+                </>
+            )}
+        </span>
       </div>
       <div className="flex items-center">
         <ActionLink
