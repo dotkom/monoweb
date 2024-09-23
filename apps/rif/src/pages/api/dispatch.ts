09af36a9a20e3dd89c6c7a320da5b150f845b9e5
Mats Larsen <mats@jun.codes>
2024-09-23 13:34:43 +0200

    Add back missing import

--- apps/rif/src/pages/api/dispatch.ts
+++ apps/rif/src/pages/api/dispatch.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { formSchema } from "../../app/form-schema"
 import { deliverConfirmationEmail, deliverNotificationEmail } from "../../email"
+import { createSpreadsheetRow } from "../../spreadsheet"
 
 export default async function Route(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
@@ -16,15 +17,14 @@ export default async function Route(req: NextApiRequest, res: NextApiResponse) {
   await createSpreadsheetRow(body)
 
   let response: Response = await deliverConfirmationEmail(body)
-  const data = await response.text()
 
   if (!response.ok) {
-    return res.status(response.status).send(data)
+    return res.status(response.status).send(await response.text())
   }
 
   response = await deliverNotificationEmail(body)
   if (!response.ok) {
-    return res.status(response.status).send(data)
+    return res.status(response.status).send(await response.text())
   }
 
   res.status(200).send("OK")
