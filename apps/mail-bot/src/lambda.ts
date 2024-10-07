be71f83f032465fcd0a2f66ad1048933a3fe141b
henrikskog <henrikskog01@gmail.com>
2024-10-07 16:09:30 +0200

    add prod guard for sentry.init

--- apps/mail-bot/src/lambda.ts
+++ apps/mail-bot/src/lambda.ts
@@ -4,15 +4,17 @@ import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Handler } from "a
 import { createExternalClients, createServiceLayer } from "./core"
 import { env } from "./env"
 
-Sentry.init({
-  dsn: env.SENTRY_DSN,
-  integrations: [nodeProfilingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: env.SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-})
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  })
+}
 
 export const handler: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2> = Sentry.wrapHandler(async (event) => {
   try {
