dd7591b3d070b0b2894ffdbcce1e358d087a3075
Sondre Alfnes <sondre.alfnes@gmail.com>
2024-10-16 19:57:07 +0200

    Fix lint

--- apps/dashboard/src/modules/job-listing/modals/create-job-listing-modal.tsx
+++ apps/dashboard/src/modules/job-listing/modals/create-job-listing-modal.tsx
@@ -8,8 +8,8 @@ export const CreateJobListingModal: FC<ContextModalProps> = ({ context, id }) =>
   const create = useCreateJobListingMutation()
   const FormComponent = useJobListingWriteForm({
     onSubmit: (data) => {
-      const createdAt = new Date();
-      create.mutate({...data, createdAt})
+      const createdAt = new Date()
+      create.mutate({ ...data, createdAt })
       close()
     },
   })
