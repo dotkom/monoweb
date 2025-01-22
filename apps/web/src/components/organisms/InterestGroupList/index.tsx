7ce4fd655ddf6ae31f4655eefad5616cc9c7632c
Sondre Alfnes <sondre.alfnes@gmail.com>
2025-01-22 19:55:30 +0100

    Linting

--- apps/web/src/components/organisms/InterestGroupList/index.tsx
+++ apps/web/src/components/organisms/InterestGroupList/index.tsx
@@ -1,20 +1,17 @@
-import InterestGroupListItem from "@/components/molecules/InterestGroupListItem";
-import { getServerClient } from "@/utils/trpc/serverClient";
+import InterestGroupListItem from "@/components/molecules/InterestGroupListItem"
+import { getServerClient } from "@/utils/trpc/serverClient"
 
 export const InterestGroupList = async () => {
-    const serverClient = await getServerClient();
-    const interestGroups = (await serverClient.interestGroup.all()).data;
+  const serverClient = await getServerClient()
+  const interestGroups = (await serverClient.interestGroup.all()).data
 
-    return (
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] w-10/12 2xl:grid-cols-4 gap-12 mx-auto">
-            {interestGroups.map((interestGroup, index) => (
-                <InterestGroupListItem
-                    key={index}
-                    interestGroup={interestGroup}
-                />
-            ))}
-        </div>
-    );
-};
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] w-10/12 2xl:grid-cols-4 gap-12 mx-auto">
+      {interestGroups.map((interestGroup, index) => (
+        <InterestGroupListItem key={index} interestGroup={interestGroup} />
+      ))}
+    </div>
+  )
+}
 
-export default InterestGroupList;
+export default InterestGroupList
