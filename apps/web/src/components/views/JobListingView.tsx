5e38fa2c4c9834c1e76c68a7423b7f5d5f4e7859
Sondre Alfnes <sondre.alfnes@gmail.com>
2025-01-22 19:33:58 +0100

    Lint

--- apps/web/src/components/views/JobListingView.tsx
+++ apps/web/src/components/views/JobListingView.tsx
@@ -1,17 +1,15 @@
-import type { JobListing } from "@dotkomonline/types";
-import { Button, Icon } from "@dotkomonline/ui";
-import { formatDate } from "@dotkomonline/utils";
-import Image from "next/image";
-import Link from "next/link";
-import type { FC } from "react";
+import type { JobListing } from "@dotkomonline/types"
+import { Button, Icon } from "@dotkomonline/ui"
+import { formatDate } from "@dotkomonline/utils"
+import Image from "next/image"
+import Link from "next/link"
+import type { FC } from "react"
 
 interface JobListingViewProps {
-  jobListing: JobListing;
+  jobListing: JobListing
 }
 
-export const JobListingView: FC<JobListingViewProps> = (
-  props: JobListingViewProps
-) => {
+export const JobListingView: FC<JobListingViewProps> = (props: JobListingViewProps) => {
   const {
     title,
     applicationEmail,
@@ -28,20 +26,14 @@ export const JobListingView: FC<JobListingViewProps> = (
     locations,
     start,
     deadline,
-  } = props.jobListing;
+  } = props.jobListing
 
   return (
     <div className="mx-auto mt-10 flex w-10/12 justify-between">
       <div className="w-1/3">
         <div className="relative pb-10">
           {company.image && (
-            <Image
-              src={company.image}
-              width={4000}
-              height={250}
-              alt="company_image"
-              className="rounded-lg"
-            />
+            <Image src={company.image} width={4000} height={250} alt="company_image" className="rounded-lg" />
           )}
         </div>
 
@@ -50,65 +42,33 @@ export const JobListingView: FC<JobListingViewProps> = (
         <div className="bg-slate-12 mx-auto mb-14 mt-10 h-[0.5px] w-full" />
         <Link href="/career">
           <div className="flex items-center">
-            <Icon
-              icon="mdi:arrow-left"
-              className="text-blue-9"
-              width={20}
-              height={20}
-            />
-            <p className="m-0 border-0 pl-2 text-base text-blue-500">
-              {" "}
-              ANDRE MULIGHETER
-            </p>
+            <Icon icon="mdi:arrow-left" className="text-blue-9" width={20} height={20} />
+            <p className="m-0 border-0 pl-2 text-base text-blue-500"> ANDRE MULIGHETER</p>
           </div>
         </Link>
         <div className="bg-slate-9 mb-7 mt-3 h-[0.5px] w-full" />
         <div className="my-3 flex items-center">
-          <Icon
-            icon="mdi:globe"
-            className="text-blue-9"
-            width={20}
-            height={20}
-          />
+          <Icon icon="mdi:globe" className="text-blue-9" width={20} height={20} />
           <p className="m-0 pl-2">{locations}</p>
         </div>
         <div className="bg-slate-9 my-7 h-[0.5px] w-full" />
         <div className="my-3 flex items-center">
           <div className="text-blue-9 mb-[-3px] inline">
-            <Icon
-              icon="mdi:clock-outline"
-              className="text-blue-9"
-              width={20}
-              height={20}
-            />
+            <Icon icon="mdi:clock-outline" className="text-blue-9" width={20} height={20} />
           </div>
-          {deadline ? (
-            <p className="m-0 pl-2">{formatDate(deadline)}</p>
-          ) : (
-            <p className="m-0 pl-2">Ingen frist</p>
-          )}
+          {deadline ? <p className="m-0 pl-2">{formatDate(deadline)}</p> : <p className="m-0 pl-2">Ingen frist</p>}
         </div>
         <div className="bg-slate-9 my-7 h-[0.5px] w-full" />
         <div className="my-3 flex items-center">
           <div className="text-blue-9 mb-[-3px] inline">
-            <Icon
-              icon="mdi:briefcase-outline"
-              className="text-blue-9"
-              width={20}
-              height={20}
-            />
+            <Icon icon="mdi:briefcase-outline" className="text-blue-9" width={20} height={20} />
           </div>
           <p className="m-0 pl-2">{employment}</p>
         </div>
         <div className="bg-slate-9 my-7 h-[0.5px] w-full" />
         <div className="my-3 flex items-center">
           <div className="text-blue-9 mb-[-3px] inline">
-            <Icon
-              icon="mdi:content-copy"
-              className="text-blue-9"
-              width={20}
-              height={20}
-            />
+            <Icon icon="mdi:content-copy" className="text-blue-9" width={20} height={20} />
           </div>
           <p className="m-0 pl-2">{employment}</p>
         </div>
@@ -135,5 +95,5 @@ export const JobListingView: FC<JobListingViewProps> = (
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
