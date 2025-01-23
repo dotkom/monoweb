dotkom/monoweb

commit 7e321a0369cc83605918da6e21a8bad2c208cb09

Add interest group page (#1052)
Co-authored-by: Victoria Barseth <victobar@stud.ntnu.no>
Co-authored-by: André Klarpås <andre.klarpaas@gmail.com>
Co-authored-by: Eline Fondevik <elifon@live.no>
Co-authored-by: Henrik Hørlück Berg <henrik@horluck.no>

apps/web/src/app/interest-groups/[id]/page.tsx
@@ -1,0 +1,49 @@
+import OnlineIcon from "@/components/atoms/OnlineIcon";
+import { getServerClient } from "@/utils/trpc/serverClient";
+import Link from "next/link";
+
+const InterestPage = async ({ params: { id } }: { params: { id: string } }) => {
+    const serverClient = await getServerClient();
+    const interestGroup = await serverClient.interestGroup.get(id);
+
+    return interestGroup != null ? (
+        <div className="p-14 my-16 mx-auto border-slate-3 rounded-lg border shadow-md w-10/12 ">
+            <Link
+                className="bg-transparent p-0 hover:underline mb-4"
+                href={`/interest-groups`}>
+                {"<"} Tilbake
+            </Link>
+            <div className="flex md:flex-row flex-col-reverse">
+                <div className="mr-4">
+                    <h2 className="text-lg border-none !mt-4">
+                        {interestGroup.name}
+                    </h2>
+                    <p className="mt-2">
+                        {interestGroup.longDescription
+                            ? interestGroup.longDescription
+                            : interestGroup.description}
+                    </p>
+                    {interestGroup.joinInfo && (
+                        <section className="mt-10">
+                            <h3 className="text-lg border-none">Kontakt</h3>
+                            {interestGroup.joinInfo}
+                        </section>
+                    )}
+                    {interestGroup.link && (
+                        <Link
+                            className="hover:underline text-blue-10 block mt-4"
+                            href={`${interestGroup.link}`}
+                        >
+                            Gå til Wikisiden her
+                        </Link>
+                    )}
+                </div>
+                <OnlineIcon className="max-w-[200px] min-w-[200px] py-auto m-auto sm:mb-auto mb-9 mt-2" />
+            </div>
+        </div>
+    ) : (
+        <h3>Not found</h3>
+    );
+};
+
+export default InterestPage;

apps/web/src/components/organisms/InterestGroupList/index.tsx
@@ -1,0 +1,20 @@
+import InterestGroupListItem from "@/components/molecules/InterestGroupListItem";
+import { getServerClient } from "@/utils/trpc/serverClient";
+
+export const InterestGroupList = async () => {
+    const serverClient = await getServerClient();
+    const interestGroups = (await serverClient.interestGroup.all()).data;
+
+    return (
+        <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] w-10/12 2xl:grid-cols-4 gap-12 mx-auto">
+            {interestGroups.map((interestGroup, index) => (
+                <InterestGroupListItem
+                    key={index}
+                    interestGroup={interestGroup}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default InterestGroupList;

apps/web/src/app/interest-groups/page.tsx
@@ -1,0 +1,7 @@
+import InterestGroupsView from "@/components/views/InterestGroupsView";
+
+const InterestPage = async () => {
+    return <InterestGroupsView />;
+};
+
+export default InterestPage;

apps/web/src/components/molecules/InterestGroupListItem/index.tsx
@@ -1,0 +1,31 @@
+import OnlineIcon from "@/components/atoms/OnlineIcon";
+import { InterestGroup } from "@dotkomonline/types";
+import Link from "next/link";
+import { FC } from "react";
+
+export interface InterestGroupListItemProps {
+    interestGroup: InterestGroup;
+}
+
+const InterestGroupListItem: FC<InterestGroupListItemProps> = (
+    props: InterestGroupListItemProps
+) => (
+    <div className="p-4 py-8 text-center m-1 shadow-md rounded-lg border-slate-3 border min-w-[250px] flex flex-col">
+        <OnlineIcon className="w-5/12 mx-auto max-w-[150px] min-w-[120px]" />
+        <h2 className="!text-3xl border-none !mt-4">
+            {props.interestGroup.name}
+            {!props.interestGroup.isActive && <p>[inaktiv]</p>}
+        </h2>
+        <p className="mt-2 mb-6 text-left px-3">
+            {props.interestGroup.description}
+        </p>
+        <Link
+            className="mt-auto text-xl hover:underline"
+            href={`interest-groups/${props.interestGroup.id}`}
+        >
+            Les mer
+        </Link>
+    </div>
+);
+
+export default InterestGroupListItem;

apps/web/src/components/views/InterestGroupsView/index.tsx
@@ -1,0 +1,49 @@
+import InterestGroupList from "@/components/organisms/InterestGroupList";
+import { Button } from "@dotkomonline/ui";
+import Link from "next/link";
+
+export const InterestGroupsView = () => {
+    return (
+        <div className="my-8">
+            <h1 className="mb-5">Interessegrupper</h1>
+            <p>
+                På denne siden finner du informasjon om alle de forskjellige
+                interessegruppene i online. Ser du noe som ser interessant ut?
+                Ta kontakt og møt noen med samme interesser som deg.
+                Interessegruppene i Online er grupper for alle mulige slags
+                interesser. Har du og en kompis eller to en
+                sær/stilig/fantastisk interesse? Opprett en interessegruppe!
+            </p>
+            <p className="pt-4">
+                Mer informasjon om hvordan dette gjøres finnes{" "}
+                <Link
+                    className="hover:underline text-blue-8"
+                    href={
+                        "https://wiki.online.ntnu.no/info/innsikt-og-interface/interessegrupper/"
+                    }
+                >
+                    her.
+                </Link>
+            </p>
+            <Link
+                href={
+                    "https://docs.google.com/forms/d/e/1FAIpQLSebaBslZ3nmh2wubQ_mPJYYU2XNIRlJZ1BooFuH7y6wxylaWA/viewform"
+                }
+            >
+                <Button className="mr-4 mt-4">Opprett interessegruppe</Button>
+            </Link>
+            <Link
+                href={
+                    "https://docs.google.com/forms/d/e/1FAIpQLScr27q7C4gDvzHXajydznfFxPs7JaGpgYrNX4RPiVRvUHXVGg/viewform?pli=1"
+                }
+            >
+                <Button>Søk om støtte</Button>
+            </Link>
+            <div className="mt-16">
+                <InterestGroupList />
+            </div>
+        </div>
+    );
+};
+
+export default InterestGroupsView;

apps/dashboard/src/app/(dashboard)/interest-group/write-form.tsx
@@ -30,9 +30,19 @@ export const useInterestGroupWriteForm = ({
         withAsterisk: true,
         rows: 5,
       }),
+      longDescription: createTextareaInput({
+        label: "Lang beskrivelse",
+        withAsterisk: false,
+        rows: 5,
+      }),
+      joinInfo: createTextareaInput({
+        label: "Hvordan bli med",
+        withAsterisk: false,
+        rows: 5,
+      }),
       link: createTextInput({
-        label: "Slack Link",
-        placeholder: "https://onlinentnu.slack.com/archives/C03S8TX1L",
+        label: "Wiki Link",
+        placeholder: "https://wiki.online.ntnu.no/info/innsikt-og-interface/interessegrupper/",
         type: "url",
       }),
       isActive: createCheckboxInput({

apps/dashboard/src/modules/interest-group/use-interest-group-table.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type { InterestGroup } from "@dotkomonline/types"
-import { Icon } from "@iconify/react"
 import { Anchor } from "@mantine/core"
 import { createColumnHelper, getCoreRowModel, useReactTable } from "@tanstack/react-table"
 import Link from "next/link"
@@ -24,14 +23,14 @@ export const useInterestGroupTable = ({ data }: Props) => {
         cell: (info) => info.getValue(),
       }),
       columnHelper.accessor("link", {
-        header: () => "Slack Link",
+        header: () => "Wiki Link",
         cell: (info) => {
           const link = info.getValue()
           if (!link) return
 
           return (
             <Anchor href={link} target="_blank" rel="noopener noreferrer">
-              <Icon icon="logos:slack-icon" width={23} height={23} />
+              Wiki
             </Anchor>
           )
         },

commit 6ca5fb23e4a7b320efd4ec32af95d6425082c3c1

Fix broken lint regression from 7e321a03

apps/web/src/app/interest-groups/[id]/page.tsx
@@ -8,7 +8,7 @@ const InterestPage = async ({ params: { id } }: { params: { id: string } }) => {
 
   return interestGroup != null ? (
     <div className="p-14 my-16 mx-auto border-slate-3 rounded-lg border shadow-md w-10/12 ">
-      <Link className="bg-transparent p-0 hover:underline mb-4" href={`/interest-groups`}>
+      <Link className="bg-transparent p-0 hover:underline mb-4" href={"/interest-groups"}>
         {"<"} Tilbake
       </Link>
       <div className="flex md:flex-row flex-col-reverse">

apps/web/src/components/organisms/InterestGroupList/index.tsx
@@ -8,6 +8,7 @@ export const InterestGroupList = async () => {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] w-10/12 2xl:grid-cols-4 gap-12 mx-auto">
       {interestGroups.map((interestGroup, index) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: This is a static list
         <InterestGroupListItem key={index} interestGroup={interestGroup} />
       ))}
     </div>