9df4c1fa95f7904faf881bbcbb3e4bb7682299fa
Mats Larsen <mats@jun.codes>
2025-01-25 13:16:53 +0900

    Support optional certificate authority for database connection

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,27 @@
 import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely"
-import pg from "pg"
+import pg, { type PoolConfig } from "pg"
 import type { DB } from "./db.generated"
 export type { DB as Database } from "./db.generated"
 
 export { createMigrator } from "./migrator"
 
-export const createKysely = (url: string) => {
+export const createKysely = (url: string, certificateAuthority?: string) => {
+  // If the caller has provided a certificate authority, we pass it down to
+  // node-postgres. This is required in production, but not for a local postgres
+  // database from the docker compose configuration.
+  let sslOptions: PoolConfig["ssl"] | undefined = undefined
+  if (certificateAuthority !== undefined) {
+    sslOptions = {
+      rejectUnauthorized: true,
+      ca: certificateAuthority,
+    }
+  }
+
   return new Kysely<DB>({
     dialect: new PostgresDialect({
       pool: new pg.Pool({
         connectionString: url,
+        ssl: sslOptions,
       }),
     }),
     plugins: [new CamelCasePlugin()],
--- tools/migrator/src/env.ts
+++ tools/migrator/src/env.ts
@@ -2,4 +2,5 @@ import { createEnvironment, variable } from "@dotkomonline/environment"
 
 export const env = createEnvironment({
   DATABASE_URL: variable,
+  AWS_RDS_CERTIFICATE_AUTHORITY: variable.optional(),
 })
