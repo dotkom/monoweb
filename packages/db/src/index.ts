6dc7f729f88fb84e6e7e536fb7b42933368b3931
Mats Larsen <mats@jun.codes>
2025-01-25 13:36:07 +0900

    Emit warning if no certificate authority is provided

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,8 +2,10 @@ import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely"
 import pg, { type PoolConfig } from "pg"
 import type { DB } from "./db.generated"
 export type { DB as Database } from "./db.generated"
+import { getLogger } from "@dotkomonline/logger"
 
 export { createMigrator } from "./migrator"
+const logger = getLogger("@dotkomonline/db")
 
 export const createKysely = (url: string, certificateAuthority?: string) => {
   // If the caller has provided a certificate authority, we pass it down to
@@ -15,6 +17,15 @@ export const createKysely = (url: string, certificateAuthority?: string) => {
       rejectUnauthorized: true,
       ca: certificateAuthority,
     }
+  }
+
+  // If there are no certificates attached, we issue a warning so that it's easy
+  // to catch misconfigurations in non-local environments.
+  if (sslOptions === undefined) {
+    logger.warn(
+      "No certificate authority provided. This is required if you are connecting to staging/production databases."
+    )
+    logger.warn("- This configuration is only supported for local development.")
   }
 
   return new Kysely<DB>({
