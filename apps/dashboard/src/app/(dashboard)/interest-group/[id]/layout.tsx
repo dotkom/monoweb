18747a55a577d197e60e736de90fd4f07414187c
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-03 12:48:51 +0100

    Rewrite illegal async client layout component

--- apps/dashboard/src/app/(dashboard)/interest-group/[id]/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/interest-group/[id]/layout.tsx
@@ -1,15 +1,15 @@
 "use client"
 
 import { Loader } from "@mantine/core"
-import { type PropsWithChildren, useMemo } from "react"
+import { type PropsWithChildren, use, useMemo } from "react"
 import { trpc } from "../../../../trpc"
 import { InterestGroupDetailsContext } from "./provider"
 
-export default async function InterestGroupDetailsLayout({
+export default function InterestGroupDetailsLayout({
   children,
   params,
 }: PropsWithChildren<{ params: Promise<{ id: string }> }>) {
-  const { id } = await params
+  const { id } = use(params)
   const { data, isLoading } = trpc.interestGroup.get.useQuery(id)
   const value = useMemo(
     () =>
