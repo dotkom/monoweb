890229ce9a3745e5eb02a981c635cb0816a1e4c9
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-03 12:41:12 +0100

    Fix invalid react keys

--- apps/web/src/components/organisms/Footer/sections/SoMeSection.tsx
+++ apps/web/src/components/organisms/Footer/sections/SoMeSection.tsx
@@ -3,17 +3,17 @@ import { FacebookIcon, GitHubIcon, InstagramIcon, SlackIcon } from "../../../ico
 
 export const SoMeSection = () => {
   const links = [
-    { icon: <SlackIcon key="slack" />, url: "https://onlinentnu.slack.com/" },
-    { icon: <GitHubIcon key="github" />, url: "https://github.com/dotkom/monoweb" },
-    { icon: <InstagramIcon key="instagram" />, url: "https://www.instagram.com/online_ntnu/" },
-    { icon: <FacebookIcon key="facebook" />, url: "https://www.facebook.com/groups/1547182375336132" },
+    { icon: <SlackIcon />, url: "https://onlinentnu.slack.com/", key: "slack" },
+    { icon: <GitHubIcon />, url: "https://github.com/dotkom/monoweb", key: "github" },
+    { icon: <InstagramIcon />, url: "https://www.instagram.com/online_ntnu/", key: "instagram" },
+    { icon: <FacebookIcon />, url: "https://www.facebook.com/groups/1547182375336132", key: "facebook" },
   ]
 
   return (
     <ul className="mx-8 mb-4 flex sm:justify-center">
       {links.map((link) => (
-        <Link href={link.url} key={link.icon.key}>
-          <li key={link.icon.key} className="mx-4 w-16 cursor-pointer">
+        <Link href={link.url} key={link.key}>
+          <li className="mx-4 w-16 cursor-pointer">
             {link.icon}
           </li>
         </Link>
