298bbad3928c370664da4555b9294913a47b685e
Johannes Aas <johhaa@stud.ntnu.no>
2025-02-05 22:34:25 +0100

    Fix jos crappy merge

--- apps/web/src/app/events/calendar/[year]/[month]/page.tsx
+++ apps/web/src/app/events/calendar/[year]/[month]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
-import { getServerClient } from "@/utils/trpc/serverClient"
 import { getCalendarArray } from "@/components/organisms/EventCalendar/getCalendarArray"
 import EventCalendar from "@/components/organisms/EventCalendar"
+import { server } from '@/utils/trpc/server';
 
 const EventPage = async ({ params }: { params: { year: string; month: string } }) => {
 
@@ -12,16 +12,17 @@ const EventPage = async ({ params }: { params: { year: string; month: string } }
         redirect('/events/calendar'); // Redirects to current month if invalid
     }
 
-    const serverClient = await getServerClient()
-
     // TODO: get all events between to given dates (start and end of month inc. days from adjesent months to fill first and last week)
-    const events = await serverClient.event.all({take: 50})
+    const events = await server.event.all({take: 50})
 
     // -1 because zero indexed months
     let cal = getCalendarArray(year, month - 1, events)
     
     return (
+        <>
+        <h1>Arrangement</h1>
         <EventCalendar cal={cal} />
+        </>
     );
 };
 
--- apps/web/src/app/events/calendar/page.tsx
+++ apps/web/src/app/events/calendar/page.tsx
@@ -1,14 +1,11 @@
-import { getServerClient } from "@/utils/trpc/serverClient"
 import { getCalendarArray } from "@/components/organisms/EventCalendar/getCalendarArray"
 import EventCalendar from "@/components/organisms/EventCalendar"
 import { Icon } from "@dotkomonline/ui"
+import { server } from "@/utils/trpc/server";
 
 const EventPage = async () => {
-
-  const serverClient = await getServerClient()
-
   // TODO: get all events between to given dates for the current month
-  const events = await serverClient.event.all({take: 50})
+  const events = await server.event.all({take: 50})
 
   const now = new Date();
 
