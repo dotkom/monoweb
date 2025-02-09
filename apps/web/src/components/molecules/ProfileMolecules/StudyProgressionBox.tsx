1f6654659d3f2b7f577422c2ee04a44fb91420ce
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-09 15:12:24 +0100

    Add grade calculation and other stuff

--- apps/web/src/components/molecules/ProfileMolecules/StudyProgressionBox.tsx
+++ apps/web/src/components/molecules/ProfileMolecules/StudyProgressionBox.tsx
@@ -1,21 +1,39 @@
 import type { FC } from "react"
 import StudentProgress from "../StudentProgress/StudentProgress"
+import type { Membership, User } from "@dotkomonline/types"
+import { membershipGrade } from "@dotkomonline/types"
+
+function memberDescription(membership: Membership) {
+  switch (membership.type) {
+    case "BACHELOR":
+      return "Bachelor"
+    case "MASTER":
+      return "Master"
+    case "SOCIAL":
+      return "Sosialt medlem"
+    case "KNIGHT":
+      return "Ridder"
+  }
+}
 
 type StudyProgressionBoxProps = {
   className?: string
+  user: User
 }
 
-const StudyProgressionBox: FC<StudyProgressionBoxProps> = ({ className }) => {
-  // TODO: Implement dynamic way of getting grade
-  const startYear = 2022
-  const grade = 3
+const StudyProgressionBox: FC<StudyProgressionBoxProps> = ({ className, user }) => {
+  if (!user.membership) {
+    return null
+  }
+
+  const grade = membershipGrade(user.membership);
 
   return (
     <div className={`flex flex-col items-center justify-center gap-3 ${className ?? ""}`}>
-      <p>{grade}. klasse</p>
-      <p>Studiesett: {startYear}</p>
+      { grade && <p className="text-3xl">{grade}. klasse</p> }
+      <p className="text-3xl">{memberDescription(user.membership)}</p>
       <div className="transform scale-90">
-        <StudentProgress year={grade} />
+        { grade && <StudentProgress year={grade} /> }
       </div>
     </div>
   )
