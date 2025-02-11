f29de35195c59a084578aea828526ddc1a2cc069
Andrefkl <andre.klarpaas@gmail.com>
2025-02-11 22:58:21 +0100

    feat: add interest group member

--- packages/gateway-trpc/src/modules/interest-group/interest-group-router.ts
+++ packages/gateway-trpc/src/modules/interest-group/interest-group-router.ts
@@ -1,5 +1,5 @@
 import { PaginateInputSchema } from "@dotkomonline/core"
-import { InterestGroupSchema, InterestGroupWriteSchema } from "@dotkomonline/types"
+import { InterestGroupSchema, InterestGroupWriteSchema, UserSchema } from "@dotkomonline/types"
 import { z } from "zod"
 import { protectedProcedure, publicProcedure, t } from "../../trpc"
 
@@ -24,4 +24,18 @@ export const interestGroupRouter = t.router({
   delete: protectedProcedure
     .input(InterestGroupSchema.shape.id)
     .mutation(async ({ input, ctx }) => await ctx.interestGroupService.delete(input)),
+  getByMember: publicProcedure
+    .input(UserSchema.shape.id)
+    .query(async ({ input, ctx }) => await ctx.interestGroupService.getByMember(input)),
+  getMembers: publicProcedure
+    .input(InterestGroupSchema.shape.id)
+    .query(async ({ input, ctx }) => await ctx.interestGroupService.getMembers(input)),
+  addMember: protectedProcedure
+    .input(z.object({ interestGroupId: InterestGroupSchema.shape.id, userId: UserSchema.shape.id }))
+    .mutation(async ({ input, ctx }) => await ctx.interestGroupService.addMember(input.interestGroupId, input.userId)),
+  removeMember: protectedProcedure
+    .input(z.object({ interestGroupId: InterestGroupSchema.shape.id, userId: UserSchema.shape.id }))
+    .mutation(
+      async ({ input, ctx }) => await ctx.interestGroupService.removeMember(input.interestGroupId, input.userId)
+    ),
 })
