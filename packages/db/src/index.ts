89ead1ddf175932f2b2d232a9eeef6bc3b58af8d
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-12 19:53:24 +0100

    Revert "Make sure testcontainers aren't imported in vercel"
    This reverts commit 681c13391040c2278fadf0d16948c2de2d293a6c.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,7 @@
 import { type Prisma, PrismaClient } from "@prisma/client"
 import type { DefaultArgs } from "@prisma/client/runtime/library"
 
+export * from "./testDatabases"
 export * as dbSchemas from "./generatedSchemas"
 
 export type DBClient = PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
@@ -8,5 +9,3 @@ export const createPrisma = (databaseUrl: string): DBClient =>
   new PrismaClient({
     datasourceUrl: databaseUrl,
   })
-
-export const testDatabases = process.env.VERCEL === "1" ? undefined : await import("./testDatabases")
