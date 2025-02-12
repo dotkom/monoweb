ce333425c01a46255d598c4afc5bb8f27dea6def
Andrefkl <78028923+Andrefkl@users.noreply.github.com>
2025-02-12 18:42:57 +0100

    Add interest group members (#1167)

--- packages/core/src/modules/interest-group/interest-group-service.ts
+++ packages/core/src/modules/interest-group/interest-group-service.ts
@@ -1,4 +1,10 @@
-import type { InterestGroup, InterestGroupId, InterestGroupWrite } from "@dotkomonline/types"
+import type {
+  InterestGroup,
+  InterestGroupId,
+  InterestGroupMember,
+  InterestGroupWrite,
+  UserId,
+} from "@dotkomonline/types"
 import type { Collection, Pageable } from "../../query"
 import type { InterestGroupRepository } from "./interest-group-repository"
 
@@ -8,6 +14,10 @@ export interface InterestGroupService {
   create(values: InterestGroupWrite): Promise<InterestGroup>
   update(id: InterestGroupId, values: InterestGroupWrite): Promise<InterestGroup>
   delete(id: InterestGroupId): Promise<void>
+  getMembers(id: InterestGroupId): Promise<InterestGroupMember[]>
+  getByMember(userId: UserId): Promise<InterestGroup[]>
+  addMember(interestGroupId: InterestGroupId, userId: UserId): Promise<InterestGroupMember>
+  removeMember(interestGroupId: InterestGroupId, userId: UserId): Promise<void>
 }
 
 export class InterestGroupServiceImpl implements InterestGroupService {
@@ -31,5 +41,21 @@ export class InterestGroupServiceImpl implements InterestGroupService {
 
   async delete(id: InterestGroupId): Promise<void> {
     return this.interestGroupRepository.delete(id)
+  }
+
+  async getMembers(id: InterestGroupId): Promise<InterestGroupMember[]> {
+    return this.interestGroupRepository.getAllMembers(id)
+  }
+
+  async getByMember(userId: UserId): Promise<InterestGroup[]> {
+    return this.interestGroupRepository.getAllByMember(userId)
+  }
+
+  async addMember(interestGroupId: InterestGroupId, userId: UserId): Promise<InterestGroupMember> {
+    return this.interestGroupRepository.addMember(interestGroupId, userId)
+  }
+
+  async removeMember(interestGroupId: InterestGroupId, userId: UserId): Promise<void> {
+    this.interestGroupRepository.removeMember(interestGroupId, userId)
   }
 }
