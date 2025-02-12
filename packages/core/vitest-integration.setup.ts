a3b5a3ee2153080a1dc3d4c5bf9cffd4cf820118
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-12 20:01:49 +0100

    Fix container stuff?

--- packages/core/vitest-integration.setup.ts
+++ packages/core/vitest-integration.setup.ts
@@ -1,11 +1,17 @@
 import type { S3Client } from "@aws-sdk/client-s3"
-import { type DBClient, getTestContainerDatabase } from "@dotkomonline/db"
+import { createPrisma, type DBClient, migrateTestDatabase } from "@dotkomonline/db"
 import type { ManagementClient } from "auth0"
 import { afterAll, afterEach, beforeAll, beforeEach } from "vitest"
 import { mockDeep } from "vitest-mock-extended"
 import { type StripeAccount, createServiceLayer } from "./src"
+import { PostgreSqlContainer } from "@testcontainers/postgresql"
 
 const MIGRATION_TIMEOUT = 10_000
+
+const POSTGRES_IMAGE = "postgres:15-alpine"
+const DB_USERNAME = "owuser"
+const DB_PASSWORD = "owpassword"
+const DB_NAME = "test"
 
 export async function createServiceLayerForTesting() {
   const s3Client = mockDeep<S3Client>()
@@ -21,11 +27,28 @@ export async function createServiceLayerForTesting() {
   })
 }
 
+export async function getTestContainerDatabase() {
+  const container = await new PostgreSqlContainer(POSTGRES_IMAGE)
+    .withUsername(DB_USERNAME)
+    .withPassword(DB_PASSWORD)
+    .withDatabase(DB_NAME)
+    .withReuse()
+    .start()
+
+  console.log(`Container started: ${container}`)
+
+  return `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${container.getHost()}:${container.getFirstMappedPort()}/${DB_NAME}` 
+}
+
 let dbClient: DBClient
 export let core: Awaited<ReturnType<typeof createServiceLayerForTesting>>
 
 beforeAll(async () => {
-  dbClient = await getTestContainerDatabase()
+  const dbUrl = await getTestContainerDatabase();
+
+  await migrateTestDatabase(dbUrl)
+
+  dbClient = await createPrisma(dbUrl)
   core = await createServiceLayerForTesting()
 }, MIGRATION_TIMEOUT)
 
--- packages/db/src/testDatabases.ts
+++ packages/db/src/testDatabases.ts
@@ -1,16 +1,9 @@
-import { spawn } from "node:child_process"
-import { PostgreSqlContainer } from "@testcontainers/postgresql"
-import { createPrisma } from "."
+import { spawn } from "child_process"
 
-const SCHEMA_FILE_PATH = `${import.meta.dirname}/../prisma/schema.prisma`
-const PRISMA_BIN_PATH = `${import.meta.dirname}/../node_modules/.bin/prisma`
+const SCHEMA_FILE_PATH = import.meta.dirname + "/../prisma/schema.prisma"
+const PRISMA_BIN_PATH = import.meta.dirname + "/../node_modules/.bin/prisma"
 
-const POSTGRES_IMAGE = "postgres:15-alpine"
-const DB_USERNAME = "owuser"
-const DB_PASSWORD = "owpassword"
-const DB_NAME = "test"
-
-function migrateTestDatabase(dbUrl: string) {
+export function migrateTestDatabase(dbUrl: string) {
   return new Promise<void>((resolve, reject) => {
     const proc = spawn(
       PRISMA_BIN_PATH,
@@ -35,19 +28,4 @@ function migrateTestDatabase(dbUrl: string) {
   })
 }
 
-export async function getTestContainerDatabase() {
-  const container = await new PostgreSqlContainer(POSTGRES_IMAGE)
-    .withUsername(DB_USERNAME)
-    .withPassword(DB_PASSWORD)
-    .withDatabase(DB_NAME)
-    .withReuse()
-    .start()
 
-  console.log(`Container started: ${container}`)
-
-  const dbUrl = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${container.getHost()}:${container.getFirstMappedPort()}/${DB_NAME}`
-
-  await migrateTestDatabase(dbUrl)
-
-  return createPrisma(dbUrl)
-}
