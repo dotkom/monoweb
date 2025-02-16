883de9871459ca9a349a4be68a9702eb27bb7a61
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-16 01:57:52 +0100

    Fix test container setup

--- packages/core/vitest-integration.setup.ts
+++ packages/core/vitest-integration.setup.ts
@@ -1,17 +1,10 @@
 import type { S3Client } from "@aws-sdk/client-s3"
-import { type DBClient, createPrisma, migrateTestDatabase } from "@dotkomonline/db"
-import { PostgreSqlContainer } from "@testcontainers/postgresql"
+import { type DBClient, createPrisma } from "@dotkomonline/db"
 import type { ManagementClient } from "auth0"
-import { afterAll, afterEach, beforeAll, beforeEach } from "vitest"
+import { afterAll, afterEach, beforeEach } from "vitest"
 import { mockDeep } from "vitest-mock-extended"
 import { type StripeAccount, createServiceLayer } from "./src"
-
-const MIGRATION_TIMEOUT = 10_000
-
-const POSTGRES_IMAGE = "postgres:15-alpine"
-const DB_USERNAME = "owuser"
-const DB_PASSWORD = "owpassword"
-const DB_NAME = "test"
+import { getTestClient } from "@dotkomonline/db/src/testDatabases"
 
 export async function createServiceLayerForTesting() {
   const s3Client = mockDeep<S3Client>()
@@ -27,19 +20,6 @@ export async function createServiceLayerForTesting() {
   })
 }
 
-export async function getTestContainerDatabase() {
-  const container = await new PostgreSqlContainer(POSTGRES_IMAGE)
-    .withUsername(DB_USERNAME)
-    .withPassword(DB_PASSWORD)
-    .withDatabase(DB_NAME)
-    .withReuse()
-    .start()
-
-  console.log(`Container started: ${container}`)
-
-  return `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${container.getHost()}:${container.getFirstMappedPort()}/${DB_NAME}`
-}
-
 let dbClient: DBClient
 export let core: Awaited<ReturnType<typeof createServiceLayerForTesting>>
 
@@ -48,17 +28,6 @@ afterAll(async () => {
 })
 
 beforeEach(async () => {
-  const dbUrl = await getTestContainerDatabase()
-
-  await migrateTestDatabase(dbUrl)
-  dbClient = await createPrisma(dbUrl)
+  dbClient = await getTestClient();
   core = await createServiceLayerForTesting()
-
-  await dbClient.$queryRaw`BEGIN TRANSACTION;`
-})
-
-afterEach(async () => {
-  await dbClient.$queryRaw`ROLLBACK;`
-
-  console.log("after:", await core.jobListingService.getLocations())
 })
--- packages/db/src/testDatabases.ts
+++ packages/db/src/testDatabases.ts
@@ -1,11 +1,29 @@
 // biome-ignore lint/style/useNodejsImportProtocol: Cannot import with node path on vercel
-import { spawn } from "child_process"
+import { spawn } from "node:child_process"
+import { PostgreSqlContainer } from "@testcontainers/postgresql"
+import { createPrisma } from "."
 
 const SCHEMA_FILE_PATH = `${import.meta.dirname}/../prisma/schema.prisma`
 const PRISMA_BIN_PATH = `${import.meta.dirname}/../node_modules/.bin/prisma`
 
-export function migrateTestDatabase(dbUrl: string) {
-  return new Promise<void>((resolve, reject) => {
+const POSTGRES_IMAGE = "postgres:15-alpine"
+const DB_USERNAME = "owuser"
+const DB_PASSWORD = "owpassword"
+const DB_NAME = "test"
+
+async function getTestContainerDatabase() {
+  const container = await new PostgreSqlContainer(POSTGRES_IMAGE)
+    .withUsername(DB_USERNAME)
+    .withPassword(DB_PASSWORD)
+    .withDatabase(DB_NAME)
+    .withReuse()
+    .start()
+
+  return `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${container.getHost()}:${container.getFirstMappedPort()}/${DB_NAME}`
+}
+
+function migrateTestDatabase(dbUrl: string) {
+  return new Promise<void>(async (resolve, reject) => {
     const proc = spawn(
       PRISMA_BIN_PATH,
       ["migrate", "reset", "--force", "--skip-generate", "--schema", SCHEMA_FILE_PATH],
@@ -20,11 +38,18 @@ export function migrateTestDatabase(dbUrl: string) {
 
     proc.on("exit", (code) => {
       if (code === 0) {
-        console.log("MIGRATION COMPLETED!")
         resolve()
       } else {
         reject(`Test database migration failed with code ${code}`)
       }
     })
   })
+}
+
+export async function getTestClient() {
+  const dbUrl = await getTestContainerDatabase()
+
+  await migrateTestDatabase(dbUrl);
+
+  return createPrisma(dbUrl)
 }
