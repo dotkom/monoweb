aeb58b19049f97ce83f081d97c1cbc8792330cb0
Andrefkl <andre.klarpaas@gmail.com>
2025-02-24 22:01:54 +0100

    remove generateStaticParams from committee page

--- apps/web/src/app/committees/[id]/page.tsx
+++ apps/web/src/app/committees/[id]/page.tsx
@@ -1,11 +1,6 @@
 import { CommitteeView } from "@/components/views/CommitteeView"
 import { server } from "@/utils/trpc/server"
 
-export const generateStaticParams = async () => {
-  const committeeIds = await server.group.allIdsByType.query("COMMITTEE")
-  return committeeIds.map((id) => ({ id }))
-}
-
 const CommitteePage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
   const committee = await server.group.getByType.query({ groupId: id, type: "COMMITTEE" })
