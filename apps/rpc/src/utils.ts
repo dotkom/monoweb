517b7cc7783b300b6f2983b748edeb9019cdb677
henrikskog <henrikskog01@gmail.com>
2025-02-28 18:48:56 +0100

    lint

--- apps/rpc/src/utils.ts
+++ apps/rpc/src/utils.ts
@@ -1,16 +1,18 @@
-import { env } from "./env";
+import { env } from "./env"
 
 // Verify AWS credentials using STS
 export async function verifyAwsCredentials() {
-    try {
-      // Import the STS client and GetCallerIdentityCommand
-      const { STSClient, GetCallerIdentityCommand } = await import("@aws-sdk/client-sts");
-      const stsClient = new STSClient({ region: env.AWS_REGION });
+  try {
+    // Import the STS client and GetCallerIdentityCommand
+    const { STSClient, GetCallerIdentityCommand } = await import("@aws-sdk/client-sts")
+    const stsClient = new STSClient({ region: env.AWS_REGION })
 
     // GetCallerIdentity is the lightest-weight operation to verify credentials
     const identity = await stsClient.send(new GetCallerIdentityCommand({}))
     console.info(`AWS credentials verified successfully. Using identity: \n${identity.Arn}`)
   } catch (error) {
-    console.warn("WARNING: Unable to verify AWS credentials. Functionality involving aws services, e.g. uploading files, will fail.")
+    console.warn(
+      "WARNING: Unable to verify AWS credentials. Functionality involving aws services, e.g. uploading files, will fail."
+    )
   }
-  }
+}
