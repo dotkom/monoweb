64496a462e24f87799f05f6657e9e7c46645501b
Mats Jun Larsen <mats@jun.codes>
2025-03-13 13:43:06 +0100

    Configure Sentry for RPC application

--- apps/rpc/src/env.ts
+++ apps/rpc/src/env.ts
@@ -16,4 +16,6 @@ export const env = createEnvironment({
   MANAGEMENT_OAUTH_CLIENT_ID: variable,
   MANAGEMENT_OAUTH_CLIENT_SECRET: variable,
   MANAGEMENT_TENANT_DOMAIN_ID: variable,
+  // Sentry DSN should only be set in production
+  SENTRY_DSN: variable.optional(),
 })
---
+++ apps/rpc/src/sentry.ts
@@ -0,0 +1,8 @@
+import Sentry from "@sentry/node"
+import { env } from "./env"
+
+if (env.SENTRY_DSN !== undefined) {
+  Sentry.init({
+    dsn: env.SENTRY_DSN,
+  })
+}
