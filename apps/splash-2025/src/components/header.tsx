85ec004e6eaad5b8194a0ade5540518d0dcc940e
henrikskog <henrikskog01@gmail.com>
2025-03-19 18:55:59 +0100

    Start implementing header

--- apps/splash-2025/src/components/header.tsx
+++ apps/splash-2025/src/components/header.tsx
@@ -1,14 +1,10 @@
 export const Header = () => {
 	return (
-		<header className="bg-[#0d5474] grid justify-center box-border w-full">
-			<img
-				src={"https://wiki.online.ntnu.no/attachments/681-Online_hvit.png"}
-				alt="Linjeforeningen Online"
-				className="w-[26rem] max-w-[100vw] box-border p-6"
-			/>
-			<h2 className="text-white text-center tracking-wider mb-4">
-				Kvitteringsskjema
-			</h2>
+		<header className="bg-[#0d5474] flex justify-center w-full">
+			<a href="https://online.ntnu.no" className="text-white">Program</a>
+			<a href="https://online.ntnu.no" className="text-white">Ta kontakt</a>
+			<a href="https://online.ntnu.no" className="text-white">Søk komite</a>
+		
 		</header>
 	);
 };
