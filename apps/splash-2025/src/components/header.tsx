d25d9eca5c300e1b34e757e88c20cdb9b0dfc84d
henrikskog <henrikskog01@gmail.com>
2025-03-19 19:13:29 +0100

    fix ci stuff

--- apps/splash-2025/src/components/header.tsx
+++ apps/splash-2025/src/components/header.tsx
@@ -1,10 +1,15 @@
 export const Header = () => {
 	return (
 		<header className="bg-[#0d5474] flex justify-center w-full">
-			<a href="https://online.ntnu.no" className="text-white">Program</a>
-			<a href="https://online.ntnu.no" className="text-white">Ta kontakt</a>
-			<a href="https://online.ntnu.no" className="text-white">Søk komite</a>
-		
+			<a href="https://online.ntnu.no" className="text-white">
+				Program
+			</a>
+			<a href="https://online.ntnu.no" className="text-white">
+				Ta kontakt
+			</a>
+			<a href="https://online.ntnu.no" className="text-white">
+				Søk komite
+			</a>
 		</header>
 	);
 };
