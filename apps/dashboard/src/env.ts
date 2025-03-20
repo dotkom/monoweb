598700f5bb395a203def8d175f3e3c31ef122047
Mats Jun Larsen <mats@jun.codes>
2025-03-20 04:07:15 +0100

    Add env vars to dashboard dockerfile

--- apps/dashboard/src/env.ts
+++ apps/dashboard/src/env.ts
@@ -6,6 +6,9 @@ export const env = createEnvironment(
     OAUTH_CLIENT_SECRET: variable,
     OAUTH_ISSUER: variable,
     AUTH_SECRET: variable,
+    // Behind a proxy (e.g AWS ALB) the Host header will be the proxy's host, so
+    // we tell AuthJs to trust the X-Forwarded-Host header instead.
+    AUTH_TRUST_HOST: variable.optional(),
     NEXT_PUBLIC_ORIGIN: variable.default("http://localhost:3002"),
     RPC_HOST: variable,
   },
