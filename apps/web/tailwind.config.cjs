d1c04d69198436a6aff924b058cf1a4647514f7a
Johannes Aas <johhaa@stud.ntnu.no>
2025-03-26 22:04:58 +0100

    Remove safelist patterns from Tailwind CSS configuration

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -3,5 +3,4 @@ const base = require("@dotkomonline/config/tailwind-preset")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
-  safelist: [{ pattern: /col-start-\d+/ }, { pattern: /col-span-\d+/ }],
 }
