ffb70eb9a1c208fcd83bc68dedcad9c92a245ce4
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-03-26 17:39:06 +0100

    Add basic styling to navbar

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from "./components/header";
 
 function App() {
 	return (
-		<div>
+		<div className="bg-[#6B1414] min-h-screen">
 			<Header />
 			<main>Team onboarding ftw 1</main>
 		</div>
--- apps/splash-2025/src/components/header.tsx
+++ apps/splash-2025/src/components/header.tsx
@@ -1,19 +1,25 @@
 export const Header = () => {
 	return (
-		<header className="bg-brand flex justify-center w-full">
-			<a href="https://online.ntnu.no" className="flex flex-col text-white">
-				<img src="" alt="" />
-				Tilbake til hovedsiden
+		<header className="w-full sticky text-white p-10 flex justify-center">
+			<a href="https://online.ntnu.no" className="absolute mx-12 top-0 left-0">
+				<div className="bg-brand items-center p-5 flex flex-col gap-3 text-center font-bold">
+					<img src="/Online_hvit_o.svg" alt="Online logo" className="size-10" />
+					Gå til<br />hovedsiden
+				</div>
+				<div className="border-b-[2rem] border-b-transparent border-x-[4rem] border-x-brand" />
 			</a>
-			<a href="#program" className="text-white">
-				Program
-			</a>
-			<a href="https://online.ntnu.no" className="text-white">
-				Ta kontakt
-			</a>
-			<a href="https://online.ntnu.no" className="text-white">
-				Søk komite
-			</a>
+
+			<div className="flex justify-center gap-14 text-2xl pt-6 px-14 border-t-[3px] border-white">
+				<a href="#program" className="">
+					Program
+				</a>
+				<a href="https://online.ntnu.no" className="">
+					Ta kontakt
+				</a>
+				<a href="https://online.ntnu.no" className="">
+					Søk komite
+				</a>
+			</div>
 		</header>
 	);
 };
