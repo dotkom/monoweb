2f9049d1cde2fb252ca6c7fff6693a5ce54dcb4a
Johannes Aas <johhaa@stud.ntnu.no>
2025-03-26 22:33:53 +0100

    turns out I needed the tailwind safelist

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -3,4 +3,6 @@ const base = require("@dotkomonline/config/tailwind-preset")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
+  // don't remove the col-start- and col-span- safelisting, the events in the EventCalendar needs them to be placed correctly, trust me.
+  safelist: [{ pattern: /col-start-\d+/ }, { pattern: /col-span-\d+/ }],
 }
