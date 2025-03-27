e8614d9f0c7bf36b5f48867931a84b52b43adb79
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-03-27 13:21:31 +0100

    Style navbar for splash 2025 (#1324)

--- apps/splash-2025/src/components/header.tsx
+++ apps/splash-2025/src/components/header.tsx
@@ -1,19 +1,49 @@
 export const Header = () => {
 	return (
-		<header className="bg-brand flex justify-center w-full">
-			<a href="https://online.ntnu.no" className="flex flex-col text-white">
-				<img src="" alt="" />
-				Tilbake til hovedsiden
+		<header className="w-full sticky top-0 text-white p-10 flex justify-center">
+			<a
+				href="https://online.ntnu.no"
+				className="absolute mx-[5%] top-0 left-0 group"
+			>
+				<div className="bg-brand items-center py-6 pt-1 flex flex-col text-center font-bold">
+					<img
+						src="/Online_hvit_o.svg"
+						alt="Online logo"
+						className="size-10 m-5"
+					/>
+					<p className="group-hover:scale-[1.15] transition-all duration-500">
+						Gå til
+						<br />
+						hovedsiden
+					</p>
+				</div>
+				<div className="border-b-[2.5rem] border-b-transparent border-x-[4rem] border-x-brand" />
 			</a>
-			<a href="#program" className="text-white">
-				Program
-			</a>
-			<a href="https://online.ntnu.no" className="text-white">
-				Ta kontakt
-			</a>
-			<a href="https://online.ntnu.no" className="text-white">
-				Søk komite
-			</a>
+
+			<div className="flex justify-center gap-20 text-2xl font-light pt-6 px-[5%] border-t-2 border-white max-md:opacity-0">
+				<a
+					href="#program"
+					className="hover:scale-[1.15] transition-all duration-500"
+				>
+					Program
+				</a>
+				<a
+					href="https://docs.google.com/forms/d/e/1FAIpQLScvjEqVsiRIYnVqCNqbH_-nmYk3Ux6la8a7KZzsY3sJDbW-iA/viewform"
+					target="_blank"
+					className="hover:scale-[1.15] transition-all duration-500"
+					rel="noreferrer"
+				>
+					Ta kontakt
+				</a>
+				<a
+					href="https://opptak.online.ntnu.no"
+					target="_blank"
+					className="hover:scale-[1.15] transition-all duration-500"
+					rel="noreferrer"
+				>
+					Søk komité
+				</a>
+			</div>
 		</header>
 	);
 };
