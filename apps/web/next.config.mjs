aed62821fbbdd41432aa1fb02a1c07b947ac2d64
Mats Jun Larsen <mats@jun.codes>
2025-03-27 17:49:39 +0100

    Shamefully hoist prisma client
    See https://github.com/vercel/next.js/issues/68805

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -13,14 +13,6 @@ const config = {
       },
     ],
   },
-  transpilePackages: [
-    "@dotkomonline/auth",
-    "@dotkomonline/db",
-    "@dotkomonline/gateway-edge-nextjs",
-    "@dotkomonline/gateway-trpc",
-    "@dotkomonline/types",
-    "@dotkomonline/ui",
-  ],
   output: "standalone",
 }
 
