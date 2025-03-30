6e8a584ffca1eab9ee5771f7236e10e76839db62
henrikskog <henrikskog01@gmail.com>
2025-03-30 18:57:37 +0200

    Fetch event data from monoweb

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -1,5 +1,8 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Cloud } from "./components/cloud";
+import { Events } from "./components/events";
 import { Header } from "./components/header";
+import { queryClient } from "./lib/trpc";
 
 function App() {
 	return (
@@ -9,6 +12,9 @@ function App() {
 			<main>
 				Team onboarding ftw 1
 				<Cloud />
+				<QueryClientProvider client={queryClient}>
+					<Events />
+				</QueryClientProvider>
 			</main>
 		</div>
 	);
