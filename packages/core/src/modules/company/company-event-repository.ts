274df4e2a0010d9b9b668fa9d44149d9352039db
Mats Jun Larsen <mats@jun.codes>
2025-03-30 20:07:34 +0200

    Display all organizers together (#1343)

--- packages/core/src/modules/company/company-event-repository.ts
+++ packages/core/src/modules/company/company-event-repository.ts
@@ -1,8 +1,9 @@
 import type { DBClient } from "@dotkomonline/db"
-import type { CompanyId, Event } from "@dotkomonline/types"
+import type { Company, CompanyId, Event, EventId } from "@dotkomonline/types"
 
 export interface CompanyEventRepository {
   getEventsByCompanyId(company: CompanyId): Promise<Event[]>
+  getCompaniesByEventId(event: EventId): Promise<Company[]>
 }
 
 export class CompanyEventRepositoryImpl implements CompanyEventRepository {
@@ -14,5 +15,9 @@ export class CompanyEventRepositoryImpl implements CompanyEventRepository {
 
   async getEventsByCompanyId(companyId: CompanyId) {
     return this.db.event.findMany({ where: { companies: { some: { companyId } } } })
+  }
+
+  async getCompaniesByEventId(eventId: EventId) {
+    return this.db.company.findMany({ where: { events: { some: { eventId } } } })
   }
 }
