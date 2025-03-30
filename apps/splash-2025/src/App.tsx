40da3a0c4e0601029cb3f55261cb1926ba9e531b
henrikskog <henrikskog01@gmail.com>
2025-03-30 18:57:37 +0200

    Fetch event data from monoweb

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -1,25 +1,29 @@
-import { DebugInfo } from "./components/DebugInfo.tsx"
-import { LinjeforeningInfo } from "./components/LinjeforeningInfo.tsx"
-import { Footer } from "./components/footer"
+import { DebugInfo } from "@/components/DebugInfo.tsx"
+import { LinjeforeningInfo } from "@/components/LinjeforeningInfo.tsx"
+import { Footer } from "@/components/footer.tsx"
+import { Hero } from "@/components/hero.tsx"
+import { SponsorPlaceholderIcon } from "@/components/icons/SponsorPlaceholderIcon.tsx"
+import { HeroData } from "@/data/hero.ts"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { Events } from "./components/events"
 import { Header } from "./components/header"
-import { Hero } from "./components/hero"
-import { SponsorPlaceholderIcon } from "./components/icons/SponsorPlaceholderIcon.tsx"
-import { HeroData } from "./data/hero"
+import { queryClient } from "./lib/trpc"
 
 export function App() {
   return (
-    <div className="bg-[#6B1414] min-h-screen">
-      <Header />
-
-      <main>
-        <Hero {...HeroData} />
-        <DebugInfo />
-        <div className="bg-brand">
-          <LinjeforeningInfo />
-        </div>
-      </main>
-
-      <Footer sponsorLogo={<SponsorPlaceholderIcon />} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-[#6B1414] min-h-screen">
+        <Header />
+        <main>
+          <Hero {...HeroData} />
+          <DebugInfo />
+          <div className="bg-brand">
+            <LinjeforeningInfo />
+          </div>
+        </main>
+        <Events />
+        <Footer sponsorLogo={<SponsorPlaceholderIcon />} />
+      </div>
+    </QueryClientProvider>
   )
 }
