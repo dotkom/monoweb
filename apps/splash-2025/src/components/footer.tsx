ea73d1353bb72ec961bc89b676166bd5f1639241
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-04-02 18:20:52 +0200

    Create waves for footer

--- apps/splash-2025/src/components/footer.tsx
+++ apps/splash-2025/src/components/footer.tsx
@@ -1,12 +1,16 @@
 import type { ReactNode } from "react";
 import { SoMeLinks } from "./someLinks";
+import { FooterWave } from "./icons";
 
 export const Footer = ({ sponsorLogo }: { sponsorLogo?: ReactNode }) => {
 	return (
 		<footer className="text-white">
-			<div className="bg-[#C1842E] flex flex-col items-center p-4">
-				<h5 className="text-2xl m-3">Har du noen spørsmål?</h5>
-				<p>
+			<div className="overflow-hidden">
+				<FooterWave color="#C1842E" className="size-[135%]" />
+			</div>
+			<div className="bg-[#C1842E] flex flex-col items-center pb-4">
+				<h5 className="text-4xl m-3 mt-0">Har du noen spørsmål?</h5>
+				<p className="text-xl text-center mx-4">
 					Dersom du lurer på noe, ikke nøl med å ta kontakt! Du kan nå oss på{" "}
 					<a href="mailto:kontakt@online.ntnu.no" className="underline">
 						kontakt@online.ntnu.no
@@ -16,8 +20,13 @@ export const Footer = ({ sponsorLogo }: { sponsorLogo?: ReactNode }) => {
 			</div>
 
 			{sponsorLogo && (
-				<div className="bg-[#F9B759] flex justify-center p-4">
-					{sponsorLogo}
+				<div className="overflow-hidden">
+					<div className="bg-[#C1842E] ">
+						<FooterWave color="#F9B759" className="h-[100%] w-[135%] relative left-[-30%]" />
+					</div>
+					<div className="bg-[#F9B759] flex justify-center p-10">
+						{sponsorLogo}
+					</div>
 				</div>
 			)}
 		</footer>
--- apps/splash-2025/src/components/icons/index.ts
+++ apps/splash-2025/src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as FacebookIcon } from "./FacebookIcon";
 export { default as InstagramIcon } from "./InstagramIcon";
 export { default as SlackIcon } from "./SlackIcon";
 export { default as SponsorPlaceholderIcon } from "./SponsorPlaceholderIcon";
+export { default as FooterWave } from "./FooterWave";
