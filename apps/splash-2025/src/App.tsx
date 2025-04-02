73f22ea0bad2564cdde8183368c8e12f843e70fa
tharyng <theary.ngu@gmail.com>
2025-04-02 19:00:13 +0200

    [Splash] Responsive code update + linjeforening info (#1358)

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -1,14 +1,17 @@
-import { Cloud } from "./components/cloud";
+import { Debug_info } from "./components/debug_info";
 import { Header } from "./components/header";
+import { Linjeforening_info } from "./components/linjeforening_info";
 
 function App() {
 	return (
-		<div className="bg-[#6B1414] min-h-screen bg-[url(/public/bakgrunnsBilde.jpg)] bg-no-repeat bg-cover bg-center">
+		<div className="bg-[#6B1414] min-h-screen">
+			<div className="bg-[url(/bakgrunnsBilde.jpg)] h-screen bg-no-repeat bg-cover bg-center" />
 			<Header />
 
 			<main>
 				Team onboarding ftw 1
-				<Cloud />
+				<Debug_info />
+				<Linjeforening_info />
 			</main>
 		</div>
 	);
