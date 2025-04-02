8f444cc142fb9e14ef242d1b8c378ee513d3ad86
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-04-02 19:24:40 +0200

    Extract hero to its own component

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -1,16 +1,17 @@
 import { Debug_info } from "./components/debug_info";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import { Hero } from "./components/hero";
 import { SponsorPlaceholderIcon } from "./components/icons";
 import { Linjeforening_info } from "./components/linjeforening_info";
 
 function App() {
 	return (
 		<div className="bg-[#6B1414] min-h-screen">
-			<div className="bg-[url(/bakgrunnsBilde.jpg)] h-screen bg-no-repeat bg-cover bg-center" />
 			<Header />
 
 			<main>
+				<Hero />
 				<Debug_info />
 				<Linjeforening_info />
 			</main>
