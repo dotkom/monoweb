19011c48ed09ae7e88370595627fbaa28f9c9da4
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-04-02 19:20:32 +0200

    Nuke all navbar elements except return to homepage from splash

--- apps/splash-2025/src/components/header.tsx
+++ apps/splash-2025/src/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
 	return (
-		<header className="w-full sticky top-0 text-white p-10 flex justify-center z-50">
+		<header className="w-full fixed top-0 text-white p-10 flex justify-center z-50">
 			<a
 				href="https://online.ntnu.no"
 				className="absolute mx-[5%] top-0 left-0 group"
@@ -19,31 +19,6 @@ export const Header = () => {
 				</div>
 				<div className="border-b-[2.5rem] border-b-transparent border-x-[4rem] border-x-brand" />
 			</a>
-
-			<div className="flex justify-center gap-20 text-2xl font-light pt-6 px-[5%] border-t-2 border-white max-md:opacity-0">
-				<a
-					href="#program"
-					className="hover:scale-[1.15] transition-all duration-500"
-				>
-					Program
-				</a>
-				<a
-					href="https://docs.google.com/forms/d/e/1FAIpQLScvjEqVsiRIYnVqCNqbH_-nmYk3Ux6la8a7KZzsY3sJDbW-iA/viewform"
-					target="_blank"
-					className="hover:scale-[1.15] transition-all duration-500"
-					rel="noreferrer"
-				>
-					Ta kontakt
-				</a>
-				<a
-					href="https://opptak.online.ntnu.no"
-					target="_blank"
-					className="hover:scale-[1.15] transition-all duration-500"
-					rel="noreferrer"
-				>
-					Søk komité
-				</a>
-			</div>
 		</header>
 	);
 };
