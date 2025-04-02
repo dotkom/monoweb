4b930bb386f188d1a08a126ecb46634509b3fc43
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-02 15:00:20 +0200

    Lint

--- packages/gateway-trpc/src/modules/event/event-router.ts
+++ packages/gateway-trpc/src/modules/event/event-router.ts
@@ -69,10 +69,12 @@ export const eventRouter = t.router({
   // TODO: N+1 query, eventHostingGroupService and eventService should probably be merged
   all: publicProcedure
     .input(
-      z.object({
-        page: PaginateInputSchema,
-        filter: EventFilterSchema.optional(),
-      }).optional()
+      z
+        .object({
+          page: PaginateInputSchema,
+          filter: EventFilterSchema.optional(),
+        })
+        .optional()
     )
     .query(async ({ input, ctx }) => {
       const events = await ctx.eventService.getEvents(input?.page, input?.filter)
@@ -80,17 +82,17 @@ export const eventRouter = t.router({
       const interestGroups = events.map(async (e) => ctx.interestGroupService.getAllByEventId(e.id))
       const companies = events.map(async (e) => ctx.companyEventService.getCompaniesByEventId(e.id))
 
-    const groupResults = await Promise.all(groups)
-    const interestGroupResults = await Promise.all(interestGroups)
-    const companyResults = await Promise.all(companies)
+      const groupResults = await Promise.all(groups)
+      const interestGroupResults = await Promise.all(interestGroups)
+      const companyResults = await Promise.all(companies)
 
-    return events.map((event, i) => ({
-      ...event,
-      groups: groupResults[i],
-      interestGroups: interestGroupResults[i],
-      companies: companyResults[i],
-    }))
-  }),
+      return events.map((event, i) => ({
+        ...event,
+        groups: groupResults[i],
+        interestGroups: interestGroupResults[i],
+        companies: companyResults[i],
+      }))
+    }),
 
   // TODO: N+1 query, eventHostingGroupService and eventService should probably be merged
   recommended: publicProcedure.input(PaginateInputSchema).query(async ({ input, ctx }) => {
