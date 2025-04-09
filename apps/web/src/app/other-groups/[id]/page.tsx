f45bd8bcfa10c0f958c174ec281dccab268f5438
André Klarpås <78028923+andrefkl@users.noreply.github.com>
2025-04-09 21:40:31 +0200

    Other groups pages (#1383)

---
+++ apps/web/src/app/other-groups/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { OtherGroupView } from "@/components/views/OtherGroupView"
+import { server } from "@/utils/trpc/server"
+
+const OtherGroupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params
+  const otherGroup = await server.group.getByType.query({ groupId: id, type: "OTHERGROUP" })
+
+  return <OtherGroupView otherGroup={otherGroup} />
+}
+
+export default OtherGroupPage
