41e4fffda574f6d8459433c37ccbd9aded8bd52b
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-04-09 19:13:24 +0200

    [Splash] Create hero (#1380)

--- apps/splash-2025/src/components/hero.tsx
+++ apps/splash-2025/src/components/hero.tsx
@@ -1,5 +1,30 @@
-export const Hero = () => {
+import type { FC } from "react";
+import type { HeroProps } from "../data/hero";
+
+export const Hero: FC<HeroProps> = ({ background, banner }) => {
 	return (
-		<div className="bg-[url(/bakgrunnsBilde.jpg)] h-screen bg-no-repeat bg-cover bg-center" />
+		<div
+			className={`${background} h-screen bg-no-repeat bg-cover bg-center max-md:p-16 p-32 flex flex-col justify-center items-center text-white`}
+		>
+			<img
+				src={banner}
+				alt="Logo for Online's fadderuker"
+				className="scale-75"
+			/>
+			<h3 className="max-md:text-2xl text-4xl mb-5 text-center">
+				Velkommen til Online, linjeforeningen for informatikkstudenter ved NTNU!
+			</h3>
+			<p className="text-center md:text-lg">
+				Det er vi som sørger for at studietiden blir den beste tiden i ditt liv!
+				Vi i Online arrangerer utflukter, turer, fester, holder kurs og
+				bedriftspresentasjoner gjennom hele året.
+				<br />
+				Ny på master? Se{" "}
+				<a href="https://online.ntnu.no" className="underline text-brand-8">
+					viktig info om IT-ekskursjonen
+				</a>{" "}
+				OBS - kort frist!
+			</p>
+		</div>
 	);
 };
