a0f3b41e73cfc22edca3da6b4be243d3520ae702
Mats Jun Larsen <mats@jun.codes>
2025-04-12 16:10:49 +0900

    Pass down DOPPLER environment variables (#1410)

--- apps/invoicification/src/instrumentation-client.ts
+++ apps/invoicification/src/instrumentation-client.ts
@@ -4,6 +4,7 @@ import { env } from "./env"
 if (env.NEXT_PUBLIC_SENTRY_DSN !== undefined) {
   Sentry.init({
     dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+    environment: env.NEXT_PUBLIC_DOPPLER_ENVIRONMENT,
     debug: false,
   })
 }
