bc5248578fec5aef521011cd9b27aac69fe73dfb
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-04-23 23:39:34 +0200

    Implement footer design (#1428)

---
+++ apps/web/src/components/Footer/LogoSection.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@dotkomonline/ui"
+import { OnlineLogo } from "../atoms/OnlineLogo"
+
+export const LogoSection = () => (
+  <div className="flex flex-col md:flex-row gap-8 md:gap-16 w-full md:items-end">
+    <OnlineLogo className="h-12 w-fit" />
+    <Text element="h2" className="text-xl">
+      Linjeforening for informatikk ved NTNU
+    </Text>
+  </div>
+)
