1f0e791be8c845f56dcb1a9e831a264663abcf04
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-04-23 23:39:34 +0200

    Implement footer design (#1428)

--- apps/web/src/components/Footer/ContactSection.tsx
+++ apps/web/src/components/Footer/ContactSection.tsx
@@ -1,14 +1,78 @@
 import { Text } from "@dotkomonline/ui"
+import Link from "next/link"
 import type { FC } from "react"
 
 export const ContactSection: FC = () => (
-  <div className="text-white flex flex-col text-center font-medium">
-    <Text>Oppdaget en feil på nettsiden?</Text>
-    <Text>
-      Ta kontakt med{" "}
-      <a className="underline" href="mailto:dotkom@online.ntnu.no">
-        Dotkom
-      </a>
-    </Text>
+  <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 md:flex md:flex-row md:gap-24">
+    <div className="flex flex-col gap-2">
+      <Text element="h3" className="font-medium">
+        Hjelp oss bli bedre!
+      </Text>
+
+      <div>
+        <Text>Oppdaget en feil på nettsiden?</Text>
+        <Text>
+          Ta kontakt med{" "}
+          <a className="underline" href="mailto:dotkom@online.ntnu.no">
+            Dotkom
+          </a>
+        </Text>
+      </div>
+
+      <div>
+        <Text>Har du lyst til å bidra?</Text>
+        <Text>
+          Kildekoden finner du på{" "}
+          <a className="underline" href="https://www.github.com/dotkom">
+            Github
+          </a>
+        </Text>
+      </div>
+    </div>
+
+    <div className="flex flex-col gap-2">
+      <Text element="h3" className="font-medium">
+        Besøksadresse
+      </Text>
+
+      <div>
+        <Text>A4-137, Realfagbygget</Text>
+        <Text>Høgskoleringen 5</Text>
+        <Text>NTNU Gløshaugen</Text>
+      </div>
+    </div>
+
+    <div className="flex flex-col gap-2">
+      <Text element="h3" className="font-medium">
+        Kontaktinformasjon
+      </Text>
+
+      <div>
+        <Text>992 548 045 (org. nr.)</Text>
+        <Text>
+          <Link href="mailto:kontakt@online.ntnu.no" className="hover:underline">
+            kontakt@online.ntnu.no
+          </Link>
+        </Text>
+        <Text>
+          <Link href="mailto:okonomi@online.ntnu.no" className="hover:underline">
+            okonomi@online.ntnu.no
+          </Link>
+        </Text>
+        <Text>+47 986 69 907</Text>
+      </div>
+    </div>
+
+    <div className="flex flex-col gap-2">
+      <Text element="h3" className="font-medium">
+        Post og faktura
+      </Text>
+
+      <div>
+        <Text>Online Linjeforening</Text>
+        <Text>Sem Sælands vei 9</Text>
+        <Text>7491 Trondheim</Text>
+      </div>
+    </div>
   </div>
 )
--- apps/web/src/components/Footer/SocialSection.tsx
+++ apps/web/src/components/Footer/SocialSection.tsx
@@ -2,23 +2,45 @@ import { FacebookIcon } from "@/components/icons/FacebookIcon"
 import { GitHubIcon } from "@/components/icons/GitHubIcon"
 import { InstagramIcon } from "@/components/icons/InstagramIcon"
 import { SlackIcon } from "@/components/icons/SlackIcon"
+import { Text } from "@dotkomonline/ui"
 import Link from "next/link"
 
 export const SocialSection = () => {
+  const slate_6 = "#8b8d98"
+
   const links = [
-    { icon: <SlackIcon />, url: "https://onlinentnu.slack.com/", key: "slack" },
-    { icon: <GitHubIcon />, url: "https://github.com/dotkom/monoweb", key: "github" },
-    { icon: <InstagramIcon />, url: "https://www.instagram.com/online_ntnu/", key: "instagram" },
-    { icon: <FacebookIcon />, url: "https://www.facebook.com/groups/1547182375336132", key: "facebook" },
+    {
+      icon: <SlackIcon className="h-6" fill={slate_6} />, //
+      url: "https://onlinentnu.slack.com/",
+      key: "Slack",
+    },
+    {
+      icon: <InstagramIcon className="h-6" fill={slate_6} />,
+      url: "https://www.instagram.com/online_ntnu/",
+      key: "Instagram",
+    },
+    {
+      icon: <FacebookIcon className="h-6" fill={slate_6} />,
+      url: "https://www.facebook.com/groups/1547182375336132",
+      key: "Facebook",
+    },
+    {
+      icon: <GitHubIcon className="h-6" fill={slate_6} />, //
+      url: "https://github.com/dotkom/monoweb",
+      key: "Github",
+    },
   ]
 
   return (
-    <ul className="flex justify-center">
+    <div className="inline-grid grid-cols-1 sm:grid-cols-2 gap-6 justify-items-start md:flex md:flex-row md:gap-12">
       {links.map((link) => (
-        <Link href={link.url} key={link.key}>
-          <li className="mx-4 w-16 cursor-pointer">{link.icon}</li>
+        <Link href={link.url} key={link.key} className="flex flex-row gap-2 items-center hover:underline">
+          {link.icon}{" "}
+          <Text element="p" className="text-lg">
+            {link.key}
+          </Text>
         </Link>
       ))}
-    </ul>
+    </div>
   )
 }
