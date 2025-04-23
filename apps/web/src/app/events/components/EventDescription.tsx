8579d2c0a7c913f91f98c2ef49a0204fe536e02e
ErlendSae <128706170+ErlendSae@users.noreply.github.com>
2025-04-23 17:42:23 +0200

    fix: fixed eventdescription mdx (#1432)

--- apps/web/src/app/events/components/EventDescription.tsx
+++ apps/web/src/app/events/components/EventDescription.tsx
@@ -7,23 +7,26 @@ import {
   linkDialogPlugin,
   linkPlugin,
   listsPlugin,
+  markdownShortcutPlugin,
   thematicBreakPlugin,
 } from "@mdxeditor/editor"
 
 export function EventDescription({ description }: { description: string }) {
   return (
-    <MDXEditor
-      trim
-      readOnly
-      markdown={description}
-      plugins={[
-        listsPlugin(),
-        headingsPlugin(),
-        linkPlugin(),
-        linkDialogPlugin(),
-        thematicBreakPlugin(),
-        frontmatterPlugin(),
-      ]}
-    />
+    <div className="prose prose-headings:font-fraunces prose-headings:font-bold prose-headings:text-slate-12 dark:prose-headings:text-slate-1">
+      <MDXEditor
+        readOnly
+        markdown={description}
+        plugins={[
+          listsPlugin(),
+          headingsPlugin(),
+          linkPlugin(),
+          linkDialogPlugin(),
+          thematicBreakPlugin(),
+          frontmatterPlugin(),
+          markdownShortcutPlugin(),
+        ]}
+      />
+    </div>
   )
 }
