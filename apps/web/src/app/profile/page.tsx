80ca308264c7792cf6e8be0128955c754b359ce1
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-24 17:45:54 +0200

    Fix: prevent crash when not signed in

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth"
 import { ProfilePoster } from "@/components/views/ProfileView"
 import { server } from "@/utils/trpc/server"
+import { redirect } from "next/navigation"
 
 export default async function ProfilePage() {
+  const session = await auth.getServerSession()
+
+  if (session === null) {
+    redirect("/")
+  }
+
   const user = await server.user.getMe.query()
 
   return <ProfilePoster user={user} />
