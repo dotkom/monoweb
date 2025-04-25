f9604f4b01572e0b0b7053fac2568c16cca0642b
Mats Jun Larsen <mats@jun.codes>
2025-04-25 16:00:32 +0900

    Align footer with header and body (#1448)

--- apps/web/src/components/Footer/Footer.tsx
+++ apps/web/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { SocialSection } from "./SocialSection"
 
 export const Footer = () => (
   <footer className="pt-24 pb-12 text-slate-10">
-    <div className="max-w-screen-xl px-4 lg:px-12 flex flex-col gap-8 md:gap-12">
+    <div className="max-w-screen-xl px-10 flex flex-col gap-12">
       <LogoSection />
       <SocialSection />
       <ContactSection />
