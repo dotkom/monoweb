151e8ad809a190b7d7c8ed30df2da821bc7753f6
brage-andreas <bragerino+github@gmail.com>
2025-04-25 10:25:02 +0200

    Remove markdown rendering in event description
    This should be added back at some point

--- apps/web/src/app/events/components/EventDescription.tsx
+++ apps/web/src/app/events/components/EventDescription.tsx
@@ -1,32 +1,9 @@
-"use client"
-
-import {
-  MDXEditor,
-  frontmatterPlugin,
-  headingsPlugin,
-  linkDialogPlugin,
-  linkPlugin,
-  listsPlugin,
-  markdownShortcutPlugin,
-  thematicBreakPlugin,
-} from "@mdxeditor/editor"
+import { Text } from "@dotkomonline/ui";
 
 export function EventDescription({ description }: { description: string }) {
   return (
-    <div className="prose prose-headings:font-fraunces prose-headings:font-bold prose-headings:text-slate-12 dark:prose-headings:text-slate-1">
-      <MDXEditor
-        readOnly
-        markdown={description}
-        plugins={[
-          listsPlugin(),
-          headingsPlugin(),
-          linkPlugin(),
-          linkDialogPlugin(),
-          thematicBreakPlugin(),
-          frontmatterPlugin(),
-          markdownShortcutPlugin(),
-        ]}
-      />
-    </div>
+    <Text className="whitespace-pre-line">
+      {description}
+    </Text>
   )
 }
