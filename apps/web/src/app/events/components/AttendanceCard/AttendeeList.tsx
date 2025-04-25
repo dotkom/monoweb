ac33c1348ce6018424c6c9e4930dac542d1872ef
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-04-25 17:01:28 +0200

    Add numbering to attendees list (#1451)

--- apps/web/src/app/events/components/AttendanceCard/AttendeeList.tsx
+++ apps/web/src/app/events/components/AttendanceCard/AttendeeList.tsx
@@ -1,17 +1,29 @@
 import type { Attendee } from "@dotkomonline/types"
 import { Avatar, AvatarFallback, Icon, Text } from "@dotkomonline/ui"
+import clsx from "clsx"
 
 interface AttendeeListProps {
   attendees: Attendee[]
+  maxNumberOfAttendees: number
 }
 
-export const AttendeeList = ({ attendees }: AttendeeListProps) => {
+export const AttendeeList = ({ attendees, maxNumberOfAttendees }: AttendeeListProps) => {
   if (!attendees.length) {
     return <Text className="text-slate-10 text-sm">Ingen påmeldte</Text>
   }
-  return attendees.map((attendee: Attendee) => (
-    <div key={attendee.id} className="flex items-center justify-between">
-      <div className="flex items-center gap-4">
+
+  return attendees.map((attendee: Attendee, index) => (
+    <div key={attendee.id} className="flex flex-row gap-2 items-center">
+      <Text
+        className={clsx(
+          "text-slate-8 text-right text-sm font-mono",
+          `min-w-[${maxNumberOfAttendees.toString().length + 1}ch]`
+        )}
+      >
+        {index + 1}.
+      </Text>
+
+      <div className="flex flex-row items-center gap-4">
         <Avatar className="h-10 w-10">
           <AvatarFallback className="bg-slate-6">
             <Icon className="text-lg" icon="tabler:user" />
