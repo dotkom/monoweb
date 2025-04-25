eb7b980936369f184b907a785c9660e70df70ae7
Mats Jun Larsen <mats@jun.codes>
2025-04-25 16:00:32 +0900

    Align footer with header and body (#1448)

--- apps/web/src/components/Footer/LogoSection.tsx
+++ apps/web/src/components/Footer/LogoSection.tsx
@@ -5,7 +5,7 @@ export const LogoSection = () => (
   <div className="flex flex-col md:flex-row gap-8 md:gap-16 w-full md:items-end">
     <OnlineLogo className="h-12 w-fit" />
     <Text element="h2" className="text-xl">
-      Linjeforening for informatikk ved NTNU
+      Linjeforeningen for informatikk ved NTNU
     </Text>
   </div>
 )
