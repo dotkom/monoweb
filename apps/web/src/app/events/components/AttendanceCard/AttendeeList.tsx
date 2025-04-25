ca88c0ebed8e462614fe18f2cfb5d71810fc1504
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-25 13:58:19 +0200

    Add waitlisted and non-waitlisted section in attendee list (#1450)
    Co-authored-by: brage-andreas <bragerino+github@gmail.com>

---
+++ apps/web/src/app/events/components/AttendanceCard/AttendeeList.tsx
@@ -0,0 +1,33 @@
+import type { Attendee } from "@dotkomonline/types"
+import { Avatar, AvatarFallback, Icon, Text } from "@dotkomonline/ui"
+
+interface AttendeeListProps {
+  attendees: Attendee[]
+}
+
+export const AttendeeList = ({ attendees }: AttendeeListProps) => {
+  if (!attendees.length) {
+    return <Text className="text-slate-10 text-sm">Ingen påmeldte</Text>
+  }
+  return (
+    <div className="flex flex-col gap-4 max-h-[75dvh] overflow-y-auto">
+      {attendees.map((attendee: Attendee) => (
+        <div key={attendee.id} className="flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar className="h-10 w-10">
+              <AvatarFallback className="bg-slate-6">
+                <Icon className="text-lg" icon="tabler:user" />
+              </AvatarFallback>
+            </Avatar>
+            <div>
+              <Text>{attendee.displayName}</Text>
+              <Text size="sm" className="text-slate-10">
+                {attendee.userGrade ? `${attendee.userGrade}. klasse` : "Ingen klasse"}
+              </Text>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
--- apps/web/src/app/events/components/AttendanceCard/ViewAttendeesButton.tsx
+++ apps/web/src/app/events/components/AttendanceCard/ViewAttendeesButton.tsx
@@ -1,3 +1,4 @@
+import { useTRPC } from "@/utils/trpc/client"
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -7,8 +8,10 @@ import {
   Button,
   Icon,
   Text,
+  Title,
 } from "@dotkomonline/ui"
-import { AttendeesList } from "./AttendeesList"
+import { useQuery } from "@tanstack/react-query"
+import { AttendeeList } from "./AttendeeList"
 
 interface ViewAttendeesDialogButtonProps {
   attendeeListOpen: boolean
@@ -20,25 +23,59 @@ export const ViewAttendeesDialogButton = ({
   attendeeListOpen,
   setAttendeeListOpen,
   attendanceId,
-}: ViewAttendeesDialogButtonProps) => (
-  <AlertDialog open={attendeeListOpen} onOpenChange={setAttendeeListOpen}>
-    <AlertDialogTrigger asChild>
-      <Button className="w-full text-black rounded-lg bg-slate-4 hover:bg-slate-5 h-fit min-h-[4rem]">
-        <Icon className="text-lg" icon="tabler:users" />
-        <Text className="font-medium">Vis påmeldte</Text>
-      </Button>
-    </AlertDialogTrigger>
-    <AlertDialogContent className="w-full max-w-2xl p-4 rounded-lg" onOutsideClick={() => setAttendeeListOpen(false)}>
-      <div className="flex items-center justify-between">
-        <AlertDialogTitle className="font-semibold text-2xl">Påmeldte</AlertDialogTitle>
-        <AlertDialogCancel asChild className="p-0 hover:bg-transparent">
-          <Button variant="ghost" size="icon" className="h-8 w-8">
-            <Icon className="text-lg" icon="tabler:x" />
-          </Button>
-        </AlertDialogCancel>
-      </div>
+}: ViewAttendeesDialogButtonProps) => {
+  const trpc = useTRPC()
 
-      <AttendeesList attendanceId={attendanceId} />
-    </AlertDialogContent>
-  </AlertDialog>
-)
+  const { data: attendees, isLoading } = useQuery(
+    trpc.attendance.getAttendees.queryOptions(
+      {
+        attendanceId,
+      },
+      {
+        enabled: attendeeListOpen,
+      }
+    )
+  )
+
+  const reservedAttendees = attendees?.filter((attendee) => attendee.reserved)
+  const waitlistAttendees = attendees?.filter((attendee) => !attendee.reserved)
+
+  return (
+    <AlertDialog open={attendeeListOpen} onOpenChange={setAttendeeListOpen}>
+      <AlertDialogTrigger asChild>
+        <Button className="w-full text-black rounded-lg bg-slate-4 hover:bg-slate-5 h-fit min-h-[4rem]">
+          <Icon className="text-lg" icon="tabler:users" />
+          <Text className="font-medium">Vis påmeldte</Text>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent
+        className="flex flex-col gap-4 w-full bg-slate-2 outline outline-slate-4 max-w-2xl p-4 rounded-lg"
+        onOutsideClick={() => setAttendeeListOpen(false)}
+      >
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <AlertDialogTitle asChild>
+              <Title className="font-poppins font-medium text-xl">Påmeldte</Title>
+            </AlertDialogTitle>
+            <AlertDialogCancel asChild className="p-0 hover:bg-transparent">
+              <Button variant="ghost" size="icon" className="h-8 w-8">
+                <Icon className="text-lg" icon="tabler:x" />
+              </Button>
+            </AlertDialogCancel>
+          </div>
+          {reservedAttendees !== undefined && <AttendeeList attendees={reservedAttendees} />}
+        </div>
+
+        {waitlistAttendees && waitlistAttendees.length > 0 && (
+          <>
+            <hr className="border border-slate-4" />
+            <div className="flex flex-col gap-2">
+              <Title className="font-poppins font-medium text-xl">Venteliste</Title>
+              <AttendeeList attendees={waitlistAttendees} />
+            </div>
+          </>
+        )}
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
