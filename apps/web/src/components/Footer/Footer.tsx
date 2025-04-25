af93798604258f043843fdbc9b6784cc1a796392
Mats Jun Larsen <mats@jun.codes>
2025-04-25 12:22:44 +0900

    Align footer with header and body
    Or maybe this was intended to be like this? It just looked a bit off

--- apps/web/src/components/Footer/Footer.tsx
+++ apps/web/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { SocialSection } from "./SocialSection"
 
 export const Footer = () => (
   <footer className="pt-24 pb-12 text-slate-10">
-    <div className="max-w-screen-xl px-12 lg:px-24 flex flex-col gap-12">
+    <div className="max-w-screen-xl px-10 flex flex-col gap-12">
       <LogoSection />
       <SocialSection />
       <ContactSection />
