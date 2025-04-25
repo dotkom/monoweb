ddb62eafb16f569dc3f4dc9eb322f157493acdab
brage-andreas <bragerino+github@gmail.com>
2025-04-25 09:46:09 +0200

    fix: make button consistent

--- apps/web/src/app/events/components/AttendanceCard/ViewAttendeesButton.tsx
+++ apps/web/src/app/events/components/AttendanceCard/ViewAttendeesButton.tsx
@@ -26,7 +26,7 @@ export const ViewAttendeesDialogButton = ({
     <AlertDialogTrigger asChild>
       <Button className="w-full text-black rounded-lg bg-slate-4 hover:bg-slate-5 h-fit min-h-[4rem]">
         <Icon className="text-lg" icon="tabler:users" />
-        <Text>Vis påmeldte</Text>
+        <Text className="font-medium">Vis påmeldte</Text>
       </Button>
     </AlertDialogTrigger>
     <AlertDialogContent className="w-full max-w-2xl" onOutsideClick={() => setAttendeeListOpen(false)}>
