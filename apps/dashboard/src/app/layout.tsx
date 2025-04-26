d40b6161de7a31369c0de1d217ceff038292e484
Mats Jun Larsen <mats@jun.codes>
2025-04-26 10:30:04 +0900

    fix lint

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,14 +1,14 @@
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from "@mantine/core"
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from "@mantine/core"
 import "@mantine/core/styles.css"
 import "@mantine/dates/styles.css"
 import "@mantine/notifications/styles.css"
 import "@mdxeditor/editor/style.css"
 import { SessionProvider } from "@dotkomonline/oauth2/react"
+import { Notifications } from "@mantine/notifications"
 import type { PropsWithChildren } from "react"
 import { auth } from "../auth"
-import { QueryProvider } from "./QueryProvider"
 import { ModalProvider } from "./ModalProvider"
-import { Notifications } from "@mantine/notifications"
+import { QueryProvider } from "./QueryProvider"
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   const session = await auth.getServerSession()
