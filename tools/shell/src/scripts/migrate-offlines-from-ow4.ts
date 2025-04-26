210eb9c7cf72085e522171c332a6546aa2f90095
henrikskog <henrikskog01@gmail.com>
2025-04-26 21:30:58 +0200

    read data from sanity

--- tools/shell/src/scripts/migrate-offlines-from-ow4.ts
+++ tools/shell/src/scripts/migrate-offlines-from-ow4.ts
@@ -1,16 +1,40 @@
-import { prisma } from "../services"
-// @ts-ignore data.json is not in source control
-import data from "./data.json" assert { type: "json" }
-
 // see migrating-from-ow4.md for more information
 
-for (const item of data) {
-  await prisma.offline.create({
-    data: {
-      title: item.title,
-      published: new Date(item.release_date),
-      fileUrl: item.issue,
-      imageUrl: item.image_id.toString(),
-    },
-  })
+import fs from "node:fs"
+import { prisma } from "../services"
+
+// @ts-ignore does not exist before running dumpSanityData
+import dumpedData from "./dumpedData.json" assert { type: "json" }
+
+async function dumpSanityData() {
+  // Sanity configuration
+  const SANITY_PROJECT_ID = "wsqi2mae"
+  const SANITY_DATASET = "production"
+  const SANITY_API_VERSION = "2023-05-03"
+
+  // Sanity query
+  const query = `*[_type == "offline"]`
+  const url = `https://${SANITY_PROJECT_ID}.api.sanity.io/v${SANITY_API_VERSION}/data/query/${SANITY_DATASET}?query=${encodeURIComponent(
+    query
+  )}`
+
+  const response = await fetch(url)
+  const { result } = await response.json()
+  fs.writeFileSync("./dumpedData.json", JSON.stringify(result, null, 2))
 }
+
+async function insertDump() {
+  for (const item of dumpedData) {
+    await prisma.offline.create({
+      data: {
+        title: item.title,
+        published: new Date(item.release_date),
+        fileUrl: item.pdf.asset._ref,
+        imageUrl: item.thumbnail.asset._ref,
+      },
+    })
+  }
+}
+
+await dumpSanityData()
+await insertDump()
