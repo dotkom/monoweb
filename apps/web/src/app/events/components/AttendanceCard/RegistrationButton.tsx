816abe7ec0a0dee26a595a46167cf7439ecfdd20
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-26 04:30:34 +0200

    Fix invalid html structure (#1456)
    Co-authored-by: Mats Jun Larsen <mats@jun.codes>

--- apps/web/src/app/events/components/AttendanceCard/RegistrationButton.tsx
+++ apps/web/src/app/events/components/AttendanceCard/RegistrationButton.tsx
@@ -86,7 +86,7 @@ export const RegistrationButton: FC<Props> = ({
       variant="solid"
       icon={buttonIcon}
     >
-      <Text className="font-medium">{buttonContent}</Text>
+      <Text element="div" className="font-medium">{buttonContent}</Text>
     </Button>
   )
 
