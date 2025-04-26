02aeab235829b02822f0cb176403a43aeefa3cf5
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-26 04:33:36 +0200

    Fix auto dark mode in dashboard (#1459)

--- apps/dashboard/src/app/(dashboard)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 import type { PropsWithChildren } from "react"
 import { auth } from "../../auth"
-import { ApplicationShell } from "../ApplicationShell"
+import { ApplicationShell } from "./ApplicationShell"
 
 export const dynamic = "force-dynamic"
 
