cc653586f986fbbfeb759b3e09f3b3280bb91833
henrikskog <henrikskog01@gmail.com>
2025-04-30 18:00:05 +0200

    lint

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -1,15 +1,15 @@
-import { Cloud } from "./components/cloud";
-import { Header } from "./components/header";
+import { Cloud } from "./components/cloud"
+import { Header } from "./components/header"
 
 export function App() {
   return (
     <div className="bg-[#6B1414] min-h-screen">
       <Header />
 
-			<main>
-				Team onboarding ftw 1
-				<Cloud />
-			</main>
-		</div>
-	);
+      <main>
+        Team onboarding ftw 1
+        <Cloud />
+      </main>
+    </div>
+  )
 }
