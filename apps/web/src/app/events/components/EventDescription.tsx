46b26580dfbf8a1d20d0ed27bf6d4988275df043
brage-andreas <bragerino+github@gmail.com>
2025-05-05 14:28:16 +0200

    fix: add default description

--- apps/web/src/app/events/components/EventDescription.tsx
+++ apps/web/src/app/events/components/EventDescription.tsx
@@ -1,5 +1,9 @@
 import { Text } from "@dotkomonline/ui"
 
-export function EventDescription({ description }: { description: string }) {
+export function EventDescription({ description }: { description: string | null }) {
+  if (!description) {
+    return <Text className="text-slate-10">Ingen beskrivelse</Text>
+  }
+
   return <Text className="whitespace-pre-line">{description}</Text>
 }
