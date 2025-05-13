2a02e8e06be423dcffa8460142eeca3a863c6511
Andrefkl <andre.klarpaas@gmail.com>
2025-05-13 19:58:41 +0200

    cleanup

--- apps/web/src/app/om-linjeforeningen/page.tsx
+++ apps/web/src/app/om-linjeforeningen/page.tsx
@@ -1,5 +1,6 @@
 import { OnlineIcon } from "@/components/atoms/OnlineIcon"
 import { server } from "@/utils/trpc/server"
+import type { Group } from "@dotkomonline/types"
 import { Text, Title } from "@dotkomonline/ui"
 import Image from "next/image"
 import Link from "next/link"
@@ -11,28 +12,28 @@ export default async function AboutOnlinePage() {
 
   return (
     <div>
-      <div className="flex flex-col items-center text-center rounded-xl bg-blue-3 p-8">
-        <div>
-          <Title size="xl" element="h1" className="w-fit mb-10 md:text-center text-left">
-            Bli kjent med{" "}
-            <span className="relative inline-block">
-              Online linjeforening
-              <span className="absolute left-0 -bottom-6 h-4 w-full bg-blue-7 rounded-2xl" />
-            </span>
-          </Title>
-        </div>
+      <div className="flex flex-col items-center sm:text-center rounded-xl bg-blue-3 p-8">
+        <Title size="xl" element="h1" className="mb-10">
+          Bli kjent med{" "}
+          <span className="relative inline-block">
+            Online linjeforening
+            <span className="absolute left-0 -bottom-6 h-4 w-full bg-blue-7 rounded-2xl" />
+          </span>
+        </Title>
         <Text>
           Nysjerrig på hva de ulike delene av Online <em>egentlig</em> er?
+          <br />
+          Har du noen gang lurt på hvordan alt henger sammen?
+          <br />
+          Her får du et innblikk til linjeforeningens struktur - komiteer, grupper og hovedstyret!
         </Text>
-        <Text>Har du noen gang lurt på hvordan alt henger sammen?</Text>
-        <Text>Her får du et innblikk til linjeforeningens struktur - komiteer, grupper og hovedstyret!</Text>
       </div>
       <div className="flex flex-col items-center text-center mt-7">
         <Title size={"lg"} element="h2" className="border-b border-slate-6 pb-1 w-full mb-4">
           Generalforsamlingen
         </Title>
 
-        <div className="flex flex-col md:flex-row w-full justify-center px-12 items-center">
+        <div className="flex flex-col md:flex-row w-full justify-center sm:px-12 items-center">
           <div className="w-full md:w-1/2 max-w-md md:mr-10">
             <Image
               src="/genfors-banner.jpeg"
@@ -59,61 +60,54 @@ export default async function AboutOnlinePage() {
         <Title size={"lg"} element="h2" className="border-b border-slate-6 pb-1 w-full mb-4">
           Komiteer
         </Title>
-        <Text className="md:w-1/2 mx-12 text-left md:text-center ">
+        <Text className="md:w-2/3 lg:w-1/2 sm:mx-12 text-left md:text-center">
           Foreningen har et bredt spekter av komiteer som tar for seg alt fra det sosiale til det faglige. Her er en
           oversikt over noen av komiteene som bidrar til å skape et levende studentmiljø for informatikkstudentene:
         </Text>
-
-        <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-6 px-12 pt-8">
-          {committees.map(
-            (committee) =>
-              committee.image && (
-                <Card imageUrl={committee.image} title={committee.name} description={committee.description} />
-              )
-          )}
-        </ul>
+        <GroupList groups={committees} />
       </div>
       <div className="flex flex-col items-center text-center mt-7">
         <Title size={"lg"} element="h2" className="border-b border-slate-6 pb-1 w-full mb-4">
           Nodekomiteer
         </Title>
-        <Text className="w-1/2">Blablabla TODO:</Text>
-
-        <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-6 px-12 pt-8">
-          {nodeCommittees.map(
-            (nodeCommittee) =>
-              nodeCommittee.image && (
-                <Card
-                  imageUrl={nodeCommittee.image}
-                  title={nodeCommittee.name}
-                  description={nodeCommittee.description}
-                />
-              )
-          )}
-        </ul>
+        <Text className="md:w-2/3 lg:w-1/2 sm:mx-12 text-left md:text-center">
+          Nodekomiteer er underkomiteer til kjernekomiteene i Online, eller direkte underlagt Hovedstyret.
+        </Text>
+        <GroupList groups={nodeCommittees} />
       </div>
     </div>
   )
 }
 
-export type CardProps = {
+type GroupListProps = {
+  groups: Group[]
+}
+
+const GroupList: FC<GroupListProps> = ({ groups }: GroupListProps) => {
+  return (
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-6 sm:px-12 pt-8">
+      {groups.map(
+        (group) =>
+          group.image && (
+            <Card key={group.id} imageUrl={group.image} title={group.name} description={group.description} />
+          )
+      )}
+    </ul>
+  )
+}
+
+type CardProps = {
   imageUrl: string | null
   title: string
   description: string
 }
 
-export const Card: FC<CardProps> = ({ imageUrl, title, description }: CardProps) => {
+const Card: FC<CardProps> = ({ imageUrl, title, description }: CardProps) => {
   return (
     <li className="flex items-center text-left">
       <div className="w-[150px] mr-4 flex-shrink-0">
         {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt="Genfors banner"
-            className="object-contain h-auto rounded-full"
-            width={150}
-            height={150}
-          />
+          <Image src={imageUrl} alt={title} className="object-contain h-auto rounded-full" width={150} height={150} />
         ) : (
           <OnlineIcon width={150} height={150} />
         )}
