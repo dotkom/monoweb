d37e0e34ca8485a87010828ddafa6ef3d9a51edf
brage-andreas <bragerino+github@gmail.com>
2025-06-06 01:02:30 +0200

    fix: add missing argument

--- packages/core/src/modules/attendance/__test__/attendance-service.spec.ts
+++ packages/core/src/modules/attendance/__test__/attendance-service.spec.ts
@@ -1,4 +1,5 @@
 import { describe, vi } from "vitest"
+import { JobServiceImpl } from "../../job/job-service.js"
 import { AttendanceRepositoryImpl } from "../attendance-repository"
 import { AttendanceServiceImpl } from "../attendance-service"
 import { AttendeeRepositoryImpl } from "../attendee-repository"
@@ -6,6 +7,7 @@ import { AttendeeRepositoryImpl } from "../attendee-repository"
 describe("AttendanceService", () => {
   const attendanceRepository = vi.mocked(AttendanceRepositoryImpl.prototype)
   const attendeeRepository = vi.mocked(AttendeeRepositoryImpl.prototype)
+  const jobService = vi.mocked(JobServiceImpl.prototype)
 
-  const attendanceService = new AttendanceServiceImpl(attendanceRepository, attendeeRepository)
+  const attendanceService = new AttendanceServiceImpl(attendanceRepository, attendeeRepository, jobService)
 })
