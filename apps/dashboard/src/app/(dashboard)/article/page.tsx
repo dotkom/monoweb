f16a2f06f5f1a1cb690280ab76bf8f1810212fa2
Andrefkl <andre.klarpaas@gmail.com>
2025-06-20 16:09:21 +0200

    move create buttons to above tables

--- apps/dashboard/src/app/(dashboard)/article/page.tsx
+++ apps/dashboard/src/app/(dashboard)/article/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Icon } from "@iconify/react"
-import { Button, ButtonGroup, Group, Skeleton, Stack } from "@mantine/core"
+import { Box, Button, ButtonGroup, Skeleton, Stack } from "@mantine/core"
 import { useCreateArticleModal } from "../../../modules/article/modals/create-article-modal"
 import { useArticleAllQuery } from "../../../modules/article/queries/use-article-all-query"
 import { AllArticlesTable } from "./all-articles-table"
@@ -13,18 +13,18 @@ export default function CompanyPage() {
   return (
     <Skeleton visible={isArticlesLoading}>
       <Stack>
-        <AllArticlesTable articles={articles} />
-        <Group justify="space-between">
+        <Box>
           <Button onClick={open}>Opprett artikkel</Button>
-          <ButtonGroup>
-            <Button variant="subtle">
-              <Icon icon="tabler:caret-left" />
-            </Button>
-            <Button variant="subtle">
-              <Icon icon="tabler:caret-right" />
-            </Button>
-          </ButtonGroup>
-        </Group>
+        </Box>
+        <AllArticlesTable articles={articles} />
+        <ButtonGroup ml="auto">
+          <Button variant="subtle">
+            <Icon icon="tabler:caret-left" />
+          </Button>
+          <Button variant="subtle">
+            <Icon icon="tabler:caret-right" />
+          </Button>
+        </ButtonGroup>
       </Stack>
     </Skeleton>
   )
