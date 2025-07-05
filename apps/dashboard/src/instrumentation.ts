f12538cbbb663f138e273818801f1e5ef38f37ed
Mats Jun Larsen <mats@jun.codes>
2025-07-05 17:59:10 +0900

    Correct NEXT_RUNTIME check for OpenTelemetry

--- apps/dashboard/src/instrumentation.ts
+++ apps/dashboard/src/instrumentation.ts
@@ -1,7 +1,8 @@
 import * as Sentry from "@sentry/nextjs"
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME !== "node") {
+  if (process.env.NEXT_RUNTIME !== "nodejs") {
+    console.warn("Instrumentation is only supported in Node.js runtime.")
     return
   }
   const { getLogger, getResource, startOpenTelemetry } = await import("@dotkomonline/logger")
