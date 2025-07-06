3dc6cf2c8bd7869c8e2cc0bf9f8360f96dc82749
Mats Jun Larsen <mats@jun.codes>
2025-07-07 04:13:00 +0900

    Ensure transactional consistency in Group procedures

--- packages/core/src/modules/group/group-service.ts
+++ packages/core/src/modules/group/group-service.ts
@@ -1,3 +1,4 @@
+import type { DBHandle } from "@dotkomonline/db"
 import type { Group, GroupId, GroupMember, GroupMemberWrite, GroupType, GroupWrite, UserId } from "@dotkomonline/types"
 import { GroupNotFoundError } from "./group-error"
 import type { GroupRepository } from "./group-repository"
@@ -8,90 +9,70 @@ export interface GroupService {
    *
    * @throws {GroupNotFoundError} if the group does not exist
    */
-  getGroup(groupId: GroupId): Promise<Group>
+  getById(handle: DBHandle, groupId: GroupId): Promise<Group>
+  getByIdAndType(handle: DBHandle, groupId: GroupId, groupType: GroupType): Promise<Group>
+  getAll(handle: DBHandle): Promise<Group[]>
   /**
    * Get a group by its id and type
    *
    * @throws {GroupNotFoundError} if the group does not exist
    */
-  getGroupByType(groupId: GroupId, groupType: GroupType): Promise<Group>
-  getGroups(): Promise<Group[]>
-  getGroupsByType(groupType: GroupType): Promise<Group[]>
-  createGroup(payload: GroupWrite): Promise<Group>
-  updateGroup(groupId: GroupId, values: Partial<GroupWrite>): Promise<Group>
-  deleteGroup(groupId: GroupId): Promise<void>
-  getAllGroupIds(): Promise<GroupId[]>
-  getAllGroupIdsByType(groupType: GroupType): Promise<GroupId[]>
-  getMembers(groupId: GroupId): Promise<GroupMember[]>
-  getGroupsByMember(userId: UserId): Promise<Group[]>
-  addMember(data: GroupMemberWrite): Promise<GroupMember>
-  removeMember(userId: UserId, groupId: GroupId): Promise<void>
+  getAllByType(handle: DBHandle, groupType: GroupType): Promise<Group[]>
+  create(handle: DBHandle, payload: GroupWrite): Promise<Group>
+  update(handle: DBHandle, groupId: GroupId, values: Partial<GroupWrite>): Promise<Group>
+  delete(handle: DBHandle, groupId: GroupId): Promise<void>
+  getAllIds(handle: DBHandle): Promise<GroupId[]>
+  getAllIdsByType(handle: DBHandle, groupType: GroupType): Promise<GroupId[]>
+  getMembers(handle: DBHandle, groupId: GroupId): Promise<GroupMember[]>
+  getAllByMember(handle: DBHandle, userId: UserId): Promise<Group[]>
+  addMember(handle: DBHandle, data: GroupMemberWrite): Promise<GroupMember>
+  removeMember(handle: DBHandle, userId: UserId, groupId: GroupId): Promise<void>
 }
 
-export class GroupServiceImpl implements GroupService {
-  private readonly groupRepository: GroupRepository
-
-  constructor(groupRepository: GroupRepository) {
-    this.groupRepository = groupRepository
-  }
-
-  public async getGroup(groupId: GroupId) {
-    const group = await this.groupRepository.getById(groupId)
-    if (!group) {
-      throw new GroupNotFoundError(groupId)
-    }
-    return group
-  }
-
-  public async getGroupByType(groupId: GroupId, groupType: GroupType) {
-    const group = await this.groupRepository.getById(groupId)
-    if (!group || group.type !== groupType) {
-      throw new GroupNotFoundError(groupId)
-    }
-    return group
-  }
-
-  public async createGroup(payload: GroupWrite) {
-    return await this.groupRepository.create(payload)
-  }
-
-  public async updateGroup(groupId: GroupId, values: Partial<GroupWrite>) {
-    return await this.groupRepository.update(groupId, values)
-  }
-
-  public async deleteGroup(groupId: GroupId) {
-    await this.groupRepository.delete(groupId)
-  }
-
-  public async getGroups() {
-    return await this.groupRepository.getAll()
-  }
-
-  public async getGroupsByType(groupType: GroupType) {
-    return await this.groupRepository.getAllByType(groupType)
-  }
-
-  public async getAllGroupIds() {
-    return await this.groupRepository.getAllIds()
-  }
-
-  public async getAllGroupIdsByType(groupType: GroupType) {
-    return await this.groupRepository.getAllIdsByType(groupType)
-  }
-
-  public async getMembers(groupId: GroupId) {
-    return await this.groupRepository.getMembers(groupId)
-  }
-
-  public async getGroupsByMember(userId: UserId) {
-    return await this.groupRepository.getAllByMember(userId)
-  }
-
-  public async addMember(data: GroupMemberWrite) {
-    return await this.groupRepository.addMember(data)
-  }
-
-  public async removeMember(userId: UserId, groupId: GroupId) {
-    await this.groupRepository.removeMember(userId, groupId)
+export function getGroupService(groupRepository: GroupRepository): GroupService {
+  return {
+    async getById(handle, groupId) {
+      const group = await groupRepository.getById(handle, groupId)
+      if (!group) throw new GroupNotFoundError(groupId)
+      return group
+    },
+    async getByIdAndType(handle, groupId, groupType) {
+      const group = await groupRepository.getById(handle, groupId)
+      if (!group || group.type !== groupType) throw new GroupNotFoundError(groupId)
+      return group
+    },
+    async getAll(handle) {
+      return groupRepository.getAll(handle)
+    },
+    async getAllByType(handle, groupType) {
+      return groupRepository.getAllByType(handle, groupType)
+    },
+    async create(handle, payload) {
+      return groupRepository.create(handle, payload)
+    },
+    async update(handle, groupId, values) {
+      return groupRepository.update(handle, groupId, values)
+    },
+    async delete(handle, groupId) {
+      await groupRepository.delete(handle, groupId)
+    },
+    async getAllIds(handle) {
+      return groupRepository.getAllIds(handle)
+    },
+    async getAllIdsByType(handle, groupType) {
+      return groupRepository.getAllIdsByType(handle, groupType)
+    },
+    async getMembers(handle, groupId) {
+      return groupRepository.getMembers(handle, groupId)
+    },
+    async getAllByMember(handle, userId) {
+      return groupRepository.getAllByMember(handle, userId)
+    },
+    async addMember(handle, data) {
+      return groupRepository.addMember(handle, data)
+    },
+    async removeMember(handle, userId, groupId) {
+      await groupRepository.removeMember(handle, userId, groupId)
+    },
   }
 }
