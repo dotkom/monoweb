577d7b5fc5b5c96ab1d86884597199178ff0ba13
brage-andreas <bragerino+github@gmail.com>
2025-07-27 23:02:48 +0200

    Add event list front-end to splash

--- apps/splash-2025/src/App.tsx
+++ apps/splash-2025/src/App.tsx
@@ -17,15 +17,27 @@ export function App() {
         <main>
           <Hero {...HeroData} />
           <DebugInfo />
-          <div className="bg-brand">
+          <div className="relative bg-brand">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 32" role="img" aria-labelledby="desc">
               <desc id="desc">A decorative slant</desc>
               <path fill="white" fill-opacity="1" d="M0,32L1440,0L1440,0L0,0Z" />
             </svg>
             <LinjeforeningInfo />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 32"
+              role="img"
+              aria-labelledby="desc"
+              className="block"
+            >
+              <desc id="desc">A decorative slant</desc>
+              <path fill="white" d="M1440,0.5L0,32L0,32L1440,32Z" />
+            </svg>
+            {/* Line of white to hide pixel gap under svg */}
+            <div className="absolute bottom-0 left-0 w-full h-px bg-white pointer-events-none" />
           </div>
+          <Events />
         </main>
-        <Events />
         <Footer sponsorLogo={<SponsorPlaceholderIcon />} />
       </div>
     </QueryClientProvider>
--- apps/splash-2025/src/components/events.tsx
+++ apps/splash-2025/src/components/events.tsx
@@ -1,19 +1,104 @@
+import type { EventDetail } from "@dotkomonline/types"
+import { Icon, Text } from "@dotkomonline/ui"
 import { useQuery } from "@tanstack/react-query"
+import { compareAsc, formatDate, getDate } from "date-fns"
+import { nb } from "date-fns/locale"
+import { slugify } from "../../../../packages/utils/src/slugify"
 import { trpc } from "../lib/trpc"
 
 export const Events = () => {
-  const { data: events, ...query } = useQuery(trpc.event.all.queryOptions())
+  // TODO: filter by "Velkom"
+  const { data: events, isLoading } = useQuery(trpc.event.all.queryOptions())
 
-  if (events === undefined || query.isLoading) {
-    return <div>Loading...</div>
+  if (isLoading) {
+    return (
+      <div className="bg-white min-h-[500px] w-full flex gap-2 justify-center items-center">
+        <Icon icon="tabler:loader-2" className="animate-spin text-2xl" />
+        <Text className="text-2xl">Laster...</Text>
+      </div>
+    )
   }
 
+  if (!events || events.length === 0) {
+    return (
+      <div className="bg-white min-h-[500px] w-full flex gap-2 justify-center items-center">
+        <Text className="text-2xl">Ingen arrangementer funnet</Text>
+      </div>
+    )
+  }
+
+  const eventsByDate = events
+    .toSorted((a, b) => compareAsc(a.event.start, b.event.start))
+    .reduce((acc, eventDetail) => {
+      const month = formatDate(eventDetail.event.start, "MMMM", { locale: nb })
+      const date = getDate(eventDetail.event.start)
+
+      const eventsInMonth = acc.get(month) || new Map<number, EventDetail[]>()
+      const eventsInDate = eventsInMonth.get(date) || []
+
+      eventsInDate.push(eventDetail)
+      eventsInMonth.set(date, eventsInDate)
+      acc.set(month, eventsInMonth)
+
+      return acc
+    }, new Map<string, Map<number, EventDetail[]>>())
+
   return (
-    <div className="w-full bg-white h-[500px] p-4 overflow-y-auto">
-      {events.map((event) => (
-        // show nice formatted json
-        <pre key={event.event.id}>{JSON.stringify(event, null, 2)}</pre>
-      ))}
+    <div className="bg-white min-h-[500px] w-full">
+      <div className="relative max-w-screen-xl py-20 px-8 md:px-16 mx-auto">
+        <div className="absolute inset-y-0 left-1/4 w-1 rounded-full bg-gray-300" />
+
+        <div className="space-y-18">
+          {eventsByDate.entries().map(([month, eventsInMonth]) => (
+            <div key={month} className="space-y-4">
+              <div className="w-1/4 pr-12 flex justify-end-safe">
+                <Text className="relative w-fit z-1 text-2xl font-bold capitalize p-1 -m-1 bg-white rounded-md">
+                  {month}
+                </Text>
+              </div>
+
+              <div className="space-y-12">
+                {eventsInMonth.entries().map(([date, details]) => (
+                  <div className="space-y-4" key={`${month}-${date}`}>
+                    <Text className="text-gray-500 uppercase font-bold text-xs max-sm:relative max-sm:left-1/4 sm:w-1/4 sm:pr-12 sm:text-right mb-1 sm:-mb-0.5">
+                      {details[0]
+                        ? formatDate(new Date(details[0]?.event.start), "EEEE dd.", { locale: nb })
+                        : "Ukjent ukedag"}
+                    </Text>
+
+                    {details.map((detail) => (
+                      <div key={detail.event.id} className="flex items-center">
+                        <Text className="w-1/4 pr-12 text-right">
+                          {formatDate(detail.event.start, "HH:mm", { locale: nb })}
+                        </Text>
+
+                        <div className="relative w-3/4">
+                          <div className="absolute -left-[14px] md:-left-[30px] top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                            <div className="h-3 w-3 rounded-full bg-brand" />
+                          </div>
+
+                          <a
+                            /* TODO: update href */
+                            href={`http://localhost:3000/arrangementer/${slugify(detail.event.title)}/${detail.event.id}`}
+                            className="flex items-center gap-2 transition-colors p-2 -m-2 rounded-lg hover:bg-gray-100"
+                          >
+                            <img
+                              src={detail.event.imageUrl ?? "https://placehold.co/120x90?text=Online"}
+                              alt={detail.event.title}
+                              className="w-14 h-auto aspect-4/3 object-cover rounded-sm"
+                            />
+                            <Text className="text-lg/5">{detail.event.title}</Text>
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
