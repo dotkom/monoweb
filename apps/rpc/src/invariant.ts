59f55db5249ae75bf488a10fb404d0a624696ebc
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-07-31 19:39:14 +0200

    Remove debug changes

--- apps/rpc/src/invariant.ts
+++ apps/rpc/src/invariant.ts
@@ -13,9 +13,7 @@ const logger = getLogger("db-query-invariant-checker")
 export function parseOrReport<T>(schema: z.ZodSchema<T>, value: T): T {
   const result = schema.safeParse(value)
   if (!result.success) {
-    logger.error("Database failed to parse value into schema:")
-    logger.error("Value:", JSON.stringify(value, null, 2))
-    logger.error(JSON.stringify(result.error.issues, null, 2))
+    logger.error("Database failed to parse value into schema: %s emitted for object %o", result.error.message, value)
     throw new Error("Database returned value that does not conform to schema")
   }
   return result.data
