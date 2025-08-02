c805bbefea7033fe804cdfc563b2f410edf94f05
Andrefkl <andre.klarpaas@gmail.com>
2025-08-02 22:04:57 +0200

    make group table filterable

--- apps/dashboard/src/app/group/all-groups-table.tsx
+++ apps/dashboard/src/app/group/all-groups-table.tsx
@@ -1,16 +1,17 @@
 "use client"
 
-import { type Group, getGroupTypeName } from "@dotkomonline/types"
+import { FilterableTable } from "@/components/molecules/FilterableTable/FilterableTable"
+import { type Group, GroupTypeSchema, getGroupTypeName } from "@dotkomonline/types"
 import { Anchor } from "@mantine/core"
-import { createColumnHelper, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { createColumnHelper, getCoreRowModel } from "@tanstack/react-table"
 import Link from "next/link"
 import { useMemo } from "react"
 
 interface Props {
-  data: Group[]
+  groups: Group[]
 }
 
-export const useGroupTable = ({ data }: Props) => {
+export const AllGroupsTable = ({ groups }: Props) => {
   const columnHelper = createColumnHelper<Group>()
 
   const columns = useMemo(
@@ -18,6 +19,7 @@ export const useGroupTable = ({ data }: Props) => {
       columnHelper.accessor((group) => group, {
         id: "abbreviation",
         header: () => "Kort navn",
+        sortingFn: "alphanumeric",
         cell: (info) => (
           <Anchor component={Link} size="sm" href={`/group/${info.getValue().slug}`}>
             {info.getValue().abbreviation}
@@ -27,13 +29,16 @@ export const useGroupTable = ({ data }: Props) => {
       columnHelper.accessor("name", {
         header: () => "Navn",
         cell: (info) => info.getValue(),
+        sortingFn: "alphanumeric",
       }),
       columnHelper.accessor("email", {
         header: () => "Kontakt-e-post",
         cell: (info) => info.getValue(),
+        sortingFn: "alphanumeric",
       }),
       columnHelper.accessor("contactUrl", {
         header: () => "Kontakt-lenke",
+        enableSorting: false,
         cell: (info) => {
           const val = info.getValue()
           if (!val) {
@@ -48,6 +53,7 @@ export const useGroupTable = ({ data }: Props) => {
       }),
       columnHelper.accessor("imageUrl", {
         header: () => "Bilde",
+        enableSorting: false,
         cell: (info) => {
           const val = info.getValue()
           if (!val) {
@@ -60,24 +66,49 @@ export const useGroupTable = ({ data }: Props) => {
           )
         },
       }),
-      columnHelper.accessor("type", {
-        header: () => "Type",
-        cell: (info) => {
-          return getGroupTypeName(info.getValue())
-        },
+      columnHelper.accessor((group) => getGroupTypeName(group.type), {
+        id: "type",
+        header: "Type",
+        cell: (info) => info.getValue(),
+        sortingFn: "alphanumeric",
+        filterFn: "arrIncludes",
       }),
-      columnHelper.accessor((group) => group, {
+      columnHelper.accessor((group) => (!group.deactivatedAt ? "Aktiv" : "Inaktiv"), {
         id: "status",
-        header: () => "Status",
-        cell: (info) => (!info.getValue().deactivatedAt ? "Aktiv" : "Inaktiv"),
+        header: "Status",
+        cell: (info) => info.getValue(),
+        sortingFn: "alphanumeric",
+        filterFn: "arrIncludes",
       }),
     ],
     [columnHelper]
   )
 
-  return useReactTable({
-    data,
-    getCoreRowModel: getCoreRowModel(),
-    columns,
-  })
+  const tableOptions = useMemo(
+    () => ({
+      data: groups,
+      getCoreRowModel: getCoreRowModel(),
+      columns,
+    }),
+    [groups, columns]
+  )
+
+  return (
+    <FilterableTable
+      tableOptions={tableOptions}
+      filters={[
+        { columnId: "status", label: "Aktiv", value: "Aktiv" },
+        { columnId: "status", label: "Inaktiv", value: "Inaktiv" },
+        ...Object.values(GroupTypeSchema.Values).map((groupType) => {
+          const typeName = getGroupTypeName(groupType)
+
+          return {
+            columnId: "type",
+            label: typeName,
+            value: typeName,
+          }
+        }),
+      ]}
+    />
+  )
 }
