e77701f9b747fc4f838edfdbd15161c6cac21596
brage-andreas <bragerino+github@gmail.com>
2025-08-04 16:58:18 +0200

    Fix infinite redirect loop on missing `profileSlug`

--- apps/web/src/app/profil/page.tsx
+++ apps/web/src/app/profil/page.tsx
@@ -2,12 +2,18 @@ import { auth } from "@/auth"
 import { server } from "@/utils/trpc/server"
 import { redirect } from "next/navigation"
 
-export default async function ProfilePage() {
+export default async function Page() {
   const session = await auth.getServerSession()
-  if (session) {
-    const user = await server.user.getMe.query()
-    redirect(`/profil/${user.profileSlug}`)
-  } else {
+
+  if (!session) {
     redirect("/")
   }
+
+  const user = await server.user.getMe.query()
+
+  if (!user.profileSlug) {
+    return redirect("/")
+  }
+
+  redirect(`/profil/${user.profileSlug}`)
 }
