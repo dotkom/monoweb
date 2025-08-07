0485f74a7b0ce115ee6eab3b9dc4491a4addb0cb
brage-andreas <bragerino+github@gmail.com>
2025-08-07 17:19:36 +0200

    feat: copy S3 from dashboard to web

---
+++ apps/web/src/lib/uploadFile.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { useTRPC } from "../utils/trpc/client"
+import { uploadFileToS3PresignedUrl } from "./s3"
+
+export const useUploadFile = () => {
+  const trpc = useTRPC()
+  const presignedPostMut = useMutation(trpc.offline.createPresignedPost.mutationOptions())
+
+  return async (file: File) => {
+    const presignedPost = await presignedPostMut.mutateAsync({
+        filename: `${file.name}`,
+        mimeType: file.type,
+    })
+
+    return await uploadFileToS3PresignedUrl(file, presignedPost.fields, presignedPost.url)
+  }
+}
