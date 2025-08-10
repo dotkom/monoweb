331a2633e5304a757fa032a719e8e5952f0af6b5
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-08-10 13:34:07 +0200

    Remove log and add comment

--- packages/utils/src/ogjoin.ts
+++ packages/utils/src/ogjoin.ts
@@ -1,8 +1,8 @@
+// Bare for OGs
 export const ogJoin = (names: string[]) => {
   if (names.length < 2) {
     return names ? names[0] : null
   }
-  console.log(names)
 
   return `${names.splice(0, -1).join(", ")} og ${names.splice(-1)[0]}`
 }
