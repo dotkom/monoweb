77c96aae5815bfc9289232622ceb183166b344aa
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-08-11 14:53:18 +0200

    Add home button on mobile nav menu (#1843)

--- apps/web/src/components/Navbar/MobileNavigation.tsx
+++ apps/web/src/components/Navbar/MobileNavigation.tsx
@@ -6,26 +6,29 @@ import Link from "next/link"
 import { type FC, Fragment, useState } from "react"
 
 import type { MenuLink } from "@/components/Navbar/Navbar"
+import { env } from "@/env"
 
 export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
   const [open, setOpen] = useState(false)
 
+  const linksWithHome = [{ title: "Hjem", href: env.NEXT_PUBLIC_HOME_URL }, ...links]
+
   return (
     <div className="block grow md:hidden md:grow-0">
       <Popover.Root onOpenChange={(val) => setOpen(val)}>
-        <Popover.Trigger>
-          <div>{open ? <Icon height={32} icon="tabler:x" /> : <Icon height={32} icon="tabler:menu-2" />}</div>
+        <Popover.Trigger className="flex flex-row items-center">
+          {open ? <Icon height={32} icon="tabler:x" /> : <Icon height={32} icon="tabler:menu-2" />}
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className="w-screen border-t bg-slate-50 animate-in fade-in-20 p-6 mt-8">
+          <Popover.Content className="w-screen bg-white dark:bg-stone-800 animate-in fade-in-20 p-6 mt-8">
             <nav className="flex flex-col gap-4">
-              {links.map((link) => (
+              {linksWithHome.map((link) => (
                 <Fragment key={link.title}>
                   <Popover.Close asChild>
                     <Link
                       href={"href" in link ? link.href : "#"}
                       className={cn(
-                        "inline-flex py-2 text-lg font-semibold text-black hover:bg-gray-100 rounded-md px-2 transition-colors",
+                        "font-body inline-flex py-2 text-lg font-semibold hover:bg-gray-100 rounded-md px-2 transition-colors",
                         "href" in link && "font-normal"
                       )}
                     >
