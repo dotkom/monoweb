bacbaea49229d35bef6110f895aadddaad56f82c
madsab <mads.barnes@hotmail.com>
2025-08-11 12:23:40 +0200

    add Structure section to about page

--- apps/web/src/app/om-linjeforeningen/page.tsx
+++ apps/web/src/app/om-linjeforeningen/page.tsx
@@ -1,4 +1,5 @@
 import { Divider, ImageCard, Text, Title } from "@dotkomonline/ui"
+import Link from "next/link"
 
 export default async function AboutOnlinePage() {
   return (
@@ -6,19 +7,20 @@ export default async function AboutOnlinePage() {
       <WhoAreWeCard />
       <Statstics />
       <OurGoalsCard />
+      <Structure />
     </div>
   )
 }
 
 const WhoAreWeCard = () => {
   return (
-    <ImageCard image="/genfors-banner.jpeg" imagePosition="left" className="bg-blue-100 ">
+    <ImageCard image="/online-logo-o.svg" imagePosition="left" className="bg-blue-100 ">
       <div className="flex flex-col items-center justify-center my-8 sm:my-24">
         <Title size={"xxl"} className="pb-1 font-extrabold">
           Hvem er vi?
         </Title>
-        <Divider className="w-2/3 sm:w-1/2 bg-blue-300 h-3 rounded-full " />
-        <Text className="m-4 sm:w-2/3">
+        <Divider className="w-2/3 sm:w-1/2 bg-blue-300 h-3 rounded-full border-none " />
+        <Text className="m-4 max-sm:text-center sm:w-2/3">
           Online er linjeforeningen for informatikkstudenter ved NTNU i Trondheim. Linjeforeningens oppgave er å
           forbedre studiemiljøet ved å fremme sosialt samvær, faglig kompetanse og kontakt med næringslivet.
         </Text>
@@ -28,6 +30,10 @@ const WhoAreWeCard = () => {
 }
 
 const Statstics = () => {
+  const memberCount = 600 // This would ideally be fetched from a service
+  const activityCount = 2500 // This would ideally be fetched from a service
+  const committeeCount = 30 // This would ideally be fetched from a service
+
   const StatisticsNumber = ({ number, title }: { number: number; title: string }) => {
     return (
       <div className="flex flex-col items-center">
@@ -38,19 +44,19 @@ const Statstics = () => {
   }
   return (
     <>
-      <Text>
+      <Text className="mx-8">
         Linjeforeningen startet som en liten gruppe studenter høsten 1985, og har siden den gang vokst til å bli en av
-        NTNU's største linjeforeninger, med over 1 000 medlemmer. Foreningen drives av et stort antall frivillige som
-        legger ned betydelig innsats for å skape et variert og engasjerende tilbud for medlemmene. Gjennom årene har
-        Online arrangert mer enn 2 000 aktiviteter inkludert faglige kurs, bedriftspresentasjoner og sosiale
-        sammenkomster, som til sammen bidrar til å styrke både det faglige og det sosiale fellesskapet. Denne
-        kombinasjonen av profesjonelle arrangementer og sosiale møteplasser har gjort Online til en sentral arena for
-        kompetanseutvikling, nettverksbygging og trivsel blant medlemmene.
+        NTNU's større linjeforeninger innen teknologi, med over {memberCount} medlemmer. Foreningen drives av et stort
+        antall frivillige som legger ned betydelig innsats for å skape et variert og engasjerende tilbud for medlemmene.
+        Gjennom årene har Online arrangert mer enn {activityCount} aktiviteter inkludert faglige kurs,
+        bedriftspresentasjoner og sosiale sammenkomster, som til sammen bidrar til å styrke både det faglige og det
+        sosiale fellesskapet. Denne kombinasjonen av profesjonelle arrangementer og sosiale møteplasser har gjort Online
+        til en sentral arena for kompetanseutvikling, nettverksbygging og trivsel blant medlemmene.
       </Text>
-      <div className="flex flex-col sm:flex-row justify-around items-center gap-6">
-        <StatisticsNumber number={600} title="Aktive Medlemmer" />
-        <StatisticsNumber number={2000} title="Arrangerte aktiviteter" />
-        <StatisticsNumber number={30} title="Aktive komiteer" />
+      <div className="flex flex-col sm:flex-row justify-around items-center gap-8">
+        <StatisticsNumber number={memberCount} title="Aktive Medlemmer" />
+        <StatisticsNumber number={activityCount} title="Arrangerte aktiviteter" />
+        <StatisticsNumber number={committeeCount} title="Aktive komiteer" />
       </div>
     </>
   )
@@ -61,14 +67,37 @@ const OurGoalsCard = () => {
     <ImageCard image="/genfors-banner.jpeg" imagePosition="right" className="bg-yellow-100">
       <div className="flex flex-col items-center justify-center my-8 sm:my-24">
         <Title size={"xxl"} className="pb-1 font-extrabold">
-          Våre Mål
+          Vårt Mål
         </Title>
-        <Divider className="w-2/3 sm:w-1/2 bg-yellow-300 h-3 rounded-full" />
-        <Text className="mt-4 w-2/3">
+        <Divider className="w-2/3 sm:w-1/2 bg-yellow-300 h-3 rounded-full border-none" />
+        <Text className="m-4 max-sm:text-center w-2/3">
           Online skal arbeide for å skape sterkere bånd mellom medlemmer på ulike årstrinn og være kontaktledd mellom
           medlemmene og eksterne aktører.
         </Text>
       </div>
     </ImageCard>
+  )
+}
+
+const Structure = () => {
+  return (
+    <div className="flex flex-col">
+      <Title size={"xxl"} className="pb-1 font-extrabold">
+        Struktur
+      </Title>
+      <Text className="mt-4">
+        Generalforsamlingen utgjør den øverste besluttende myndighet i Online, og kan sammenlignes med et stortingsvalg
+        for en ny regjering. Mellom generalforsamlingene ivaretas den daglige driften av linjeforeningen av hovedstyret
+        og komiteene. Disse komiteene har spesifikke ansvarsområder, som spenner fra sosiale arrangementer til faglige
+        kurs. Dette gir medlemmene muligheten til å engasjere seg i sine interessefelt, samtidig som det sikrer at
+        foreningen kan tilby et variert spekter av aktiviteter og tjenester.
+      </Text>
+      <Text className="mt-4">
+        Ønsker du å vite mer om hvordan Online er organisert, kan du lese mer på vår egen{" "}
+        <Link href={"https://wiki.online.ntnu.no"} target="_blank" className="text-blue-900 underline">
+          Online Wiki
+        </Link>
+      </Text>
+    </div>
   )
 }
