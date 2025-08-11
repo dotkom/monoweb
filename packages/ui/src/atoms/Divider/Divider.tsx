0f76463a379b6f14988be779ae4a5876b64537a1
madsab <mads.barnes@hotmail.com>
2025-08-11 12:23:40 +0200

    add Structure section to about page

--- packages/ui/src/atoms/Divider/Divider.tsx
+++ packages/ui/src/atoms/Divider/Divider.tsx
@@ -9,12 +9,12 @@ export const Divider = forwardRef<HTMLDivElement, DividerProps>(({ vertical, ...
   return (
     <div
       ref={ref}
+      {...props}
       className={cn(
         "border-gray-200",
-        props.className,
-        vertical ? "w-2 border-r-1 mr-2" : "w-full border-t-1 h-2 mx-1 self-center"
+        vertical ? "w-2 border-r-1 mr-2" : "w-full border-t-1 h-2 mx-1 self-center",
+        props.className
       )}
-      {...props}
     />
   )
 })
