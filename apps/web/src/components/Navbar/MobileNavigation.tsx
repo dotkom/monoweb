ed4543f19db9ae8c58b923d4fd9d6d1786de760e
brage-andreas <bragerino+github@gmail.com>
2025-08-11 15:35:24 +0200

    feat: update mobile nav

--- apps/web/src/components/Navbar/MobileNavigation.tsx
+++ apps/web/src/components/Navbar/MobileNavigation.tsx
@@ -11,7 +11,12 @@ import { env } from "@/env"
 export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
   const [open, setOpen] = useState(false)
 
-  const linksWithHome = [{ title: "Hjem", href: env.NEXT_PUBLIC_HOME_URL }, ...links]
+  const homeLink: MenuLink = {
+    title: "Hjem",
+    href: env.NEXT_PUBLIC_HOME_URL,
+    icon: "tabler:home",
+  }
+  const linksWithHome = [homeLink, ...links]
 
   return (
     <div className="block grow md:hidden md:grow-0">
@@ -20,37 +25,27 @@ export const MobileNavigation: FC<{ links: MenuLink[] }> = ({ links }) => {
           {open ? <Icon height={32} icon="tabler:x" /> : <Icon height={32} icon="tabler:menu-2" />}
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className="w-screen bg-white dark:bg-stone-800 animate-in fade-in-20 p-6 mt-8">
-            <nav className="flex flex-col gap-4">
-              {linksWithHome.map((link) => (
-                <Fragment key={link.title}>
-                  <Popover.Close asChild>
-                    <Link
-                      href={"href" in link ? link.href : "#"}
-                      className={cn(
-                        "font-body inline-flex py-2 text-lg font-semibold hover:bg-gray-100 rounded-md px-2 transition-colors",
-                        "href" in link && "font-normal"
-                      )}
-                    >
-                      {link.title}
-                    </Link>
-                  </Popover.Close>
-                  {"items" in link &&
-                    link.items.map((link) => (
-                      <Popover.Close asChild key={link.title}>
-                        <Link
-                          href={"href" in link ? link.href : "#"}
-                          className={cn(
-                            "ml-4 inline-flex py-2 text-base font-medium text-black hover:bg-gray-100 rounded-md px-2 transition-colors",
-                            "href" in link && "font-normal"
-                          )}
-                        >
-                          {link.title}
-                        </Link>
-                      </Popover.Close>
-                    ))}
-                </Fragment>
-              ))}
+          <Popover.Content className="w-screen max-h-screen overflow-y-auto overscroll-contain bg-white dark:bg-stone-800 animate-in fade-in-20 p-4 mt-4">
+            <nav className="flex flex-col gap-4 pt-4 pb-64">
+              {linksWithHome.map((link) =>
+                ("items" in link ? link.items : [link]).map((link) => (
+                  <Fragment key={link.title}>
+                    <Popover.Close asChild>
+                      <Link
+                        href={"href" in link ? link.href : "#"}
+                        className={cn(
+                          "font-body flex items-center gap-3 p-4 bg-gray-50 rounded-xl",
+                          "text-lg font-semibold",
+                          "href" in link && "font-medium"
+                        )}
+                      >
+                        <Icon className="text-xl" icon={link.icon} />
+                        {link.title}
+                      </Link>
+                    </Popover.Close>
+                  </Fragment>
+                ))
+              )}
             </nav>
           </Popover.Content>
         </Popover.Portal>
