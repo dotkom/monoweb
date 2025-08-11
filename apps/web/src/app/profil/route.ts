a513b45916b6d6a2fd1686c634390d4d4b553c86
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-08-11 15:31:44 +0200

    Create error when automatic membership fails

--- apps/web/src/app/profil/route.ts
+++ apps/web/src/app/profil/route.ts
@@ -1,19 +1,19 @@
 import { auth } from "@/auth"
 import { server } from "@/utils/trpc/server"
 import { redirect } from "next/navigation"
+import type { NextRequest } from "next/server"
 
-export default async function Page() {
+export async function GET(req: NextRequest) {
   const session = await auth.getServerSession()
 
   if (!session) {
     redirect("/")
   }
-
   const user = await server.user.getMe.query()
 
   if (!user.profileSlug) {
-    return redirect("/")
+    redirect("/")
   }
 
-  redirect(`/profil/${user.profileSlug}`)
+  redirect(`/profil/${user.profileSlug}${req.nextUrl.search}`)
 }
