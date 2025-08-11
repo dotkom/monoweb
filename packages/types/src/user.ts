193da4d01738e17bebcd09bfe555d24bf70bf530
Mats Jun Larsen <mats@jun.codes>
2025-08-11 17:22:22 +0200

    Clamp master students study grade between 4 and 5 (#1855)

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -87,17 +87,23 @@ export function findActiveMembership(user: User): Membership | null {
 }
 
 export function getMembershipGrade(membership: Membership): 1 | 2 | 3 | 4 | 5 | null {
+  // Take the difference, and add one because if `startYear == currentYear` they are in their first year
+  const delta = differenceInYears(getAcademicStart(getCurrentUtc()), getAcademicStart(membership.start)) + 1
+
   switch (membership.type) {
     case "KNIGHT":
     case "PHD_STUDENT":
       return 5
     case "SOCIAL_MEMBER":
       return 1
-    case "BACHELOR_STUDENT":
+    case "BACHELOR_STUDENT": {
+      // Bachelor students are clamped at 1-3, regardless of how many years they used to take the degree.
+      return Math.max(1, Math.min(3, delta)) as 1 | 2 | 3
+    }
     case "MASTER_STUDENT": {
-      // Take the difference, and add one because if `startYear == currentYear` they are in their first year
-      const delta = differenceInYears(getAcademicStart(getCurrentUtc()), getAcademicStart(membership.start)) + 1
-      return Math.max(1, Math.min(5, delta)) as 1 | 2 | 3 | 4 | 5
+      // Master students must be clamped at 4-5 because they can only be in their first or second year, but are always
+      // considered to have a bachelor's degree from beforehand.
+      return Math.max(4, Math.min(5, delta)) as 4 | 5
     }
     case "OTHER":
       return null
