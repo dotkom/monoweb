8e427d9da8096bc34d96259e8f9bf7a540afd58b
Mats Jun Larsen <mats@jun.codes>
2025-08-11 14:14:58 +0200

    fix lint

--- apps/rpc/src/modules/user/user-service.ts
+++ apps/rpc/src/modules/user/user-service.ts
@@ -374,15 +374,15 @@ function estimateStudyGrade(studyPlanCourses: StudyplanCourse[], coursesTaken: N
   }
 
   // Find the key with highest value - JS has ZERO nice utility functions :(
-  let highestCredits = 0;
-  let highestGrade = 0;
+  let highestCredits = 0
+  let highestGrade = 0
   for (const [grade, credits] of Object.entries(totalGradeIndications)) {
     if (credits > highestCredits) {
-      highestCredits = credits;
-      highestGrade = parseInt(grade);
+      highestCredits = credits
+      highestGrade = Number.parseInt(grade)
     }
   }
-  return highestGrade;
+  return highestGrade
 }
 
 function getSpecializationFromCode(code: string): MembershipSpecialization {
