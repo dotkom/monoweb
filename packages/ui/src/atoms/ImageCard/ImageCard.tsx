817ad5892e46971ccbaad8da0920063f0204af1d
madsab <mads.barnes@hotmail.com>
2025-08-12 20:10:13 +0200

    add Structure section

--- packages/ui/src/atoms/ImageCard/ImageCard.tsx
+++ packages/ui/src/atoms/ImageCard/ImageCard.tsx
@@ -15,32 +15,27 @@ export const ImageCard = forwardRef<HTMLDivElement, ImageCardProps>(
         className={cn(
           props.className,
           "relative flex overflow-hidden rounded-2xl max-sm:flex-col ",
-          imagePosition === "left" ? " justify-end" : " justify-start"
+          imagePosition === "left" ? " justify-end" : "justify-start"
         )}
       >
         <img
           src={image}
           alt="Image Card"
           className={cn(
-            "object-cover",
+            "object-cover max-sm:h-1/3 max-sm:w-full",
             imagePosition === "right" && "sm:hidden",
-            imagePosition === "left" && "sm:absolute sm:left-0 sm:top-0 sm:h-full sm:w-2/5 "
+            imagePosition === "left" &&
+              "sm:absolute sm:left-0 sm:top-0 sm:h-full sm:w-2/5 sm:[clip-path:polygon(0%_0%,_100%_0%,_85%_100%,_0%_100%)]"
           )}
-          style={{
-            clipPath: imagePosition === "left" ? "polygon(0 0, 100% 0, 85% 100%, 0 100%)" : "none",
-            WebkitClipPath: imagePosition === "left" ? "polygon(0 0, 100% 0, 85% 100%, 0 100%)" : "none",
-          }}
         />
         <div className="sm:w-3/5">{children}</div>
         {imagePosition === "right" && (
           <img
             src={image}
             alt="Image Card"
-            className={cn("absolute right-0 top-0 h-full w-2/5 object-cover max-sm:hidden")}
-            style={{
-              clipPath: "polygon(15% 0, 100% 0, 100% 100%, 0 100%)",
-              WebkitClipPath: "polygon(15% 0, 100% 0, 100% 100%, 0 100%)",
-            }}
+            className={cn(
+              "absolute right-0 top-0 h-full w-2/5 object-cover max-sm:hidden [clip-path:polygon(15%_0%,_100%_0%,_100%_100%,_0%_100%)]"
+            )}
           />
         )}
       </div>
