ffe807747793413d45cac65927d299a47764512d
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-08-14 17:25:32 +0200

    Add "read more" to rich text (#1875)
    This fixes a regression removing "read more" on mobile view for event descriptions

--- packages/ui/src/molecules/RichText/RichText.tsx
+++ packages/ui/src/molecules/RichText/RichText.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import DOMPurify from "isomorphic-dompurify"
+import { useLayoutEffect, useRef, useState } from "react"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../../atoms/Collapsible/Collapsible"
 import { Text } from "../../atoms/Typography/Text"
 import { cn } from "../../utils"
 
@@ -6,28 +10,97 @@ interface RichTextProps {
   content: string
   colorLinks?: boolean
   className?: string
+  lineClamp?: `line-clamp-${number}`
+  readMoreText?: string
+  readLessText?: string
+  hideToggleButton?: boolean
+  toggleButtonClassName?: string
 }
 
-export function RichText({ content, colorLinks, className }: RichTextProps) {
-  return (
+export function RichText({
+  content,
+  className,
+  colorLinks = false,
+  lineClamp,
+  readMoreText = "Vis mer...",
+  readLessText = "Vis mindre",
+  hideToggleButton = false,
+  toggleButtonClassName,
+}: RichTextProps) {
+  const [open, setOpen] = useState(false)
+  const [isOverflowed, setIsOverflowed] = useState(false)
+  const [previousHeight, setPreviousHeight] = useState<number | null>(null)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const contentRef = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (open || previousHeight === null || !containerRef.current) {
+      return
+    }
+
+    const newHeight = containerRef.current.getBoundingClientRect().height
+    const delta = previousHeight - newHeight
+
+    if (delta > 0) {
+      window.scrollBy({ top: -delta })
+    }
+
+    setPreviousHeight(0)
+  }, [open, previousHeight])
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: This uses these dependencies indirectly
+  useLayoutEffect(() => {
+    const element = contentRef.current
+
+    if (element) {
+      setIsOverflowed(element.scrollHeight > element.clientHeight)
+    }
+  }, [content, lineClamp])
+
+  const handleToggle = () => {
+    if (open && containerRef.current) {
+      setPreviousHeight(containerRef.current.getBoundingClientRect().height)
+    }
+
+    setOpen(!open)
+  }
+
+  const richText = (
     <Text
       element="div"
-      // biome-ignore lint/security/noDangerouslySetInnerHtml: HTML is sanitized
+      ref={contentRef}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: sanitized
       dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
-      // prose would be nice to use here, but the styles are really ugly
-      // swag we are using prose anyway
       className={cn(
-        "prose dark:prose-invert",
+        "prose dark:prose-invert whitespace-pre-line overflow-hidden",
+        !open && lineClamp,
         "[&_a]:underline",
-        "dark:text-white",
         colorLinks && "[&_a]:text-blue-700 dark:[&_a]:text-blue-300",
         "[&_ul]:list-disc [&_ul]:pl-6",
         "[&_ol]:list-decimal [&_ol]:pl-6",
         "[&_li]:my-1",
-        // Kinda wack but better than nothing
         "[&_code]:px-1 [&_code]:py-0.5 [&_code]:bg-black/10 [&_code]:dark:bg-white/10 [&_code]:rounded-md",
         className
       )}
     />
+  )
+
+  if (!isOverflowed || hideToggleButton) return richText
+
+  return (
+    <Collapsible ref={containerRef} open={open} onOpenChange={setOpen}>
+      <CollapsibleContent forceMount>{richText}</CollapsibleContent>
+      <CollapsibleTrigger
+        asChild
+        onClick={handleToggle}
+        className={cn(
+          "mt-2 transition-colors text-gray-500 dark:text-stone-500 hover:text-black dark:hover:text-white [font-size:inherit]",
+          toggleButtonClassName
+        )}
+      >
+        <Text element="button">{open ? readLessText : readMoreText}</Text>
+      </CollapsibleTrigger>
+    </Collapsible>
   )
 }
