3bb4c68ed94bc17b3f2660748a5cf02cc0a2b560
brage-andreas <bragerino+github@gmail.com>
2025-08-14 16:08:43 +0200

    feat: add read more to rich text

--- apps/web/src/app/arrangementer/components/EventDescription.tsx
+++ apps/web/src/app/arrangementer/components/EventDescription.tsx
@@ -1,5 +1,14 @@
 import { RichText } from "@dotkomonline/ui"
 
 export function EventDescription({ description }: { description: string }) {
-  return <RichText colorLinks content={description} />
+  return (
+    <>
+      <div className="min-md:hidden">
+        <RichText colorLinks content={description} lineClamp="line-clamp-3" />
+      </div>
+      <div className="max-md:hidden">
+        <RichText colorLinks content={description} />
+      </div>
+    </>
+  );
 }
