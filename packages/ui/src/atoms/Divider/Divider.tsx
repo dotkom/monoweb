46dae69f4f029d0f13ae9179d0e1fd97275fb289
madsab <mads.barnes@hotmail.com>
2025-08-15 20:27:20 +0200

    seperate AboutsPage sections into files

--- packages/ui/src/atoms/Divider/Divider.tsx
+++ packages/ui/src/atoms/Divider/Divider.tsx
@@ -1,4 +1,5 @@
-import React, { forwardRef } from "react"
+import type React from "react"
+import { forwardRef } from "react"
 import { cn } from "../../utils"
 
 interface DividerProps extends React.HTMLAttributes<HTMLDivElement> {
