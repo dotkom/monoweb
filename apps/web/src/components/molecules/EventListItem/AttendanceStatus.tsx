f0f7c715dc5ea3d8460a3c3f1b35bb9f4f7163ea
brage-andreas <bragerino+github@gmail.com>
2025-08-18 16:09:53 +0200

    Update event list attendance status

--- apps/web/src/components/molecules/EventListItem/AttendanceStatus.tsx
+++ apps/web/src/components/molecules/EventListItem/AttendanceStatus.tsx
@@ -2,8 +2,8 @@
 
 import { getAttendanceStatus } from "@/app/arrangementer/components/attendanceStatus"
 import { type Attendance, getAttendanceCapacity, getReservedAttendeeCount } from "@dotkomonline/types"
-import { Icon, Text, cn } from "@dotkomonline/ui"
-import { formatDistanceToNowStrict } from "date-fns"
+import { Icon, Text, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger, cn } from "@dotkomonline/ui"
+import { formatDistanceToNowStrict, isFuture } from "date-fns"
 import { nb } from "date-fns/locale"
 import type { FC } from "react"
 
@@ -24,8 +24,9 @@ export const AttendanceStatus: FC<EventListItemAttendanceStatusProps> = ({
   const numberOfAttendees = getReservedAttendeeCount(attendance)
   const capacity = getAttendanceCapacity(attendance)
 
-  const justAttendanceClosed = attendanceStatus === "Closed" && !isReserved && !isUnreserved && !eventEndInPast
-  const inPast = attendanceStatus === "Closed" || eventEndInPast
+  const showLock =
+    !eventEndInPast &&
+    (isReserved || isUnreserved ? !isFuture(attendance.deregisterDeadline) : attendanceStatus === "Closed")
 
   const hasCapacity = capacity > 0
 
@@ -33,48 +34,63 @@ export const AttendanceStatus: FC<EventListItemAttendanceStatusProps> = ({
     <div
       className={cn(
         "flex flex-row items-center gap-2",
-        (inPast || attendanceStatus === "NotOpened") &&
+        (eventEndInPast || attendanceStatus === "NotOpened") &&
           "text-gray-600 dark:text-stone-700 group-hover:text-gray-800 dark:group-hover:text-stone-500"
       )}
     >
       <Icon
         icon="tabler:users"
-        className={cn("text-sm md:text-base", !inPast && "text-gray-800 dark:text-stone-500")}
+        className={cn("text-sm md:text-base", !eventEndInPast && "text-gray-800 dark:text-stone-500")}
       />
 
-      <div
-        className={cn(
-          "flex flex-row gap-1 items-center",
-          attendanceStatus === "NotOpened" && "text-gray-500 dark:text-stone-500",
-          (justAttendanceClosed || isReserved || isUnreserved) && "px-1 py-0.5 rounded-md",
-          justAttendanceClosed && "bg-gray-100 dark:bg-stone-800 text-gray-700 dark:text-stone-300",
-          isReserved && [
-            "text-green-700 dark:text-green-300",
-            !inPast ? "bg-green-100 dark:bg-green-950" : "bg-gray-100 dark:bg-stone-800",
-          ],
-          isUnreserved && [
-            "text-yellow-700 dark:text-yellow-300",
-            !inPast ? "bg-yellow-100 dark:bg-yellow-950" : "bg-gray-100 dark:bg-stone-800",
-          ]
-        )}
-      >
-        <Text className="text-xs md:text-sm">
-          {numberOfAttendees}
-          {hasCapacity && `/${capacity}`}
-        </Text>
+      <div className="flex flex-row gap-1">
+        <div
+          className={cn(
+            "flex flex-row gap-1 items-center",
+            attendanceStatus === "NotOpened" && "text-gray-500 dark:text-stone-500",
+            (isReserved || isUnreserved) && "px-1 py-0-5 rounded-md bg-gray-100 dark:bg-stone-800",
+            isReserved && [
+              "text-green-800 dark:text-green-200",
+              !eventEndInPast && (showLock ? "bg-green-100 dark:bg-green-950" : "bg-green-200 dark:bg-green-900"),
+            ],
+            isUnreserved && [
+              "text-amber-800 dark:text-amber-200",
+              !eventEndInPast && (showLock ? "bg-amber-100 dark:bg-amber-600/25" : "bg-amber-200 dark:bg-amber-600/50"),
+            ]
+          )}
+        >
+          <Text className="text-xs md:text-sm">
+            {numberOfAttendees}
+            {hasCapacity && `/${capacity}`}
+          </Text>
 
-        {attendanceStatus === "Closed" ? (
-          <Icon icon="tabler:lock" className="text-sm" />
-        ) : isReserved ? (
-          <Icon icon="tabler:check" className="text-sm" />
-        ) : isUnreserved ? (
-          <Icon icon="tabler:clock" className="text-sm" />
-        ) : null}
+          {isReserved ? (
+            <Icon icon="tabler:check" className="text-sm" />
+          ) : isUnreserved ? (
+            <Icon icon="tabler:clock" className="text-sm" />
+          ) : null}
+        </div>
+
+        {showLock && (
+          <TooltipProvider>
+            <Tooltip delayDuration={100}>
+              <TooltipTrigger asChild>
+                <div
+                  className={cn("px-1 rounded-md", "bg-gray-100 dark:bg-stone-800 text-gray-700 dark:text-stone-300")}
+                >
+                  <Icon icon="tabler:lock" className="text-sm" />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <Text>{isReserved || isUnreserved ? "Avmeldingsfristen er utgått" : "Påmeldingen er avsluttet"}</Text>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
       </div>
 
       {attendanceStatus === "NotOpened" && (
         <Text className="hidden md:block text-sm text-black dark:text-stone-400">
-          {/* TODO: remove this locale */}
           Åpner {formatDistanceToNowStrict(attendance.registerStart, { addSuffix: true, locale: nb })}
         </Text>
       )}
