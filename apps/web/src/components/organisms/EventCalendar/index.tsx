056d8bc1138f6d7746c57eb1166dc6ce59aa0387
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-08-20 20:01:29 +0200

    Hide fadderuka events in calendar view (#1956)

--- apps/web/src/components/organisms/EventCalendar/index.tsx
+++ apps/web/src/components/organisms/EventCalendar/index.tsx
@@ -91,6 +91,7 @@ export const EventCalendar: FC<CalendarProps> = async ({ year, month }) => {
           min: new Date(year, month, 1),
           max: new Date(year, month + 1, 0),
         },
+        excludingOrganizingGroup: ["velkom"],
       },
       take: 100,
     }),
