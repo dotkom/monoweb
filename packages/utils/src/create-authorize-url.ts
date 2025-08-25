e31974d073e2d26fc933f85cc4e519c302c5e7d0
brage-andreas <bragerino+github@gmail.com>
2025-08-25 16:57:32 +0200

    refactor: rename param and add jsdoc

--- packages/utils/src/create-authorize-url.ts
+++ packages/utils/src/create-authorize-url.ts
@@ -1,7 +1,14 @@
-export const createAuthorizeUrl = (...params: { [name: string]: string }[]) => {
+/**
+ * Creates an authorize URL with the given search parameters.
+ *
+ * @example
+ * const pathname = usePathname()
+ * const url = createAuthorizeUrl({ connection: "FEIDE", redirectAfter: pathname })
+ */
+export const createAuthorizeUrl = (...searchParams: { [name: string]: string }[]) => {
   const url = new URL("/api/auth/authorize", window.location.origin)
-  for (const param of params) {
-    for (const [name, value] of Object.entries(param)) {
+  for (const searchParam of searchParams) {
+    for (const [name, value] of Object.entries(searchParam)) {
       url.searchParams.set(name, value)
     }
   }
