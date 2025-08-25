f2e3d9928959ecdd3f27e7993242ca4dda311b06
brage-andreas <bragerino+github@gmail.com>
2025-08-25 17:17:48 +0200

    feat: split into relative and absolute

--- packages/utils/src/create-authorize-url.ts
+++ packages/utils/src/create-authorize-url.ts
@@ -1,3 +1,5 @@
+const AUTHORIZE_ENDPOINT = "/api/auth/authorize"
+
 /**
  * Creates an authorize URL with the given search parameters.
  *
@@ -6,7 +8,25 @@
  * const url = createAuthorizeUrl({ connection: "FEIDE", redirectAfter: pathname })
  */
 export const createAuthorizeUrl = (...parameters: ConstructorParameters<typeof URLSearchParams>) => {
-  const url = new URL("/api/auth/authorize", window.location.origin)
+  const searchParams = new URLSearchParams(...parameters).toString()
+  if (!searchParams) {
+    return AUTHORIZE_ENDPOINT
+  }
+  return `${AUTHORIZE_ENDPOINT}?${searchParams}`
+}
+
+/**
+ * Creates an authorize URL with the given search parameters.
+ *
+ * @example
+ * const pathname = usePathname()
+ * const url = createAbsoluteAuthorizeUrl(window.location.origin, { connection: "FEIDE", redirectAfter: pathname })
+ */
+export const createAbsoluteAuthorizeUrl = (
+  origin: string,
+  ...parameters: ConstructorParameters<typeof URLSearchParams>
+) => {
+  const url = new URL(AUTHORIZE_ENDPOINT, origin)
   url.search = new URLSearchParams(...parameters).toString()
   return url.toString()
 }
