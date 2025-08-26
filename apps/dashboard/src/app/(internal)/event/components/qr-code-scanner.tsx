ff6e714ad7354107a4b2422171d3ee480051a5aa
Brage <37003366+brage-andreas@users.noreply.github.com>
2025-08-26 19:40:51 +0200

    Add camera mirror switch (#2010)
    Co-authored-by: Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>

--- apps/dashboard/src/app/(internal)/event/components/qr-code-scanner.tsx
+++ apps/dashboard/src/app/(internal)/event/components/qr-code-scanner.tsx
@@ -1,7 +1,7 @@
 import type { Attendance } from "@dotkomonline/types"
-import { AspectRatio, Button, Group, Loader, Skeleton, Stack, Text } from "@mantine/core"
+import { ActionIcon, AspectRatio, Button, Group, Loader, Skeleton, Stack, Text } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
-import { IconQrcode, IconQrcodeOff } from "@tabler/icons-react"
+import { IconFlipVertical, IconQrcode, IconQrcodeOff } from "@tabler/icons-react"
 import { type FC, useRef, useState } from "react"
 import { useZxing } from "react-zxing"
 import z from "zod"
@@ -59,7 +59,7 @@ export const QrCodeScanner: FC<QrCodeScannerProps> = ({ attendance }) => {
   const videoElement = ref.current
   const track = videoElement?.srcObject instanceof MediaStream ? videoElement.srcObject.getVideoTracks()[0] : null
   const settings = track?.getSettings()
-  const isCameraMirrored = settings?.facingMode === "user"
+  const [mirror, { toggle: toggleMirror }] = useDisclosure(settings?.facingMode === "user")
 
   return (
     <Stack gap="xs">
@@ -89,21 +89,39 @@ export const QrCodeScanner: FC<QrCodeScannerProps> = ({ attendance }) => {
         )}
       </Group>
 
-      {scannerOpen && !videoReady && <Skeleton height={450} width={450} radius="sm" />}
-      <AspectRatio w={scannerOpen && videoReady ? 450 : 0}>
-        <video
-          ref={ref}
-          muted
-          onCanPlay={() => setVideoReady(true)}
-          playsInline
-          autoPlay
-          style={{
-            display: videoReady ? "block" : "none",
-            transform: isCameraMirrored ? "scaleX(-1)" : undefined,
-            borderRadius: "4px",
-          }}
-        />
-      </AspectRatio>
+      <div style={{ width: "fit-content", position: "relative" }}>
+        {scannerOpen && !videoReady && <Skeleton height={450} width={450} radius="sm" />}
+        <AspectRatio w={scannerOpen && videoReady ? 450 : 0}>
+          <video
+            ref={ref}
+            muted
+            onCanPlay={() => setVideoReady(true)}
+            playsInline
+            autoPlay
+            style={{
+              display: videoReady && scannerOpen ? "block" : "none",
+              transform: mirror ? "scaleX(-1)" : undefined,
+              borderRadius: "4px",
+            }}
+          />
+
+          <ActionIcon
+            size="input-xs"
+            bg="rgba(0,0,0,0.25)"
+            variant="transparent"
+            w="fit-content"
+            onClick={toggleMirror}
+            style={{
+              display: scannerOpen && videoReady ? "block" : "none",
+              position: "absolute",
+              top: 12,
+              right: 12,
+            }}
+          >
+            <IconFlipVertical color="white" size={48} />
+          </ActionIcon>
+        </AspectRatio>
+      </div>
     </Stack>
   )
 }
