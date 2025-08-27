bb98efd255365f61c47400d5c1c49d675922b9fb
Mats Jun Larsen <mats@jun.codes>
2025-08-27 15:32:11 +0200

    Include tRPC procedure type and path in OTEL span name (#2019)

--- apps/rpc/src/trpc.ts
+++ apps/rpc/src/trpc.ts
@@ -102,47 +102,49 @@ export const router = t.router
  * server call.
  */
 export const procedure = t.procedure.use(async ({ ctx, path, type, next }) => {
-  return await trace.getTracer("@dotkomonline/rpc/trpc-request").startActiveSpan("tRPC procedure", async (span) => {
-    // See https://opentelemetry.io/docs/specs/semconv/registry/attributes/rpc/ and https://opentelemetry.io/docs/specs/semconv/registry/attributes/http/
-    // for the meaning of these attributes.
-    span.setAttribute("rpc.service", "@dotkomonline/rpc")
-    span.setAttribute("rpc.system", "trpc")
-    span.setAttribute("http.request.method", "_OTHER")
-    span.setAttribute("http.request.method_original", type)
-    span.setAttribute("http.route", path)
-    try {
-      const logger = getLogger("@dotkomonline/rpc/trpc")
-      const result = await next({ ctx })
-      // This is how tRPC middlewares capture results of the procedure call. Infact, the try-finally block above is not
-      // related to error handling at all, but rather to ensure the OpenTelemetry tracing span is ALWAYS ended.
-      if (result.ok) {
-        return result
-      }
-      // This means an error occurred in the procedure call, and we need to report it anonymously to the user, and send
-      // the telemetry off to the OpenTelemetry backend.
-      const traceId = span?.spanContext().traceId ?? "<missing traceId>"
-      logger.error(
-        "tRPC error triggered by Principal(Subject=%s) in Request(Path=%s, Method=%s) traced by Trace(TraceID=%s): %o",
-        ctx?.principal?.subject ?? "<anonymous>",
-        path,
-        type,
-        traceId,
-        result.error
-      )
-      captureException(result.error)
-      span.recordException(result.error)
-      span.setStatus({ code: SpanStatusCode.ERROR })
+  return await trace
+    .getTracer("@dotkomonline/rpc/trpc-request")
+    .startActiveSpan(`tRPC ${type} ${path}`, async (span) => {
+      // See https://opentelemetry.io/docs/specs/semconv/registry/attributes/rpc/ and https://opentelemetry.io/docs/specs/semconv/registry/attributes/http/
+      // for the meaning of these attributes.
+      span.setAttribute("rpc.service", "@dotkomonline/rpc")
+      span.setAttribute("rpc.system", "trpc")
+      span.setAttribute("http.request.method", "_OTHER")
+      span.setAttribute("http.request.method_original", type)
+      span.setAttribute("http.route", path)
+      try {
+        const logger = getLogger("@dotkomonline/rpc/trpc")
+        const result = await next({ ctx })
+        // This is how tRPC middlewares capture results of the procedure call. Infact, the try-finally block above is not
+        // related to error handling at all, but rather to ensure the OpenTelemetry tracing span is ALWAYS ended.
+        if (result.ok) {
+          return result
+        }
+        // This means an error occurred in the procedure call, and we need to report it anonymously to the user, and send
+        // the telemetry off to the OpenTelemetry backend.
+        const traceId = span?.spanContext().traceId ?? "<missing traceId>"
+        logger.error(
+          "tRPC error triggered by Principal(Subject=%s) in Request(Path=%s, Method=%s) traced by Trace(TraceID=%s): %o",
+          ctx?.principal?.subject ?? "<anonymous>",
+          path,
+          type,
+          traceId,
+          result.error
+        )
+        captureException(result.error)
+        span.recordException(result.error)
+        span.setStatus({ code: SpanStatusCode.ERROR })
 
-      const message = `${result.error.code} occurred in Trace(TraceID=${traceId})`
-      return {
-        marker: result.marker,
-        ok: false,
-        error: new TRPCError({ code: result.error.code, message }),
-      } satisfies MiddlewareResult<unknown>
-    } finally {
-      span.end()
-    }
-  })
+        const message = `${result.error.code} occurred in Trace(TraceID=${traceId})`
+        return {
+          marker: result.marker,
+          ok: false,
+          error: new TRPCError({ code: result.error.code, message }),
+        } satisfies MiddlewareResult<unknown>
+      } finally {
+        span.end()
+      }
+    })
 })
 
 export const authenticatedProcedure = procedure.use(({ ctx, next }) => {
