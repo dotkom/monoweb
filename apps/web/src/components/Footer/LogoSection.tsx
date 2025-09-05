d27b4ce4e232ed8ef4c9e341ff38871124ba76d5
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-09-05 22:54:15 +0200

    Fix improper logo alignment in footer in certain browsers (#2065)

--- apps/web/src/components/Footer/LogoSection.tsx
+++ apps/web/src/components/Footer/LogoSection.tsx
@@ -3,8 +3,8 @@ import { OnlineLogo } from "../atoms/OnlineLogo"
 
 export const LogoSection = () => (
   <div className="flex flex-col md:flex-row gap-4 md:gap-16 w-full md:items-end">
-    <OnlineLogo style="brand" className="h-10 sm:h-12 w-fit dark:hidden" />
-    <OnlineLogo style="black" className="h-10 sm:h-12 w-fit hidden dark:block" />
+    <OnlineLogo style="brand" className="h-10 sm:h-12 dark:hidden" />
+    <OnlineLogo style="black" className="h-10 sm:h-12 hidden dark:block" />
 
     <Text element="h2" className="text-lg md:text-xl">
       Linjeforeningen for informatikk ved NTNU
