ab9d6d2885fd46a31fd22dd1f8a93055edbf1ce1
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-09-05 22:45:36 +0200

    Move div into OnlineLogo component

--- apps/web/src/components/Footer/LogoSection.tsx
+++ apps/web/src/components/Footer/LogoSection.tsx
@@ -3,10 +3,8 @@ import { OnlineLogo } from "../atoms/OnlineLogo"
 
 export const LogoSection = () => (
   <div className="flex flex-col md:flex-row gap-4 md:gap-16 w-full md:items-end">
-    <div className="aspect-[643/167] h-10 sm:h-12">
-      <OnlineLogo style="brand" className="h-full dark:hidden" />
-      <OnlineLogo style="black" className="h-full hidden dark:block" />
-    </div>
+    <OnlineLogo style="brand" className="h-10 sm:h-12 dark:hidden" />
+    <OnlineLogo style="black" className="h-10 sm:h-12 hidden dark:block" />
 
     <Text element="h2" className="text-lg md:text-xl">
       Linjeforeningen for informatikk ved NTNU
