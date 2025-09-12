50e55e17b52a4eedf2f3c5235ca480ace8993b37
brage-andreas <bragerino+github@gmail.com>
2025-09-12 15:39:57 +0200

    chore: remove redundant enum entry

--- packages/db/prisma/migrations/20250903184923_add_workspace_user_id_and_mail_auditor/migration.sql
+++ packages/db/prisma/migrations/20250903184923_add_workspace_user_id_and_mail_auditor/migration.sql
@@ -2,4 +2,3 @@ ALTER TABLE "ow_user" ADD COLUMN     "workspaceUserId" TEXT;
 CREATE UNIQUE INDEX "ow_user_workspaceUserId_key" ON "ow_user"("workspaceUserId");
 ALTER TABLE "group" ADD COLUMN     "workspaceGroupId" TEXT;
 CREATE UNIQUE INDEX "group_workspaceGroupId_key" ON "group"("workspaceGroupId");
-ALTER TYPE "membership_type" ADD VALUE 'MAIL_AUDITOR';
