82d8bb9a1f9257a72f639ac9b20725b084e250ad
brage-andreas <bragerino+github@gmail.com>
2025-09-15 15:02:18 +0200

    fix: clean migration mess

--- packages/db/prisma/migrations/20250911223815_add_missing_group_role_types/migration.sql
+++ packages/db/prisma/migrations/20250911223815_add_missing_group_role_types/migration.sql
@@ -1,8 +1,20 @@
-ALTER TABLE "group" RENAME COLUMN "description" TO "shortDescription";
-ALTER TABLE "group" RENAME COLUMN "about" TO "description";
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
 
-ALTER TABLE "event" RENAME COLUMN "subtitle" TO "shortDescription";
-ALTER TABLE "event" RENAME COLUMN "about" TO "description";
+BEGIN;
 
-ALTER TABLE "job_listing" RENAME COLUMN "description" TO "shortDescription";
-ALTER TABLE "job_listing" RENAME COLUMN "about" TO "description";
+ALTER TYPE "group_role_type" ADD VALUE 'TREASURER';
+ALTER TYPE "group_role_type" ADD VALUE 'DEPUTY_LEADER';
+ALTER TYPE "group_role_type" ADD VALUE 'TRUSTEE';
+ALTER TYPE "group_role_type" ADD VALUE 'EMAIL_ONLY';
+
+COMMIT;
+
+-- Manually update types of existing roles
+UPDATE group_role SET "type" = 'DEPUTY_LEADER' WHERE "name" = 'Nestleder';
+UPDATE group_role SET "type" = 'TREASURER' WHERE "name" = 'Økonomiansvarlig';
+UPDATE group_role SET "type" = 'TRUSTEE' WHERE "name" = 'Tillitsvalgt';
