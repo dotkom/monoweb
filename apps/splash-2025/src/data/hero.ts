c257cc401721c13136242969f0db6925be167788
Mats Jun Larsen <mats@jun.codes>
2025-09-17 12:27:33 +0200

    Decomission Splash 2025 application (#2183)

--- apps/splash-2025/src/data/hero.ts
+++
@@ -1,9 +0,0 @@
-export interface HeroProps {
-  background: string
-  banner: string
-}
-
-export const HeroData: HeroProps = {
-  background: "bg-[url(/bakgrunnsBilde.jpg)]",
-  banner: "./online-logo-darkmode.svg",
-}
