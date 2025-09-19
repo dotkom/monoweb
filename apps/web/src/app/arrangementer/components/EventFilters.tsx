b93741391cd5fcaf14ac96450745329d411c7953
André Klarpås <78028923+andrefkl@users.noreply.github.com>
2025-09-20 00:27:11 +0200

    Add more filters to event list (#2207)

--- apps/web/src/app/arrangementer/components/EventFilters.tsx
+++ apps/web/src/app/arrangementer/components/EventFilters.tsx
@@ -1,31 +1,210 @@
 "use client"
 
-import type { EventFilterQuery } from "@dotkomonline/types"
-import { TextInput } from "@dotkomonline/ui"
-import { useEffect } from "react"
-import { useForm, useWatch } from "react-hook-form"
+import {
+  type EventFilterQuery,
+  EventFilterQuerySchema,
+  EventSchema,
+  EventTypeSchema,
+  type Group,
+  GroupSchema,
+  mapEventTypeToLabel,
+} from "@dotkomonline/types"
+import {
+  Label,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  TextInput,
+} from "@dotkomonline/ui"
+import { useEffect, useMemo, useState } from "react"
+import { Controller, useForm, useWatch } from "react-hook-form"
 import { useDebounce } from "use-debounce"
+import { z } from "zod"
+import { type EventListViewMode, EventListViewModeSchema } from "./EventList"
+
+const EVENT_TYPE_OPTIONS = Object.values(EventTypeSchema.Values).map((type) => ({
+  value: type,
+  label: mapEventTypeToLabel(type),
+}))
+
+const FormSchema = EventFilterQuerySchema.pick({
+  bySearchTerm: true,
+}).extend({
+  byType: EventSchema.shape.type.or(z.literal("ALL")),
+  byOrganizingGroup: GroupSchema.shape.slug,
+  viewMode: EventListViewModeSchema,
+})
+
+type FormValues = z.infer<typeof FormSchema>
 
 interface Props {
-  onChange(filters: EventFilterQuery): void
+  onChange(filters: EventFilterQuery, viewMode: EventListViewMode): void
+  groups: Group[]
 }
 
-export const EventFilters = ({ onChange }: Props) => {
-  const form = useForm<EventFilterQuery>()
-  const data = useWatch(form) as EventFilterQuery
+export const EventFilters = ({ onChange, groups }: Props) => {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      bySearchTerm: "",
+      byType: "ALL",
+      byOrganizingGroup: "ALL",
+      viewMode: "ATTENDANCE",
+    },
+  })
+  const data = useWatch({ control: form.control }) as FormValues
   const [debouncedData] = useDebounce(data, 300)
 
   useEffect(() => {
-    onChange(debouncedData)
-  }, [onChange, debouncedData])
+    handleSubmit(debouncedData)
+  }, [debouncedData])
 
-  const handleSubmit = (values: EventFilterQuery) => {
-    onChange(values)
+  const handleSubmit = (values: FormValues) => {
+    onChange(
+      {
+        bySearchTerm: values.bySearchTerm,
+        byType: values.byType !== "ALL" ? [values.byType] : [],
+        byOrganizingGroup: values.byOrganizingGroup !== "ALL" ? [values.byOrganizingGroup] : [],
+      },
+      values.viewMode
+    )
   }
 
   return (
-    <form onSubmit={form.handleSubmit(handleSubmit)}>
-      <TextInput placeholder="Søk etter arrangementer..." {...form.register("bySearchTerm")} />
-    </form>
+    <div className="border-gray-200 dark:border-stone-700 h-fit rounded-lg border shadow-b-sm py-4">
+      <div className="mx-4">
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-col gap-4">
+          <div>
+            <Label htmlFor="bySearchTerm" className="mb-1 text-md">
+              Søk
+            </Label>
+            <TextInput placeholder="Søk etter arrangementer..." {...form.register("bySearchTerm")} id="bySearchTerm" />
+          </div>
+          <Controller
+            control={form.control}
+            name="byType"
+            render={({ field: { onChange, value } }) => (
+              <div>
+                <Label htmlFor="byType" className="mb-1 text-md">
+                  Type
+                </Label>
+                <Select value={value} onValueChange={onChange}>
+                  <SelectTrigger id="byType">
+                    <SelectValue />
+                  </SelectTrigger>
+
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="ALL" key="ALL">
+                        Alle
+                      </SelectItem>
+                      {EVENT_TYPE_OPTIONS.map((type) => (
+                        <SelectItem value={type.value} key={type.value}>
+                          {type.label}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            )}
+          />
+
+          <Controller
+            control={form.control}
+            name="byOrganizingGroup"
+            render={({ field: { onChange, value } }) => (
+              <div>
+                <Label htmlFor="byOrganizingGroup" className="mb-1 text-md">
+                  Arrangør
+                </Label>
+                <GroupSelect onChange={onChange} groups={groups} value={value} />
+              </div>
+            )}
+          />
+
+          <Controller
+            control={form.control}
+            name="viewMode"
+            render={({ field: { onChange, value } }) => (
+              <div>
+                <Label htmlFor="viewMode" className="mb-1 text-md">
+                  Sorter
+                </Label>
+                <Select value={value} onValueChange={onChange}>
+                  <SelectTrigger id="viewMode">
+                    <SelectValue />
+                  </SelectTrigger>
+
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="ATTENDANCE">Påmelding</SelectItem>
+                      <SelectItem value="CHRONOLOGICAL">Kronologisk</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            )}
+          />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+const GroupSelect = ({
+  value,
+  onChange,
+  groups,
+}: {
+  value?: string
+  onChange: (value: string | undefined) => void
+  groups: Group[]
+}) => {
+  const [search, setSearch] = useState("")
+
+  const filtered = useMemo(() => {
+    const searchValue = search.trim().toLowerCase()
+
+    return groups.filter((group) => group.slug === value || group.abbreviation.toLowerCase().includes(searchValue))
+  }, [search, groups, value])
+
+  const handleOpenChange = (open: boolean) => {
+    if (open) {
+      setSearch("")
+    }
+  }
+
+  return (
+    <Select value={value} onValueChange={(val) => onChange(val)} onOpenChange={handleOpenChange}>
+      <SelectTrigger id="byOrganizingGroup">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className="max-h-72" hideScrollUpButton>
+        <div
+          className="sticky top-0 z-10 -translate-y-1 p-2
+               bg-white dark:bg-stone-800
+               border-b border-gray-200 dark:border-stone-700"
+        >
+          <TextInput
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={(e) => e.stopPropagation()}
+            placeholder="Søk…"
+          />
+        </div>
+
+        <SelectGroup>
+          <SelectItem value="ALL">Alle</SelectItem>
+          {filtered.map((group) => (
+            <SelectItem key={group.slug} value={group.slug}>
+              {group.abbreviation}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
