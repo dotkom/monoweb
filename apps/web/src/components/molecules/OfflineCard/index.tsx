f981c9248df113eb651b1cb9630b9d29943637bc
Johannes Aas <johannes.hansen.aas@gmail.com>
2025-09-20 16:41:13 +0200

    Add 3d hover effect to Offline cover

--- apps/web/src/components/molecules/OfflineCard/index.tsx
+++ apps/web/src/components/molecules/OfflineCard/index.tsx
@@ -12,13 +12,24 @@ export const OfflineCard = ({ offline }: OfflineCardProps) => {
     <div className="flex flex-col gap-3 text-wrap max-w-56">
       {offline.imageUrl && offline.fileUrl && (
         <Link href={offline.fileUrl}>
-          <Image
-            src={offline.imageUrl}
-            width={200}
-            height={250}
-            alt={offline.title}
-            className="rounded-sm cursor-pointer w-auto transition-transform duration-200 hover:scale-105 shadow-xs hover:shadow-md"
-          />
+          <div className="perspective-[1000px] bg-gray-300 rounded-r-md">
+            <div
+              className="
+                transform-style-[preserve-3d]
+                transition-transform duration-[600ms] ease-in-out
+                origin-left hover:rotate-y-[-20deg]
+                shadow-md
+              "
+            >
+              <Image
+                src="https://s3.eu-north-1.amazonaws.com/cdn.online.ntnu.no/%2F1755548141016-e7d24ab0-b81e-4b10-bce5-cd975420396f-offline.png"
+                width={200}
+                height={250}
+                alt={offline.title}
+                className="w-auto shadow-lg rounded-r-md object-cover block backface-visibility-[hidden]"
+              />
+            </div>
+          </div>
         </Link>
       )}
       <Text className="text-gray-950 dark:text-white">{offline.title}</Text>
