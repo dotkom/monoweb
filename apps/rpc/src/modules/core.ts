ff65a7277c0a3cbbeb38665b416a4b0dd6c21b71
Mats Jun Larsen <mats@jun.codes>
2025-09-26 12:25:22 +0200

    Remove leftover console.log

--- apps/rpc/src/modules/core.ts
+++ apps/rpc/src/modules/core.ts
@@ -89,8 +89,6 @@ function getDirectory(): admin_directory_v1.Admin {
     throw new IllegalStateError("Google Workspace integration is not enabled or missing configuration variables")
   }
 
-  console.log(configuration)
-
   const serviceAccountJson = JSON.parse(configuration.googleWorkspace.serviceAccount)
   const result = workspaceServiceAccountJsonSchema.safeParse(serviceAccountJson)
 
