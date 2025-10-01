3f0a7aeaee813103fb71c299fb76565d81808478
Jacob <123555141+Skardery@users.noreply.github.com>
2025-10-01 19:54:05 +0200

    Fix type check

--- apps/rpc/src/modules/event/event.e2e-spec.ts
+++ apps/rpc/src/modules/event/event.e2e-spec.ts
@@ -16,6 +16,7 @@ export function getMockGroup(input: Partial<GroupWrite> = {}): GroupWrite {
     imageUrl: faker.image.url(),
     deactivatedAt: null,
     workspaceGroupId: null,
+    showMembers: "ALL_MEMBERS",
     slug: "dotkom",
     showLeaderAsContact: false,
     ...input,
