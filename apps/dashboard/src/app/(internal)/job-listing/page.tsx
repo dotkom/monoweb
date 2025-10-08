f6314385fd08fd414e41ad82e99479bb22b75382
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-09 00:39:11 +0200

    Add more info to job listing table and add search

--- apps/dashboard/src/app/(internal)/job-listing/page.tsx
+++ apps/dashboard/src/app/(internal)/job-listing/page.tsx
@@ -1,23 +1,31 @@
 "use client"
 
 import { GenericTable } from "@/components/GenericTable"
-import { Box, Button, Skeleton, Stack } from "@mantine/core"
+import type { JobListingFilterQuery } from "@dotkomonline/types"
+import { Box, Button, Group, Skeleton, Stack } from "@mantine/core"
+import { useState } from "react"
+import { JobListingFilters } from "./components/job-listing-filter"
 import { useCreateJobListingModal } from "./modals/create-job-listing-modal"
 import { useJobListingAllQuery } from "./queries/use-job-listing-all-query"
 import { useJobListingTable } from "./use-job-listing-table"
 
 export default function JobListingPage() {
-  const { jobListings, isLoading: isJobListingsLoading } = useJobListingAllQuery()
+  const [filter, setFilter] = useState<JobListingFilterQuery>({})
+  const { jobListings, isLoading: isJobListingsLoading, fetchNextPage } = useJobListingAllQuery({ filter })
   const open = useCreateJobListingModal()
   const table = useJobListingTable({ data: jobListings })
 
   return (
     <Skeleton visible={isJobListingsLoading}>
       <Stack>
-        <Box>
-          <Button onClick={open}>Opprett stillingsannonse</Button>
-        </Box>
-        <GenericTable table={table} />
+        <Group justify="space-between">
+          <JobListingFilters onChange={setFilter} />
+
+          <Box>
+            <Button onClick={open}>Opprett stillingsannonse</Button>
+          </Box>
+        </Group>
+        <GenericTable table={table} onLoadMore={fetchNextPage} />
       </Stack>
     </Skeleton>
   )
--- apps/dashboard/src/app/(internal)/job-listing/queries/use-job-listing-all-query.ts
+++ apps/dashboard/src/app/(internal)/job-listing/queries/use-job-listing-all-query.ts
@@ -1,11 +1,23 @@
 import { useTRPC } from "@/lib/trpc-client"
+import type { JobListingFilterQuery } from "@dotkomonline/types"
 import { useInfiniteQuery } from "@tanstack/react-query"
+import type { Pageable } from "node_modules/@dotkomonline/rpc/src/query"
 import { useMemo } from "react"
 
-export const useJobListingAllQuery = () => {
+interface UseJobListingAllProps {
+  filter: JobListingFilterQuery
+  page?: Pageable
+}
+
+export const useJobListingAllQuery = ({ filter, page }: UseJobListingAllProps) => {
   const trpc = useTRPC()
   const { data: jobListings, ...query } = useInfiniteQuery({
-    ...trpc.jobListing.findMany.infiniteQueryOptions(),
+    ...trpc.jobListing.findMany.infiniteQueryOptions({
+      filter: {
+        ...filter,
+      },
+      ...page,
+    }),
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   })
   return { jobListings: useMemo(() => jobListings?.pages.flatMap((page) => page.items) ?? [], [jobListings]), ...query }
