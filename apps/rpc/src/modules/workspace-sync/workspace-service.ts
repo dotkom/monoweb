ab68c9f331907e3bb38f57b36d7ccbf9c709cd0d
brage-andreas <bragerino+github@gmail.com>
2025-10-28 19:22:43 +0100

    feat: ignore old accounts in Workspace sync state

--- apps/rpc/src/modules/workspace-sync/workspace-service.ts
+++ apps/rpc/src/modules/workspace-sync/workspace-service.ts
@@ -16,6 +16,7 @@ import {
   getActiveGroupMembership,
 } from "@dotkomonline/types"
 import { slugify } from "@dotkomonline/utils"
+import { isAfter } from "date-fns"
 import type { admin_directory_v1 } from "googleapis"
 import { GaxiosError, type GaxiosResponseWithHTTP2 } from "googleapis-common"
 import invariant from "tiny-invariant"
@@ -23,6 +24,7 @@ import type { ConfigurationWithGoogleWorkspace } from "../../configuration"
 import { NotFoundError } from "../../error"
 import type { GroupService } from "../group/group-service"
 import type { UserService } from "../user/user-service"
+import { TZDate } from "@date-fns/tz"
 
 // Google Workspace enforces a minimum password length of 8 characters
 const TEMPORARY_PASSWORD_LENGTH = 8
@@ -249,6 +251,22 @@ export function getWorkspaceService(
   }
 
   function getWorkspaceMemberSyncState(memberLink: Omit<WorkspaceMemberLink, "syncState">): WorkspaceMemberSyncState {
+    // Some of the oldest users do not have Workspace accounts, and we do not want to give "Needs linking" warnings for
+    // them, since it has been over a decade since their last group membership ended. We do not want to create new
+    // accounts for these old users either, so we treat them as synced.
+
+    // Some group members have their latest membership end set to 2016-01-01T00:00:00Z and do not have Workspace accounts.
+    // This cutoff is chosen because everyone with their latest membership end after this date has a Workspace account.
+    // January 2nd is just to be safe.
+    const oldUserLastGroupMembershipCutoff = new TZDate("2016-01-02T00:00:00Z")
+    const isOldUser = memberLink.groupMember?.groupMemberships.every(
+      ({ end }) => end && !isAfter(end, oldUserLastGroupMembershipCutoff)
+    )
+
+    if (isOldUser) {
+      return "SYNCED"
+    }
+
     if (memberLink.groupMember && !memberLink.groupMember?.workspaceUserId) {
       return "PENDING_LINK"
     }
