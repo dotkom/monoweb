9b38d5411fc93d1835029e8bd7ae43c23bd51981
brage-andreas <bragerino+github@gmail.com>
2025-10-31 13:35:18 +0100

    fix jubileum notice

--- apps/web/src/app/jubileum-notice.tsx
+++ apps/web/src/app/jubileum-notice.tsx
@@ -1,50 +1,51 @@
 "use client"
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { Text } from '@dotkomonline/ui';
-import { differenceInDays, intervalToDuration } from 'date-fns';
+import { TZDate } from "@date-fns/tz"
+import { Text } from "@dotkomonline/ui"
+import { differenceInDays, intervalToDuration } from "date-fns"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
-const getTimeDiff = ((startDate: string | Date, endDate: string | Date) => {
+const formatCountdown = (now: Date, target: Date) => {
+  const duration = intervalToDuration({ start: now, end: target })
+  const days = Math.max(differenceInDays(target, now), 0)
 
-  const duration = intervalToDuration({ start: startDate, end: endDate });
+  let string = `${days.toString().padStart(2, "0")}d`
+  string += ` ${(duration.hours ?? 0).toString().padStart(2, "0")}t`
+  string += ` ${(duration.minutes ?? 0).toString().padStart(2, "0")}m`
+  string += ` ${(duration.seconds ?? 0).toString().padStart(2, "0")}s`
 
-  const days = differenceInDays(endDate, startDate);
+  return string
+}
 
-  return `${(days < 10 ? `0${days}` : `${days}`)}d ${Number(duration.hours) < 10 ? `0${duration.hours}` : `${duration.hours}`}t ${Number(duration.minutes) < 10 ? `0${duration.minutes}` : `${duration.minutes}`}m ${Number(duration.seconds) < 10 ? `0${duration.seconds}` : `${duration.seconds}`}s`;
-})
-
-export default function JubNotice() {
-  const [now, setNow] = useState(new Date());
-  const targetDate = new Date(2026, 1, 16);
+export const JubileumNotice = () => {
+  const [now, setNow] = useState(new Date())
+  const jubileumStartDateUtc = new TZDate("2026-02-16T17:30:00", "+02:00")
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setNow(new Date());
-    }, 1000);
+      setNow(new Date())
+    }, 1000)
 
-    return () => clearInterval(interval);
-  }, []);
-  
-  const timeLeft = getTimeDiff(now, targetDate);
+    return () => clearInterval(interval)
+  }, [])
+
+  const countdown = formatCountdown(now, jubileumStartDateUtc)
 
   return (
-    <div className='bg-black rounded-2xl h-40'>
-        <div className="relative h-full bg-[linear-gradient(rgba(255,255,255,0.1)_50%,transparent_50%)] bg-[length:10%_10px] animate-scan rounded-2xl" />
+    <div className="bg-black rounded-2xl h-32">
+      <div className="relative h-full bg-[linear-gradient(rgba(255,255,255,0.1)_50%,transparent_50%)] bg-[length:10%_10px] animate-scan rounded-2xl" />
 
-        <Link href="https://jub.online.ntnu.no/" target="_blank" rel="noopener noreferrer"
-        className="flex flex-col justify-center items-center animate-flicker -mt-40 z-50 h-full"
-        >
-            <Text
-            className={`font-glass text-white text-2xl lg:text-4xl z-50`}
-            >
-            Online Jubileum
-            </Text>
-            <Text
-            className={`font-glass text-white text-2xl lg:text-4xl z-50`} suppressHydrationWarning={true}
-            >
-            {timeLeft}
-            </Text>
-        </Link>
+      <Link
+        href="https://jub.online.ntnu.no/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex flex-col justify-center items-center animate-flicker -mt-32 h-full"
+      >
+        <Text className="font-glass text-white text-2xl lg:text-4xl">Online Jubileum</Text>
+        <Text className="font-glass text-white text-2xl lg:text-4xl" suppressHydrationWarning={true}>
+          {countdown}
+        </Text>
+      </Link>
     </div>
-  );
+  )
 }
