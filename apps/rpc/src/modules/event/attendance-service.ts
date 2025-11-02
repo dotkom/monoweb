a7df9af944edbbbe118d7726878ebc642c750a72
André Klarpås <78028923+andrefkl@users.noreply.github.com>
2025-11-02 12:41:09 +0100

    Fix attendees on waitlist not being reserved by task (#2430)

--- apps/rpc/src/modules/event/attendance-service.ts
+++ apps/rpc/src/modules/event/attendance-service.ts
@@ -630,23 +630,35 @@ export function getAttendanceService(
       const attendance = await this.getAttendanceById(handle, attendanceId)
       const event = await eventService.getByAttendanceId(handle, attendance.id)
       const attendee = attendance.attendees.find((a) => a.id === attendeeId)
+
       // NOTE: If the attendee does not exist, we have a non-critical bug in the app. The circumstances where this is
       // possible is when the attendee was removed from the attendance after the task was scheduled AND the task was not
       // cancelled.
       if (attendee === undefined) {
         throw new NotFoundError(`Attendee(ID=${attendeeId}) not found in Attendance(ID=${attendanceId})`)
       }
+
       if (attendee.reserved) {
         return
       }
 
       const pool = attendance.pools.find((pool) => pool.id === attendee.attendancePoolId)
       invariant(pool !== undefined)
+
       const adjacentAttendees = attendance.attendees.filter((a) => a.attendancePoolId === pool.id && a.reserved)
       const isPoolAtMaxCapacity = adjacentAttendees.length >= pool.capacity
-      const isPastReservationTime = isPast(attendee.earliestReservationAt)
-      if (isPoolAtMaxCapacity || isPastReservationTime) {
+      const isFutureReservationTime = isFuture(attendee.earliestReservationAt)
+
+      if (isPoolAtMaxCapacity) {
         return
+      }
+
+      if (isFutureReservationTime) {
+        logger.warn(
+          "ReserveAttendee task for Attendee(ID=%s) ran before earliestReservationAt %s.",
+          attendee.id,
+          attendee.earliestReservationAt.toUTCString()
+        )
       }
 
       const data = AttendeeWriteSchema.parse(attendee)
