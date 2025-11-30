9ecde0b4835a2803e57000c312be1d5214363ae4
brage-andreas <bragerino+github@gmail.com>
2025-11-30 14:05:58 +0100

    feat: standardize audit log service and repository layer

--- apps/rpc/src/modules/audit-log/audit-log-repository.ts
+++ apps/rpc/src/modules/audit-log/audit-log-repository.ts
@@ -1,48 +1,23 @@
 import type { DBHandle } from "@dotkomonline/db"
-import type { AuditLog, AuditLogFilterQuery } from "@dotkomonline/types"
+import type { AuditLog, AuditLogFilterQuery, AuditLogId, UserId } from "@dotkomonline/types"
 import { type Pageable, pageQuery } from "../../query"
 
 export interface AuditLogRepository {
-  getById(handle: DBHandle, id: string): Promise<AuditLog | null>
-  getAll(handle: DBHandle, page: Pageable): Promise<AuditLog[]>
+  findById(handle: DBHandle, auditLogId: AuditLogId): Promise<AuditLog | null>
   findMany(handle: DBHandle, query: AuditLogFilterQuery, page: Pageable): Promise<AuditLog[]>
-  getByUserId(handle: DBHandle, userId: string, page: Pageable): Promise<AuditLog[]>
+  findManyByUserId(handle: DBHandle, userId: UserId, page: Pageable): Promise<AuditLog[]>
 }
 
 export function getAuditLogRepository(): AuditLogRepository {
   return {
-    async getById(handle, id) {
+    async findById(handle, auditLogId) {
       const auditLog = await handle.auditLog.findUnique({
-        where: { id },
+        where: { id: auditLogId },
         include: { user: true },
       })
-      if (!auditLog) {
-        throw new Error(`AuditLog with id ${id} not found`)
-      }
       return auditLog
     },
-    async getByUserId(handle, userId, page) {
-      const auditLogs = await handle.auditLog.findMany({
-        where: { userId },
-        include: {
-          user: true,
-        },
-        ...pageQuery(page),
-      })
-      return auditLogs
-    },
-    async getAll(handle, page) {
-      const auditLogs = await handle.auditLog.findMany({
-        ...pageQuery(page),
-        include: {
-          user: true,
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-      })
-      return auditLogs
-    },
+
     async findMany(handle, query, page) {
       const auditLogs = await handle.auditLog.findMany({
         ...pageQuery(page),
@@ -97,6 +72,17 @@ export function getAuditLogRepository(): AuditLogRepository {
         },
       })
 
+      return auditLogs
+    },
+
+    async findManyByUserId(handle, userId, page) {
+      const auditLogs = await handle.auditLog.findMany({
+        where: { userId },
+        include: {
+          user: true,
+        },
+        ...pageQuery(page),
+      })
       return auditLogs
     },
   }
--- apps/rpc/src/modules/audit-log/audit-log-service.ts
+++ apps/rpc/src/modules/audit-log/audit-log-service.ts
@@ -1,36 +1,38 @@
 import type { DBHandle } from "@dotkomonline/db"
-import type { AuditLog, AuditLogFilterQuery } from "@dotkomonline/types"
+import type { AuditLog, AuditLogFilterQuery, AuditLogId, UserId } from "@dotkomonline/types"
+import { NotFoundError } from "../../error"
 import type { Pageable } from "../../query"
 import type { AuditLogRepository } from "./audit-log-repository"
 
 export interface AuditLogService {
-  getAuditLogs(handle: DBHandle, page: Pageable): Promise<AuditLog[]>
+  findById(handle: DBHandle, auditLogId: AuditLogId): Promise<AuditLog | null>
+  getById(handle: DBHandle, auditLogId: AuditLogId): Promise<AuditLog>
   findMany(handle: DBHandle, query: AuditLogFilterQuery, page: Pageable): Promise<AuditLog[]>
-  getAuditLogById(handle: DBHandle, id: string): Promise<AuditLog>
-  getAuditLogsByUserId(handle: DBHandle, userId: string, page: Pageable): Promise<AuditLog[]>
+  findManyByUserId(handle: DBHandle, userId: UserId, page: Pageable): Promise<AuditLog[]>
 }
 
 export function getAuditLogService(auditLogRepository: AuditLogRepository): AuditLogService {
   return {
-    async getAuditLogs(handle: DBHandle, page: Pageable): Promise<AuditLog[]> {
-      const auditLogs = await auditLogRepository.getAll(handle, page)
-      return auditLogs
-    },
-    async findMany(handle: DBHandle, query: AuditLogFilterQuery, page: Pageable): Promise<AuditLog[]> {
-      const auditLogs = await auditLogRepository.findMany(handle, query, page)
-      return auditLogs
+    async findById(handle, auditLogId) {
+      const auditLog = await auditLogRepository.findById(handle, auditLogId)
+      return auditLog
     },
 
-    async getAuditLogById(handle: DBHandle, id: string): Promise<AuditLog> {
-      const auditLog = await auditLogRepository.getById(handle, id)
+    async getById(handle, auditLogId) {
+      const auditLog = await this.findById(handle, auditLogId)
       if (!auditLog) {
-        throw new Error(`Audit log with ID ${id} not found`)
+        throw new NotFoundError(`AuditLog(ID=${auditLogId}) not found`)
       }
       return auditLog
     },
 
-    async getAuditLogsByUserId(handle: DBHandle, userId: string, page: Pageable): Promise<AuditLog[]> {
-      const auditLog = await auditLogRepository.getByUserId(handle, userId, page)
+    async findMany(handle, query, page) {
+      const auditLogs = await auditLogRepository.findMany(handle, query, page)
+      return auditLogs
+    },
+
+    async findManyByUserId(handle, userId, page) {
+      const auditLog = await auditLogRepository.findManyByUserId(handle, userId, page)
       return auditLog
     },
   }
