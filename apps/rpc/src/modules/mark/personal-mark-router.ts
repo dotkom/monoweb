9d542d8fbb861aa4059e8c01eb7ba5147e250837
Mats Jun Larsen <mats@jun.codes>
2025-12-02 15:50:09 +0100

    Migrate `PersonalMarkRouter` to Authorization Middlewares (#2578)

--- apps/rpc/src/modules/mark/personal-mark-router.ts
+++ apps/rpc/src/modules/mark/personal-mark-router.ts
@@ -1,58 +1,108 @@
 import { CreatePersonalMarkSchema, PersonalMarkSchema, UserSchema } from "@dotkomonline/types"
+import type { inferProcedureInput, inferProcedureOutput } from "@trpc/server"
 import { z } from "zod"
+import { isEditor } from "../../authorization"
+import { withAuditLogEntry, withAuthentication, withAuthorization, withDatabaseTransaction } from "../../middlewares"
 import { PaginateInputSchema } from "../../query"
-import { authenticatedProcedure, staffProcedure, t } from "../../trpc"
+import { authenticatedProcedure, procedure, t } from "../../trpc"
+
+export type GetPersonalMarksByUserInput = inferProcedureInput<typeof getPersonalMarksByUserProcedure>
+export type GetPersonalMarksByUserOutput = inferProcedureOutput<typeof getPersonalMarksByUserProcedure>
+const getPersonalMarksByUserProcedure = authenticatedProcedure
+  .input(z.object({ userId: UserSchema.shape.id }))
+  .use(withAuthentication())
+  .use(withDatabaseTransaction())
+  .query(async ({ input, ctx }) => {
+    ctx.authorize.requireMe(input.userId)
+    return ctx.personalMarkService.findMarksByUserId(ctx.handle, input.userId)
+  })
+
+export type GetVisibleInformationInput = inferProcedureInput<typeof getVisibleInformationProcedure>
+export type GetVisibleInformationOutput = inferProcedureOutput<typeof getVisibleInformationProcedure>
+const getVisibleInformationProcedure = authenticatedProcedure
+  .input(z.object({ userId: UserSchema.shape.id, paginate: PaginateInputSchema }))
+  .use(withAuthentication())
+  .use(withDatabaseTransaction())
+  .query(async ({ ctx, input }) => {
+    ctx.authorize.requireMe(input.userId)
+    return ctx.personalMarkService.listVisibleInformationForUser(ctx.handle, ctx.principal.subject)
+  })
+
+export type GetPersonalMarksByMarkInput = inferProcedureInput<typeof getPersonalMarksByMarkProcedure>
+export type GetPersonalMarksByMarkOutput = inferProcedureOutput<typeof getPersonalMarksByMarkProcedure>
+const getPersonalMarksByMarkProcedure = procedure
+  .input(z.object({ markId: PersonalMarkSchema.shape.markId, paginate: PaginateInputSchema }))
+  .use(withAuthentication())
+  .use(withAuthorization(isEditor()))
+  .use(withDatabaseTransaction())
+  .query(async ({ input, ctx }) => {
+    return ctx.personalMarkService.findPersonalMarksByMarkId(ctx.handle, input.markId)
+  })
+
+export type GetPersonalMarkDetailsByMarkInput = inferProcedureInput<typeof getPersonalMarkDetailsByMarkProcedure>
+export type GetPersonalMarkDetailsByMarkOutput = inferProcedureOutput<typeof getPersonalMarkDetailsByMarkProcedure>
+const getPersonalMarkDetailsByMarkProcedure = procedure
+  .input(z.object({ markId: PersonalMarkSchema.shape.markId, paginate: PaginateInputSchema }))
+  .use(withAuthentication())
+  .use(withAuthorization(isEditor()))
+  .use(withDatabaseTransaction())
+  .query(async ({ input, ctx }) => {
+    return ctx.personalMarkService.findPersonalMarkDetails(ctx.handle, input.markId)
+  })
+
+export type AddPersonalMarkToUserInput = inferProcedureInput<typeof addPersonalMarkToUserProcedure>
+export type AddPersonalMarkToUserOutput = inferProcedureOutput<typeof addPersonalMarkToUserProcedure>
+const addPersonalMarkToUserProcedure = procedure
+  .input(CreatePersonalMarkSchema)
+  .use(withAuthentication())
+  .use(withAuthorization(isEditor()))
+  .use(withDatabaseTransaction())
+  .use(withAuditLogEntry())
+  .mutation(async ({ input, ctx }) => {
+    return ctx.personalMarkService.addToUser(ctx.handle, input.userId, input.markId, ctx.principal.subject)
+  })
+
+export type CountUsersWithMarkInput = inferProcedureInput<typeof countUsersWithMarkProcedure>
+export type CountUsersWithMarkOutput = inferProcedureOutput<typeof countUsersWithMarkProcedure>
+const countUsersWithMarkProcedure = procedure
+  .input(z.object({ markId: PersonalMarkSchema.shape.markId }))
+  .use(withAuthentication())
+  .use(withAuthorization(isEditor()))
+  .use(withDatabaseTransaction())
+  .query(async ({ input, ctx }) => {
+    return ctx.personalMarkService.countUsersByMarkId(ctx.handle, input.markId)
+  })
+
+export type RemovePersonalMarkFromUserInput = inferProcedureInput<typeof removePersonalMarkFromUserProcedure>
+export type RemovePersonalMarkFromUserOutput = inferProcedureOutput<typeof removePersonalMarkFromUserProcedure>
+const removePersonalMarkFromUserProcedure = procedure
+  .input(PersonalMarkSchema.pick({ userId: true, markId: true }))
+  .use(withAuthentication())
+  .use(withAuthorization(isEditor()))
+  .use(withDatabaseTransaction())
+  .use(withAuditLogEntry())
+  .mutation(async ({ input, ctx }) => {
+    return ctx.personalMarkService.removeFromUser(ctx.handle, input.userId, input.markId)
+  })
+
+export type GetExpiryDateForUserInput = inferProcedureInput<typeof getExpiryDateForUserProcedure>
+export type GetExpiryDateForUserOutput = inferProcedureOutput<typeof getExpiryDateForUserProcedure>
+const getExpiryDateForUserProcedure = authenticatedProcedure
+  .input(z.object({ userId: UserSchema.shape.id }))
+  .use(withAuthentication())
+  .use(withDatabaseTransaction())
+  .query(async ({ input, ctx }) => {
+    ctx.authorize.requireMe(input.userId)
+    return ctx.personalMarkService.findPunishmentByUserId(ctx.handle, input.userId)
+  })
 
 export const personalMarkRouter = t.router({
-  getByUser: authenticatedProcedure.input(z.object({ userId: UserSchema.shape.id })).query(async ({ input, ctx }) => {
-    ctx.authorize.requireMeOrEditorRole(input.userId, [])
-    return ctx.executeTransaction(async (handle) => ctx.personalMarkService.findMarksByUserId(handle, input.userId))
-  }),
-  getVisibleInformation: authenticatedProcedure
-    .input(z.object({ userId: UserSchema.shape.id, paginate: PaginateInputSchema }))
-    .query(async ({ ctx, input }) => {
-      ctx.authorize.requireMeOrEditorRole(input.userId, [])
-      return ctx.executeTransaction(async (handle) => {
-        return ctx.personalMarkService.listVisibleInformationForUser(handle, ctx.principal.subject)
-      })
-    }),
-  getByMark: staffProcedure
-    .input(z.object({ markId: PersonalMarkSchema.shape.markId, paginate: PaginateInputSchema }))
-    .query(async ({ input, ctx }) => {
-      return ctx.executeTransaction(async (handle) =>
-        ctx.personalMarkService.findPersonalMarksByMarkId(handle, input.markId)
-      )
-    }),
-  getPersonalMarkDetailsByMark: staffProcedure
-    .input(z.object({ markId: PersonalMarkSchema.shape.markId, paginate: PaginateInputSchema }))
-    .query(({ input, ctx }) =>
-      ctx.executeTransaction((handle) => ctx.personalMarkService.findPersonalMarkDetails(handle, input.markId))
-    ),
-  addToUser: staffProcedure
-    .input(CreatePersonalMarkSchema)
-    .mutation(async ({ input, ctx }) =>
-      ctx.executeAuditedTransaction(async (handle) =>
-        ctx.personalMarkService.addToUser(handle, input.userId, input.markId, ctx.principal.subject)
-      )
-    ),
-  countUsersWithMark: staffProcedure
-    .input(z.object({ markId: PersonalMarkSchema.shape.markId }))
-    .query(async ({ input, ctx }) =>
-      ctx.executeTransaction(async (handle) => ctx.personalMarkService.countUsersByMarkId(handle, input.markId))
-    ),
-  removeFromUser: staffProcedure
-    .input(PersonalMarkSchema.pick({ userId: true, markId: true }))
-    .mutation(async ({ input, ctx }) =>
-      ctx.executeAuditedTransaction(async (handle) =>
-        ctx.personalMarkService.removeFromUser(handle, input.userId, input.markId)
-      )
-    ),
-  getExpiryDateForUser: authenticatedProcedure
-    .input(z.object({ userId: UserSchema.shape.id }))
-    .query(async ({ input, ctx }) => {
-      ctx.authorize.requireMeOrEditorRole(input.userId, [])
-      return ctx.executeTransaction(async (handle) =>
-        ctx.personalMarkService.findPunishmentByUserId(handle, input.userId)
-      )
-    }),
+  getByUser: getPersonalMarksByUserProcedure,
+  getVisibleInformation: getVisibleInformationProcedure,
+  getByMark: getPersonalMarksByMarkProcedure,
+  getPersonalMarkDetailsByMark: getPersonalMarkDetailsByMarkProcedure,
+  addToUser: addPersonalMarkToUserProcedure,
+  countUsersWithMark: countUsersWithMarkProcedure,
+  removeFromUser: removePersonalMarkFromUserProcedure,
+  getExpiryDateForUser: getExpiryDateForUserProcedure,
 })
