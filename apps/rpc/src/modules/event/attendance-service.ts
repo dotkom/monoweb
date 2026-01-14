af8746f8ec3026a62260c886085dd95bac7a3293
brage-andreas <bragerino+github@gmail.com>
2026-01-14 15:50:55 +0100

    Revert temporary Pubgolf payment deadline override

--- apps/rpc/src/modules/event/attendance-service.ts
+++ apps/rpc/src/modules/event/attendance-service.ts
@@ -740,20 +740,7 @@ export function getAttendanceService(
         attendee.paymentChargedAt !== null
 
       if (attendance.attendancePrice !== null && attendance.attendancePrice > 0 && !hasExistingPayment) {
-        let paymentDeadline = addHours(getCurrentUTC(), 24)
-
-        // DELETE START: Temporary code to override Pubgolf payment deadline
-        // set the let above to const after deleting
-        const PUBGOLF_ATTENDANCE_ID = "bac685fb-53f8-4d41-ac58-24a74d160524"
-        if (attendance.id === PUBGOLF_ATTENDANCE_ID) {
-          paymentDeadline = set(addWeeks(TZDate.tz("Europe/Oslo"), 2), {
-            hours: 16,
-            minutes: 0,
-            seconds: 0,
-            milliseconds: 0,
-          })
-        }
-        // DELETE END
+        const paymentDeadline = addHours(getCurrentUTC(), 24)
 
         const payment = await this.startAttendeePayment(handle, attendee.id, paymentDeadline)
 
