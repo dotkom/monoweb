977d9c2f89cf91b1d0610d00e14be4233e816ef1
Brage <37003366+brage-andreas@users.noreply.github.com>
2026-01-19 20:52:24 +0100

    Update committee applications notice (#2756)
    Makes the content variable and displays how long it's present.

--- apps/web/src/components/notices/committee-applications-notice.tsx
+++ apps/web/src/components/notices/committee-applications-notice.tsx
@@ -1,35 +1,54 @@
 import { OnlineIcon } from "@/components/atoms/OnlineIcon"
 import { Button, Text, Title } from "@dotkomonline/ui"
-import { getCurrentUTC } from "@dotkomonline/utils"
-import { IconArrowUpRight } from "@tabler/icons-react"
-import { type Interval, isWithinInterval } from "date-fns"
+import { capitalizeFirstLetter, getCurrentUTC } from "@dotkomonline/utils"
+import { IconArrowUpRight, IconClock } from "@tabler/icons-react"
+import { formatDistanceToNow, type Interval, isWithinInterval } from "date-fns"
+import type { PropsWithChildren } from "react"
+import { nb } from "date-fns/locale"
 
-export const CommitteeApplicationsNotice = (interval: Interval) => {
-  if (!isWithinInterval(getCurrentUTC(), interval)) {
+type CommitteeApplicationsNoticeProps = PropsWithChildren<
+  {
+    hideCountdown?: boolean
+  } & Interval
+>
+
+export const CommitteeApplicationsNotice = ({ children, start, end }: CommitteeApplicationsNoticeProps) => {
+  if (!isWithinInterval(getCurrentUTC(), { start, end })) {
     return null
   }
+
+  const content = children ?? (
+    <>
+      <Title className="text-lg md:text-xl font-bold">Søk komité nå!</Title>{" "}
+      <Text>
+        Komiteene har opptak, og de ser etter akkurat deg! <span className="font-semibold">Trykk her</span> for å gå til
+        opptakssiden (opptak.online.ntnu.no).
+      </Text>
+    </>
+  )
 
   return (
     <Button
       element="a"
       href="https://opptak.online.ntnu.no"
-      className="group flex flex-row justify-between w-full items-center p-5 min-h-24 bg-indigo-200 hover:bg-indigo-100 dark:bg-indigo-500/50 dark:hover:bg-indigo-400/50 rounded-2xl"
+      className="relative group flex flex-row w-full justify-between p-5 min-h-24 bg-indigo-200 hover:bg-indigo-100 dark:bg-indigo-500/50 dark:hover:bg-indigo-400/50 rounded-2xl"
     >
-      <div className="flex flex-row gap-5 items-center">
-        <div className="transform translate-y-[12.5%]">
-          <OnlineIcon size={32} className="animate-bounce" />
-        </div>
-
-        <div className="flex flex-col gap-0.5">
-          <Title className="text-lg md:text-xl font-bold">Søk komité nå!</Title>
-          <Text>
-            Komiteene har opptak, og de ser etter akkurat deg! <span className="font-semibold">Trykk her</span> for å gå
-            til opptakssiden (opptak.online.ntnu.no).
-          </Text>
-        </div>
+      <div className="absolute top-3 right-6 flex flex-row items-center gap-1.5 text-indigo-800 dark:text-indigo-300">
+        <IconClock size="0.8rem" />
+        <Text className="text-xs">{capitalizeFirstLetter(formatDistanceToNow(end, { locale: nb }))}</Text>
       </div>
 
-      <IconArrowUpRight className="size-6" />
+      <div className="flex flex-row w-full gap-5 items-center">
+        <div className="shrink-0 transform translate-y-[12.5%] max-sm:hidden">
+          <div className="p-1 not-dark:bg-white rounded-full animate-bounce">
+            <OnlineIcon size={32} />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">{content}</div>
+      </div>
+
+      <IconArrowUpRight className="size-6 shrink-0" />
     </Button>
   )
 }
