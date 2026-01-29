8ae3eb77abaa25b37c9cbd9b0c04589b0caeef6e
Mats Jun Larsen <mats@jun.codes>
2026-01-29 22:23:01 +0100

    Fix broken configuration in grades

--- apps/grades-backend/src/configuration.ts
+++ apps/grades-backend/src/configuration.ts
@@ -5,7 +5,6 @@ export const createConfiguration = () =>
   defineConfiguration({
     ALLOWED_ORIGINS: config(process.env.ALLOWED_ORIGINS, {
       prd: "https://grades.no",
-      stg: "https://staging.grades.no",
       dev: "http://localhost:5001",
     }),
     DATABASE_URL: config(process.env.DATABASE_URL),
