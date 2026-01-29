ac26c585db926163ad136ce659c7057782c3c4fb
Mats Jun Larsen <mats@jun.codes>
2026-01-29 21:30:49 +0000

    Remove standalone build from rif and invoicification (#2815)

--- apps/grades-frontend/src/env.ts
+++ apps/grades-frontend/src/env.ts
@@ -3,17 +3,14 @@ import { config, defineConfiguration } from "@dotkomonline/environment"
 export const env = defineConfiguration({
   BACKEND_HOST: config(process.env.BACKEND_HOST, {
     prd: "https://api.grades.no",
-    stg: "https://staging.api.grades.no",
     dev: "http://localhost:5555",
   }),
   NEXT_PUBLIC_ORIGIN: config(process.env.NEXT_PUBLIC_ORIGIN, {
     prd: "https://grades.no",
-    stg: "https://staging.grades.no",
     dev: "http://localhost:5001",
   }),
   NEXT_PUBLIC_BACKEND_HOST: config(process.env.NEXT_PUBLIC_BACKEND_HOST, {
     prd: "https://api.grades.no",
-    stg: "https://staging.api.grades.no",
     dev: "http://localhost:5555",
   }),
   NEXT_PUBLIC_HOME_URL: config(process.env.NEXT_PUBLIC_HOME_URL, "/"),
