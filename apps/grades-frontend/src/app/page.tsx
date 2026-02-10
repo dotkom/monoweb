53f1785f6bfd048d46646a460a9e64cee64683a6
Andrefkl <andre.klarpaas@gmail.com>
2026-02-10 22:56:37 +0100

    feat: add course to grades schema

--- apps/grades-frontend/src/app/page.tsx
+++ apps/grades-frontend/src/app/page.tsx
@@ -3,5 +3,5 @@ import { server } from "@/utils/trpc/server"
 export default async function App() {
   const courses = await server.course.findCourses.query()
 
-  return <section className="flex flex-col gap-16 w-full">{courses.map((course) => course.name)}</section>
+  return <section className="flex flex-col gap-16 w-full">{courses.map((course) => course.norwegianName)}</section>
 }
