3c4f2dfd77f7d56469e01765b7712fcacb02521d
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2026-02-11 20:20:39 +0100

    lint

--- apps/grades-frontend/src/app/page.tsx
+++ apps/grades-frontend/src/app/page.tsx
@@ -2,12 +2,16 @@ import { server } from "@/utils/trpc/server"
 
 export default async function App() {
   const courses = await server.course.findCourses.query()
-  const grades = courses.length > 0 ?  await server.grade.findGrades.query(courses[0].code) : []
+  const grades = courses.length > 0 ? await server.grade.findGrades.query(courses[0].code) : []
 
   return (
     <div>
       <section className="flex flex-col gap-16 w-full">{courses.map((course) => course.norwegianName)}</section>
-      <section className="flex flex-col gap-2 w-full">{grades.map((grade) => (<span key={grade.id}>{grade.averageGrade}</span>))}</section>
+      <section className="flex flex-col gap-2 w-full">
+        {grades.map((grade) => (
+          <span key={grade.id}>{grade.averageGrade}</span>
+        ))}
+      </section>
     </div>
   )
 }
