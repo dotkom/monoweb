5847be3d3e2c5728274289bc319a73b837aea7b4
brage-andreas <bragerino+github@gmail.com>
2026-02-16 15:08:32 +0100

    Revert event header aspect ratio
    Prokom has already made new event banners for 24/9.

--- apps/web/src/app/arrangementer/components/EventHeader.tsx
+++ apps/web/src/app/arrangementer/components/EventHeader.tsx
@@ -29,7 +29,7 @@ export const EventHeader: FC<Props> = ({ event, showDashboardLink }) => {
         glareMaxOpacity={0.1}
         className="rounded-xl bg-gray-100 dark:bg-stone-800"
       >
-        <div className="group relative w-full aspect-video md:aspect-5/2 overflow-hidden rounded-xl">
+        <div className="group relative w-full aspect-video md:aspect-24/9 overflow-hidden rounded-xl">
           {event.imageUrl ? (
             <Image
               src={event.imageUrl}
