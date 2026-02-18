c280eabad0f4c5585021afc454160a0bc53ef96f
Mats Jun Larsen <mats@jun.codes>
2026-02-18 17:33:37 +0000

    Remove kvittering applications (#2915)

--- apps/kvittering-frontend/src/lib/alert.ts
+++
@@ -1,6 +0,0 @@
-export function alertFormSubmission(message: string) {
-  fetch(import.meta.env.VITE_SLACK_ALERT_WEBHOOK_URL, {
-    method: "POST",
-    body: JSON.stringify({ text: message }),
-  })
-}
