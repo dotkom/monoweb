2e7d0b0843319f5df20290ac87f883310e57d9ef
sofie <sofiereginekjolaas@gmail.com>
2026-02-25 20:14:33 +0100

    no more kuse

--- apps/grades-frontend/src/app/Navbar.tsx
+++ apps/grades-frontend/src/app/Navbar.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import Link from "next/link";
+import Link from "next/link"
 
 export const Navbar = () => {
   return (
@@ -22,5 +22,5 @@ export const Navbar = () => {
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
--- apps/grades-frontend/src/app/layout.tsx
+++ apps/grades-frontend/src/app/layout.tsx
@@ -5,28 +5,23 @@ import "../globals.css"
 import { setDefaultOptions as setDateFnsDefaultOptions } from "date-fns"
 import { nb } from "date-fns/locale"
 import PlausibleProvider from "next-plausible"
-import {Navbar} from "./Navbar"
-import {Footer} from "./Footer"
+import { Navbar } from "./Navbar"
+import { Footer } from "./Footer"
 
 setDateFnsDefaultOptions({ locale: nb })
-
-
-
-
-
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     // suppressHydrationWarning is needed for next-themes, see https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
     <html lang="no" suppressHydrationWarning>
-      <body className = "dark:bg-[#141821] bg-pink-400">
+      <body className="dark:bg-[#141821] bg-pink-400">
         <PlausibleProvider domain="grades.no">
           <QueryProvider>
             <ThemeProvider defaultTheme="system" enableSystem attribute="data-theme">
               <div className="min-h-screen flex flex-col gap-8 w-full max-w-screen-xl mx-auto px-4 lg:px-12">
-                 <Navbar />
+                <Navbar />
                 <main className="grow">{children}</main>
-                <Footer/>
+                <Footer />
               </div>
             </ThemeProvider>
           </QueryProvider>
