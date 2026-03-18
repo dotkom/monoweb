6e5293b13efe6416ae826ad18f9ac493bfb821d0
Nicolay B. Rennemo <rennemo@proton.me>
2026-03-18 19:26:41 +0100

    Added fonts from OW and made bg white

--- apps/grades-frontend/src/app/layout.tsx
+++ apps/grades-frontend/src/app/layout.tsx
@@ -5,16 +5,22 @@ import "../globals.css"
 import { setDefaultOptions as setDateFnsDefaultOptions } from "date-fns"
 import { nb } from "date-fns/locale"
 import PlausibleProvider from "next-plausible"
+import { Figtree, Inter } from "next/font/google"
+import { cn } from "@dotkomonline/ui"
 import { Navbar } from "./Navbar"
 import { Footer } from "./Footer"
 
 setDateFnsDefaultOptions({ locale: nb })
 
+const fontBody = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-body" })
+const fontTitle = Figtree({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-title" })
+
+
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     // suppressHydrationWarning is needed for next-themes, see https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
     <html lang="no" suppressHydrationWarning>
-      <body className="dark:bg-[#141821] bg-red-100">
+      <body className={cn(fontTitle.variable, fontBody.variable, "dark:bg-[#141821] bg-white")}>
         <PlausibleProvider domain="grades.no">
           <QueryProvider>
             <ThemeProvider defaultTheme="system" enableSystem attribute="data-theme">
