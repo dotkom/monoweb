b2b3d2d2c570c4dcf06266416e1ab9e9bb0ff5f3
ninaflaaten <nifl1349@gmail.com>
2026-03-18 19:20:58 +0100

    Add join button in interest group page (#2925)

--- packages/types/src/group.ts
+++ packages/types/src/group.ts
@@ -42,6 +42,7 @@ export const GroupWriteSchema = GroupSchema.pick({
   imageUrl: true,
   email: true,
   contactUrl: true,
+  slackUrl: true,
   showLeaderAsContact: true,
   memberVisibility: true,
   deactivatedAt: true,
