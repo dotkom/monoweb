aa665687801d7efa62cf1497fa4a07795417d166
Sofie <sofiereginekjolaas@gmail.com>
2026-03-25 20:28:05 +0100

    add createMany method

--- apps/grades-backend/src/modules/grade/grade-service.ts
+++ apps/grades-backend/src/modules/grade/grade-service.ts
@@ -1,16 +1,20 @@
 import type { DBHandle } from "@dotkomonline/grades-db"
 import type { GradeRepository } from "./grade-repository"
-import type { Grade } from "./grade-types"
+import type { Grade, GradeWrite } from "./grade-types"
 import type { CourseCode } from "../course/course-types"
 
 export interface GradeService {
   findMany(handle: DBHandle, courseCode: CourseCode): Promise<Grade[]>
+  createMany(handle: DBHandle, data: GradeWrite[]): Promise<Grade[]>
 }
 
 export function getGradeService(gradeRepository: GradeRepository): GradeService {
   return {
     async findMany(handle, courseCode) {
       return gradeRepository.findMany(handle, courseCode)
+    },
+    async createMany(handle, data) {
+      return gradeRepository.createMany(handle, data)
     },
   }
 }
