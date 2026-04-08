8c4c52dbf6354810c6c03c1aa53fca756cb08792
brage-andreas <bragerino+github@gmail.com>
2026-04-08 21:04:50 +0200

    chore: add contest data model

---
+++ packages/db/prisma/migrations/20260415180215_add_contest_data_model/migration.sql
@@ -0,0 +1,74 @@
+-- CreateEnum
+CREATE TYPE "contest_result_type" AS ENUM ('SCORE', 'DURATION', 'WINNER');
+
+-- CreateEnum
+CREATE TYPE "contest_result_order" AS ENUM ('asc', 'desc');
+
+-- CreateTable
+CREATE TABLE "contest" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "result_type" "contest_result_type" NOT NULL,
+    "result_order" "contest_result_order" NOT NULL,
+    "winner_contestant" TEXT,
+
+    CONSTRAINT "contest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contestant" (
+    "id" TEXT NOT NULL,
+    "contest_id" TEXT NOT NULL,
+    "user_id" TEXT,
+    "result_value" INTEGER,
+
+    CONSTRAINT "contestant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contest_team" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "contestant_id" TEXT NOT NULL,
+
+    CONSTRAINT "contest_team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ContestTeamMember" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ContestTeamMember_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contest_winner_contestant_key" ON "contest"("winner_contestant");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contestant_contest_id_user_id_key" ON "contestant"("contest_id", "user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contest_team_contestant_id_key" ON "contest_team"("contestant_id");
+
+-- CreateIndex
+CREATE INDEX "_ContestTeamMember_B_index" ON "_ContestTeamMember"("B");
+
+-- AddForeignKey
+ALTER TABLE "contest" ADD CONSTRAINT "contest_winner_contestant_fkey" FOREIGN KEY ("winner_contestant") REFERENCES "contestant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contestant" ADD CONSTRAINT "contestant_contest_id_fkey" FOREIGN KEY ("contest_id") REFERENCES "contest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contestant" ADD CONSTRAINT "contestant_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "ow_user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contest_team" ADD CONSTRAINT "contest_team_contestant_id_fkey" FOREIGN KEY ("contestant_id") REFERENCES "contestant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ContestTeamMember" ADD CONSTRAINT "_ContestTeamMember_A_fkey" FOREIGN KEY ("A") REFERENCES "contest_team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ContestTeamMember" ADD CONSTRAINT "_ContestTeamMember_B_fkey" FOREIGN KEY ("B") REFERENCES "ow_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
