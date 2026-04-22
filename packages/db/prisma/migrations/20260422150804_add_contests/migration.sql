5ae68d3d5f3a9969f67326f40a240cb9e0fcb090
Sondre Alfnes <sondre.alfnes@gmail.com>
2026-04-22 17:08:29 +0200

    Update migration script

--- packages/db/prisma/migrations/20260422150804_add_contests/migration.sql
+++ packages/db/prisma/migrations/20260422150804_add_contests/migration.sql
@@ -11,6 +11,7 @@ CREATE TABLE "contest" (
     "description" TEXT,
     "result_type" "contest_result_type" NOT NULL,
     "result_order" "contest_result_order" NOT NULL,
+    "group_id" TEXT NOT NULL,
     "winner_contestant_id" TEXT,
 
     CONSTRAINT "contest_pkey" PRIMARY KEY ("id")
@@ -56,16 +57,19 @@ CREATE UNIQUE INDEX "contest_team_contestant_id_key" ON "contest_team"("contesta
 CREATE INDEX "_ContestTeamMember_B_index" ON "_ContestTeamMember"("B");
 
 -- AddForeignKey
+ALTER TABLE "contest" ADD CONSTRAINT "contest_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "group"("slug") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "contest" ADD CONSTRAINT "contest_winner_contestant_id_fkey" FOREIGN KEY ("winner_contestant_id") REFERENCES "contestant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "contestant" ADD CONSTRAINT "contestant_contest_id_fkey" FOREIGN KEY ("contest_id") REFERENCES "contest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "contestant" ADD CONSTRAINT "contestant_contest_id_fkey" FOREIGN KEY ("contest_id") REFERENCES "contest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "contestant" ADD CONSTRAINT "contestant_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "ow_user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "contest_team" ADD CONSTRAINT "contest_team_contestant_id_fkey" FOREIGN KEY ("contestant_id") REFERENCES "contestant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "contest_team" ADD CONSTRAINT "contest_team_contestant_id_fkey" FOREIGN KEY ("contestant_id") REFERENCES "contestant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ContestTeamMember" ADD CONSTRAINT "_ContestTeamMember_A_fkey" FOREIGN KEY ("A") REFERENCES "contest_team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
