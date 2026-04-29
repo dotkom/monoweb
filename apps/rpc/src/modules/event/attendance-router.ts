c3e0c5e5619987776c565c1777cf793b4e6320ae
Christoffer Nguyen <christofferhoang@gmail.com>
2026-04-29 13:33:11 +0200

    fix idor

--- apps/rpc/src/modules/event/attendance-router.ts
+++ apps/rpc/src/modules/event/attendance-router.ts
@@ -355,6 +355,8 @@ const updateSelectionResponsesProcedure = procedure
   .use(withDatabaseTransaction())
   .use(withAuditLogEntry())
   .mutation(async ({ input, ctx }) => {
+    const attendee = await ctx.attendanceService.getAttendeeById(ctx.handle, input.attendeeId)
+    await ctx.addAuthorizationGuard(or(isCommitteeMember(), isSameSubject(() => attendee.userId)), input)
     await ctx.attendanceService.updateAttendeeById(ctx.handle, input.attendeeId, { selections: input.options })
   })
 
