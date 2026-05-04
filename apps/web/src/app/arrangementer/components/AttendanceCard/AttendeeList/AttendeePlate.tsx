df2229d5295d3ce2cae02f9136bc4ff8476b7f5e
brage-andreas <bragerino+github@gmail.com>
2026-05-04 20:37:28 +0200

    kkowds

--- apps/web/src/app/arrangementer/components/AttendanceCard/AttendeeList/AttendeePlate.tsx
+++ apps/web/src/app/arrangementer/components/AttendanceCard/AttendeeList/AttendeePlate.tsx
@@ -1,4 +1,11 @@
-import { type Attendee, isVanityVerified, type User, isExceptionallyDistinguished, FlagName } from "@dotkomonline/types"
+import {
+  type Attendee,
+  isVanityVerified,
+  type User,
+  isExceptionallyDistinguished,
+  FlagName,
+  isKnight,
+} from "@dotkomonline/types"
 import type { FC, JSX, ReactNode } from "react"
 import { GenericPlate } from "./GenericPlate"
 import { getVanityVerifiedSmallIcon, VanityVerifiedPlate } from "./VanityVerifiedPlate"
@@ -7,6 +14,7 @@ import {
   getExceptionallyDistinguishedLargeIcon,
   getExceptionallyDistinguishedSmallIcon,
 } from "./ExceptionallyDistinguished"
+import { getKnightLargeIcon, getKnightSmallIcon, KnightPlate } from "./KnightPlate"
 
 export interface PlateProps {
   attendee: Attendee
@@ -18,6 +26,10 @@ export interface PlateProps {
 }
 
 export function getAttendeePlate(attendee: Attendee): FC<PlateProps> {
+  if (isKnight(attendee.user)) {
+    return KnightPlate
+  }
+
   if (isExceptionallyDistinguished(attendee.user)) {
     return ExceptionallyDistinguishedPlate
   }
@@ -32,6 +44,14 @@ export function getAttendeePlate(attendee: Attendee): FC<PlateProps> {
 export function getAttendeeIcons(attendee: Attendee) {
   const smallIcons: JSX.Element[] = []
   let largeIcon: JSX.Element | null = null
+
+  if (isKnight(attendee.user)) {
+    if (largeIcon === null) {
+      largeIcon = getKnightLargeIcon()
+    } else {
+      smallIcons.push(getKnightSmallIcon())
+    }
+  }
 
   if (isExceptionallyDistinguished(attendee.user)) {
     const flags = attendee.user.flags.filter(({ name }) => name === FlagName.EXCEPTIONALLY_DISTINGUISHED)
