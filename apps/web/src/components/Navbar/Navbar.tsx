fa38c99e5dab1085c4c9deed9095ecbebb3b22b1
Magnus Strømseng <magnus.stromseng@gmail.com>
2026-05-21 15:27:48 +0200

    Fix navbar login button height

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -181,7 +181,7 @@ export const Navbar: FC = () => {
       </div>
 
       {showLoginButton && (
-        <div className="h-full shrink-0 rounded-l-lg rounded-r-4xl bg-blue-100/80 dark:bg-stone-800/90 backdrop-blur-xl shadow-sm">
+        <div className="self-stretch shrink-0 rounded-l-lg rounded-r-4xl bg-blue-100/80 dark:bg-stone-800/90 backdrop-blur-xl shadow-sm">
           <Button
             element="a"
             variant="solid"
