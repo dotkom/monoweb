ba01f439fa0cc6a9897af63ca971ea8b4cb1d9d0
brage-andreas <bragerino+github@gmail.com>
2026-08-06 11:09:24 +0200

    Add email notification for  committee email creation

--- packages/utils/src/slugify.ts
+++ packages/utils/src/slugify.ts
@@ -1,6 +1,6 @@
 import createSlug from "slugify"
 
-type SlugifyOptions = Exclude<Parameters<typeof createSlug>[1], string | undefined>
+export type SlugifyOptions = Exclude<Parameters<typeof createSlug>[1], string | undefined>
 
 export function slugify(text: string, options: SlugifyOptions = {}): string {
   // https://www.npmjs.com/package/slugify
