49f64c640d0d1eda7a87fdedba1974fbe7eee9ec
Johannes Aas <johannes.hansen.aas@gmail.com>
2026-08-06 14:09:51 +0200

    cn

--- apps/web/src/components/Navbar/Hamburger.tsx
+++ apps/web/src/components/Navbar/Hamburger.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@dotkomonline/ui"
+
 type HamburgerProps = {
   open: boolean
   size?: number
@@ -8,17 +10,19 @@ export function Hamburger({ open, className = "" }: HamburgerProps) {
   return (
     <span
       aria-hidden
-      className={`w-10 h-10 relative inline-flex items-center justify-center rounded-full ${className}`}
+      className={cn("w-10 h-10 relative inline-flex items-center justify-center rounded-full", className)}
     >
       <span
-        className={`absolute left-1/2 -translate-x-1/2 block h-[0.15rem] w-5 bg-black dark:bg-white rounded-full transition-all duration-300 ease-out ${
+        className={cn(
+          "absolute left-1/2 -translate-x-1/2 block h-[0.15rem] w-5 bg-black dark:bg-white rounded-full transition-all duration-300 ease-out",
           open ? "translate-y-0 rotate-45" : "translate-y-[5px] rotate-0"
-        }`}
+        )}
       />
       <span
-        className={`absolute left-1/2 -translate-x-1/2 block h-[0.15rem] w-5 bg-black dark:bg-white rounded-full transition-all duration-300 ease-out ${
+        className={cn(
+          "absolute left-1/2 -translate-x-1/2 block h-[0.15rem] w-5 bg-black dark:bg-white rounded-full transition-all duration-300 ease-out",
           open ? "translate-y-0 -rotate-45" : "-translate-y-[5px] rotate-0"
-        }`}
+        )}
       />
     </span>
   )
