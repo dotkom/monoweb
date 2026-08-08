eb7aaae30b2f4e9523468a002c1fc0295568cec1
Johannes Aas <johannes.hansen.aas@gmail.com>
2026-08-08 11:09:45 +0200

    svg fix

--- apps/web/src/components/icons/GridIcon.tsx
+++ apps/web/src/components/icons/GridIcon.tsx
@@ -8,9 +8,9 @@ export const GridIcon = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <title>Grid</title>
