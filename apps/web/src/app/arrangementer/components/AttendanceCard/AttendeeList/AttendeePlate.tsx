90ac51fac33f6d6275346cb4de4e1d5ccb075aa9
Brage <37003366+brage-andreas@users.noreply.github.com>
2026-08-27 22:16:30 +0200

    Add Knight attendee plate (#3192)

--- apps/web/src/app/arrangementer/components/AttendanceCard/AttendeeList/AttendeePlate.tsx
+++ apps/web/src/app/arrangementer/components/AttendanceCard/AttendeeList/AttendeePlate.tsx
@@ -2,17 +2,22 @@ import type { FC, JSX } from "react"
 import { GenericPlate } from "./GenericPlate"
 import { getVanityVerifiedSmallIcon, VanityVerifiedPlate } from "./VanityVerifiedPlate"
 import type { Attendee } from "@dotkomonline/rpc/attendance"
-import { FlagNameSchema, isVanityVerified, isExceptionallyDistinguished } from "@dotkomonline/rpc/user"
+import { FlagNameSchema, isVanityVerified, isExceptionallyDistinguished, isKnight } from "@dotkomonline/rpc/user"
 import type { PlateProps } from "./Plate"
 import {
   ExceptionallyDistinguishedPlate,
   getExceptionallyDistinguishedLargeIcon,
   getExceptionallyDistinguishedSmallIcon,
 } from "./ExceptionallyDistinguishedPlate"
+import { getKnightLargeIcon, getKnightSmallIcon, KnightPlate } from "./KnightPlate"
 
 export type { PlateProps }
 
 export function getAttendeePlate(attendee: Attendee): FC<PlateProps> {
+  if (isKnight(attendee.user)) {
+    return KnightPlate
+  }
+
   if (isExceptionallyDistinguished(attendee.user)) {
     return ExceptionallyDistinguishedPlate
   }
@@ -27,6 +32,14 @@ export function getAttendeePlate(attendee: Attendee): FC<PlateProps> {
 export function getAttendeeIcons(attendee: Attendee) {
   const smallIcons: JSX.Element[] = []
   let largeIcon: JSX.Element | null = null
+
+  if (isKnight(attendee.user)) {
+    if (largeIcon === null) {
+      largeIcon = getKnightLargeIcon()
+    } else {
+      smallIcons.push(getKnightSmallIcon())
+    }
+  }
 
   const exceptionallyDistinguishedFlags = attendee.user.flags.filter(
     ({ name }) => name === FlagNameSchema.enum.EXCEPTIONALLY_DISTINGUISHED
@@ -43,7 +56,7 @@ export function getAttendeeIcons(attendee: Attendee) {
   const vanityVerifiedFlag = attendee.user.flags.find(({ name }) => name === FlagNameSchema.enum.VANITY_VERIFIED)
 
   if (vanityVerifiedFlag !== undefined) {
-    const withWhiteBackground = isExceptionallyDistinguished(attendee.user)
+    const withWhiteBackground = isKnight(attendee.user) || isExceptionallyDistinguished(attendee.user)
 
     smallIcons.push(
       getVanityVerifiedSmallIcon({
