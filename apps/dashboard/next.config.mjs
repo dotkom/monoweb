5c3a0640ed68fe24c1f0910e7fcefbfb9afda030
Andrefkl <andre.klarpaas@gmail.com>
2026-09-03 20:01:35 +0200

    Upgrade dashboard to Next 16

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -30,20 +30,6 @@ const config = {
   ],
   // Explicitly ensure the transpiled packages are not treated as external
   serverExternalPackages: [],
-
-  // @auth0/nextjs-auth0 v4 intentionally constructs the "crypto" import path at runtime ("cry" + "pto") so bundlers
-  // don't bundle Node's crypto. webpack still flags the dynamic import() expression as a "Critical dependency" warning,
-  // which is safe to ignore here.
-  webpack: (webpackConfig) => {
-    webpackConfig.ignoreWarnings = [
-      ...(webpackConfig.ignoreWarnings ?? []),
-      {
-        module: /@auth0[\\/]nextjs-auth0[\\/]dist[\\/]utils[\\/]dpopUtils\.js/,
-        message: /Critical dependency: the request of a dependency is an expression/,
-      },
-    ]
-    return webpackConfig
-  },
 }
 
 export default withSentryConfig(config, {
@@ -54,13 +40,8 @@ export default withSentryConfig(config, {
   authToken: process.env.SENTRY_AUTH_TOKEN,
   widenClientFileUpload: true,
   hideSourceMaps: true,
-  webpack: {
-    reactComponentAnnotation: true,
-    treeshake: {
-      removeDebugLogging: true,
-    },
-    unstable_sentryWebpackPluginOptions: {
-      applicationKey: "monoweb-dashboard",
-    },
+  _experimental: {
+    turbopackReactComponentAnnotation: { enabled: true },
+    turbopackApplicationKey: "monoweb-dashboard",
   },
 })
