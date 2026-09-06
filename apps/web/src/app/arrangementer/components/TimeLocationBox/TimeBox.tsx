c1764f6d1d01e9d9f131f1b2437bac540866ce04
brage-andreas <bragerino+github@gmail.com>
2026-09-06 17:35:45 +0200

    Replace `TimeBox` link with calendar subscription dialog

--- apps/web/src/app/arrangementer/components/TimeLocationBox/TimeBox.tsx
+++ apps/web/src/app/arrangementer/components/TimeLocationBox/TimeBox.tsx
@@ -1,30 +1,18 @@
-"use client"
-
 import type { Event } from "@dotkomonline/rpc/event"
-import { cn, Text } from "@dotkomonline/ui"
-import { IconArrowRight, IconArrowUpRight } from "@tabler/icons-react"
+import { Text } from "@dotkomonline/ui"
+import { IconArrowRight } from "@tabler/icons-react"
 import { format as formatDate, isSameDay, isSameYear, isThisYear } from "date-fns"
 import { nb } from "date-fns/locale"
 import type { FC } from "react"
-import { createGoogleCalendarLink } from "./utils"
 import { CalendarBox } from "@/components/atoms/CalendarBox"
 import { capitalizeFirstLetter } from "@dotkomonline/utils"
-import Link from "next/link"
 
 interface TimeBoxProps {
   event: Event
 }
 
 export const TimeBox: FC<TimeBoxProps> = ({ event }) => {
-  const { start, end, locationAddress, description, title: eventSummary } = event
-
-  const gcalLink = createGoogleCalendarLink({
-    title: eventSummary,
-    location: locationAddress ?? "",
-    description: description ?? "",
-    start,
-    end,
-  })
+  const { start, end } = event
 
   const sameDay = isSameDay(start, end)
   const showYear = !isSameYear(start, end) || !isThisYear(start) || !isThisYear(end)
@@ -34,21 +22,12 @@ export const TimeBox: FC<TimeBoxProps> = ({ event }) => {
     capitalizeFirstLetter(formatDate(date, showYear ? "dd.MM.yyyy" : "EEEE dd. MMMM", { locale: nb }))
 
   return (
-    <Link
-      className={cn(
-        "group/time-box flex w-full min-w-0 flex-row items-center gap-3 p-2 -mx-2 rounded-xl sm:gap-4",
-        "transition-colors hover:bg-gray-100 dark:hover:bg-stone-800"
-      )}
-      href={gcalLink}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <section className="flex w-full min-w-0 flex-row items-center gap-3 p-2 -mx-2 sm:gap-4">
       <CalendarBox
         start={start}
         end={end}
         className="shrink-0 bg-background dark:border-stone-700"
         containerClassName="shrink-0"
-        titleClassName="transition-colors"
       />
 
       {sameDay ? (
@@ -75,11 +54,6 @@ export const TimeBox: FC<TimeBoxProps> = ({ event }) => {
           </div>
         </div>
       )}
-
-      <IconArrowUpRight
-        aria-hidden
-        className="size-5 transition-[transform,color] group-hover/time-box:scale-120 shrink-0 text-muted-foreground group-hover/time-box:text-foreground"
-      />
-    </Link>
+    </section>
   )
 }
