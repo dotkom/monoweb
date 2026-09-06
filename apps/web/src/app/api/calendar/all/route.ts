d32974fc362819ab84898519b7b5ffcbe5940eec
Brage <37003366+brage-andreas@users.noreply.github.com>
2026-09-07 00:26:15 +0200

    Filter all events calendar by date with lookback limit (#3604)

--- apps/web/src/app/api/calendar/all/route.ts
+++ apps/web/src/app/api/calendar/all/route.ts
@@ -1,14 +1,30 @@
 import { createCalendar, createCalendarEvent, createCalendarFeedResponse } from "@/app/api/calendar/ical"
 import { server } from "@/utils/trpc/server"
+import { getCurrentUTC } from "@dotkomonline/utils"
+import { subMonths } from "date-fns"
 import type { NextRequest, NextResponse } from "next/server"
 
+const ALL_EVENTS_CALENDAR_LIMIT = 500
+
 export async function GET(req: NextRequest): Promise<NextResponse> {
-  // TODO: Support paginating through the results
   void req
-  const events = await server.event.all.query()
+
+  const { items: events } = await server.event.all.query({
+    take: ALL_EVENTS_CALENDAR_LIMIT,
+    filter: {
+      orderBy: "asc",
+      byEndDate: {
+        min: subMonths(getCurrentUTC(), 1),
+        max: null,
+      },
+    },
+  })
+
   const calendar = createCalendar("Linjeforeningen Onlines arrangementer")
-  for (const { event } of events.items) {
+
+  for (const { event } of events) {
     calendar.createEvent(createCalendarEvent(event))
   }
-  return createCalendarFeedResponse(calendar.toString())
+
+  return createCalendarFeedResponse(calendar.toString(), "public")
 }
