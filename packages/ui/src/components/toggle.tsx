ab709d7757596d5724549178a6267cf2f5512fad
Brage <37003366+brage-andreas@users.noreply.github.com>
2026-09-06 19:13:27 +0200

    Add color option to toggle and toggle group (#3588)

--- packages/ui/src/components/toggle.tsx
+++ packages/ui/src/components/toggle.tsx
@@ -18,10 +18,18 @@ const toggleVariants = cva(
         sm: "h-7 min-w-7 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
         lg: "h-9 min-w-9 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
       },
+      color: {
+        default: "",
+        blue: [
+          "hover:bg-blue-100 aria-pressed:bg-blue-100 data-[state=on]:bg-blue-100",
+          "dark:hover:bg-sky-900/50 dark:aria-pressed:bg-sky-900/50 dark:data-[state=on]:bg-sky-900/50",
+        ],
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      color: "default",
     },
   }
 )
@@ -30,9 +38,16 @@ function Toggle({
   className,
   variant = "default",
   size = "default",
+  color = "default",
   ...props
 }: TogglePrimitive.Props & VariantProps<typeof toggleVariants>) {
-  return <TogglePrimitive data-slot="toggle" className={cn(toggleVariants({ variant, size }), className)} {...props} />
+  return (
+    <TogglePrimitive
+      data-slot="toggle"
+      className={cn(toggleVariants({ variant, size, color }), className)}
+      {...props}
+    />
+  )
 }
 
 export { Toggle, toggleVariants }
