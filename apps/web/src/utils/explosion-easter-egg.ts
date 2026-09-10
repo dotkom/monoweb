6a68d1fe331340b8e4403dd7a88bb067faed98b8
Johan Rong <johanhrong@icloud.com>
2026-09-10 11:23:47 +0200

    Remove debug console.logs

--- apps/web/src/utils/explosion-easter-egg.ts
+++ apps/web/src/utils/explosion-easter-egg.ts
@@ -4,7 +4,6 @@ const themeChangeCountTracker = {
 
 export function incrementThemeChangeCounter() {
   themeChangeCountTracker.count += 1;
-  console.log(`Function ran! Current count: ${themeChangeCountTracker.count}`);
 }
 
 export function hasPassedThemeChangeThreshold(): boolean {
@@ -12,7 +11,5 @@ export function hasPassedThemeChangeThreshold(): boolean {
 }
 
 export function getRandomExplosionSize(): number {
-  const num = (Math.random() + Math.random() + 1) * (100 * Math.random() + 100)
-  console.log(num)
-  return num
+  return (Math.random() + Math.random() + 1) * (100 * Math.random() + 100)
 } 
