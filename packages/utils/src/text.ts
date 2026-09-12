a277fda67a34e2e00478c2fee0529b608758f78b
André Klarpås <78028923+andrefkl@users.noreply.github.com>
2026-09-12 16:20:29 +0200

    Fix ogJoin (#3645)

--- packages/utils/src/text.ts
+++ packages/utils/src/text.ts
@@ -1,10 +1,14 @@
 // Bare for OGs
 export const ogJoin = (names: string[]) => {
-  if (names.length < 2) {
-    return names ? names[0] : null
+  if (names.length === 0) {
+    return null
   }
 
-  return `${names.splice(0, -1).join(", ")} og ${names.splice(-1)[0]}`
+  if (names.length === 1) {
+    return names[0]
+  }
+
+  return `${names.slice(0, -1).join(", ")} og ${names.at(-1)}`
 }
 
 export const capitalizeFirstLetter = (string: string) => `${string.charAt(0).toUpperCase()}${string.slice(1)}`
