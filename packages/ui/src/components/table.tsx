9ce3d5a7698ed1fef67f3bfc48978f9c29cfa705
Andrefkl <andre.klarpaas@gmail.com>
2026-09-15 18:38:02 +0200

    Create DataTable component

--- packages/ui/src/components/table.tsx
+++ packages/ui/src/components/table.tsx
@@ -4,9 +4,13 @@ import type * as React from "react"
 
 import { cn } from "#lib/utils"
 
-function Table({ className, ...props }: React.ComponentProps<"table">) {
+function Table({
+  className,
+  containerClassName,
+  ...props
+}: React.ComponentProps<"table"> & { containerClassName?: string }) {
   return (
-    <div data-slot="table-container" className="relative w-full overflow-x-auto">
+    <div data-slot="table-container" className={cn("relative w-full overflow-x-auto", containerClassName)}>
       <table data-slot="table" className={cn("w-full caption-bottom text-sm", className)} {...props} />
     </div>
   )
