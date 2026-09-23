7f07bb2f1bfb6576cd3066389cf3d4d00470d28d
brage-andreas <bragerino+github@gmail.com>
2026-09-24 00:52:48 +0200

    Fix dashboard name in login prompt

--- apps/dashboard/src/app/(auth)/login/page.tsx
+++ apps/dashboard/src/app/(auth)/login/page.tsx
@@ -13,12 +13,12 @@ export default async function Page() {
     <div className="flex justify-center">
       <div className="mt-8 w-full max-w-md rounded-md border p-6">
         <div className="flex flex-col gap-2">
-          <Title element="h1" size="xl">
+          <Title element="h1" size="lg">
             Logg inn
           </Title>
-          <Text className="text-muted-foreground">Vennligst logg inn for å bruke Monoweb Admin</Text>
+          <Text className="text-muted-foreground">Vennligst logg inn for å bruke OnlineWeb dashboard</Text>
           <Button element="a" variant="default" className="mt-8" href={createAuthorizeUrl()}>
-            Logg inn via Monoweb
+            Logg inn
           </Button>
         </div>
       </div>
