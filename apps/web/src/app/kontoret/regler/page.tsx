e4a12939a2b23d5d1e623b2198d0ec729a2cba90
Nicolay B. Rennemo <rennemo@proton.me>
2026-09-23 19:47:37 +0200

    Created Kontoret rules page, just placeholder for now

---
+++ apps/web/src/app/kontoret/regler/page.tsx
@@ -0,0 +1,15 @@
+import { Title, Text } from "@dotkomonline/ui"
+
+const OfficeRulesPage = async () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <Title element="h1" size="xl">
+        Kontorets Regler
+      </Title>
+
+      <Text className="text-gray-600 dark:text-stone-300">Idk spør trikom lol</Text>
+    </div>
+  )
+}
+
+export default OfficeRulesPage
